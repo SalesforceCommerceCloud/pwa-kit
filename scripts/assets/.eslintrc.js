cc65b0682e2794ef181119c38268e77fb2c514aa
Brian Redmond <bredmond@salesforce.com>
2021-08-19 13:04:57 -0400

    Update copyright header template

--- scripts/assets/.eslintrc.js
+++ scripts/assets/.eslintrc.js
@@ -29,14 +29,15 @@ module.exports = {
             2,
             'block',
             [
-                ' * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * *',
+                '',
                 {
-                    pattern:
-                        '^ \\* Copyright \\(c\\) \\d{4} Mobify Research & Development Inc\\. All rights reserved\\. \\*$',
-                    template:
-                        ' * Copyright (c) 2021 Mobify Research & Development Inc. All rights reserved. *'
+                    pattern: '^ \\* Copyright \\(c\\) \\d{4}, salesforce.com, inc\\.$',
+                    template: ' * Copyright (c) 2021, salesforce.com, inc.'
                 },
-                ' * * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * '
+                ' * All rights reserved.',
+                ' * SPDX-License-Identifier: BSD-3-Clause',
+                ' * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause',
+                ' '
             ]
         ],
         'prettier/prettier': ['error'],
