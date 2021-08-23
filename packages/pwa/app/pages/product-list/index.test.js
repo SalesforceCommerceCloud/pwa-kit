46aad2321c910cbc4412292290d353f330f9033b
Alex Vuong <alex.vuong@salesforce.com>
2021-08-23 11:00:24 -0700

    fix test

--- packages/pwa/app/pages/product-list/index.test.js
+++ packages/pwa/app/pages/product-list/index.test.js
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import React from 'react'
+import React, {useEffect} from 'react'
 import PropTypes from 'prop-types'
 
 import {rest} from 'msw'
