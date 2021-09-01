491c92effe6de1393b02e02bd9b1bb7ad8e8ae9a
jkeanesf <78918962+jkeanesf@users.noreply.github.com>
2021-09-01 14:19:13 -0400

    Update packages/pwa/app/pages/product-list/partials/color-refinements.jsx
    Co-authored-by: Adam Raya <adamraya@users.noreply.github.com>

--- packages/pwa/app/pages/product-list/partials/color-refinements.jsx
+++ packages/pwa/app/pages/product-list/partials/color-refinements.jsx
@@ -66,7 +66,7 @@ const ColorRefinements = ({filter, toggleFilter, selectedFilters}) => {
                                                 cssColorGroups[value.presentationId.toLowerCase()]
                                             }
                                             background={
-                                                value.value.toLowerCase() === 'miscellaneous' &&
+                                                value.presentationId.toLowerCase() === 'miscellaneous' &&
                                                 cssColorGroups[value.presentationId.toLowerCase()]
                                             }
                                         />
