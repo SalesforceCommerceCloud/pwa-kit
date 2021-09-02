d80e1e659ff213f4abc02584b29aa7687637c04c
Alex Vuong <alex.vuong@salesforce.com>
2021-09-02 15:09:01 -0700

    remove console

--- packages/pwa/app/components/_error/index.jsx
+++ packages/pwa/app/components/_error/index.jsx
@@ -17,7 +17,6 @@ import {Helmet} from 'react-helmet'
 // It must not throw an error. Keep it as simple as possible.
 const Error = (props) => {
     const {stack, status} = props
-    console.log('props', props)
 
     const isNotFoundError = status === 404
     const title = isNotFoundError
