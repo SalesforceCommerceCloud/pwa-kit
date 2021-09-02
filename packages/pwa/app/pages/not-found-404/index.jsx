eb7c3e091c28b64b8bc2d5d749ee9d8a5d174c34
Alex Vuong <alex.vuong@salesforce.com>
2021-09-02 15:48:38 -0700

    linting

--- packages/pwa/app/pages/not-found-404/index.jsx
+++ packages/pwa/app/pages/not-found-404/index.jsx
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
 import React from 'react'
 import {Box, Heading, Flex, Button, Stack, Text} from '@chakra-ui/react'
 import {Helmet} from 'react-helmet'
