aa1a51d624f12d9a69279abca0742367b09adedd
Adam Raya Navarro <arayanavarro@salesforce.com>
2021-09-09 09:55:43 -0700

    Remove devcenter

--- packages/devcenter/mainNavConst.js
+++
@@ -1,8 +0,0 @@
-const mainNav = {
-    GET_STARTED: 'get-started',
-    APIS_AND_SDKS: 'apis-and-sdks',
-    HOW_TO_GUIDES: 'how-to-guides',
-    RELEASE_NOTES: 'release-notes'
-}
-
-module.exports = mainNav
