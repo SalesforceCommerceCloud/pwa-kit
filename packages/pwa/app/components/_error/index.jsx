d888da93d5faf03abef21e0e965c16d1c6718780
Alex Vuong <alex.vuong@salesforce.com>
2021-09-10 14:06:59 -0700

    pull out intl stuff

--- packages/pwa/app/components/_error/index.jsx
+++ packages/pwa/app/components/_error/index.jsx
@@ -18,10 +18,8 @@ import {
     useMultiStyleConfig
 } from '@chakra-ui/react'
 
-import {createIntl, createIntlCache, RawIntlProvider} from 'react-intl'
 import {BrandLogo, FileIcon} from '../icons'
 import {useHistory} from 'react-router-dom'
-import {DEFAULT_LOCALE} from '../../locale'
 
 // <Error> is rendered when:
 //
@@ -31,125 +29,65 @@ import {DEFAULT_LOCALE} from '../../locale'
 //
 // It must not throw an error. Keep it as simple as possible.
 
-const messages = {
-    en: {
-        logo: 'Logo',
-        title: "The page isn't working",
-        description:
-            'An error has occurred and we’re working to fix the problem. \n' +
-            'Try refreshing the page or if you need immediate help please contact support',
-        stackTrace: 'Stack Trace',
-        contactSupport: 'Contact Support',
-        refreshThePage: 'Refresh the page'
-    },
-    fr: {
-        logo: 'Logo',
-        title: 'La page ne fonctionne pas',
-        description:
-            "Une erreur s'est produite et nous travaillons pour résoudre le problème. \n" +
-            "Essayez d'actualiser la page ou si vous avez besoin d'une aide immédiate, veuillez contacter le support'",
-        stackTrace: 'Stack Trace',
-        contactSupport: 'Contactez le support',
-        refreshThePage: 'Actualiser la page'
-    }
-}
-
-// const targetLocale = getTargetLocale([DEFAULT_LOCALE], SUPPORTED_LOCALES, DEFAULT_LOCALE)
-// console.log('targetLocale', targetLocale)
-const initialLocale = DEFAULT_LOCALE
-
-// This is optional but highly recommended
-// since it prevents memory leak
-// https://formatjs.io/docs/react-intl/api/#createintl
-const cache = createIntlCache()
-/**
- * You can use this variable in other files even after reassigning it.
- * As error component has no access to the intl, we need to create one to be able to use formatMessage for translation
- * This mean this component will handle its own translation message
- * */
-let intl = createIntl({locale: initialLocale, messages: messages[initialLocale]}, cache)
-let formatMessage = intl.formatMessage
 const Error = (props) => {
-    const {stack, location} = props
+    const {stack} = props
     const history = useHistory()
     const styles = useMultiStyleConfig('Error')
 
-    useEffect(() => {
-        const isHome = location.pathname === '/'
-        const activeLocale = isHome ? DEFAULT_LOCALE : location.pathname.split('/')[1]
-        intl = createIntl({locale: activeLocale, messages: messages[activeLocale]}, cache)
-        formatMessage = intl.formatMessage
-    }, [])
-
+    const title = "This page isn't working"
     return (
-        <RawIntlProvider value={intl}>
-            <Flex id="sf-app" {...styles.container} flexDirection="column">
-                <Helmet>
-                    <title>{formatMessage({id: 'title'})}</title>
-                </Helmet>
+        <Flex id="sf-app" {...styles.container} flexDirection="column">
+            <Helmet>
+                <title>{title}</title>
+            </Helmet>
 
-                <Box as="header" {...styles.header}>
-                    <Box {...styles.headerContent}>
-                        <IconButton
-                            aria-label={formatMessage({
-                                id: 'logo'
-                            })}
-                            icon={<BrandLogo {...styles.logo} />}
-                            {...styles.icons}
-                            variant="unstyled"
-                            onClick={() => history.push('/')}
-                        />
-                    </Box>
+            <Box as="header" {...styles.header}>
+                <Box {...styles.headerContent}>
+                    <IconButton
+                        aria-label="logo"
+                        icon={<BrandLogo {...styles.logo} />}
+                        {...styles.icons}
+                        variant="unstyled"
+                        onClick={() => history.push('/')}
+                    />
                 </Box>
-                <Box
-                    as="main"
-                    id="app-main"
-                    role="main"
-                    flex={1}
-                    layerStyle="page"
-                    {...styles.main}
-                >
-                    <Flex {...styles.content} flexDirection="column" justify="center">
-                        <Stack align="center" mb={20}>
-                            <FileIcon boxSize={['30px', '32px']} mb={8} />
+            </Box>
+            <Box as="main" id="app-main" role="main" flex={1} layerStyle="page" {...styles.main}>
+                <Flex {...styles.content} flexDirection="column" justify="center">
+                    <Stack align="center" mb={20}>
+                        <FileIcon boxSize={['30px', '32px']} mb={8} />
 
-                            <Heading as="h2" fontSize={['xl', '2xl', '2xl', '3xl']} mb={2}>
-                                {formatMessage({id: 'title'})}
-                            </Heading>
+                        <Heading as="h2" fontSize={['xl', '2xl', '2xl', '3xl']} mb={2}>
+                            {title}
+                        </Heading>
 
-                            <Box maxWidth="700px">
-                                <Text align="center">{formatMessage({id: 'description'})}</Text>
-                            </Box>
-
-                            <Stack direction={['column', 'row']} width={['100%', 'auto']}>
-                                <Button variant="outline" bg="white" borderColor={'gray.200'}>
-                                    {formatMessage({
-                                        id: 'contactSupport'
-                                    })}
-                                </Button>
-                                <Button onClick={() => window.location.reload()}>
-                                    {formatMessage({
-                                        id: 'refreshThePage'
-                                    })}
-                                </Button>
-                            </Stack>
-                        </Stack>
-                        <Box>
-                            <Text fontWeight="700">
-                                {formatMessage({
-                                    id: 'stackTrace'
-                                })}
+                        <Box maxWidth="700px">
+                            <Text align="center">
+                                An error has occurred. Try refreshing the page or if you need
+                                immediate help please contact support
                             </Text>
-                            {stack && (
-                                <Box as="pre" {...styles.pre}>
-                                    {stack}
-                                </Box>
-                            )}
                         </Box>
-                    </Flex>
-                </Box>
-            </Flex>
-        </RawIntlProvider>
+
+                        <Stack direction={['column', 'row']} width={['100%', 'auto']}>
+                            <Button variant="outline" bg="white" borderColor={'gray.200'}>
+                                Contact Support
+                            </Button>
+                            <Button onClick={() => window.location.reload()}>
+                                Refresh the page
+                            </Button>
+                        </Stack>
+                    </Stack>
+                    <Box>
+                        <Text fontWeight="700">Stack Trace</Text>
+                        {stack && (
+                            <Box as="pre" {...styles.pre}>
+                                {stack}
+                            </Box>
+                        )}
+                    </Box>
+                </Flex>
+            </Box>
+        </Flex>
     )
 }
 
