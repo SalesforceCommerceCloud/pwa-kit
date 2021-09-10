2a187a11963a8eb26d4395735ed750516027d1bf
Alex Vuong <alex.vuong@salesforce.com>
2021-09-10 15:21:36 -0700

    fix tests

--- packages/pwa/app/components/_error/index.test.js
+++ packages/pwa/app/components/_error/index.test.js
@@ -6,31 +6,20 @@
  */
 import React from 'react'
 import {screen} from '@testing-library/react'
-import {renderWithReactIntl} from '../../utils/test-utils'
+import {renderWithProviders, renderWithReactIntl} from '../../utils/test-utils'
 import Error from './index'
 
 test('Error renders without errors', () => {
-    expect(renderWithReactIntl(<Error />))
+    expect(renderWithProviders(<Error />))
 })
 
 test('Error status 500', () => {
-    renderWithReactIntl(<Error status={500} />)
-    expect(screen.getByRole('heading', {level: 1})).toHaveTextContent(
-        'Sorry, there is a problem with this page 😔'
-    )
-})
-
-test('Error status 404', () => {
-    renderWithReactIntl(<Error status={404} />)
-    expect(screen.getByRole('heading', {level: 1})).toHaveTextContent(
-        'Sorry, we cannot find this page 😓'
-    )
+    renderWithProviders(<Error status={500} />)
+    expect(screen.getByRole('heading', {level: 2})).toHaveTextContent("This page isn't working")
 })
 
 test('Error status 500 with stack trace', () => {
-    renderWithReactIntl(<Error status={500} stack={'Stack trace error message'} />)
-    expect(screen.getByRole('heading', {level: 1})).toHaveTextContent(
-        'Sorry, there is a problem with this page 😔'
-    )
+    renderWithProviders(<Error status={500} stack={'Stack trace error message'} />)
+    expect(screen.getByRole('heading', {level: 2})).toHaveTextContent("This page isn't working")
     expect(screen.getByText(/stack trace error message/i)).toBeInTheDocument()
 })
