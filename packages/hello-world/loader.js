eecf60419b765e4a601812f2540e08aca3503b9c
Oliver Brook <o.brook@salesforce.com>
2021-09-13 09:54:51 +0200

    Fix copyright headers for hello-world app

--- packages/hello-world/loader.js
+++ packages/hello-world/loader.js
@@ -1,2 +1,6 @@
-// Intentionally empty.
-// This unused file is required to upload a bundle.
+/*
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
