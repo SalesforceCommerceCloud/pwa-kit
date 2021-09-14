2eea8c22d805cca928291b80569d246f835717bc
Kevin He <kevin.he@salesforce.com>
2021-09-13 17:15:35 -0700

    remove old code

--- packages/pwa/app/hooks/use-wishlist.js
+++ packages/pwa/app/hooks/use-wishlist.js
@@ -13,55 +13,6 @@ const useWishlist = () => {
     const wishlist = useCustomerProductList(PWA_DEFAULT_WISHLIST_NAME)
 
     return wishlist
-    // if (!enableToast) {
-    //     return wishlist
-    // }
-
-    // const onError = () =>
-    //     toast({
-    //         title: API_ERROR_MESSAGE,
-    //         status: 'error'
-    //     })
-
-    // const onSuccessAdd = ({quantity}) => {
-    //     toast({
-    //         title: `${quantity} items added to wishlist`,
-    //         status: 'success',
-    //         action: (
-    //             // @TODO: Link depends on locale
-    //             <Button as={Link} variant="link" to="/account/wishlist">
-    //                 View
-    //             </Button>
-    //             // null
-    //         )
-    //     })
-    // }
-
-    // const onSuccessRemove = () => {
-    //     toast({
-    //         title: 'Item removed from wishlist',
-    //         status: 'success'
-    //     })
-    // }
-
-    // return {
-    //     ...wishlist,
-    //     addItem: withAsync(wishlist.addItem, {
-    //         onSuccess: onSuccessAdd,
-    //         onError
-    //     }),
-    //     updateItem: withAsync(wishlist.updateItem, {
-    //         onError
-    //     }),
-    //     removeItem: withAsync(wishlist.removeItem, {
-    //         onSuccess: onSuccessRemove,
-    //         onError
-    //     }),
-    //     removeItemByProductId: withAsync(wishlist.removeItemByProductId, {
-    //         onSuccess: onSuccessRemove,
-    //         onError
-    //     })
-    // }
 }
 
 export default useWishlist
