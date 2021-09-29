c30c4d40f4c43a779543352e1a8fe3d772cd507d
Vincent Marta <vmarta@salesforce.com>
2021-09-29 12:34:02 -0700

    Add some jsdoc

--- packages/pwa/app/localization.config.js
+++ packages/pwa/app/localization.config.js
@@ -8,6 +8,12 @@
 import {defineMessage} from 'react-intl'
 import packageJson from '../package.json'
 
+/**
+ * @property {string} defaultLocale
+ * @property {Object[]} supportedLocales
+ * @property {string} supportedLocales[].id - OCAPI and Commerce API require that id follows the format: <language code>-<country code>. See {@link https://documentation.b2c.commercecloud.salesforce.com/DOC1/topic/com.demandware.dochelp/OCAPI/current/usage/Localization.html}
+ * @property {Object} supportedLocales[].message - Translation object for your locale selector UI
+ */
 export const localizationConfig = {
     defaultLocale: packageJson.l10n.defaultLocale,
     supportedLocales: [
