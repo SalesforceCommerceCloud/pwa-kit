1873a3250e8b7e2f19e91ad6c850a90989b97508
Ben Chypak <bchypak@salesforce.com>
2021-10-18 14:30:25 -0700

    Revert removal of intlprovider to side step merge conflict, we remove after

--- packages/pwa/app/components/_app/index.jsx
+++ packages/pwa/app/components/_app/index.jsx
@@ -152,102 +152,119 @@ const App = (props) => {
 
     return (
         <Box className="sf-app" {...styles.container}>
-            <CategoriesProvider categories={allCategories}>
-                <CurrencyProvider currency={currency}>
-                    <Seo>
-                        <meta name="theme-color" content="#0288a7" />
-                        <meta
-                            name="apple-mobile-web-app-title"
-                            content="PWA-Kit-Retail-React-App"
-                        />
-                        <link
-                            rel="apple-touch-icon"
-                            href={getAssetUrl('static/img/global/apple-touch-icon.png')}
-                        />
-                        <link rel="manifest" href={getAssetUrl('static/manifest.json')} />
+            <IntlProvider
+                onError={(err) => {
+                    if (err.code === 'MISSING_TRANSLATION') {
+                        // NOTE: Remove the console error for missing translations during development,
+                        // as we knew translations would be added later.
+                        console.warn('Missing translation', err.message)
+                        return
+                    }
+                    throw err
+                }}
+                locale={targetLocale}
+                defaultLocale={defaultLocale}
+                messages={messages}
+            >
+                <CategoriesProvider categories={allCategories}>
+                    <CurrencyProvider currency={currency}>
+                        <Seo>
+                            <meta name="theme-color" content="#0288a7" />
+                            <meta
+                                name="apple-mobile-web-app-title"
+                                content="PWA-Kit-Retail-React-App"
+                            />
+                            <link
+                                rel="apple-touch-icon"
+                                href={getAssetUrl('static/img/global/apple-touch-icon.png')}
+                            />
+                            <link rel="manifest" href={getAssetUrl('static/manifest.json')} />
 
-                        {/* Urls for all localized versions of this page (including current page)
+                            {/* Urls for all localized versions of this page (including current page)
                             For more details on hrefLang, see https://developers.google.com/search/docs/advanced/crawling/localized-versions */}
-                        {SUPPORTED_LOCALES.map((locale) => (
+                            {SUPPORTED_LOCALES.map((locale) => (
+                                <link
+                                    rel="alternate"
+                                    hrefLang={locale.id.toLowerCase()}
+                                    href={`${appOrigin}${getUrlWithLocale(locale.id, {location})}`}
+                                    key={locale.id}
+                                />
+                            ))}
+                            {/* A general locale as fallback. For example: "en" if default locale is "en-GB" */}
                             <link
                                 rel="alternate"
-                                hrefLang={locale.id.toLowerCase()}
-                                href={`${appOrigin}${getUrlWithLocale(locale.id, {location})}`}
-                                key={locale.id}
+                                hrefLang={defaultLocale.slice(0, 2)}
+                                href={`${appOrigin}${getUrlWithLocale(defaultLocale, {location})}`}
                             />
-                        ))}
-                        {/* A general locale as fallback. For example: "en" if default locale is "en-GB" */}
-                        <link
-                            rel="alternate"
-                            hrefLang={defaultLocale.slice(0, 2)}
-                            href={`${appOrigin}${getUrlWithLocale(defaultLocale, {location})}`}
-                        />
-                        {/* A wider fallback for user locales that the app does not support */}
-                        <link rel="alternate" hrefLang="x-default" href={`${appOrigin}/`} />
-                    </Seo>
+                            {/* A wider fallback for user locales that the app does not support */}
+                            <link rel="alternate" hrefLang="x-default" href={`${appOrigin}/`} />
+                        </Seo>
 
-                    <ScrollToTop />
+                        <ScrollToTop />
 
-                    <Box id="app" display="flex" flexDirection="column" flex={1}>
-                        <SkipNavLink zIndex="skipLink">Skip to Content</SkipNavLink>
+                        <Box id="app" display="flex" flexDirection="column" flex={1}>
+                            <SkipNavLink zIndex="skipLink">Skip to Content</SkipNavLink>
 
-                        <Box {...styles.headerWrapper}>
-                            {!isCheckout ? (
-                                <Header
-                                    onMenuClick={onOpen}
-                                    onLogoClick={onLogoClick}
-                                    onMyCartClick={onCartClick}
-                                    onMyAccountClick={onAccountClick}
-                                    onWishlistClick={onWishlistClick}
+                            <Box {...styles.headerWrapper}>
+                                {!isCheckout ? (
+                                    <Header
+                                        onMenuClick={onOpen}
+                                        onLogoClick={onLogoClick}
+                                        onMyCartClick={onCartClick}
+                                        onMyAccountClick={onAccountClick}
+                                        onWishlistClick={onWishlistClick}
+                                    >
+                                        <HideOnDesktop>
+                                            <DrawerMenu
+                                                isOpen={isOpen}
+                                                onClose={onClose}
+                                                onLogoClick={onLogoClick}
+                                                root={allCategories[DEFAULT_ROOT_CATEGORY]}
+                                            />
+                                        </HideOnDesktop>
+
+                                        <HideOnMobile>
+                                            <ListMenu root={allCategories[DEFAULT_ROOT_CATEGORY]} />
+                                        </HideOnMobile>
+                                    </Header>
+                                ) : (
+                                    <CheckoutHeader />
+                                )}
+                            </Box>
+
+                            {!isOnline && <OfflineBanner />}
+
+                            <AddToCartModalProvider>
+                                <SkipNavContent
+                                    style={{
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        flex: 1,
+                                        outline: 0
+                                    }}
                                 >
-                                    <HideOnDesktop>
-                                        <DrawerMenu
-                                            isOpen={isOpen}
-                                            onClose={onClose}
-                                            onLogoClick={onLogoClick}
-                                            root={allCategories[DEFAULT_ROOT_CATEGORY]}
-                                        />
-                                    </HideOnDesktop>
+                                    <Box
+                                        as="main"
+                                        id="app-main"
+                                        role="main"
+                                        display="flex"
+                                        flexDirection="column"
+                                        flex="1"
+                                    >
+                                        <OfflineBoundary isOnline={false}>
+                                            {children}
+                                        </OfflineBoundary>
+                                    </Box>
+                                </SkipNavContent>
 
-                                    <HideOnMobile>
-                                        <ListMenu root={allCategories[DEFAULT_ROOT_CATEGORY]} />
-                                    </HideOnMobile>
-                                </Header>
-                            ) : (
-                                <CheckoutHeader />
-                            )}
+                                {!isCheckout ? <Footer /> : <CheckoutFooter />}
+
+                                <AuthModal {...authModal} />
+                            </AddToCartModalProvider>
                         </Box>
-
-                        {!isOnline && <OfflineBanner />}
-
-                        <AddToCartModalProvider>
-                            <SkipNavContent
-                                style={{
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    flex: 1,
-                                    outline: 0
-                                }}
-                            >
-                                <Box
-                                    as="main"
-                                    id="app-main"
-                                    role="main"
-                                    display="flex"
-                                    flexDirection="column"
-                                    flex="1"
-                                >
-                                    <OfflineBoundary isOnline={false}>{children}</OfflineBoundary>
-                                </Box>
-                            </SkipNavContent>
-
-                            {!isCheckout ? <Footer /> : <CheckoutFooter />}
-
-                            <AuthModal {...authModal} />
-                        </AddToCartModalProvider>
-                    </Box>
-                </CurrencyProvider>
-            </CategoriesProvider>
+                    </CurrencyProvider>
+                </CategoriesProvider>
+            </IntlProvider>
         </Box>
     )
 }
