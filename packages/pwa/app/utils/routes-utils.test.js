706e9f0519b447a474f81d39d928c12ab0b0b898
Alex Vuong <alex.vuong@salesforce.com>
2021-10-28 17:16:18 -0700

    fix text

--- packages/pwa/app/utils/routes-utils.test.js
+++ packages/pwa/app/utils/routes-utils.test.js
@@ -6,65 +6,25 @@
  */
 
 import React from 'react'
-import {getUrlsConfig} from './utils'
-import {routesModifier} from './routes-utils'
+import {getUrlConfig} from './utils'
+import {getLocaleAndSiteBasePath} from './routes-utils'
 
 jest.mock('./utils', () => {
     const original = jest.requireActual('./utils')
     return {
         ...original,
-        getUrlsConfig: jest.fn()
+        getUrlConfig: jest.fn()
     }
 })
 
 describe('routeModifier', function() {
     test('should return modified routes', () => {
-        getUrlsConfig.mockImplementation(() => ({
-            alias: 'path',
+        getUrlConfig.mockImplementation(() => ({
             locale: 'path'
         }))
-        const fakeComp = () => <div>fake component</div>
-        const routes = [
-            {
-                path: '/reset-password',
-                component: fakeComp,
-                exact: true
-            }
-        ]
-        const newRoutes = routesModifier(routes)
-        expect(newRoutes[0].path).toEqual('/:alias/:locale/reset-password')
-    })
-
-    test('should return modified routes with only :alias in the path', () => {
-        getUrlsConfig.mockImplementation(() => ({
-            alias: 'path',
-            locale: 'query_param'
-        }))
-        const fakeComp = () => <div>fake component</div>
-        const routes = [
-            {
-                path: '/reset-password',
-                component: fakeComp,
-                exact: true
-            }
-        ]
-        const newRoutes = routesModifier(routes)
-        expect(newRoutes[0].path).toEqual('/:alias/reset-password')
-    })
-
-    test('should return non-modified url as the config type is invalid', () => {
-        getUrlsConfig.mockImplementation(() => ({
-            alias: 'something',
-            locale: 'query_param'
-        }))
-        const fakeComp = () => <div>fake component</div>
-        const routes = [
-            {
-                path: '/reset-password',
-                component: fakeComp,
-                exact: true
-            }
-        ]
-        expect(() => routesModifier(routes)).toThrow()
+        const locale = 'en-GB'
+        const url = getLocaleAndSiteBasePath(locale)
+        console.log('nurlew', url)
+        expect(url).toEqual('/en-GB')
     })
 })
