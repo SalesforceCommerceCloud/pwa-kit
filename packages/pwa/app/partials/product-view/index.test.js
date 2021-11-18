75cb5fa10624e190a488e1b935867d0260703179
vcua-mobify <vcua@salesforce.com>
2021-11-18 13:54:22 -0800

    Fix product-view test

--- packages/pwa/app/partials/product-view/index.test.js
+++ packages/pwa/app/partials/product-view/index.test.js
@@ -12,16 +12,26 @@ import mockProductDetail from '../../commerce-api/mocks/variant-750518699578M'
 import ProductView from './index'
 import {renderWithProviders} from '../../utils/test-utils'
 import useCustomer from '../../commerce-api/hooks/useCustomer'
-import {setupServer} from 'msw/node'
-import {rest} from 'msw'
-import {exampleTokenReponse, mockedRegisteredCustomer} from '../../commerce-api/mock-data'
 import userEvent from '@testing-library/user-event'
+
+const mockCustomer = {
+    authType: 'registered',
+    customerId: 'registeredCustomerId'
+}
 
 jest.mock('../../commerce-api/utils', () => {
     const originalModule = jest.requireActual('../../commerce-api/utils')
     return {
         ...originalModule,
         isTokenValid: jest.fn().mockReturnValue(true)
+    }
+})
+
+jest.mock('../../commerce-api/auth', () => {
+    return class AuthMock {
+        login() {
+            return mockCustomer
+        }
     }
 })
 
@@ -47,6 +57,18 @@ const MockComponent = ({product, addToCart, addToWishlist, updateWishlist}) => {
     )
 }
 
+jest.mock('commerce-sdk-isomorphic', () => {
+    const sdk = jest.requireActual('commerce-sdk-isomorphic')
+    return {
+        ...sdk,
+        ShopperCustomers: class ShopperCustomersMock extends sdk.ShopperCustomers {
+            async getCustomer() {
+                return mockCustomer
+            }
+        }
+    }
+})
+
 MockComponent.propTypes = {
     product: PropTypes.object,
     addToCart: PropTypes.func,
@@ -54,60 +76,14 @@ MockComponent.propTypes = {
     updateWishlist: PropTypes.func
 }
 
-const server = setupServer(
-    rest.post('*/customers/actions/login', (req, res, ctx) =>
-        res(ctx.set('authorization', `Bearer guesttoken`), ctx.json(mockedRegisteredCustomer))
-    ),
-    rest.post('*/oauth2/authorize', (req, res, ctx) =>
-        res(ctx.delay(0), ctx.status(303), ctx.set('location', `/testcallback`))
-    ),
-    rest.get('*/oauth2/authorize', (req, res, ctx) =>
-        res(ctx.delay(0), ctx.status(303), ctx.set('location', `/testcallback`))
-    ),
-    rest.get('*/testcallback', (req, res, ctx) => {
-        return res(ctx.delay(0), ctx.status(200))
-    }),
-    rest.post('*/oauth2/login', (req, res, ctx) =>
-        res(ctx.delay(0), ctx.status(200), ctx.json(mockedRegisteredCustomer))
-    ),
-    rest.get('*/customers/:customerId', (req, res, ctx) =>
-        res(ctx.delay(0), ctx.status(200), ctx.json(mockedRegisteredCustomer))
-    ),
-    rest.post('*/oauth2/token', (req, res, ctx) =>
-        res(
-            ctx.delay(0),
-            ctx.json({
-                customer_id: 'test',
-                access_token: 'testtoken',
-                refresh_token: 'testrefeshtoken',
-                usid: 'testusid',
-                enc_user_id: 'testEncUserId'
-            })
-        )
-    ),
-    rest.get('*/oauth2/logout', (req, res, ctx) =>
-        res(ctx.delay(0), ctx.status(200), ctx.json(exampleTokenReponse))
-    )
-)
-
 // Set up and clean up
 beforeEach(() => {
     jest.resetModules()
-    server.listen({onUnhandledRequest: 'error'})
-
-    // Since we're testing some navigation logic, we are using a simple Router
-    // around our component. We need to initialize the default route/path here.
-    window.history.pushState({}, 'Account', '/en/account')
 })
-afterEach(() => {
-    localStorage.clear()
-    server.resetHandlers()
-})
-afterAll(() => server.close())
 
 test('ProductView Component renders properly', () => {
     const addToCart = jest.fn()
-    renderWithProviders(<MockComponent product={mockProductDetail} addToCart={addToCart} />)
+    renderWithProviders(<ProductView product={mockProductDetail} addToCart={addToCart} />)
 
     expect(screen.getAllByText(/Black Single Pleat Athletic Fit Wool Suit/i).length).toEqual(2)
     expect(screen.getAllByText(/299.99/).length).toEqual(2)
@@ -118,7 +94,7 @@ test('ProductView Component renders properly', () => {
 
 test('ProductView Component renders with addToCart event handler', () => {
     const addToCart = jest.fn()
-    renderWithProviders(<MockComponent product={mockProductDetail} addToCart={addToCart} />)
+    renderWithProviders(<ProductView product={mockProductDetail} addToCart={addToCart} />)
 
     const addToCartButton = screen.getAllByText(/add to cart/i)[0]
     fireEvent.click(addToCartButton)
@@ -126,34 +102,6 @@ test('ProductView Component renders with addToCart event handler', () => {
 })
 
 test('ProductView Component renders with addToWishList event handler', async () => {
-    server.use(
-        rest.post('*/oauth2/login', (req, res, ctx) =>
-            res(ctx.delay(0), ctx.status(303), ctx.set('location', `/testcallback`))
-        ),
-        rest.get('*/testcallback', (req, res, ctx) => {
-            return res(ctx.delay(0), ctx.status(200))
-        }),
-
-        rest.post('*/oauth2/token', (req, res, ctx) =>
-            res(
-                ctx.delay(0),
-                ctx.json({
-                    customer_id: 'test',
-                    access_token: 'testtoken',
-                    refresh_token: 'testrefeshtoken',
-                    usid: 'testusid',
-                    enc_user_id: 'testEncUserId'
-                })
-            )
-        ),
-        rest.get('*/customers/:customerId', (req, res, ctx) =>
-            res(
-                ctx.json({
-                    ...mockedRegisteredCustomer
-                })
-            )
-        )
-    )
     const addToWishlist = jest.fn()
 
     renderWithProviders(<MockComponent product={mockProductDetail} addToWishlist={addToWishlist} />)
@@ -171,34 +119,6 @@ test('ProductView Component renders with addToWishList event handler', async () 
 })
 
 test('ProductView Component renders with updateWishlist event handler', async () => {
-    server.use(
-        rest.post('*/oauth2/login', (req, res, ctx) =>
-            res(ctx.delay(0), ctx.status(303), ctx.set('location', `/testcallback`))
-        ),
-        rest.get('*/testcallback', (req, res, ctx) => {
-            return res(ctx.delay(0), ctx.status(200))
-        }),
-
-        rest.post('*/oauth2/token', (req, res, ctx) =>
-            res(
-                ctx.delay(0),
-                ctx.json({
-                    customer_id: 'test',
-                    access_token: 'testtoken',
-                    refresh_token: 'testrefeshtoken',
-                    usid: 'testusid',
-                    enc_user_id: 'testEncUserId'
-                })
-            )
-        ),
-        rest.get('*/customers/:customerId', (req, res, ctx) =>
-            res(
-                ctx.json({
-                    ...mockedRegisteredCustomer
-                })
-            )
-        )
-    )
     const updateWishlist = jest.fn()
 
     renderWithProviders(
@@ -219,7 +139,7 @@ test('ProductView Component renders with updateWishlist event handler', async ()
 
 test('Product View can update quantity', () => {
     const addToCart = jest.fn()
-    renderWithProviders(<MockComponent product={mockProductDetail} addToCart={addToCart} />)
+    renderWithProviders(<ProductView product={mockProductDetail} addToCart={addToCart} />)
     const quantityBox = screen.getByRole('spinbutton')
     expect(quantityBox).toHaveValue('1')
     // update item quantity
