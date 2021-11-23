c4f3fec5eb0dd6e2d9971125ce6b5e72ec58d8c8
Ashok Srinivasan Gurumurthy <agurumurthy@salesforce.com>
2021-11-24 04:45:38 +0530

    Added comments to the validatePhone function

--- packages/pwa/app/utils/phone-utils.js
+++ packages/pwa/app/utils/phone-utils.js
@@ -20,7 +20,11 @@ export const formatPhoneNumber = (value) => {
     }
     return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3, 6)}-${phoneNumber.slice(6, 10)}`
 }
-
+/**
+ * Validates given Phone Number
+ * @param {string} - Phone number to be validated
+ * @returns {boolean}  - Result of Validation
+ */
 export const validatePhone = (value) => {
     const regex = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/
     return {
