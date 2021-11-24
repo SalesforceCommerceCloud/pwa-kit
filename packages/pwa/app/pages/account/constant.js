20411bc5b5c243617c3aebd06cdc84679a8dc50e
Vincent Marta <vmarta@salesforce.com>
2021-11-23 17:17:44 -0800

    Message ids for account pages

--- packages/pwa/app/pages/account/constant.js
+++ packages/pwa/app/pages/account/constant.js
@@ -16,11 +16,11 @@ import {
 import {noop} from '../../utils/utils'
 
 export const messages = defineMessages({
-    profile: {defaultMessage: 'Account Details'},
-    addresses: {defaultMessage: 'Addresses'},
-    orders: {defaultMessage: 'Order History'},
-    payments: {defaultMessage: 'Payment Methods'},
-    wishlist: {defaultMessage: 'Wishlist'}
+    profile: {defaultMessage: 'Account Details', id: 'global.account.link.account_details'},
+    addresses: {defaultMessage: 'Addresses', id: 'global.account.link.addresses'},
+    orders: {defaultMessage: 'Order History', id: 'global.account.link.order_history'},
+    payments: {defaultMessage: 'Payment Methods', id: 'global.account.link.payment_methods'},
+    wishlist: {defaultMessage: 'Wishlist', id: 'global.account.link.wishlist'}
 })
 
 export const navLinks = [
@@ -52,18 +52,31 @@ export const navLinks = [
 ]
 
 export const CONFIRMATION_DIALOG_DEFAULT_CONFIG = defineMessages({
-    dialogTitle: {defaultMessage: 'Confirm Action'},
-    confirmationMessage: {defaultMessage: 'Are you sure you want to continue ?'},
-    primaryActionLabel: {defaultMessage: 'Yes'},
-    alternateActionLabel: {defaultMessage: 'No'}
+    dialogTitle: {defaultMessage: 'Confirm Action', id: 'global.account.title.confirm_action'},
+    confirmationMessage: {
+        defaultMessage: 'Are you sure you want to continue ?',
+        id: 'global.account.message.you_want_to_continue'
+    },
+    primaryActionLabel: {defaultMessage: 'Yes', id: 'global.account.action.yes'},
+    alternateActionLabel: {defaultMessage: 'No', id: 'global.account.action.no'}
 })
 
 export const REMOVE_CART_ITEM_CONFIRMATION_DIALOG_CONFIG = {
-    dialogTitle: defineMessage({defaultMessage: 'Confirm Remove Item'}),
-    confirmationMessage: defineMessage({
-        defaultMessage: 'Are you sure you want to remove this item from your cart?'
+    dialogTitle: defineMessage({
+        defaultMessage: 'Confirm Remove Item',
+        id: 'global.account.title.confirm_remove'
     }),
-    primaryActionLabel: defineMessage({defaultMessage: 'Yes, remove item'}),
-    alternateActionLabel: defineMessage({defaultMessage: 'No, keep item'}),
+    confirmationMessage: defineMessage({
+        defaultMessage: 'Are you sure you want to remove this item from your cart?',
+        id: 'global.account.message.you_want_to_remove'
+    }),
+    primaryActionLabel: defineMessage({
+        defaultMessage: 'Yes, remove item',
+        id: 'global.account.action.remove_item'
+    }),
+    alternateActionLabel: defineMessage({
+        defaultMessage: 'No, keep item',
+        id: 'global.account.action.keep_item'
+    }),
     onPrimaryAction: noop
 }
