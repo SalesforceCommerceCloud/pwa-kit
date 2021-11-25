e28d67d2fc1daa886015d7317715753627cc7ead
Kevin He <kevin.he@salesforce.com>
2021-11-25 14:36:38 -0800

    remove unused import

--- packages/pwa-kit-react-sdk/src/config/validators/v1-0-0.js
+++ packages/pwa-kit-react-sdk/src/config/validators/v1-0-0.js
@@ -4,6 +4,5 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-// import isFQDN from 'validator/es/lib/isFQDN'
 
 export default {}
