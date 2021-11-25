6c7f6f2d12e99e23fbeb93e6fac09c5c14a6c9fe
Oliver Brook <o.brook@salesforce.com>
2021-11-24 18:58:31 -0800

    Take all webpack deps out of the pwa

--- packages/pwa-kit-react-sdk/src/webpack/config.js
+++ packages/pwa-kit-react-sdk/src/webpack/config.js
@@ -180,6 +180,7 @@ const common = {
             'react-dom': projectThenSDKModules('react-dom'),
             'react-helmet': projectThenSDKModules('react-helmet'),
             bluebird: projectThenSDKModules('bluebird'),
+            'webpack/package.json': join(sdkModules('webpack'), 'package.json'),
         },
         fallback: {
             crypto: false,
