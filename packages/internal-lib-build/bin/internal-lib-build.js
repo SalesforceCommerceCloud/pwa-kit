7ca906ebeb428f7118cbe43fc1509a45106561df
Oliver Brook <o.brook@salesforce.com>
2021-11-27 12:19:04 -0800

    Pull internal build tools into a package for re-use on split packages

--- packages/internal-lib-build/bin/internal-lib-build.js
+++ packages/internal-lib-build/bin/internal-lib-build.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
+/*
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
 const p = require('path')
 const program = require('commander')
 const sh = require('shelljs')
@@ -7,18 +13,19 @@ sh.set('-e')
 
 const babel = p.resolve(p.join(__dirname, '..', 'node_modules', '.bin', 'babel'))
 const babelConfig = p.resolve(p.join(__dirname, 'babel.config.js'))
+const prepareDist = p.resolve(p.join(__dirname, 'prepare-dist.js'))
 
 const main = () => {
     program.description(`Internal build tools for monorepo libraries`)
 
-    program
-        .command('build')
-        .action(() => {
-            sh.rm('-rf', './dist')
-            sh.mkdir('./dist')
-            sh.exec(`${babel} --config-file ${babelConfig} src -x ".js",".jsx" --ignore "**/test_fixtures/*","*.test.js","test.js" --out-dir dist`)
-            sh.exec(`./prepare-dist.js`)
-        })
+    program.command('build').action(() => {
+        sh.rm('-rf', './dist')
+        sh.mkdir('./dist')
+        sh.exec(
+            `${babel} --config-file ${babelConfig} src -x ".js",".jsx" --ignore "**/test_fixtures/*","*.test.js","test.js" --out-dir dist`
+        )
+        sh.exec(prepareDist)
+    })
 
     program.parse(process.argv)
 }
