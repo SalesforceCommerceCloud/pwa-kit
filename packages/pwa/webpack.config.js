805bac4f1ee3b5afd9fffd3401768eca3ae40335
Kevin He <kevin.he@salesforce.com>
2021-12-09 11:04:49 -0800

    revert webpack config

--- packages/pwa/webpack.config.js
+++ packages/pwa/webpack.config.js
@@ -4,6 +4,6 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-const webpackConfig = require('pwa-kit-react-sdk/webpack/config')
+const config = require('pwa-kit-react-sdk/webpack/config')
 
-module.exports = webpackConfig
+module.exports = config
