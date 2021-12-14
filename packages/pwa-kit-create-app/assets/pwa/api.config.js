5225fd41d8e0c583cac84d94a3a3289a591d1d25
Kevin He <kevin.he@salesforce.com>
2021-12-14 10:27:58 -0800

    avoid newline

--- packages/pwa-kit-create-app/assets/pwa/api.config.js
+++ packages/pwa-kit-create-app/assets/pwa/api.config.js
@@ -4,8 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-exports.template = ({commerceApi, einsteinApi}) => `
-export const commerceAPIConfig = {
+exports.template = ({commerceApi, einsteinApi}) => `export const commerceAPIConfig = {
     proxyPath: \`/mobify/proxy/${commerceApi.proxyPath}\`,
     parameters: {
         clientId: '${commerceApi.clientId}',
