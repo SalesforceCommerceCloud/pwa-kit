2a6c08adfac6d2d4ebc00ec214786d7cac92907e
Adam Raya Navarro <arayanavarro@salesforce.com>
2021-12-17 18:42:38 +0100

    Add Cypress e2e test to Validate HTTP security headers

--- packages/pwa/cypress/integration/pages/home.spec.js
+++ packages/pwa/cypress/integration/pages/home.spec.js
@@ -22,4 +22,36 @@ describe('Home Page', () => {
         // elements for testing. You may or may not have to use them in your tests.
         cy.get('[data-testid="home-page"]')
     })
+
+    it('Validate HTTP security headers', () => {
+        const url = Cypress.config('baseUrl') + '/'
+        cy.intercept('/', (req) => {
+            if (req.url === url) {
+                return req.reply((res) => {
+                    expect(res.headers).to.have.property('content-security-policy')
+                    // TODO: Uncomment these assertions when we remove unsafe-eval
+                    //expect(res.headers['content-security-policy']).not.to.include('unsafe-inline')
+                    //expect(res.headers['content-security-policy']).not.to.include('unsafe-eval')
+                    expect(res.headers).to.have.property('expect-ct')
+                    expect(res.headers['expect-ct']).to.equal('max-age=0')
+                    expect(res.headers).to.have.property('referrer-policy')
+                    expect(res.headers['referrer-policy']).to.equal('no-referrer')
+                    expect(res.headers).to.have.property('strict-transport-security')
+                    expect(res.headers).to.have.property('x-content-type-options')
+                    expect(res.headers['x-content-type-options']).to.equal('nosniff')
+                    expect(res.headers).to.have.property('x-dns-prefetch-control')
+                    expect(res.headers['x-dns-prefetch-control']).to.equal('off')
+                    expect(res.headers).to.have.property('x-download-options')
+                    expect(res.headers['x-download-options']).to.equal('noopen')
+                    expect(res.headers).to.have.property('x-frame-options')
+                    expect(res.headers['x-frame-options']).to.equal('SAMEORIGIN')
+                    expect(res.headers).to.have.property('x-permitted-cross-domain-policies')
+                    expect(res.headers['x-permitted-cross-domain-policies']).to.equal('none')
+                    expect(res.headers).to.have.property('x-xss-protection')
+                    expect(res.headers['x-xss-protection']).to.equal('0')
+                })
+            }
+        })
+        cy.visit('/')
+    })
 })
