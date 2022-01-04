ea522782f5d28d1b140618f7997c0b6d2daf48c7
Alex Vuong <alex.vuong@salesforce.com>
2022-01-03 17:14:19 -0800

    add compile pseudo message action to messages.js script

--- packages/pwa/scripts/messages.js
+++ packages/pwa/scripts/messages.js
@@ -4,7 +4,12 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-// const Promise = require('bluebird')
+
+/**
+ * This script is written to support extracting and compiling messages to
+ * multiple default locales with react-intl
+ * run 'node scripts/messages --help' to know all the commands it supports
+ */
 const program = require('commander')
 const childProc = require('child_process')
 const path = require('path')
@@ -27,12 +32,26 @@ const spawnPromise = (cmd, args, opts) => {
         })
     })
 }
+
+/**
+ * append formatjs and locales to the opts object of the 'program'
+ * @param opts.formatjs - the path to formatjs from the project
+ * @param opts.locales - the localesToExtractMessages from pwa-kit.config that you want to extract messages to.
+ *
+ * If your project has more than one site,
+ * it is likely that the default locale is different for each site
+ * E.g (en-US for RefArch, en-GB for RefArchGlobal)
+ *
+ * In that case, you need to fill those locales in the field 'localesToExtractMessages' inside pwa-kit.config.json,
+ * so this function can pick it up and extract default messages into different files for your sites
+ *
+ */
 const prepare = (opts) => {
     let formatjs = path.resolve(__dirname, '..', 'node_modules', '.bin', 'formatjs')
 
     return Promise.resolve().then(() => ({
         formatjs,
-        defaultLocales: pwaKitConfig.app.sites.map((site) => site.l10n.defaultLocale),
+        locales: pwaKitConfig.app.localesToExtractMessages,
         opts
     }))
 }
@@ -42,10 +61,19 @@ const getAction = (opts) => {
     if (opts.extractMessages) {
         return extractMessages
     }
+    if (opts.compilePseudoMessages) {
+        return compiledPseudoMessages
+    }
 }
 
-const extractMessages = async ({formatjs, defaultLocales}) => {
-    const resultPromises = defaultLocales.map(async (locale) => {
+/**
+ * extract default messages for the locales
+ * @param formatjs
+ * @param locales
+ * @returns {Promise<[]>}
+ */
+const extractMessages = async ({formatjs, locales}) => {
+    const resultPromises = locales.map(async (locale) => {
         const [process, , stderr] = await spawnPromise(formatjs, [
             'extract',
             'app/**/*.{js,jsx}',
@@ -75,6 +103,34 @@ const extractMessages = async ({formatjs, defaultLocales}) => {
     return results
 }
 
+/**
+ * compile the psedocode translation for the project.
+ * @param formatjs
+ * @param locales
+ */
+const compiledPseudoMessages = async ({formatjs, locales}) => {
+    // we only need one pseudo translation
+    const [process, , stderr] = await spawnPromise(formatjs, [
+        'compile',
+        '--ast',
+        `app/translations/${locales[0]}.json`,
+        '--out-file',
+        'app/translations/compiled/en-XB.json',
+        '--pseudo-locale',
+        'en-XB'
+    ])
+    if (stderr) {
+        console.error(stderr)
+    }
+
+    return [
+        {
+            exitCode: process.exitCode,
+            stderr
+        }
+    ]
+}
+
 const main = async (opts) => {
     try {
         const preparedOpts = await prepare(opts)
@@ -88,6 +144,7 @@ const main = async (opts) => {
 program.description(`extract and compile default messages into the default locale`)
 
 program.option('--extract-messages', 'extract locales messages')
+program.option('--compile-pseudo-messages', 'compile pseudo messages from extracted messages')
 
 program.parse(process.argv)
 
