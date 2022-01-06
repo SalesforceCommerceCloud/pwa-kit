929dfc7d2d2a490389fc2a18fb891ce563f6c007
Kevin He <kevin.he@salesforce.com>
2022-01-05 22:09:23 -0800

    reformat files

--- scripts/smoke-test-npm-scripts.js
+++ scripts/smoke-test-npm-scripts.js
@@ -1,56 +1,56 @@
 #!/usr/bin/env node
 /* eslint-env node */
 
-const sh = require("shelljs");
-const program = require("commander");
-const path = require("path");
+const sh = require('shelljs')
+const program = require('commander')
+const path = require('path')
 
-sh.set("-e");
+sh.set('-e')
 
-const defaultDir = process.cwd();
+const defaultDir = process.cwd()
 
 program.description(
-  [
-    `Smoke-tests uncommonly-run NPM scripts that get shipped with PWA Kit projects `,
-    `by simply checking that those scripts exit without errors.`,
-  ].join("\n")
-);
-program.option("--dir <dir>", `Path to a PWA Kit project`, defaultDir);
+    [
+        `Smoke-tests uncommonly-run NPM scripts that get shipped with PWA Kit projects `,
+        `by simply checking that those scripts exit without errors.`,
+    ].join('\n')
+)
+program.option('--dir <dir>', `Path to a PWA Kit project`, defaultDir)
 
-program.parse(process.argv);
+program.parse(process.argv)
 
 const main = (opts) => {
-  const cwd = path.resolve(opts.dir);
+    const cwd = path.resolve(opts.dir)
 
-  const pkg = JSON.parse(sh.cat(path.join(cwd, "package.json")));
+    const pkg = JSON.parse(sh.cat(path.join(cwd, 'package.json')))
 
-  // The excluded scripts are either already part of our CI steps or are not safe to run.
-  const exclude = [
-    /^lint.*$/,
-    /^test.*$/,
-    /^push$/,
-    /^save-credentials$/,
-    /^format$/,
-    /^prod:build$/,
-    /^start.*$/,
-    /^compile-translations.*$/,
-    /^extract-default-translations.*$/,
-  ];
+    // The excluded scripts are either already part of our CI steps or are not safe to run.
+    const exclude = [
+        /^lint.*$/,
+        /^test.*$/,
+        /^push$/,
+        /^save-credentials$/,
+        /^format$/,
+        /^prod:build$/,
+        /^start.*$/,
+        /^compile-translations.*$/,
+        /^extract-default-translations.*$/,
+    ]
 
-  const scripts = Object.keys(pkg.scripts).filter(
-    (script) => !exclude.some((re) => script.match(re))
-  );
+    const scripts = Object.keys(pkg.scripts).filter(
+        (script) => !exclude.some((re) => script.match(re))
+    )
 
-  scripts.forEach((script) => {
-    const cmd = `npm run ${script}`;
-    console.log(`Testing "${cmd}"`);
-    try {
-      sh.exec(cmd, { cwd, silent: true });
-    } catch (e) {
-      console.error(e);
-      sh.exit(1);
-    }
-  });
-};
+    scripts.forEach((script) => {
+        const cmd = `npm run ${script}`
+        console.log(`Testing "${cmd}"`)
+        try {
+            sh.exec(cmd, {cwd, silent: true})
+        } catch (e) {
+            console.error(e)
+            sh.exit(1)
+        }
+    })
+}
 
-main(program);
+main(program)
