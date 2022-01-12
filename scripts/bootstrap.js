a70682ff393b184a3cb55a52c72a184836ad6fdd
vcua-mobify <vcua@salesforce.com>
2022-01-12 10:39:13 -0800

    Revert "Update bootstrap for npm 7"
    This reverts commit 415930cab6e9edace57712b6c64beb812cc342fe.

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -6,24 +6,19 @@
  * If the user did `npm install` at the root, do `npm install` for all packages,
  * if they did `npm ci` at the root use ci everywhere.
  */
-const childProc = require("child_process");
+const childProc = require('child_process')
+const argv = JSON.parse(process.env.npm_config_argv).original
 
 // Is this a CI environment?
-const ciEnvironment = Boolean(process.env.CI);
+const ciEnvironment = Boolean(process.env.CI)
 
 // Did the user explicitly invoke `npm ci`?
-const npmCmd = process.env.npm_config_argv
-  ? JSON.parse(process.env.npm_config_argv).original[0]
-  : process.env.npm_command;
-const ciCommand = npmCmd === "ci";
-
-console.log(npmCmd);
-console.log(ciCommand);
+const ciCommand = argv[0] === 'ci'
 
 // Note: We reduce concurrency and increase verbosity on CI environments.
 // They are often memory-constrained and kill processes which produce no
 // output for too long.
-const commandArgs = ciCommand ? "--ci" : "--no-ci";
-const environmentArgs = ciEnvironment ? "--concurrency 1 --loglevel debug" : "";
-const cmd = `npm run lerna -- bootstrap ${commandArgs} ${environmentArgs}`;
-childProc.execSync(cmd, { stdio: "inherit" });
+const commandArgs = ciCommand ? '--ci' : '--no-ci'
+const environmentArgs = ciEnvironment ? '--concurrency 1 --loglevel debug' : ''
+const cmd = `npm run lerna -- bootstrap ${commandArgs} ${environmentArgs}`
+childProc.execSync(cmd, {stdio: 'inherit'})
