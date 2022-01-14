8e3c1ee59757846bbaff003644affdd54591453e
Alex Vuong <alex.vuong@salesforce.com>
2022-01-13 17:51:40 -0800

    configure Route to get all permutation of route array

--- packages/pwa/app/utils/routes-utils.js
+++ packages/pwa/app/utils/routes-utils.js
@@ -6,8 +6,6 @@
  */
 
 import {getConfig} from './utils'
-import {urlPartPositions} from '../constants'
-
 /**
  * Configure the routes based on url configuration from pwa-kit.config.json file
  *
@@ -17,28 +15,112 @@ import {urlPartPositions} from '../constants'
  */
 export const configureRoutes = (routes = [], {ignoredRoutes = []}) => {
     if (!routes.length) return []
+    // const urlConfig = getConfig('app.url')
+    // if (!urlConfig) return routes
 
-    const urlConfig = getConfig('app.url')
+    // console.log('getAppOrigin()', getAppOrigin())
+    // const hostsConfig = getConfig('app.hosts')
+    // const {hostname} = new URL(getAppOrigin())
 
-    if (!urlConfig) return routes
+    // const urlConfig = hostsConfig.filter((host) => host.domain === hostname)
+    // console.log('urlConfig', urlConfig)
 
-    return routes.map((route) => {
-        const {path, ...rest} = route
-        if (ignoredRoutes.includes(path)) return route
-        let basePathSegments = []
+    const hosts = getConfig('app.hosts')
 
-        const options = ['site', 'locale']
+    // collect and flatten the result so we can have a list of site objects
+    const allSites = [].concat(...hosts.map((host) => host.sites))
 
-        options.forEach((option) => {
-            const position = urlConfig[option]
-            if (position === urlPartPositions.PATH) {
-                basePathSegments.push(`:${option}`)
-            }
-        })
-
-        return {
-            path: `${basePathSegments.length ? `/${basePathSegments.join('/')}` : ''}${path}`,
-            ...rest
+    // get a collection of all site-id and site alias from the config
+    // remove duplicates
+    const sites = [
+        ...new Set(
+            allSites
+                ?.reduce((res, site) => {
+                    return [...res, site.id, site.alias]
+                }, [])
+                .filter(Boolean)
+        )
+    ]
+    // get a collection of all locale-id and locale alias from the config
+    // remove duplicates
+    const locales = [
+        ...new Set(
+            allSites
+                ?.reduce((res, site) => {
+                    const {supportedLocales} = site.l10n
+                    supportedLocales.forEach((locale) => {
+                        res = [...res, locale.id, locale.alias]
+                    })
+                    return res
+                }, [])
+                .filter(Boolean)
+        )
+    ]
+    let outputRoutes = []
+    // reconstruct all the routes that has both site and locale
+    for (let i = 0; i < routes.length; i++) {
+        const {path, ...rest} = routes[i]
+        if (ignoredRoutes.includes(path)) {
+            outputRoutes.push(routes[i])
+        } else {
+            sites.forEach((site) => {
+                locales.forEach((locale) => {
+                    const newRoute = `/${site}/${locale}${path}`
+                    outputRoutes.push({
+                        path: newRoute,
+                        ...rest
+                    })
+                })
+            })
         }
-    })
+    }
+
+    // reconstruct the routes that either has site or locale
+    // these routes has to go after the above routes to be able to respect the matching logic of react router dom
+    for (let i = 0; i < routes.length; i++) {
+        const {path, ...rest} = routes[i]
+
+        if (ignoredRoutes.includes(path)) {
+            const isRouteExisted = outputRoutes.map((route) => route.path).includes(path)
+            if (!isRouteExisted) {
+                outputRoutes.push(routes[i])
+            }
+        } else {
+            sites.forEach((site) => {
+                outputRoutes.push({
+                    path: `/${site}${path}`,
+                    ...rest
+                })
+            })
+            locales.forEach((locale) => {
+                outputRoutes.push({
+                    path: `/${locale}${path}`,
+                    ...rest
+                })
+            })
+            // origin route will be at the bottom
+            outputRoutes.push(routes[i])
+        }
+    }
+    console.log('test', outputRoutes.length, outputRoutes.map((i) => i.path))
+    return outputRoutes
+    // return routes.map((route) => {
+    //     const {path, ...rest} = route
+    //     if (ignoredRoutes.includes(path)) return route
+    //     let basePathSegments = []
+    //
+    //     const options = ['site', 'locale']
+    //
+    //     options.forEach((option) => {
+    //         const position = urlConfig[option]
+    //         if (position === urlPartPositions.PATH) {
+    //             basePathSegments.push(`:${option}`)
+    //         }
+    //     })
+    //
+    //     return {
+    //         path: `${basePathSegments.length ? `/${basePathSegments.join('/')}` : ''}${path}`,
+    //         ...rest
+    //     }
+    // })
 }
--- packages/pwa/app/utils/routes-utils.test.js
+++ packages/pwa/app/utils/routes-utils.test.js
@@ -18,12 +18,10 @@ jest.mock('./utils', () => {
 })
 
 describe('configureRoutes', function() {
-    test('should only configured the routes with :locale as part of the path', () => {
-        getConfig.mockImplementation(() => ({
-            locale: 'path'
-        }))
+    test('should return all permutation of path including site and locales ', () => {
+        getConfig.mockImplementation(() => mockConfig.app.hosts)
         const CompA = () => <div>This is component A</div>
-        const CompB = () => <div>This is component B</div>
+        const CompC = () => <div>This is component C</div>
 
         const routes = [
             {
@@ -33,12 +31,215 @@ describe('configureRoutes', function() {
             },
             {
                 path: '/category/:categoryId',
-                component: CompB,
+                component: CompC,
                 exact: true
             }
         ]
         const configuredRoutes = configureRoutes(routes, {ignoredRoutes: '/'})
-        expect(configuredRoutes[0].path).toEqual('/')
-        expect(configuredRoutes[1].path).toEqual('/:locale/category/:categoryId')
+        expect(configuredRoutes[configuredRoutes.length - 1].path).toEqual('/category/:categoryId')
+        expect(configuredRoutes.length).toEqual(85)
+        const paths = configuredRoutes.map((route) => route.path)
+        expect(paths).toEqual(expectedPathsResult)
     })
 })
+
+const expectedPathsResult = [
+    '/',
+    '/RefArch/en-US/category/:categoryId',
+    '/RefArch/en/category/:categoryId',
+    '/RefArch/fr-FR/category/:categoryId',
+    '/RefArch/fr/category/:categoryId',
+    '/RefArch/it-IT/category/:categoryId',
+    '/RefArch/it/category/:categoryId',
+    '/RefArch/en-HK/category/:categoryId',
+    '/RefArch/en-GB/category/:categoryId',
+    '/RefArch/uk/category/:categoryId',
+    '/RefArch/de-DE/category/:categoryId',
+    '/RefArch/de/category/:categoryId',
+    '/us/en-US/category/:categoryId',
+    '/us/en/category/:categoryId',
+    '/us/fr-FR/category/:categoryId',
+    '/us/fr/category/:categoryId',
+    '/us/it-IT/category/:categoryId',
+    '/us/it/category/:categoryId',
+    '/us/en-HK/category/:categoryId',
+    '/us/en-GB/category/:categoryId',
+    '/us/uk/category/:categoryId',
+    '/us/de-DE/category/:categoryId',
+    '/us/de/category/:categoryId',
+    '/eu/en-US/category/:categoryId',
+    '/eu/en/category/:categoryId',
+    '/eu/fr-FR/category/:categoryId',
+    '/eu/fr/category/:categoryId',
+    '/eu/it-IT/category/:categoryId',
+    '/eu/it/category/:categoryId',
+    '/eu/en-HK/category/:categoryId',
+    '/eu/en-GB/category/:categoryId',
+    '/eu/uk/category/:categoryId',
+    '/eu/de-DE/category/:categoryId',
+    '/eu/de/category/:categoryId',
+    '/RefArchGlobal/en-US/category/:categoryId',
+    '/RefArchGlobal/en/category/:categoryId',
+    '/RefArchGlobal/fr-FR/category/:categoryId',
+    '/RefArchGlobal/fr/category/:categoryId',
+    '/RefArchGlobal/it-IT/category/:categoryId',
+    '/RefArchGlobal/it/category/:categoryId',
+    '/RefArchGlobal/en-HK/category/:categoryId',
+    '/RefArchGlobal/en-GB/category/:categoryId',
+    '/RefArchGlobal/uk/category/:categoryId',
+    '/RefArchGlobal/de-DE/category/:categoryId',
+    '/RefArchGlobal/de/category/:categoryId',
+    '/global/en-US/category/:categoryId',
+    '/global/en/category/:categoryId',
+    '/global/fr-FR/category/:categoryId',
+    '/global/fr/category/:categoryId',
+    '/global/it-IT/category/:categoryId',
+    '/global/it/category/:categoryId',
+    '/global/en-HK/category/:categoryId',
+    '/global/en-GB/category/:categoryId',
+    '/global/uk/category/:categoryId',
+    '/global/de-DE/category/:categoryId',
+    '/global/de/category/:categoryId',
+    '/site-de/en-US/category/:categoryId',
+    '/site-de/en/category/:categoryId',
+    '/site-de/fr-FR/category/:categoryId',
+    '/site-de/fr/category/:categoryId',
+    '/site-de/it-IT/category/:categoryId',
+    '/site-de/it/category/:categoryId',
+    '/site-de/en-HK/category/:categoryId',
+    '/site-de/en-GB/category/:categoryId',
+    '/site-de/uk/category/:categoryId',
+    '/site-de/de-DE/category/:categoryId',
+    '/site-de/de/category/:categoryId',
+    '/RefArch/category/:categoryId',
+    '/us/category/:categoryId',
+    '/eu/category/:categoryId',
+    '/RefArchGlobal/category/:categoryId',
+    '/global/category/:categoryId',
+    '/site-de/category/:categoryId',
+    '/en-US/category/:categoryId',
+    '/en/category/:categoryId',
+    '/fr-FR/category/:categoryId',
+    '/fr/category/:categoryId',
+    '/it-IT/category/:categoryId',
+    '/it/category/:categoryId',
+    '/en-HK/category/:categoryId',
+    '/en-GB/category/:categoryId',
+    '/uk/category/:categoryId',
+    '/de-DE/category/:categoryId',
+    '/de/category/:categoryId',
+    '/category/:categoryId'
+]
+const mockConfig = {
+    app: {
+        defaultSite: 'RefArch',
+        url: {
+            site: 'none',
+            locale: 'none'
+        },
+        hosts: [
+            {
+                domain: 'www.domain-1.com',
+                defaultSite: 'RefArch',
+                sites: [
+                    {
+                        id: 'RefArch',
+                        alias: 'us',
+                        l10n: {
+                            supportedCurrencies: ['USD'],
+                            defaultCurrency: 'USD',
+                            defaultLocale: 'en-US',
+                            supportedLocales: [
+                                {
+                                    id: 'en-US',
+                                    alias: 'en',
+                                    preferredCurrency: 'USD'
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        id: 'RefArch',
+                        alias: 'eu',
+                        l10n: {
+                            supportedCurrencies: ['USD'],
+                            defaultCurrency: 'USD',
+                            supportedLocales: [
+                                {
+                                    id: 'fr-FR',
+                                    alias: 'fr',
+                                    preferredCurrency: 'USD'
+                                },
+                                {
+                                    id: 'it-IT',
+                                    alias: 'it',
+                                    preferredCurrency: 'USD'
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        id: 'RefArch',
+                        defaultLocale: 'en-US',
+                        l10n: {
+                            supportedCurrencies: ['USD'],
+                            defaultCurrency: 'USD',
+                            supportedLocales: [
+                                {
+                                    id: 'en-US',
+                                    preferredCurrency: 'USD'
+                                },
+                                {
+                                    id: 'en-HK',
+                                    preferredCurrency: 'HKD'
+                                }
+                            ],
+                            defaultLocale: 'en-US'
+                        }
+                    },
+                    {
+                        id: 'RefArchGlobal',
+                        alias: 'global',
+                        l10n: {
+                            supportedCurrencies: ['GBP', 'EUR'],
+                            defaultCurrency: 'GBP',
+                            supportedLocales: [
+                                {
+                                    id: 'en-GB',
+                                    alias: 'uk',
+                                    preferredCurrency: 'GBP'
+                                },
+                                {
+                                    id: 'fr-FR',
+                                    alias: 'fr',
+                                    preferredCurrency: 'EUR'
+                                }
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
+                domain: 'www.domain-2.com',
+                defaultSite: 'site-de',
+                sites: [
+                    {
+                        id: 'site-de',
+                        l10n: {
+                            supportedCurrencies: ['EUR'],
+                            defaultCurrency: 'EUR',
+                            defaultLocale: 'de-DE',
+                            supportedLocales: [
+                                {
+                                    id: 'de-DE',
+                                    alias: 'de',
+                                    preferredCurrency: 'EUR'
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
