173035a4f5ad0043b9e598d0c54e20a2459ae6bf
Oliver Brook <o.brook@salesforce.com>
2022-01-24 17:08:46 -0800

    Lint

--- packages/template-1c/app/main.test.js
+++ packages/template-1c/app/main.test.js
@@ -4,6 +4,6 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-test('basic test', () => {
-    expect(1 + 1).toBe(2)
-})
+test("basic test", () => {
+  expect(1 + 1).toBe(2);
+});
