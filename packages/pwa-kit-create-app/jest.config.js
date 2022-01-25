16f19d12bd40e3c6dc988818cfaa2a088cf930c2
Oliver Brook <o.brook@salesforce.com>
2022-01-25 00:30:48 -0800

    Use internal-build jest tools in create-app

--- packages/pwa-kit-create-app/jest.config.js
+++ packages/pwa-kit-create-app/jest.config.js
@@ -4,13 +4,22 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+const base = require('internal-lib-build/configs/jest/jest.config')
+
 module.exports = {
-    testEnvironment: 'node',
-    verbose: true,
-    collectCoverage: true,
-    testPathIgnorePatterns: ['node_modules', 'dist'],
-    collectCoverageFrom: ['src/**/*.{js,jsx}', 'scripts/**/*.{js,jsx}', '!node_modules/**'],
-    transform: {
-        '^.+\\.(js|jsx|ts|tsx)$': './jest-babel-transform.js'
-    }
+    ...base,
+    coverageThreshold: {
+        global: {
+            branches: 0,
+            functions: 0,
+            lines: 0,
+            statements: 0
+        }
+    },
+    testPathIgnorePatterns: [
+        'bin/*',
+        'dist/*',
+        'node_modules/*',
+        'coverage/*',
+    ],
 }
