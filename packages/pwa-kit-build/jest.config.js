57af2ce6adb482cc0df08a513be54b83e2afcc65
Oliver Brook <o.brook@salesforce.com>
2022-01-24 23:22:18 -0800

    Setup jest tests for pwa-kit-build

---
+++ packages/pwa-kit-build/jest.config.js
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+const base = require('internal-lib-build/configs/jest/jest.config')
+
+module.exports = {
+    ...base,
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80
+        }
+    },
+    testPathIgnorePatterns: [
+        'bin/*',
+        'dist/*',
+        'node_modules/*',
+        'coverage/*',
+    ],
+}
--- packages/pwa-kit-build/src/configs/jest/jest.config.js
+++ packages/pwa-kit-build/src/configs/jest/jest.config.js
@@ -15,10 +15,10 @@ module.exports = {
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': path.join(
             __dirname,
-            '__mocks__/fileMock.js'
+            'mocks', 'fileMock.js'
         ),
-        '\\.(svg)$': path.join(__dirname, '__mocks__/svgMock.js'),
-        '\\.(css|less)$': path.join(__dirname, '__mocks__/styleMock.js')
+        '\\.(svg)$': path.join(__dirname, 'mocks', 'svgMock.js'),
+        '\\.(css|less)$': path.join(__dirname, 'mocks', 'styleMock.js')
     },
     setupFilesAfterEnv: [path.join(__dirname, 'jest-setup.js')],
     globals: {
