9d4b5ae680008f9ec4c13178c342a4ce4f5d27f2
Oliver Brook <o.brook@salesforce.com>
2022-01-24 16:43:56 -0800

    Move jest test configs into SDKs

---
+++ packages/pwa-kit-build/src/configs/jest/jest.config.js
@@ -0,0 +1,26 @@
+import path from 'path'
+
+module.exports = {
+    testURL: 'http://localhost/',
+    verbose: true,
+    collectCoverage: true,
+    testEnvironment: 'jsdom',
+    testPathIgnorePatterns: ['node_modules', 'build'],
+    moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            path.join(__dirname, '__mocks__/fileMock.js',),
+        '\\.(svg)$': path.join(__dirname, '__mocks__/svgMock.js',),
+        '\\.(css|less)$': path.join(__dirname, '__mocks__/styleMock.js',)
+    },
+    setupFilesAfterEnv: [path.join(__dirname, 'jest-setup.js')],
+    globals: {
+        DEBUG: true,
+        NODE_ENV: 'test',
+        Progressive: {
+            buildOrigin: '/mobify/bundle/development/'
+        }
+    },
+    transform: {
+        '^.+\\.(js|jsx|ts|tsx)$': path.join(__dirname, 'jest-babel-transform.js')
+    }
+}
