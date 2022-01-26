b29a09185b95d10aefc9468e051fef033321b01f
Adam Raya Navarro <arayanavarro@salesforce.com>
2022-01-26 10:16:36 -0800

    Update create-mobify-app.js

--- packages/pwa-kit-create-app/scripts/create-mobify-app.js
+++ packages/pwa-kit-create-app/scripts/create-mobify-app.js
@@ -152,17 +152,7 @@ const runGenerator = (answers, {outputDir}) => {
         ]
     })
 
-    const PWAKitConfigJsonTemplatePath = p.resolve(
-        p.join(
-            process.cwd(),
-            'packages',
-            'pwa-kit-create-app',
-            'assets',
-            'pwa',
-            'pwa-kit.config.json'
-        )
-    )
-    const PWAKitConfigJsonTemplate = readJson(PWAKitConfigJsonTemplatePath)
+    const PWAKitConfigJsonTemplate = require(`../assets/pwa/pwa-kit.config`)
     const PWAKitConfigJsonPath = p.resolve(outputDir, 'pwa-kit.config.json')
     writeJson(PWAKitConfigJsonPath, PWAKitConfigJsonTemplate)
 
