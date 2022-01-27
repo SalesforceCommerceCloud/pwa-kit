384b09899f263f9fb4927d4945698c2398f4a100
Oliver Brook <o.brook@salesforce.com>
2022-01-27 14:53:06 -0800

    Move test to where the code lives

--- packages/pwa-kit-runtime/src/ssr/server/express.test.js
+++ packages/pwa-kit-runtime/src/ssr/server/express.test.js
@@ -1288,17 +1288,4 @@ describe('generateCacheKey', () => {
     })
 })
 
-const once = 'TODO'
 
-describe('the once function', () => {
-    test('should prevent a function being called more than once', () => {
-        const fn = jest.fn(() => ({test: 'test'}))
-        const wrapped = once(fn)
-        expect(fn.mock.calls.length).toBe(0)
-        const v1 = wrapped()
-        expect(fn.mock.calls.length).toBe(1)
-        const v2 = wrapped()
-        expect(fn.mock.calls.length).toBe(1)
-        expect(v1).toBe(v2) // The exact same instance
-    })
-})
