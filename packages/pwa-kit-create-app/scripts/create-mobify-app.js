9536e97f06046f293d717905bb322b89d325a96e
Adam Raya <adamraya@users.noreply.github.com>
2022-02-02 10:04:42 -0800

    Update packages/pwa-kit-create-app/scripts/create-mobify-app.js
    Co-authored-by: John Boxall <jboxall@salesforce.com>

--- packages/pwa-kit-create-app/scripts/create-mobify-app.js
+++ packages/pwa-kit-create-app/scripts/create-mobify-app.js
@@ -384,7 +384,7 @@ const presetPrompt = () => {
                     value: RETAIL_REACT_APP_DEMO
                 },
                 {
-                    name: 'The Retail app using your own Commerce Cloud sandbox',
+                   name: 'The Retail app using your own Commerce Cloud instance',
                     value: RETAIL_REACT_APP
                 }
             ]
