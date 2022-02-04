f999a7300d108927da224f8d1842cef9af94d165
Kevin He <kevin.he@salesforce.com>
2022-02-04 11:37:15 -0800

    use hybrid deployment ods instance

--- packages/pwa/app/api.config.js
+++ packages/pwa/app/api.config.js
@@ -7,9 +7,9 @@
 export const commerceAPIConfig = {
     proxyPath: `/mobify/proxy/api`,
     parameters: {
-        clientId: 'c9c45bfd-0ed3-4aa2-9971-40f88962b836',
-        organizationId: 'f_ecom_zzrf_001',
-        shortCode: '8o7m175y',
+        clientId: '987fc116-d30c-4537-93cb-c2bd433c3b5a',
+        organizationId: 'f_ecom_zzrf_002',
+        shortCode: 'kv7kzm78',
         siteId: 'RefArchGlobal'
     }
 }
