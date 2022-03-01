798c18d38aef3e0bbf39ae51a79d3de33ce72e64
Alex Vuong <alex.vuong@salesforce.com>
2022-03-01 10:56:15 -0800

    adjust the logic to only create routes for site and locale as a pair of a site

--- packages/pwa/app/utils/routes-utils.js
+++ packages/pwa/app/utils/routes-utils.js
@@ -18,63 +18,48 @@ export const configureRoutes = (routes = [], {ignoredRoutes = []}) => {
     if (!routes.length) return []
 
     const allSites = getSites()
-
-    // get a collection of all site-id and site alias from the config of the current host
-    // remove any duplicates by using [...new Set([])]
-    const sites = [
-        ...new Set(
-            allSites
-                ?.reduce((res, site) => {
-                    return [...res, site.alias, site.id]
-                }, [])
-                .filter(Boolean)
-        )
-    ]
-    // get a collection of all locale-id and locale alias from the config of the current host
-    // remove any duplicates by using [...new Set([])]
-    const locales = [
-        ...new Set(
-            allSites
-                .reduce((res, {l10n}) => {
-                    l10n.supportedLocales.forEach((locale) => {
-                        res = [...res, locale.alias, locale.id]
-                    })
-                    return res
-                }, [])
-                .filter(Boolean)
-        )
-    ]
+    if (!allSites) return routes
 
     let outputRoutes = []
     for (let i = 0; i < routes.length; i++) {
         const {path, ...rest} = routes[i]
+
         if (ignoredRoutes.includes(path)) {
             outputRoutes.push(routes[i])
         } else {
-            // construct all the routes that has both site and locale
-            sites.forEach((site) => {
-                locales.forEach((locale) => {
-                    const newRoute = `/${site}/${locale}${path}`
+            allSites.forEach((site) => {
+                const sites = [site.alias, site.id].filter(Boolean)
+                let locales = []
+                site.l10n.supportedLocales.forEach((locale) => {
+                    locales.push(locale.alias)
+                    locales.push(locale.id)
+                })
+                locales = locales.filter(Boolean)
+                // construct all the routes that has both site and locale
+                sites.forEach((site) => {
+                    locales.forEach((locale) => {
+                        const newRoute = `/${site}/${locale}${path}`
+                        outputRoutes.push({
+                            path: newRoute,
+                            ...rest
+                        })
+                    })
+                })
+
+                // construct the routes that only has site id or alias
+                sites.forEach((site) => {
                     outputRoutes.push({
-                        path: newRoute,
+                        path: `/${site}${path}`,
                         ...rest
                     })
                 })
-            })
 
-            // construct the routes that only has site id or alias
-            sites.forEach((site) => {
-                outputRoutes.push({
-                    path: `/${site}${path}`,
-                    ...rest
-                })
-            })
-
-            // construct the routes that only has locale id or alias
-            locales.forEach((locale) => {
-                outputRoutes.push({
-                    path: `/${locale}${path}`,
-                    ...rest
+                // construct the routes that only has locale id or alias
+                locales.forEach((locale) => {
+                    outputRoutes.push({
+                        path: `/${locale}${path}`,
+                        ...rest
+                    })
                 })
             })
             // origin route will be at the bottom
