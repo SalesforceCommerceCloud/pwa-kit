f5585df6ebb0de2ab2a9d1fe4252972ef39bfa1d
Alex Vuong <alex.vuong@salesforce.com>
2022-03-11 16:39:19 -0800

    add more tests for site-utils

--- packages/pwa/app/utils/site-utils.test.js
+++ packages/pwa/app/utils/site-utils.test.js
@@ -5,10 +5,18 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import {resolveSiteFromUrl} from './site-utils'
+import {getDefaultSite, getSites, resolveSiteFromUrl} from './site-utils'
 import {setConfig} from 'pwa-kit-react-sdk/ssr/universal/utils'
+import {getConfig} from 'pwa-kit-react-sdk/ssr/universal/utils'
 
 import mockConfig from '../../config/mocks/default'
+jest.mock('pwa-kit-react-sdk/ssr/universal/utils', () => {
+    const origin = jest.requireActual('pwa-kit-react-sdk/ssr/universal/utils')
+    return {
+        ...origin,
+        getConfig: jest.fn()
+    }
+})
 
 beforeEach(() => {
     jest.resetModules()
@@ -28,16 +36,18 @@ describe('resolveSiteFromUrl', function() {
     })
 
     test('return site based on the site alias in the url', () => {
+        getConfig.mockImplementation(() => mockConfig)
         const result = resolveSiteFromUrl('https://www.example-site.com/us/en-US/women/dress')
         expect(result.id).toEqual('site-2')
     })
 
     test('return default site for home page', () => {
+        getConfig.mockImplementation(() => mockConfig)
         const result = resolveSiteFromUrl('https://www.example-site.com/')
         expect(result.id).toEqual('site-1')
     })
 
-    test('throw an error when no site can be found', () => {
+    test('throw an error when no matching site can be found', () => {
         // Mock the  `default` config to the window global
         const newConfig = {
             ...mockConfig,
@@ -49,6 +59,134 @@ describe('resolveSiteFromUrl', function() {
         setConfig(newConfig)
         expect(() => {
             resolveSiteFromUrl('https://www.example-site.com/site-3')
+        }).toThrow()
+    })
+})
+
+describe('getDefaultSite', function() {
+    test('returns expected default site when there is only one site in the site list', () => {
+        const siteMock = {
+            site: 'site-a',
+            l10n: {
+                defaultLocale: 'en-GB',
+                supportedLocales: [
+                    {
+                        id: 'en-GB',
+                        preferredCurrency: 'GBP'
+                    },
+                    {
+                        id: 'fr-FR',
+                        alias: 'fr',
+                        preferredCurrency: 'EUR'
+                    },
+                    {
+                        id: 'it-IT',
+                        preferredCurrency: 'EUR'
+                    }
+                ]
+            }
+        }
+        getConfig.mockImplementation(() => ({
+            app: {
+                ...mockConfig.app,
+                siteAliases: {
+                    'site-a': 'a'
+                },
+                sites: [siteMock]
+            }
+        }))
+
+        const defaultSite = getDefaultSite()
+        expect(defaultSite).toEqual(siteMock)
+    })
+
+    test('returns site-2 as the default site according to the config', () => {
+        getConfig.mockImplementation(() => ({
+            app: {
+                ...mockConfig.app,
+                defaultSite: 'site-2'
+            }
+        }))
+
+        const expectedRes = {
+            alias: 'us',
+            id: 'site-2',
+            l10n: {
+                defaultLocale: 'en-US',
+                supportedLocales: [
+                    {
+                        id: 'en-US',
+                        preferredCurrency: 'USD'
+                    },
+                    {
+                        id: 'en-CA',
+                        preferredCurrency: 'USD'
+                    }
+                ]
+            }
+        }
+
+        const defaultSite = getDefaultSite()
+        expect(defaultSite).toEqual(expectedRes)
+    })
+})
+
+describe('getSites', function() {
+    test('returns site list with alias', () => {
+        getConfig.mockImplementation(() => mockConfig)
+        const sites = getSites()
+        const expectedRes = [
+            {
+                id: 'site-1',
+                alias: 'uk',
+                l10n: {
+                    defaultLocale: 'en-GB',
+                    supportedLocales: [
+                        {
+                            id: 'en-GB',
+                            preferredCurrency: 'GBP'
+                        },
+                        {
+                            id: 'fr-FR',
+                            alias: 'fr',
+                            preferredCurrency: 'EUR'
+                        },
+                        {
+                            id: 'it-IT',
+                            preferredCurrency: 'EUR'
+                        }
+                    ]
+                }
+            },
+            {
+                id: 'site-2',
+                alias: 'us',
+                l10n: {
+                    defaultLocale: 'en-US',
+                    supportedLocales: [
+                        {
+                            id: 'en-US',
+                            preferredCurrency: 'USD'
+                        },
+                        {
+                            id: 'en-CA',
+                            preferredCurrency: 'USD'
+                        }
+                    ]
+                }
+            }
+        ]
+        expect(sites).toEqual(expectedRes)
+    })
+
+    test('throw error when there is no sites in the config', () => {
+        getConfig.mockImplementation(() => ({
+            ...mockConfig.app,
+            sites: []
+        }))
+
+        expect(() => {
+            getSites()
         }).toThrow()
     })
 })
