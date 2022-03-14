3dc86e51042fae010ca88e50af5062ee05875941
Kevin He <kevin.he@salesforce.com>
2022-03-14 16:23:29 -0700

    Make body-parser optional (#446)
    * add boolean flag enableBodyParser
    
    * add tests
    
    * add changelog
    
    * rename enableLegacyBodyParser and add deprecation warning
    
    * fix test

--- packages/pwa-kit-react-sdk/src/ssr/server/express.test.js
+++ packages/pwa-kit-react-sdk/src/ssr/server/express.test.js
@@ -96,7 +96,8 @@ const opts = (overrides = {}) => {
         fetchAgents: {
             https: httpsAgent
         },
-        enableLegacyRemoteProxying: false
+        enableLegacyRemoteProxying: false,
+        enableLegacyBodyParser: true
     }
     return {
         ...defaults,
@@ -170,7 +171,7 @@ describe('createApp validates the options object', () => {
         const warn = sandbox.spy(console, 'warn')
 
         createApp(options)
-        expect(warn.calledOnce).toBe(true)
+        expect(warn.calledWith(`Favicon file ${options.faviconPath} not found`))
         sandbox.restore()
     })
 })
@@ -1017,6 +1018,23 @@ describe('SSRServer operation', () => {
         return request(app)
             .get('/thing')
             .expect(404)
+    })
+
+    const enableLegacyBodyParserFlags = [true, false]
+
+    enableLegacyBodyParserFlags.forEach((flag) => {
+        test(`Flag enableLegacyBodyParser, should enable body parser: ${flag}`, () => {
+            const app = createApp(opts({enableLegacyBodyParser: flag}))
+            const route = (req, res) => {
+                expect(req.body).toBeDefined(flag)
+                res.end()
+            }
+            app.post('/*', route)
+            return request(app)
+                .post('/some-url')
+                .type('application/json')
+                .send({test: 'data json'})
+        })
     })
 })
 
