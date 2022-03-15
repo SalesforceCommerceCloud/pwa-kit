cab6eca7c336e4839f976f1dff4fe613ddd7d155
Alex Vuong <alex.vuong@salesforce.com>
2022-03-15 12:46:51 -0700

    fix wording

--- packages/pwa-kit-create-app/assets/pwa/sites.js
+++ packages/pwa-kit-create-app/assets/pwa/sites.js
@@ -5,7 +5,7 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 exports.template = () => `// Provide the sites for your app. Each site includes site id, and its localization configuration.
-// You can also provide alias for your locale. They will be used in place of your locale id when generating paths across the app
+// You can also provide aliases for your locale. They will be used in place of your locale id when generating paths across the app
 module.exports = [
     {
         id: 'RefArch',
