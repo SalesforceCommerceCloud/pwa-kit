4abf5d7e796183e9f1ab2da681580e17f6b83e83
vcua-mobify <vcua@salesforce.com>
2022-03-25 09:35:31 -0700

    Add progress-plugin to webpack

--- packages/pwa/webpack.config.js
+++ packages/pwa/webpack.config.js
@@ -4,6 +4,18 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-const config = require('pwa-kit-build/configs/webpack/config')
 
-module.exports = config
+const config = require('pwa-kit-build/configs/webpack/config')
+const [client, ssr, renderer, clientOptional, requestProcessor] = config;
+
+const ProgressPlugin = require('progress-webpack-plugin')
+
+console.log("in custom webpack")
+
+client.plugins.push(new ProgressPlugin(true))
+ssr.plugins.push(new ProgressPlugin(true))
+renderer.plugins.push(new ProgressPlugin(true))
+clientOptional.plugins.push(new ProgressPlugin(true))
+requestProcessor.plugins.push(new ProgressPlugin(true))
+
+module.exports = [client, ssr, renderer, clientOptional, requestProcessor]
