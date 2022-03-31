27e0ae094ca87ec3f5f23b13bbc96aadebe6dc0a
Oliver Brook <o.brook@salesforce.com>
2022-03-30 17:46:31 -0700

    Implementation ideas for an animated loading-screen

--- packages/pwa-kit-build/src/ssr/server/build-dev-server.js
+++ packages/pwa-kit-build/src/ssr/server/build-dev-server.js
@@ -18,7 +18,6 @@ import webpackHotServerMiddleware from 'webpack-hot-server-middleware'
 import open from 'open'
 import requireFromString from 'require-from-string'
 import config from '../../configs/webpack/config'
-import {loadingScreen} from './loading-screen'
 import {RemoteServerFactory} from 'pwa-kit-runtime/ssr/server/build-remote-server'
 import {proxyConfigs} from 'pwa-kit-runtime/utils/ssr-shared'
 
@@ -101,9 +100,12 @@ export const DevServerMixin = {
             return res.json({ready: app.__webpackReady()})
         })
 
-        app.use('/__mrt', (req, res) => {
-            res.send(loadingScreen())
-        })
+        app.use(
+            '/__mrt/loading-screen/',
+            express.static(path.resolve(__dirname, 'loading-screen'), {
+                dotFiles: 'deny'
+            })
+        )
     },
 
     addSSRRenderer(app) {
@@ -144,7 +146,7 @@ export const DevServerMixin = {
             if (app.__webpackReady()) {
                 middleware(req, res, next)
             } else {
-                res.redirect(301, '/__mrt?loading=1')
+                res.redirect(301, '/__mrt/loading-screen/index.html?loading=1')
             }
         })
 
