87b8a2b6a483f9975c6a2fe2e6e872ab4c753c9c
Ben Chypak <bchypak@salesforce.com>
2022-04-01 16:02:01 -0700

    Set build project coverage to pre-prerelease values

--- packages/pwa-kit-build/jest.config.js
+++ packages/pwa-kit-build/jest.config.js
@@ -10,10 +10,10 @@ module.exports = {
     ...base,
     coverageThreshold: {
         global: {
-            branches: 53.6,
-            functions: 63.2,
-            lines: 60.5,
-            statements: 62.1
+            branches: 62,
+            functions: 65.5,
+            lines: 65,
+            statements: 67
         }
     },
     testPathIgnorePatterns: ['bin/*', 'dist/*', 'node_modules/*', 'coverage/*'],
