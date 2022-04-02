14cb573c0fac26343537df59d8a2e2038bf10b00
Oliver Brook <o.brook@salesforce.com>
2022-04-01 17:50:31 -0700

    Reduce Vincent's example to the bare essentials

--- packages/pwa/webpack.config.js
+++ packages/pwa/webpack.config.js
@@ -13,49 +13,13 @@ const findInProjectThenSDK = (pkg) => {
 
 console.log("in custom webpack")
 
-const [client, ssr, renderer, clientOptional, requestProcessor] = webpackConfig
-
-client.resolve.extensions.push(".css")
-
-client.module.rules.push({
-    test: /\.css$/i,
-    use: [findInProjectThenSDK('style-loader'), findInProjectThenSDK('css-loader')]
+const updated = webpackConfig.map((config) => {
+    config.resolve.extensions.push(".css")
+    config.module.rules.push({
+        test: /\.css$/i,
+        use: [findInProjectThenSDK('style-loader'), findInProjectThenSDK('css-loader')]
+    })
+    return config
 })
 
-clientOptional.resolve.extensions.push(".css")
-
-clientOptional.module.rules.push({
-    test: /\.css$/i,
-    use: [findInProjectThenSDK('style-loader'), findInProjectThenSDK('css-loader')]
-})
-
-ssr.resolve.extensions.push(".css")
-
-ssr.module.rules.push({
-    test: /\.css$/i,
-    use: [findInProjectThenSDK('style-loader'), findInProjectThenSDK('css-loader')]
-})
-
-renderer.resolve.extensions.push(".css")
-
-renderer.module.rules.push({
-    test: /\.css$/i,
-    use: [findInProjectThenSDK('style-loader'), findInProjectThenSDK('css-loader')]
-})
-
-// console.log(client)
-
-// for (rule of client.module.rules) {
-//     console.log(rule)
-// }
-
-// // console.log(ssr)
-
-// for (rule of ssr.module.rules) {
-//     console.log(rule)
-// }
-
-
-//note: request processor import is failing the build
-module.exports = [client, ssr, renderer, clientOptional, requestProcessor]
-    .filter(Boolean)
+module.exports = updated
