1a56bc4b3262c8fb9841d8168ae448ba9780304c
Adam Raya Navarro <arayanavarro@salesforce.com>
2022-04-11 15:50:13 -0700

    Fix glob string cross env on internal-lib-build script

--- packages/internal-lib-build/bin/internal-lib-build.js
+++ packages/internal-lib-build/bin/internal-lib-build.js
@@ -57,8 +57,7 @@ const main = () => {
         .argument('<path>', 'path or glob to format')
         .action((path) => {
             const prettier = p.join(binDir, 'prettier')
-            // eslint-disable-next-line no-useless-escape
-            execSync(`${prettier} --write \"${path}\"`)
+            execSync(`${prettier} --write "${path}"`)
         })
 
     program
