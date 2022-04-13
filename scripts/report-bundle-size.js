9bb1d876be15213ffa7774a3a448569435eeb7e5
Alex Vuong <52219283+alexvuong@users.noreply.github.com>
2022-04-13 16:45:42 -0700

    [V2]Generate different reports for different bundles (#508)
    * generate different reports for different bundles
    * fix report bundle size script

--- scripts/report-bundle-size.js
+++ scripts/report-bundle-size.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /* eslint-env node */
-
+const fs = require('fs')
 const path = require('path')
 const childProc = require('child_process')
 
@@ -8,15 +8,27 @@ const childProc = require('child_process')
  * Send bundle size stats to Datadog
  */
 const main = () => {
-    const retailReactAppStats = require(path.join(path.resolve(''), 'packages', 'template-retail-react-app', 'build', 'stats.json'))
-
-    const bundles = retailReactAppStats.assets
-    bundles.forEach((bundle) => {
-        const metric = `mobify_platform_sdks.bundle_size_byte`
-        const value = bundle.size
-        childProc.spawnSync('dog', ['metric', 'post', metric, value, '--host', bundle.name])
-        console.log(`${metric} ${value} --host ${bundle.name}`)
-    })
+    const buildDir = path.resolve('packages', 'template-retail-react-app', 'build')
+    fs.promises.readdir(buildDir)
+        .then(files => {
+            files
+                .filter(file => file.includes('-analyzer-stats.json'))
+                .forEach(file => {
+                    console.log(`Analyzer stats json file found:`, file)
+                    const retailReactAppStats = require(path.join(buildDir, file))
+                    const bundles = retailReactAppStats.assets
+                    bundles.forEach((bundle) => {
+                        const metric = `mobify_platform_sdks.bundle_size_byte`
+                        const value = bundle.size
+                        childProc.spawnSync('dog', ['metric', 'post', metric, value, '--host', bundle.name])
+                        console.log(`${metric} ${value} --host ${bundle.name}`)
+                    })
+                })
+        })
+        .catch(err => {
+            console.error(err)
+            process.exit(1)
+        })
 }
 
 main()
