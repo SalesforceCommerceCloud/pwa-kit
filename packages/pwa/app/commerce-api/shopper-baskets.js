95299850c9147ce7097507c781dca68ebd46991e
Jainam Sheth <j.sheth@salesforce.com>
2022-04-19 16:09:59 -0400

    Refactored shopperBasket instantiation

--- packages/pwa/app/commerce-api/shopper-baskets.js
+++ packages/pwa/app/commerce-api/shopper-baskets.js
@@ -1,12 +1,17 @@
 import OcapiShopperBaskets from './ocapi-shopper-baskets'
-import * as sdk from 'commerce-sdk-isomorphic'
+import  * as sdk from "commerce-sdk-isomorphic";
 
 // This class is an extension to OcapiShopperBaskets class to include basket functions implemented in SCAPI.
 // Current implementation has ShopperBaskets interactiing with OCAPI which misses new functions implemented in newer SCAPI releases.
 class ShopperBaskets extends OcapiShopperBaskets {
+    constructor(config) {
+        super(config)
+        this.shopperBasketsClient = new sdk.ShopperBaskets(config)
+    }
 
     mergeBasket(args) {
-        return sdk.ShopperBaskets.mergeBasket(args)
+        console.log("Args" ,args)
+        return this.shopperBasketsClient.mergeBasket(args)
     }
 }
 
