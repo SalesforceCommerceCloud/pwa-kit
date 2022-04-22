e856fb58285cf24f2e4618548d9d08f85a5202d6
Will Harney <wharney@salesforce.com>
2022-04-22 11:42:09 -0400

    Remove lodash and bluebird.

--- packages/pwa-kit-react-sdk/scripts/file-utils.js
+++ packages/pwa-kit-react-sdk/scripts/file-utils.js
@@ -1,27 +1,22 @@
 /*
- * Copyright (c) 2021, salesforce.com, inc.
+ * Copyright (c) 2022, Salesforce, Inc.
  * All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-const Promise = require('bluebird')
 const fs = require('fs')
-const _ = require('lodash')
 
-// Explicitly promisify for more reliable testing
-const readFileAsync = Promise.promisify(fs.readFile)
-const writeFileAsync = Promise.promisify(fs.writeFile)
-const statAsync = Promise.promisify(fs.stat)
-const mkdirAsync = Promise.promisify(fs.mkdir)
-const readdirAsync = Promise.promisify(fs.readdir)
+const readFileAsync = fs.promises.readFile
+const writeFileAsync = fs.promises.writeFile
+const statAsync = fs.promises.stat
+const mkdirAsync = fs.promises.mkdir
+const readdirAsync = fs.promises.readdir
 
 const readFile = (path) => readFileAsync(path, 'utf8')
 const writeFile = (path, contents) => writeFileAsync(path, contents, 'utf8')
 
-// Equivalent to:
-// (path) => (contents) => writeFile(path, contents)
-const writeToPath = _.curry(writeFile)
+const writeToPath = (path) => (contents) => writeFile(path, contents)
 
 const mkdirIfNonexistent = (dirname) => statAsync(dirname).catch(() => mkdirAsync(dirname))
 
@@ -37,13 +32,13 @@ const existsSync = (path) => {
 const clearNulls = (items) => items.filter((item) => item !== null)
 
 const filterOnStat = (pathBuilder, statCondition) => (items) => {
-    return Promise.map(items, (item) => {
-        return statAsync(pathBuilder(item))
-            .then((stats) => {
-                return statCondition(stats) ? item : null
-            })
-            .catchReturn(null)
-    }).then(clearNulls)
+    const filterStats = item => statAsync(pathBuilder(item))
+            .then(stats => statCondition(stats) ? item : null)
+            .catch(() => null);
+    return Promise.resolve()
+        .then(() => items.map(filterStats))
+        .then(Promise.all)
+        .then(clearNulls);
 }
 
 const filterDirectories = (pathBuilder) => filterOnStat(pathBuilder, (stats) => stats.isDirectory())
