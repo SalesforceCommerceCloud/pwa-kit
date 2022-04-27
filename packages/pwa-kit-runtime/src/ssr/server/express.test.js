aedb13c5960c0e272c7f3b2ce0d22d915d9757d6
Will Harney <wharney@salesforce.com>
2022-04-27 15:29:26 -0400

    Remove dead code.

--- packages/pwa-kit-runtime/src/ssr/server/express.test.js
+++ packages/pwa-kit-runtime/src/ssr/server/express.test.js
@@ -190,14 +190,9 @@ describe('createApp validates environment variables', () => {
 describe('SSRServer operation', () => {
     const savedEnvironment = Object.assign({}, process.env)
     const sandbox = sinon.createSandbox()
-    let server
 
     afterEach(() => {
         sandbox.restore()
-        if (server) {
-            server.close()
-            server = null
-        }
         nock.cleanAll()
     })
 
