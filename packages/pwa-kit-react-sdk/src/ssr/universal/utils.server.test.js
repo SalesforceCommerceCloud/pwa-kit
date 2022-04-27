0f20c62a3b17ba95e3d389cf29ae99b2a064edbc
Will Harney <wharney@salesforce.com>
2022-04-27 12:14:45 -0400

    Don't need to ignore things that don't exist...

--- packages/pwa-kit-react-sdk/src/ssr/universal/utils.server.test.js
+++ packages/pwa-kit-react-sdk/src/ssr/universal/utils.server.test.js
@@ -2,11 +2,14 @@
  * @jest-environment node
  */
 /*
- * Copyright (c) 2021, salesforce.com, inc.
+ * Copyright (c) 2022, Salesforce, Inc.
  * All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+// Jest requires the @jest-environment comment at the start of file, which
+// conflicts with the eslint header rule.
+/* eslint-disable header/header */
 
 import * as utils from './utils'
 import {proxyConfigs} from 'pwa-kit-runtime/utils/ssr-shared'
