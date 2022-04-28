ea0d273921c0ffaa704e9ac617261a755c5c5755
Ben Chypak <bchypak@salesforce.com>
2022-04-28 12:41:18 -0700

    Fix copy headers for scripts

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /*
- * Copyright (c) 2021, salesforce.com, inc.
+ * Copyright (c) 2022, Salesforce, Inc.
  * All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
