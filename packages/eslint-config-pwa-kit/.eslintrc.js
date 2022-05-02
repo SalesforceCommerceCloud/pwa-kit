b8ed71464aee0a0443004b9a1b039721416f186a
Will Harney <wharney@salesforce.com>
2022-05-02 16:02:57 -0400

    Remove header rule.

--- packages/eslint-config-pwa-kit/.eslintrc.js
+++ packages/eslint-config-pwa-kit/.eslintrc.js
@@ -7,67 +7,50 @@
 
     // Deliberate eslint rule violation for testing eslint
 
-    require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution')
 
-    module.exports = {
-        parser: '@babel/eslint-parser',
-        parserOptions: {
-            ecmaVersion: 2017,
-            sourceType: 'module',
-            ecmaFeatures: {
-                jsx: true
-            },
-            requireConfigFile: false,
-            babelOptions: {
-                presets: ['pwa-kit']
+module.exports = {
+    parser: '@babel/eslint-parser',
+    parserOptions: {
+        ecmaVersion: 2017,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true
+        },
+        requireConfigFile: false,
+        babelOptions: {
+            presets: ['pwa-kit']
+        }
+    },
+    env: {
+        es6: true,
+        node: true,
+        browser: true,
+        jest: true
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier', 'prettier/react'],
+    plugins: ['react', 'prettier'],
+    settings: {
+        react: {
+            version: '16.8'
+        }
+    },
+    rules: {
+        'prettier/prettier': ['error'],
+        'no-console': 'off',
+        'no-unused-vars': ['error', {ignoreRestSiblings: true}]
+    },
+    overrides: [
+        {
+            files: ['**/*.ts?(x)'],
+            parser: '@typescript-eslint/parser',
+            parserOptions: {
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true
+                },
+                warnOnUnsupportedTypeScriptVersion: true
             }
-        },
-        env: {
-            es6: true,
-            node: true,
-            browser: true,
-            jest: true
-        },
-        extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier', 'prettier/react'],
-        plugins: ['header', 'react', 'prettier'],
-        settings: {
-            react: {
-                version: '16.8'
-            }
-        },
-        rules: {
-            'prettier/prettier': ['error'],
-            'no-console': 'off',
-            'no-unused-vars': ['error', {ignoreRestSiblings: true}],
-            'header/header': [
-                2,
-                'block',
-                [
-                    '',
-                    {
-                        pattern:
-                            '^ \\* Copyright \\(c\\) \\d{4}, (salesforce.com, inc|Salesforce, Inc)\\.$',
-                        template: ' * Copyright (c) 2022, salesforce.com, inc.'
-                    },
-                    ' * All rights reserved.',
-                    ' * SPDX-License-Identifier: BSD-3-Clause',
-                    ' * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause',
-                    ' '
-                ]
-            ]
-        },
-        overrides: [
-            {
-                files: ['**/*.ts?(x)'],
-                parser: '@typescript-eslint/parser',
-                parserOptions: {
-                    sourceType: 'module',
-                    ecmaFeatures: {
-                        jsx: true
-                    },
-                    warnOnUnsupportedTypeScriptVersion: true
-                }
-            }
-        ]
-    }
-    
+        }
+    ]
+}
