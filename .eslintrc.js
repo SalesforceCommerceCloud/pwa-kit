a0b8b9736e90029016c48327d1459c2e38a83a01
Kieran Haberstock <khaberstock@salesforce.com>
2022-05-06 12:19:26 -0700

    Add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,12 @@ module.exports = {
                 ' '
             ]
         ]
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "env": {
+        "es6": true,
+        "node": true
     }
 }
