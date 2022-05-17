caa25306113cd49bf7715d236814e379b56522b7
Alex Vuong <alex.vuong@salesforce.com>
2022-05-17 16:21:52 -0700

    replace fs with sh

--- scripts/bump-version.js
+++ scripts/bump-version.js
@@ -3,7 +3,6 @@
 
 const sh = require("shelljs")
 const path = require("path")
-const fs = require("fs")
 
 sh.set("-e")
 const lernaConfigPath = path.join(__dirname, '..', 'lerna.json')
@@ -13,24 +12,25 @@ const rootPkgLockPath = path.join(__dirname, '..', 'package-lock.json')
 const main = () => {
     sh.exec(`lerna version --no-push --no-git-tag-version --yes ${process.argv.slice(2).join(' ')}`)
     sh.exec(`npm install`)
-    const lernaConfig = JSON.parse(sh.cat(lernaConfigPath));
+    const lernaConfig = JSON.parse(sh.cat(lernaConfigPath))
     const rootPkg = JSON.parse(sh.cat(rootPkgPath))
     const rootLockPkg = JSON.parse(sh.cat(rootPkgLockPath))
 
     // find all monorepo packages, look inside each package json, find peerDependency that is a monorepo package
     // and update it with a new version
-    sh.exec('npm run --silent lerna -- list --all --json', function(code, stdout) {
+    sh.exec('npm run --silent lerna -- list --all --json', {silent: true}, function(code, stdout) {
         const packages = JSON.parse(stdout.toString())
 
         const lernaPackageNames = packages.map(pkg => pkg.name)
         packages.forEach(async ({location}) => {
-            const pkgFile = path.join(location, 'package.json')
-            const pkg =  await fs.promises.readFile(pkgFile, 'utf-8')
-            const peerDependencies = JSON.parse(pkg).peerDependencies
+            const pkgFilePath = path.join(location, 'package.json')
+            const pkg = JSON.parse(sh.cat(pkgFilePath))
+            const peerDependencies = pkg.peerDependencies
             peerDependencies && Object.keys(peerDependencies).forEach(dep => {
                 if (lernaPackageNames.includes(dep)) {
                     console.log(`Lerna local packages ${dep} is found as a peerDependencies`)
                     peerDependencies[dep] = `^${lernaConfig.version}`
+                    new sh.ShellString(JSON.stringify(pkg, null, 2)).to(pkgFilePath)
                 }
             })
 
