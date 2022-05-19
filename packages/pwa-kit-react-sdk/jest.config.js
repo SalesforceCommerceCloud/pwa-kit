48be2f801a50b2261e5305e7c3c80d5a8ace5ce7
Will Harney <62956339+wjhsf@users.noreply.github.com>
2022-05-19 16:09:35 -0400

    Improve test coverage (#550)
    * Ignore files that don't need to be tested.
    
    * Don't need to ignore things that don't exist...
    
    * Don't collect coverage for test fixtures.
    
    * Remove dead code.
    
    * Add basic "it works!" test.
    
    * Appease linter.
    
    * Add note about export.
    
    * Oops a newline.
    
    * Redo exports.
    
    * Clean up coverage.
    
    * Meet coverage threshold.
    
    * Bump peer dep.
    
    * Restore package-lock.json
    
    * Raise coverage threshold.
    
    * Revert to lockfileVersion 1.
    
    * Add basic test for local asset headers.
    
    * Add tests for render function.
    
    * Add tests for service service worker
    
    * Add private note.
    
    * Fix lint errors.
    
    * Bump `pwa-kit-dev` coverage thresholds & Revert serveServiceWorker change
    
    * Re-apply serveServiceWorker change using `DevServerFactory`
    
    * Change dev server tests to match remote server tests.
    
    * Revert extracting callback to private method.
    
    Co-authored-by: Adam Raya <adamraya@users.noreply.github.com>
    Co-authored-by: Adam Raya Navarro <arayanavarro@salesforce.com>
    Co-authored-by: Ben Chypak <bchypak@mobify.com>

--- packages/pwa-kit-react-sdk/jest.config.js
+++ packages/pwa-kit-react-sdk/jest.config.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, salesforce.com, inc.
+ * Copyright (c) 2022, Salesforce, Inc.
  * All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
@@ -11,43 +11,18 @@ module.exports = {
     ...base,
     setupFiles: base.setupFiles.concat(path.join(__dirname, 'setup-jest.js')),
     collectCoverageFrom: [
-        '**/*.{js,jsx}',
+        'src/**/*.{js,jsx}',
+        'scripts/**/*.{js,jsx}',
         '!**/test.{js,jsx}',
-        '!generator-assets/**/*.{js,jsx}',
-        '!coverage/**/*.js',
-        '!dist/**/*.{js,jsx}',
-        '!docs/**/*.{js,jsx}',
-        '!vendor/**/*',
-        '!*.config.js',
-        '!scripts/{setup-jsdom}.js',
-        '!src/utils/{testing,cookie-manager}.js',
-        '!src/index.js',
-        '!src/{polyfill,test-utils,load-scripts}.js',
-        '!jest-babel-transform.js',
-        '!src/integration-manager/index.js',
-        '!src/integration-manager/types.js',
-        '!src/integration-manager/results.js',
-        '!src/integration-manager/commands.js',
-        '!src/integration-manager/reducer.js',
-        '!src/integration-manager/*/{commands,results,types}.js',
-        '!bin/*.js',
-        '!src/patterns/**/*.{js,jsx}',
-        '!src/templates/**/*.{js,jsx}',
-        '!src/ssr/ssr-polyfills.js',
-        '!src/ssr/server/test_fixtures/*.{js,jsx}',
-        '!src/worker/{main,test-entrypoint}.js',
-        '!temp/**/*',
-        '!temp_static/**/*',
-        '!src/static/*',
-        '!tests/*.js',
-        '!src/webpack/**/*'
+        '!scripts/setup-jsdom.js',
+        '!scripts/version.js'
     ],
     coverageThreshold: {
         global: {
-            branches: 86.5,
-            functions: 81.2,
-            lines: 85.3,
-            statements: 85.1
+            branches: 90,
+            functions: 85,
+            lines: 85,
+            statements: 85
         }
     }
 }
--- packages/template-typescript-minimal/jest.config.js
+++ packages/template-typescript-minimal/jest.config.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, salesforce.com, inc.
+ * Copyright (c) 2022, Salesforce, Inc.
  * All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
@@ -7,5 +7,16 @@
 const base = require('pwa-kit-dev/configs/jest/jest.config.js')
 
 module.exports = {
-    ...base
+    ...base,
+    coverageThreshold: {
+        global: {
+            branches: 0,
+            functions: 0,
+            lines: 0,
+            statements: 0
+        }
+    },
+    collectCoverageFrom: [
+        'app/**'
+    ]
 }
