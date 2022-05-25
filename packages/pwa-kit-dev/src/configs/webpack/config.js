9d0cefc31a954534639360cc6986b245bfe7b694
Vincent Marta <vmarta@salesforce.com>
2022-05-25 11:00:45 -0700

    Add a comment

--- packages/pwa-kit-dev/src/configs/webpack/config.js
+++ packages/pwa-kit-dev/src/configs/webpack/config.js
@@ -254,6 +254,8 @@ const client =
                 },
                 output: {
                     ...config.output,
+                    // So that client-side hot reloading works properly,
+                    // we'll need to prepend the *.hot-update.json urls with this publicPath
                     ...(mode === development ? {publicPath: '/mobify/bundle/development/'} : {})
                 }
             }
