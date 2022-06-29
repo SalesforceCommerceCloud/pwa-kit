3067c9069cb032c0bd13898d50a7e978884bd0f7
vcua-mobify <vcua@salesforce.com>
2022-06-29 16:27:49 -0700

    Incorporate additional suggestions

--- packages/ecom-react-hooks/src/placeholder.ts
+++ packages/ecom-react-hooks/src/placeholder.ts
@@ -1,1 +1,6 @@
+/*
+    This file exists because we need to have at least 1 .ts file in the source directory or TS lint will throw an error. 
+    We can remove this once we have an actual .ts file in this package.
+ */
+
 export {}
