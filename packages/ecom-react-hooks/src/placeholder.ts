18394aa1fbc4aaca6d1f431017717a00adf9b22c
vcua-mobify <vcua@salesforce.com>
2022-06-30 09:24:42 -0700

    More lint fixes

--- packages/ecom-react-hooks/src/placeholder.ts
+++ packages/ecom-react-hooks/src/placeholder.ts
@@ -1,10 +1,9 @@
 /*
- * Copyright (c) 2022, Salesforce.com, Inc.
+ * Copyright (c) 2022, salesforce.com, inc.
  * All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-
 
 /*
     This file exists because we need to have at least 1 .ts file in the source directory or TS lint will throw an error. 
