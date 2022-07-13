00d825bff1cd9021f014e060a2d9d7b81311e421
Kevin He <kevin.he@salesforce.com>
2022-07-13 11:01:11 -0700

    add copyright header

--- packages/commerce-sdk-react/src/types.ts
+++ packages/commerce-sdk-react/src/types.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2022, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
 export interface CommonHookResponse {
     error: Error | undefined
     isLoading: boolean
