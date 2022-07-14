dcead148e6d3fe8cdeadd7b2d4aca1dc7e282e4f
Vincent Marta <vmarta@salesforce.com>
2022-07-13 17:32:20 -0700

    Some code cleanup

--- packages/commerce-sdk-react/src/provider.tsx
+++ packages/commerce-sdk-react/src/provider.tsx
@@ -7,39 +7,9 @@
 import React, {ReactElement} from 'react'
 import { ApiClients } from './hooks/types'
 
-import {
-    ShopperBaskets,
-    ShopperContexts,
-    ShopperCustomers,
-    ShopperLogin,
-    ShopperOrders,
-    ShopperProducts,
-    ShopperPromotions,
-    ShopperDiscoverySearch,
-    ShopperGiftCertificates,
-    ShopperSearch,
-} from 'commerce-sdk-isomorphic'
-
-// TODO: these types are not exported from the commerce-sdk-isomorphic
-// can we use other types?
-// if not, we need to make a change to commerce-sdk-isomorphic to expose these types
-
-import {
-    ShopperBasketsParameters,
-    ShopperContextsParameters,
-    ShopperCustomersParameters,
-    ShopperDiscoverySearchParameters,
-    ShopperGiftCertificatesParameters,
-    ShopperLoginParameters,
-    ShopperOrdersParameters,
-    ShopperProductsParameters,
-    ShopperPromotionsParameters,
-    ShopperSearchParameters,
-} from 'commerce-sdk-isomorphic'
-
 import ApiClient from './api-client'
 
-export interface CommerceAPIProviderProps {
+interface CommerceAPIProviderProps {
     children: React.ReactNode
     clientId: string
     organizationId: string
@@ -72,19 +42,6 @@ export const CommerceAPIProvider = (props: CommerceAPIProviderProps): ReactEleme
     const apiClient = new ApiClient(config)
 
     console.log('--- api client', apiClient)
-
-    // const apiClient = {
-    //     shopperBaskets: new ShopperBaskets(config),
-    //     shopperContexts: new ShopperContexts(config),
-    //     shopperCustomers: new ShopperCustomers(config),
-    //     shopperDiscoverySearch: new ShopperDiscoverySearch(config),
-    //     shopperGiftCertificates: new ShopperGiftCertificates(config),
-    //     shopperLogin: new ShopperLogin(config),
-    //     shopperOrders: new ShopperOrders(config),
-    //     shopperProducts: new ShopperProducts(config),
-    //     shopperPromotions: new ShopperPromotions(config),
-    //     shopperSearch: new ShopperSearch(config),
-    // }
 
     // TODO: use Context from useServerEffect
     // See Kevin's PR: https://github.com/SalesforceCommerceCloud/pwa-kit/pull/654/files#r914097886
