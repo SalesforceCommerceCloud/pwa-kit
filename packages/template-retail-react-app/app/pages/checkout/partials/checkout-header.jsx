ec707a8e5356e62a3b68dc0d5403c228765f2e37
Alex Vuong <alex.vuong@salesforce.com>
2022-07-15 17:07:21 -0700

    fix checkout header logo navigation

--- packages/template-retail-react-app/app/pages/checkout/partials/checkout-header.jsx
+++ packages/template-retail-react-app/app/pages/checkout/partials/checkout-header.jsx
@@ -5,25 +5,33 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import React from 'react'
-import {FormattedMessage} from 'react-intl'
-import {Badge, Box, Button, Flex, Center} from '@chakra-ui/react'
+import PropTypes from 'prop-types'
+import {FormattedMessage, useIntl} from 'react-intl'
+import {Badge, Box, Button, Flex, Center, IconButton, useMultiStyleConfig} from '@chakra-ui/react'
 import useBasket from '../../../commerce-api/hooks/useBasket'
 import Link from '../../../components/link'
 import {BasketIcon, BrandLogo} from '../../../components/icons'
+import {noop} from '../../../utils/utils'
 
-const CheckoutHeader = () => {
+const CheckoutHeader = ({onLogoClick = noop()}) => {
     const basket = useBasket()
+    const intl = useIntl()
+    const styles = useMultiStyleConfig('Header')
 
     return (
         <Box px={[4, 4, 8]} bg="white" borderBottom="1px" borderColor="gray.100">
             <Box maxWidth="container.xxxl" marginLeft="auto" marginRight="auto">
                 <Flex h={{base: '52px', md: '80px'}} align="center" justify="space-between">
-                    <Link href="/" title="Back to homepage">
-                        <BrandLogo
-                            width={{base: '35px', md: '45px'}}
-                            height={{base: '24px', md: '32px'}}
-                        />
-                    </Link>
+                    <IconButton
+                        aria-label={intl.formatMessage({
+                            id: 'header.button.assistive_msg.logo',
+                            defaultMessage: 'Logo'
+                        })}
+                        icon={<BrandLogo {...styles.logo} />}
+                        {...styles.icons}
+                        variant="unstyled"
+                        onClick={onLogoClick}
+                    />
 
                     <Button
                         as={Link}
@@ -47,6 +55,10 @@ const CheckoutHeader = () => {
             </Box>
         </Box>
     )
+}
+
+CheckoutHeader.propTypes = {
+    onLogoClick: PropTypes.func
 }
 
 export default CheckoutHeader
