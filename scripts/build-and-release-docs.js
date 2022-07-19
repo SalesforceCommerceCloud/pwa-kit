0ebdcfafa0bc8d5791a5e66e877657de07e230da
Alex Vuong <alex.vuong@salesforce.com>
2022-07-19 15:24:25 -0700

    test run

--- scripts/build-and-release-docs.js
+++ scripts/build-and-release-docs.js
@@ -9,17 +9,23 @@ const main = () => {
     const {stdout: currentVersion} = sh.exec(
         "cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g'"
     )
+
+    sh.exec('npm run build:docs')
+
+    // release the docs
+    sh.exec('npx gh-pages --dotfiles --message "[skip ci] Pushing docs to gh-pages" --dist docs')
+
+    // delete docs file
+    sh.rm('-rf', 'docs')
+
     console.log('currentVersion', currentVersion)
 
     const isPrerelease = semver.prerelease(currentVersion.trim())
     if (isPrerelease) {
         return
     }
-    console.log('isPrerelease', isPrerelease)
 
-    console.log('latestVersion', latestVersion)
     const isLatest = semver.gt(currentVersion, latestVersion)
-    console.log('isLatest', isLatest)
     // we only want to publish the docs for latest release branch
     if (isLatest) {
         console.log(`Publish docs for version ${currentVersion} to gh-pages branch`)
