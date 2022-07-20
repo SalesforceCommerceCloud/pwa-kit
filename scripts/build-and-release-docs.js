65e65c1a1d19eff6bca114c1ed65f5adc59760dd
Alex Vuong <alex.vuong@salesforce.com>
2022-07-20 15:47:25 -0700

    verify the script works

--- scripts/build-and-release-docs.js
+++ scripts/build-and-release-docs.js
@@ -3,6 +3,7 @@
 const sh = require('shelljs')
 const semver = require('semver')
 
+// this script is expected to run on release branch.
 const main = () => {
     // get the latest version from npm registry
     const {stdout: latestVersion} = sh.exec('npm view pwa-kit-react-sdk version', {silent: true})
@@ -12,12 +13,14 @@ const main = () => {
         "cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g'"
     )
 
-    // const isPrerelease = semver.prerelease(currentVersion.trim())
+    const isPrerelease = semver.prerelease(currentVersion.trim())
     // if it is a pre-release version, do nothing
-    // if (isPrerelease) {
-    //     return
-    // }
+    if (isPrerelease) {
+        return
+    }
 
+    // check if current version is larger than the latest one in npm,
+    // if it is true, it means we are about to release a new version to npm
     const isLatest = semver.gt(currentVersion, latestVersion)
     // we only want to publish the docs for latest release branch
     if (isLatest) {
