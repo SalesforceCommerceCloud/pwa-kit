60617fffe6b54e2cf65cad4232f4b40d8feb3de1
Kevin He <kevin.he@salesforce.com>
2022-07-22 14:39:01 -0700

    fix lint

--- packages/commerce-sdk-react/src/hooks/ShopperContexts/action.ts
+++ packages/commerce-sdk-react/src/hooks/ShopperContexts/action.ts
@@ -28,7 +28,7 @@ export enum ShopperContextsActions {
      * @see {@link https://developer.salesforce.com/docs/commerce/commerce-api/references/shopper-contexts?meta=updateShopperContext} for more information about the API endpoint.
      * @see {@link https://salesforcecommercecloud.github.io/commerce-sdk-isomorphic/classes/shoppercontexts.shoppercontexts-1.html#updateshoppercontext} for more information on the parameters and returned data type.
      */
-    UpdateShopperContext = 'updateShopperContext',
+    UpdateShopperContext = 'updateShopperContext'
 }
 
 /**
