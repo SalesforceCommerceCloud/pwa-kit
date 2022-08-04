33773367a6efb86e97934f1849455064614e278f
Alex Vuong <alex.vuong@salesforce.com>
2022-08-04 16:53:58 -0700

    fix Link component to work with HOME_REF

--- packages/template-retail-react-app/app/pages/checkout/partials/checkout-header.jsx
+++ packages/template-retail-react-app/app/pages/checkout/partials/checkout-header.jsx
@@ -5,33 +5,25 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import React from 'react'
-import PropTypes from 'prop-types'
-import {FormattedMessage, useIntl} from 'react-intl'
-import {Badge, Box, Button, Flex, Center, IconButton, useMultiStyleConfig} from '@chakra-ui/react'
+import {FormattedMessage} from 'react-intl'
+import {Badge, Box, Button, Flex, Center} from '@chakra-ui/react'
 import useBasket from '../../../commerce-api/hooks/useBasket'
 import Link from '../../../components/link'
 import {BasketIcon, BrandLogo} from '../../../components/icons'
-import {noop} from '../../../utils/utils'
+import {HOME_HREF} from '../../../constants'
 
-const CheckoutHeader = ({onLogoClick = noop()}) => {
+const CheckoutHeader = () => {
     const basket = useBasket()
-    const intl = useIntl()
-    const styles = useMultiStyleConfig('Header')
-
     return (
         <Box px={[4, 4, 8]} bg="white" borderBottom="1px" borderColor="gray.100">
             <Box maxWidth="container.xxxl" marginLeft="auto" marginRight="auto">
                 <Flex h={{base: '52px', md: '80px'}} align="center" justify="space-between">
-                    <IconButton
-                        aria-label={intl.formatMessage({
-                            id: 'header.button.assistive_msg.logo',
-                            defaultMessage: 'Logo'
-                        })}
-                        icon={<BrandLogo {...styles.logo} />}
-                        {...styles.icons}
-                        variant="unstyled"
-                        onClick={onLogoClick}
-                    />
+                    <Link href={HOME_HREF} title="Back to homepage">
+                        <BrandLogo
+                            width={{base: '35px', md: '45px'}}
+                            height={{base: '24px', md: '32px'}}
+                        />
+                    </Link>
 
                     <Button
                         as={Link}
@@ -55,10 +47,6 @@ const CheckoutHeader = ({onLogoClick = noop()}) => {
             </Box>
         </Box>
     )
-}
-
-CheckoutHeader.propTypes = {
-    onLogoClick: PropTypes.func
 }
 
 export default CheckoutHeader
--- packages/template-retail-react-app/app/pages/checkout/partials/checkout-header.test.js
+++ packages/template-retail-react-app/app/pages/checkout/partials/checkout-header.test.js
@@ -12,5 +12,5 @@ import {renderWithProviders} from '../../../utils/test-utils'
 
 test('renders component', () => {
     renderWithProviders(<CheckoutHeader />)
-    expect(screen.getByLabelText(/Logo/i)).toBeInTheDocument()
+    expect(screen.getByTitle(/back to homepage/i)).toBeInTheDocument()
 })
