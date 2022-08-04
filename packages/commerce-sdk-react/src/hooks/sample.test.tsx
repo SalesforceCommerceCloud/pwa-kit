fe9130da853ab9e05aacb0ff738feb04e67f09b3
Vincent Marta <vmarta@salesforce.com>
2022-08-04 13:53:51 -0700

    Refactoring for more readable test

--- packages/commerce-sdk-react/src/hooks/sample.test.tsx
+++ packages/commerce-sdk-react/src/hooks/sample.test.tsx
@@ -18,27 +18,28 @@ const {withMocks} = mockHttpResponses({directory: `${__dirname}/mock-responses`}
 // - Globally, via `npm run test:update-mocked-responses`
 // - Per individual test file, by passing in `mode` value -> mockHttpResponses({mode: 'update', directory})
 
+const ProductName = ({id}: {id: string}) => {
+    const api = useCommerceApi()
+    const [productName, setProductName] = useState('')
+
+    useEffect(() => {
+        const fetchProductName = async () => {
+            const product = await api.shopperProducts.getProduct({
+                parameters: {id}
+            })
+            setProductName(product.name as string)
+        }
+        fetchProductName()
+    }, [])
+
+    return <div>{productName}</div>
+}
+
 test(
     'useCommerceApi hook',
     withMocks(async () => {
-        const Component = () => {
-            const api = useCommerceApi()
-            const [productName, setProductName] = useState('')
-
-            useEffect(() => {
-                const fetchProductName = async () => {
-                    const product = await api.shopperProducts.getProduct({
-                        parameters: {id: '25591862M'}
-                    })
-                    setProductName(product.name as string)
-                }
-                fetchProductName()
-            }, [])
-
-            return <div>{productName}</div>
-        }
         // This util function would automatically integrate the provider components for you
-        renderWithProviders(<Component />)
+        renderWithProviders(<ProductName id="25591862M" />)
 
         const productName = 'Stripe Walking Short'
         await waitFor(() => screen.getByText(productName))
