7dd15094f7cdcaa1cf38699a1dbe42fc7e0a2db6
Vincent Marta <vmarta@salesforce.com>
2022-09-01 16:04:07 -0700

    Add jsdoc

--- packages/pwa-kit-react-sdk/src/ssr/universal/hooks/index.js
+++ packages/pwa-kit-react-sdk/src/ssr/universal/hooks/index.js
@@ -8,4 +8,14 @@
 import {useContext} from 'react'
 import {ServerContext} from '../contexts'
 
+/**
+ * Get the server context of the current page.
+ * Avoid using it for rendering the content of your component. Otherwise, there would be a mismatch in html markup during hydration.
+ * @returns {Object} Request and Response objects `{req, res}`
+ *
+ * @example
+ * const {req, res} = useServerContext()
+ * // Request, Response will be available only on the server-side
+ * if (res) { res.status(404) }
+ */
 export const useServerContext = () => useContext(ServerContext)
