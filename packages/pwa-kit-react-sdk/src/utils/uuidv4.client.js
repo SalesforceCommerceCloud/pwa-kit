9628e587dc463de76a6d151e68b7c13016f60a24
Alex Vuong <52219283+alexvuong@users.noreply.github.com>
2022-09-27 10:17:59 -0700

    Correlation Id Implementation (#728)
    * correlation id implementation
    
    Co-authored-by: Ben Chypak <bchypak@mobify.com>
    Co-authored-by: Ben Chypak <bchypak@salesforce.com>

---
+++ packages/pwa-kit-react-sdk/src/utils/uuidv4.client.js
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2022, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+/**
+ * Simple implementation to create a uuid using crypto
+ * See: https://stackoverflow.com/questions/105034/how-do-i-create-a-guid-uuid
+ * @returns {*}
+ */
+export function uuidv4() {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+        (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
+    )
+}
