f3aa9b16938638fd719ea36d341658b754d8ed65
Oliver Brook <o.brook@salesforce.com>
2022-10-05 10:10:56 -0700

    Lint

--- packages/pwa-kit-runtime/src/utils/ssr-server/utils.test.js
+++ packages/pwa-kit-runtime/src/utils/ssr-server/utils.test.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2022, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
 import * as utils from './utils'
 
 describe.each([[true], [false]])('Utils remote/local tests (isRemote: %p)', (isRemote) => {
