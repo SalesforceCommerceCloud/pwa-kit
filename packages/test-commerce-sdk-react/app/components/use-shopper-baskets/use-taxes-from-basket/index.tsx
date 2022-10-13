614c356e3d02cfa9d978ffce9a2b2d697767c593
Jainam Tushar Sheth <j.sheth@salesforce.com>
2022-10-13 12:07:53 -0400

    Added test integrations for shopperbasket queries

---
+++ packages/test-commerce-sdk-react/app/components/use-shopper-baskets/use-taxes-from-basket/index.tsx
@@ -0,0 +1,40 @@
+import React, {ReactElement} from 'react'
+import Json from '../../Json'
+import {useTaxesFromBasket} from 'commerce-sdk-react'
+
+export const UseTaxesFromBasket = ({basketId}: {basketId: string}): ReactElement => {
+    const {isLoading, error, data} = useTaxesFromBasket({basketId})
+
+    if (isLoading) {
+        return (
+            <div>
+                <h1>Taxes: </h1>
+                <h2 style={{background: 'aqua'}}>Loading...</h2>
+            </div>
+        )
+    }
+
+    if (error) {
+        return <h1 style={{color: 'red'}}>Something is wrong</h1>
+    }
+
+    return (
+        <>
+            {data && (
+                <>
+                    <p>taxes:</p>
+                    <ul>
+                        {Object.keys(data?.taxes).map((taxItemId) => (
+                            <li>{taxItemId}</li>
+                        ))}
+                    </ul>
+                    <hr />
+                    <div>
+                        <div>Returning data</div>
+                        <Json data={{isLoading, error, data}} />
+                    </div>
+                </>
+            )}
+        </>
+    )
+}
