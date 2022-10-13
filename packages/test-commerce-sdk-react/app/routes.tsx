9a7a1078220f97a72a71ecbf1937b5bb102cf89a
Alex Vuong <52219283+alexvuong@users.noreply.github.com>
2022-10-13 10:36:08 -0700

    Shopper promotions hooks (#759)
    * usePromotions and usePromotionsForCampaign hooks

--- packages/test-commerce-sdk-react/app/routes.tsx
+++ packages/test-commerce-sdk-react/app/routes.tsx
@@ -13,6 +13,8 @@ const UseProduct = loadable(() => import('./pages/use-shopper-product'))
 const UseCategories = loadable(() => import('./pages/use-shopper-categories'))
 const UseCategory = loadable(() => import('./pages/use-shopper-category'))
 const UseProductSearch = loadable(() => import('./pages/use-product-search'))
+const UsePromotions = loadable(() => import('./pages/use-promotions'))
+const UsePromotionsForCampaign = loadable(() => import('./pages/use-promotions-for-campaign'))
 const UseShopperLoginHelper = loadable(() => import('./pages/use-shopper-login-helper'))
 const UseShopperBaskets = loadable(() => import('./pages/use-shopper-baskets'))
 const QueryErrors = loadable(() => import('./pages/query-errors'))
@@ -46,6 +48,14 @@ const routes = [
     {
         path: '/search-suggestions',
         component: UseSearchSuggestions
+    },
+    {
+        path: '/use-promotions',
+        component: UsePromotions
+    },
+    {
+        path: '/use-promotions-for-campaign',
+        component: UsePromotionsForCampaign
     },
     {
         path: '/slas-helpers',
