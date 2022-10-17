64123e8d7bcf286b5631bacbb633679eaca3eee8
Oliver Brook <o.brook@salesforce.com>
2022-10-17 14:17:29 -0700

    Lint

--- packages/pwa-kit-dev/src/utils/upload2.ts
+++ packages/pwa-kit-dev/src/utils/upload2.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2022, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
 import os from 'os'
 import path from 'path'
 import archiver from 'archiver'
@@ -12,22 +18,20 @@ export const DEFAULT_CLOUD_ORIGIN = 'https://cloud.mobify.com'
 export const DEFAULT_DOCS_URL =
     'https://developer.salesforce.com/docs/commerce/pwa-kit-managed-runtime/guide/pushing-and-deploying-bundles.html'
 
-
 interface Credentials {
     username: string
     api_key: string
 }
 
-
 interface CloudAPIClientOpts {
-    credentials: Credentials,
+    credentials: Credentials
     origin?: string
-    fetch?: Function
+    fetch?: _fetch
 }
 
-
-interface StringMap {[key: string]: string;}
-
+interface StringMap {
+    [key: string]: string
+}
 
 interface Bundle {
     message: string
@@ -43,7 +47,6 @@ export const getPkgJSON = async () => {
     return JSON.parse(data.toString('utf-8'))
 }
 
-
 export class CloudAPIClient {
     private opts: Required<CloudAPIClientOpts>
 
@@ -51,14 +54,14 @@ export class CloudAPIClient {
         this.opts = {
             origin: params.origin || DEFAULT_CLOUD_ORIGIN,
             fetch: params.fetch || _fetch,
-            credentials: params.credentials,
+            credentials: params.credentials
         }
     }
 
     private getAuthHeader(): StringMap {
         const {username, api_key} = this.opts.credentials
         const encoded = Buffer.from(`${username}:${api_key}`, 'binary').toString('base64')
-        return {'Authorization': `Basic ${encoded}`}
+        return {Authorization: `Basic ${encoded}`}
     }
 
     private async getHeaders(extras: StringMap): Promise<StringMap> {
@@ -66,7 +69,7 @@ export class CloudAPIClient {
         return {
             'User-Agent': `${pkg.name}@${pkg.version}`,
             ...this.getAuthHeader(),
-            ...extras,
+            ...extras
         }
     }
 
@@ -79,7 +82,7 @@ export class CloudAPIClient {
         try {
             error = await res.json()
         } catch (err) {
-            error = {}  // Cloud doesn't always return JSON
+            error = {} // Cloud doesn't always return JSON
         }
 
         throw new Error(
@@ -103,7 +106,7 @@ export class CloudAPIClient {
         const res = await this.opts.fetch(url.toString(), {
             body,
             method: 'POST',
-            headers,
+            headers
         })
         await this.throwForStatus(res)
         return await res.json()
@@ -115,7 +118,9 @@ export const defaultMessage = (gitInstance: git = git): string => {
         return `${gitInstance.branch()}: ${gitInstance.short()}`
     } catch (err) {
         if (err.code === 'ENOENT') {
-            console.log('Using default bundle message as no message was provided and not in a Git repo.')
+            console.log(
+                'Using default bundle message as no message was provided and not in a Git repo.'
+            )
         }
         return 'PWA Kit Bundle'
     }
@@ -131,14 +136,13 @@ interface CreateBundleArgs {
 }
 
 export const createBundle = async ({
-        message,
-        ssr_parameters,
-        ssr_only,
-        ssr_shared,
-        buildDirectory,
-        projectSlug,
-    }: CreateBundleArgs) : Promise<Bundle> => {
-
+    message,
+    ssr_parameters,
+    ssr_only,
+    ssr_shared,
+    buildDirectory,
+    projectSlug
+}: CreateBundleArgs): Promise<Bundle> => {
     message = message || defaultMessage()
 
     const tmpDir = await mkdtemp(path.join(os.tmpdir(), 'pwa-kit-dev-'))
@@ -155,34 +159,33 @@ export const createBundle = async ({
             const fullPath = path.join(process.cwd(), buildDirectory)
             throw new Error(
                 `[Error: Build directory at path "${fullPath}" not found.]\n` +
-                'Run `pwa-kit-dev build` first!'
+                    'Run `pwa-kit-dev build` first!'
             )
         })
-        .then(() => new Promise((resolve, reject) => {
-            const output = createWriteStream(destination)
-            const archive = archiver('tar')
+        .then(
+            () =>
+                new Promise((resolve, reject) => {
+                    const output = createWriteStream(destination)
+                    const archive = archiver('tar')
 
-            archive.pipe(output)
+                    archive.pipe(output)
 
-            // See https://archiverjs.com/docs/global.html#TarEntryData
-            const newRoot = path.join(projectSlug, 'bld', '')
-            archive.directory(
-                buildDirectory,
-                '',
-                (entry) => {
-                    if (entry.stats.isFile()) {
-                        filesInArchive.push(entry.name)
-                    }
-                    entry.prefix = newRoot
-                    return entry
-                }
-            )
+                    // See https://archiverjs.com/docs/global.html#TarEntryData
+                    const newRoot = path.join(projectSlug, 'bld', '')
+                    archive.directory(buildDirectory, '', (entry) => {
+                        if (entry.stats.isFile()) {
+                            filesInArchive.push(entry.name)
+                        }
+                        entry.prefix = newRoot
+                        return entry
+                    })
 
-            archive.on('error', reject)
-            output.on('finish', resolve)
+                    archive.on('error', reject)
+                    output.on('finish', resolve)
 
-            archive.finalize()
-        }))
+                    archive.finalize()
+                })
+        )
         .then(() => readFile(destination))
         .then((data) => {
             const encoding = 'base64'
@@ -192,19 +195,15 @@ export const createBundle = async ({
                 data: data.toString(encoding),
                 ssr_parameters,
                 ssr_only: filesInArchive.filter(glob(ssr_only)),
-                ssr_shared: filesInArchive.filter(glob(ssr_shared)),
+                ssr_shared: filesInArchive.filter(glob(ssr_shared))
             }
         })
         .finally(() => rmdir(tmpDir, {recursive: true}))
 }
 
-
-
-
-type MatchFn = (a: string) => boolean;
+type MatchFn = (a: string) => boolean
 
 export const glob = (patterns?: string[]): MatchFn => {
-
     // The patterns can include negations, so matching is done against all
     // the patterns. A match is true if a given path matches any pattern and
     // does not match any negating patterns.
@@ -225,25 +224,29 @@ export const glob = (patterns?: string[]): MatchFn => {
     }
 }
 
-export const findHomeDir = (platform: string = process.platform) => platform === 'win32' ? process.env.USERPROFILE : process.env.HOME
+export const findHomeDir = (platform: string = process.platform) =>
+    platform === 'win32' ? process.env.USERPROFILE : process.env.HOME
 
-export const getCredentialsFile = (cloudOrigin: string, credentialsFile?: string, doFindHomeDir: () => string = findHomeDir): string => {
+export const getCredentialsFile = (
+    cloudOrigin: string,
+    credentialsFile?: string,
+    doFindHomeDir: () => string = findHomeDir
+): string => {
     if (credentialsFile) {
         return credentialsFile
     } else {
         const url = new URL(cloudOrigin)
         const host = url.host
-        const suffix = (host === 'cloud.mobify.com') ? '' : `--${host}`
+        const suffix = host === 'cloud.mobify.com' ? '' : `--${host}`
         return path.join(doFindHomeDir(), `.mobify${suffix}`)
     }
 }
-
 
 export const readCredentials = async (filepath: string): Promise<Credentials> => {
     try {
         const content = await readFile(filepath)
         const data = JSON.parse(content.toString('utf-8'))
-        return  {
+        return {
             username: data.username,
             api_key: data.api_key
         }
