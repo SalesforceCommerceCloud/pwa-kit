2ce1fc3d14e876f4fb360f8c20acd15024d77637
Yuna Kim <yunakim@salesforce.com>
2022-10-17 11:46:18 -0400

    added more tests

--- packages/commerce-sdk-react/src/hooks/ShopperOrders/query.test.tsx
+++ packages/commerce-sdk-react/src/hooks/ShopperOrders/query.test.tsx
@@ -10,7 +10,7 @@ import '@testing-library/jest-dom'
 import {mockHttpResponses, renderWithProviders} from '../../test-utils'
 import {screen, waitFor} from '@testing-library/react'
 import {ShopperLoginHelpers, useShopperLoginHelper} from '../ShopperLogin'
-import {useOrder} from './query'
+import {useOrder, usePaymentMethodsForOrder} from './query'
 
 const {withMocks} = mockHttpResponses({
     directory: path.join(__dirname, '../../../mock-responses')
@@ -42,6 +42,43 @@ const OrderComponent = ({orderNo}: {orderNo: string}): ReactElement => {
             )}
             {isLoading && <div>Loading...</div>}
             {data && <div>{data.customerName}</div>}
+            {error && <span>error</span>}
+        </>
+    )
+}
+
+const PaymentMethodsComponent = ({orderNo}: {orderNo: string}): ReactElement => {
+    // log into register account before fetching any order information when the component is mounted
+    const loginRegisteredUser = useShopperLoginHelper(ShopperLoginHelpers.LoginRegisteredUserB2C)
+    React.useEffect(() => {
+        loginRegisteredUser.mutate({username: 'alex@test.com', password: 'Test1234#'})
+    }, [])
+
+    const {data, isLoading, error} = usePaymentMethodsForOrder(
+        {
+            orderNo
+        },
+        {
+            // wait until the access_token is back before fetching order
+            enabled: !!loginRegisteredUser?.data?.access_token
+        }
+    )
+    return (
+        <>
+            {loginRegisteredUser.isLoading ? (
+                <span>Logging in...</span>
+            ) : (
+                <div>Logged in as {loginRegisteredUser?.variables?.username}</div>
+            )}
+            {isLoading && <div>Loading...</div>}
+            {data && (
+                <div>
+                    {data.applicablePaymentMethods?.map(({id}) => (
+                        <div key={id}>{id}</div>
+                    ))}
+                </div>
+            )}
+            {error && <span>error</span>}
         </>
     )
 }
@@ -55,21 +92,59 @@ const tests = [
                 assertions: withMocks(async () => {
                     const orderNo = '00014202'
                     renderWithProviders(<OrderComponent orderNo={orderNo} />)
-                    const orderItems = [
-                        'Ruffle Front Cardigan',
-                        'Porcelain Straight Leg Pant',
-                        '2 Button Front Jacket'
-                    ]
-                    // const orderTotal = 229.11
 
-                    expect(screen.queryByText(orderItems[0])).toBeNull()
-                    expect(screen.queryByText(orderItems[1])).toBeNull()
-                    expect(screen.queryByText(orderItems[2])).toBeNull()
                     await waitFor(() => screen.getByText(/alex@test.com/))
                     expect(screen.queryByText(/alex@test.com/)).toBeInTheDocument()
                     await waitFor(() => screen.getByText(/Alex V/))
 
                     expect(screen.queryByText(/Alex V/)).toBeInTheDocument()
+                })
+            },
+            {
+                name: 'returns error',
+                assertions: withMocks(async () => {
+                    const invalidOrderNo = 'abcdef'
+                    renderWithProviders(<OrderComponent orderNo={invalidOrderNo} />)
+
+                    await waitFor(() => screen.getByText(/alex@test.com/))
+                    expect(screen.queryByText(/alex@test.com/)).toBeInTheDocument()
+
+                    expect(screen.getByText('Loading...')).toBeInTheDocument()
+                    await waitFor(() => screen.getByText('error'))
+                    expect(screen.getByText('error')).toBeInTheDocument()
+                })
+            }
+        ]
+    },
+    {
+        hook: 'usePaymentMethodsForOrder',
+        cases:  [
+            {
+                name: 'returns data',
+                assertions: withMocks(async () => {
+                    const orderNo = '00014202'
+                    renderWithProviders(<PaymentMethodsComponent orderNo={orderNo} />)
+
+                    await waitFor(() => screen.getByText(/alex@test.com/))
+                    expect(screen.queryByText(/alex@test.com/)).toBeInTheDocument()
+
+                    await waitFor(() => screen.getByText('PayPal'))
+                    expect(screen.getByText('PayPal')).toBeInTheDocument()
+
+                })
+            },
+            {
+                name: 'returns error',
+                assertions: withMocks(async () => {
+                    const invalidOrderNo = 'abcdef'
+                    renderWithProviders(<PaymentMethodsComponent orderNo={invalidOrderNo} />)
+
+                    await waitFor(() => screen.getByText(/alex@test.com/))
+                    expect(screen.queryByText(/alex@test.com/)).toBeInTheDocument()
+
+                    expect(screen.getByText('Loading...')).toBeInTheDocument()
+                    await waitFor(() => screen.getByText('error'))
+                    expect(screen.getByText('error')).toBeInTheDocument()
                 })
             }
         ]
