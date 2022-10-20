42176c112d5c50f580c53c7d2eca248efbcebaad
Adam Raya Navarro <arayanavarro@salesforce.com>
2022-10-20 12:20:31 -0700

    try fix flaky test

--- packages/commerce-sdk-react/src/test-utils.tsx
+++ packages/commerce-sdk-react/src/test-utils.tsx
@@ -97,20 +97,11 @@ export const mockHttpResponses = (options: NockBackOptions) => {
             .reply(303, undefined, {
                 Location: '/callback?usid=12345&code=ABCDE'
             })
-            .get('/callback?usid=12345&code=ABCDE')
-            .reply(200)
+
         nock('http://localhost:3000')
-            .persist()
-            .post((uri) => {
-                return uri.includes('/oauth2/login')
+            .get((uri) => {
+                return uri.includes('/callback')
             })
-            .reply(303, undefined, {
-                Location:
-                    '/callback?usid=851fd6b0-ef19-4eac-b556-fa13827708ed&state=1665780553940&scope=openid%20offline_access&code=bh7WZtgxdfdC_Jyy71gl9lFbV9di21S9brt2h-lZj-w'
-            })
-            .get(
-                '/callback?usid=851fd6b0-ef19-4eac-b556-fa13827708ed&state=1665780553940&scope=openid%20offline_access&code=bh7WZtgxdfdC_Jyy71gl9lFbV9di21S9brt2h-lZj-w'
-            )
             .reply(200)
         nock('http://localhost:3000')
             .persist()
