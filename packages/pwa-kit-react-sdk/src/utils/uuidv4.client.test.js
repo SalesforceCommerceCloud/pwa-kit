4c13ae82520602b109ac2f0c2e149fd3d257d231
Kevin He <kevin.he@salesforce.com>
2022-10-26 11:48:38 -0700

    add more tests

---
+++ packages/pwa-kit-react-sdk/src/utils/uuidv4.client.test.js
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2022, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import {uuidv4} from './uuidv4.client'
+
+let originalCrypto
+
+describe('uuidv4', () => {
+    beforeEach(() => {
+        originalCrypto = global.crypto
+    })
+
+    afterEach(() => {
+        global.crypto = originalCrypto
+    })
+
+    test('returns correct format', () => {
+        global.crypto = {
+            getRandomValues: () => [123],
+        }
+
+        expect(uuidv4()).toBe('abbbbbbb-abbb-4bbb-bbbb-abbbbbbbbbbb')
+    })
+})
