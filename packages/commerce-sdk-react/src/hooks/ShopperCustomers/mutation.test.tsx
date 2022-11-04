512fbd4ad396ea198bcfb9fd394368b0e096f713
Adam Raya Navarro <arayanavarro@salesforce.com>
2022-11-03 21:55:02 -0700

    Add `updateCustomer` tests

--- packages/commerce-sdk-react/src/hooks/ShopperCustomers/mutation.test.tsx
+++ packages/commerce-sdk-react/src/hooks/ShopperCustomers/mutation.test.tsx
@@ -7,7 +7,6 @@
 import React from 'react'
 import path from 'path'
 import {mockHttpResponses, renderWithProviders} from '../../test-utils'
-import {useCustomer} from './query'
 import {fireEvent, screen, waitFor} from '@testing-library/react'
 import {useQueryClient} from '@tanstack/react-query'
 import {ShopperLoginHelpers, useShopperLoginHelper} from '../ShopperLogin'
@@ -16,6 +15,7 @@ import nock from 'nock'
 
 const {withMocks} = mockHttpResponses({directory: path.join(__dirname, '../../../mock-responses')})
 
+const CUSTOMER_EMAIL = 'kobe@test.com'
 const CUSTOMER_ID = 'abkehFwKoXkbcRmrFIlaYYwKtJ'
 const RANDOM_STR = Math.random()
     .toString(36)
@@ -24,6 +24,7 @@ const RANDOM_STR = Math.random()
 const CustomerMutationComponent = () => {
     const queryClient = useQueryClient()
     const loginRegisteredUser = useShopperLoginHelper(ShopperLoginHelpers.LoginRegisteredUserB2C)
+    const updateCustomer = useShopperCustomersMutation(ShopperCustomersMutations.UpdateCustomer)
 
     React.useEffect(() => {
         queryClient.removeQueries(['/customer'])
@@ -32,8 +33,6 @@ const CustomerMutationComponent = () => {
             password: 'Test1234!'
         })
     }, [])
-
-    const updateCustomer = useShopperCustomersMutation(ShopperCustomersMutations.UpdateCustomer)
 
     return (
         <>
@@ -54,6 +53,8 @@ const CustomerMutationComponent = () => {
                     Update Customer
                 </button>
 
+
+
                 {updateCustomer.error?.message && (
                     <p style={{color: 'red'}}>Error: {updateCustomer.error?.message}</p>
                 )}
@@ -66,10 +67,10 @@ const CustomerMutationComponent = () => {
 
 const tests = [
     {
-        hook: 'useCustomer',
+        hook: 'updateCustomer',
         cases: [
             {
-                name: 'returns data',
+                name: 'success',
                 assertions: withMocks(async () => {
                     renderWithProviders(<CustomerMutationComponent />)
                     await waitFor(() => screen.getByText(/kobe@test.com/))
@@ -84,16 +85,47 @@ const tests = [
                         .patch((uri) => {
                             return uri.includes('/customer/shopper-customers/')
                         })
-                        .reply(200, {})
+                        .reply(200, {
+                            customerId: CUSTOMER_ID,
+                            email: CUSTOMER_EMAIL,
+                            login: CUSTOMER_EMAIL,
+
+                          })
 
                     const button = screen.getByRole('button', {
                         name: /update customer/i
                     })
 
                     fireEvent.click(button)
-                    screen.debug(undefined, Infinity)
                     await waitFor(() => screen.getByText(/email/i))
                     expect(screen.getByText(/email/i)).toBeInTheDocument()
+                })
+            },
+            {
+                name: 'error',
+                assertions: withMocks(async () => {
+                    renderWithProviders(<CustomerMutationComponent />)
+                    await waitFor(() => screen.getByText(/kobe@test.com/))
+                    await waitFor(() =>
+                        screen.getByRole('button', {
+                            name: /update customer/i
+                        })
+                    )
+
+                    // Mocking the server request
+                    nock('http://localhost:3000')
+                        .patch((uri) => {
+                            return uri.includes('/customer/shopper-customers/')
+                        })
+                        .reply(500)
+
+                    const button = screen.getByRole('button', {
+                        name: /update customer/i
+                    })
+
+                    fireEvent.click(button)
+                    await waitFor(() => screen.getByText(/error/i))
+                    expect(screen.getByText(/error/i)).toBeInTheDocument()
                 })
             }
         ]
