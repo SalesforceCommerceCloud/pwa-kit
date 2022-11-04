468b1b9ead45a910b07ab7e2bff77ae8bf83bbc2
Adam Raya Navarro <arayanavarro@salesforce.com>
2022-11-04 05:56:10 -0700

    Assert query keys in cache for invalidations

--- packages/commerce-sdk-react/src/hooks/ShopperCustomers/mutation.test.tsx
+++ packages/commerce-sdk-react/src/hooks/ShopperCustomers/mutation.test.tsx
@@ -6,9 +6,8 @@
  */
 import React from 'react'
 import path from 'path'
-import {mockHttpResponses, renderWithProviders} from '../../test-utils'
+import {mockHttpResponses, renderWithProviders, queryClient} from '../../test-utils'
 import {fireEvent, screen, waitFor} from '@testing-library/react'
-import {useQueryClient} from '@tanstack/react-query'
 import {ShopperLoginHelpers, useShopperLoginHelper} from '../ShopperLogin'
 import {useShopperCustomersMutation, ShopperCustomersMutations} from './mutation'
 import nock from 'nock'
@@ -22,12 +21,10 @@ const RANDOM_STR = Math.random()
     .slice(2, 7)
 
 const CustomerMutationComponent = () => {
-    const queryClient = useQueryClient()
     const loginRegisteredUser = useShopperLoginHelper(ShopperLoginHelpers.LoginRegisteredUserB2C)
     const updateCustomer = useShopperCustomersMutation(ShopperCustomersMutations.UpdateCustomer)
 
     React.useEffect(() => {
-        queryClient.removeQueries(['/customer'])
         loginRegisteredUser.mutate({
             username: 'kobe@test.com',
             password: 'Test1234!'
@@ -52,8 +49,6 @@ const CustomerMutationComponent = () => {
                 >
                     Update Customer
                 </button>
-
-
 
                 {updateCustomer.error?.message && (
                     <p style={{color: 'red'}}>Error: {updateCustomer.error?.message}</p>
@@ -80,6 +75,30 @@ const tests = [
                         })
                     )
 
+                    // CACHE Pre-populate cache with the query keys we invalidate on success
+                    queryClient.setQueryData(
+                        ['/customers', CUSTOMER_ID, '/payment-instruments'],
+                        {}
+                    )
+                    queryClient.setQueryData(['/customers', CUSTOMER_ID, '/addresses'], {})
+                    queryClient.setQueryData(['/customers', '/external-profile'], {})
+
+                    expect(
+                        queryClient.getQueryState([
+                            '/customers',
+                            CUSTOMER_ID,
+                            '/payment-instruments'
+                        ])?.isInvalidated
+                    ).toBeFalsy()
+                    expect(
+                        queryClient.getQueryState(['/customers', CUSTOMER_ID, '/addresses'])
+                            ?.isInvalidated
+                    ).toBeFalsy()
+                    expect(
+                        queryClient.getQueryState(['/customers', '/external-profile'])
+                            ?.isInvalidated
+                    ).toBeFalsy()
+
                     // Mocking the server request
                     nock('http://localhost:3000')
                         .patch((uri) => {
@@ -88,9 +107,8 @@ const tests = [
                         .reply(200, {
                             customerId: CUSTOMER_ID,
                             email: CUSTOMER_EMAIL,
-                            login: CUSTOMER_EMAIL,
-
-                          })
+                            login: CUSTOMER_EMAIL
+                        })
 
                     const button = screen.getByRole('button', {
                         name: /update customer/i
@@ -99,6 +117,35 @@ const tests = [
                     fireEvent.click(button)
                     await waitFor(() => screen.getByText(/email/i))
                     expect(screen.getByText(/email/i)).toBeInTheDocument()
+
+                    // TODO: Remove DEBUG console.log printing queries in cache
+                    // console.log('queryClient.getQueryCache():',queryClient.getQueryCache())
+
+                    // CACHE assert updated query keys
+                    expect(
+                        queryClient.getQueryState([
+                            '/customers',
+                            CUSTOMER_ID,
+                            {customerId: CUSTOMER_ID}
+                        ])?.isInvalidated
+                    ).toBeFalsy()
+
+                    // CACHE assert invalidated query keys
+                    expect(
+                        queryClient.getQueryState([
+                            '/customers',
+                            CUSTOMER_ID,
+                            '/payment-instruments'
+                        ])?.isInvalidated
+                    ).toBeTruthy()
+                    expect(
+                        queryClient.getQueryState(['/customers', CUSTOMER_ID, '/addresses'])
+                            ?.isInvalidated
+                    ).toBeTruthy()
+                    expect(
+                        queryClient.getQueryState(['/customers', '/external-profile'])
+                            ?.isInvalidated
+                    ).toBeTruthy()
                 })
             },
             {
@@ -111,6 +158,30 @@ const tests = [
                             name: /update customer/i
                         })
                     )
+
+                    // CACHE Pre-populate cache with the query keys we invalidate on success
+                    queryClient.setQueryData(
+                        ['/customers', CUSTOMER_ID, '/payment-instruments'],
+                        {}
+                    )
+                    queryClient.setQueryData(['/customers', CUSTOMER_ID, '/addresses'], {})
+                    queryClient.setQueryData(['/customers', '/external-profile'], {})
+
+                    expect(
+                        queryClient.getQueryState([
+                            '/customers',
+                            CUSTOMER_ID,
+                            '/payment-instruments'
+                        ])?.isInvalidated
+                    ).toBeFalsy()
+                    expect(
+                        queryClient.getQueryState(['/customers', CUSTOMER_ID, '/addresses'])
+                            ?.isInvalidated
+                    ).toBeFalsy()
+                    expect(
+                        queryClient.getQueryState(['/customers', '/external-profile'])
+                            ?.isInvalidated
+                    ).toBeFalsy()
 
                     // Mocking the server request
                     nock('http://localhost:3000')
@@ -126,6 +197,23 @@ const tests = [
                     fireEvent.click(button)
                     await waitFor(() => screen.getByText(/error/i))
                     expect(screen.getByText(/error/i)).toBeInTheDocument()
+
+                    // CACHE assert invalidated query keys
+                    expect(
+                        queryClient.getQueryState([
+                            '/customers',
+                            CUSTOMER_ID,
+                            '/payment-instruments'
+                        ])?.isInvalidated
+                    ).toBeFalsy()
+                    expect(
+                        queryClient.getQueryState(['/customers', CUSTOMER_ID, '/addresses'])
+                            ?.isInvalidated
+                    ).toBeFalsy()
+                    expect(
+                        queryClient.getQueryState(['/customers', '/external-profile'])
+                            ?.isInvalidated
+                    ).toBeFalsy()
                 })
             }
         ]
@@ -136,6 +224,7 @@ tests.forEach(({hook, cases}) => {
     describe(hook, () => {
         beforeEach(() => {
             jest.clearAllMocks()
+            queryClient.clear()
         })
         cases.forEach(({name, assertions}) => {
             test(name, assertions)
