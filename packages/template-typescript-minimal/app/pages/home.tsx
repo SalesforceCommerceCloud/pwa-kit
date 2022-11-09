294d019c427e07bc257c84be6385e27047b0e490
Alex Vuong <alex.vuong@salesforce.com>
2022-11-09 11:52:15 -0800

    scom spike

--- packages/template-typescript-minimal/app/pages/home.tsx
+++ packages/template-typescript-minimal/app/pages/home.tsx
@@ -7,18 +7,21 @@
 import React, {useEffect, useState} from 'react'
 //@ts-ignore
 import {getAppOrigin} from 'pwa-kit-react-sdk/utils/url'
+import useBuyer from '../hooks/useBuyer'
 
 const Home = () => {
-    const [token, setToken] = useState('')
+    // const [token, setToken] = useState('')
+    const [token, getToken] = useBuyer()
+    console.log('token', token)
 
     return (
         <div>
             <div>
                 <a
-                    href="https://dro000000bdrl2ao.test1.my.pc-rnd.salesforce.com/services/oauth2/authorize?
+                    href={`${getAppOrigin()}/mobify/proxy/scom/services/oauth2/authorize?
 client_id=3MVG9sA57VMGPDfccCqclI6RJtAtX8wWWhlw31HVV7dAXKQrDd03vlapy1jQNtNJxdr_7VimdE1yefXDvrv2o&
 redirect_uri=https://localhost:3000&
-response_type=code"
+response_type=code`}
                 >
                     Login
                 </a>
@@ -26,25 +29,46 @@ response_type=code"
                     onClick={async () => {
                         const params = new URLSearchParams(window.location.search)
                         const code: string = params.get('code') || ''
+                        const _code = decodeURI(code)
                         const res = await fetch(
                             `${getAppOrigin()}/mobify/proxy/scom/services/oauth2/token`,
                             {
-                                method: 'post',
-                                body: JSON.stringify({
-                                    'Content-type': 'application/x-www-form-urlencoded',
-                                    code: decodeURI(code),
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/x-www-form-urlencoded'
+                                },
+                                body: new URLSearchParams({
+                                    code: _code,
                                     grant_type: 'authorization_code',
                                     client_id:
                                         '3MVG9sA57VMGPDfccCqclI6RJtAtX8wWWhlw31HVV7dAXKQrDd03vlapy1jQNtNJxdr_7VimdE1yefXDvrv2o',
                                     client_secret:
-                                        '262C3081284DA4CCBC1B6CCA9DDA4F10C746C46CD5752D7C30D0AE5A104CD7B2'
+                                        '262C3081284DA4CCBC1B6CCA9DDA4F10C746C46CD5752D7C30D0AE5A104CD7B2',
+                                    redirect_uri: 'http://localhost:3000'
                                 })
                             }
                         )
-                        console.log('res', res)
+
+                        const data = await res.json()
+                        console.log('data', data)
                     }}
                 >
                     Get the token
+                </button>
+                <button onClick={getToken}>Log in as alex.vuong@salesforce.com</button>
+                <button
+                    onClick={() => {
+                        const res = fetch(
+                            `${getAppOrigin()}/mobify/proxy/scom/services/data/v54.0/commerce/webstores/0ZERO0000000QWZ4A2/products/01tRO000000YasOYAS`,
+                            {
+                                headers: {
+                                    Authorization: `Bearer ${token}`
+                                }
+                            }
+                        )
+                    }}
+                >
+                    Get a product detail
                 </button>
             </div>
         </div>
