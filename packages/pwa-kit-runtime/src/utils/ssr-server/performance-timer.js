ae811d143f97dbe6eeb8fc2656dc188fee2023c1
Kevin He <kevin.he@salesforce.com>
2022-11-22 13:54:17 -0800

    lint

--- packages/pwa-kit-runtime/src/utils/ssr-server/performance-timer.js
+++ packages/pwa-kit-runtime/src/utils/ssr-server/performance-timer.js
@@ -1,9 +1,9 @@
-// /*
-//  * Copyright (c) 2022, Salesforce, Inc.
-//  * All rights reserved.
-//  * SPDX-License-Identifier: BSD-3-Clause
-//  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
-//  */
+/*
+ * Copyright (c) 2022, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
 // import {PerformanceObserver, performance} from 'perf_hooks'
 
 // /**
