34b5d05a8a94ceb7dc90747f55fd4e3a261f088f
Brian Feister <bfeister@salesforce.com>
2022-11-22 21:10:10 -0700

    This doesn't work, needs React suspense + removal of `@loadable/server` and `@loadable/component` libraries currently depended on by our SSR layer

--- packages/pwa-kit-react-sdk/src/ssr/server/react-rendering.js
+++ packages/pwa-kit-react-sdk/src/ssr/server/react-rendering.js
@@ -235,7 +235,7 @@ OuterApp.propTypes = {
 }
 
 const renderToString = (jsx, extractor) =>
-    ReactDOMServer.renderToString(extractor.collectChunks(jsx))
+    ReactDOMServer.renderToPipeableStream(extractor.collectChunks(jsx))
 
 const renderApp = (args) => {
     const {req, res, appStateError, appJSX, appState, config, deviceType} = args
@@ -324,7 +324,7 @@ const renderApp = (args) => {
         (tag) => helmet[tag] && helmet[tag].toComponent()
     ).filter((tag) => tag)
 
-    const html = ReactDOMServer.renderToString(
+    const html = ReactDOMServer.renderToPipeableStream(
         <Document
             head={[...helmetHeadTags]}
             html={appHtml}
