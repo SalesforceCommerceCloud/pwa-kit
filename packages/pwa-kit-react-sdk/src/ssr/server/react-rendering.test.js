237ebe7ae229bf18728fb30b04257daef6000d0d
Alex Vuong <alex.vuong@salesforce.com>
2022-12-05 15:24:59 -0800

    update comment

--- packages/pwa-kit-react-sdk/src/ssr/server/react-rendering.test.js
+++ packages/pwa-kit-react-sdk/src/ssr/server/react-rendering.test.js
@@ -716,7 +716,8 @@ describe('The Node SSR Environment', () => {
             assertions: (res) => {
                 expect(res.statusCode).toBe(404)
 
-                // Because of the prepass step we'll expect that this method is called twice.
+                // we'll expect that this method is called three times,
+                // twice for rendering pipeline because of the prepass step and one for http request logging
                 expect(console.log).toHaveBeenCalledTimes(3)
             }
         }
