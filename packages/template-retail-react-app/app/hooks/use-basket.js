89bc72328ca168b29f1b0db182f3171c0c7baf07
Alex Vuong <alex.vuong@salesforce.com>
2023-01-18 14:39:07 -0800

    PR feedback, return rest of queries

--- packages/template-retail-react-app/app/hooks/use-basket.js
+++ packages/template-retail-react-app/app/hooks/use-basket.js
@@ -25,13 +25,13 @@ export const useBasket = ({id = '', shouldFetchProductDetail = false} = {}) => {
         data: basketsData,
         isLoading: isBasketsLoading,
         error: isBasketsError,
-        ...restOfBaskets
+        ...restOfBasketsQuery
     } = useCustomerBaskets({customerId})
     // if id is not defined, by default use the first basket in the list
     const basket =
         basketsData?.baskets?.find((basket) => basket.basketId === id) || basketsData?.baskets?.[0]
     const productIds = basket?.productItems?.map(({productId}) => productId).join(',') ?? ''
-    const {data: products, isLoading: isProductsLoading} = useProducts(
+    const {data: products, isLoading: isProductsLoading, ...restOfProductQuery} = useProducts(
         {
             ids: productIds,
             allImages: true
@@ -45,11 +45,13 @@ export const useBasket = ({id = '', shouldFetchProductDetail = false} = {}) => {
         baskets: basketsData,
         isBasketsLoading,
         isBasketsError,
-        ...restOfBaskets,
+        ...restOfBasketsQuery,
         // current picked basket
         basket,
-        isLoading: isBasketsLoading && isProductsLoading,
+        isLoading: isBasketsLoading || isProductsLoading,
+        isProductsLoading,
         productDetails: products,
+        ...restOfProductQuery,
         hasBasket: basketsData?.total > 0,
         totalItems: basket?.productItems?.reduce((acc, item) => acc + item.quantity, 0)
     }
