d903d666e085decde8010f478808a6350d5fcadd
Oliver Brook <o.brook@salesforce.com>
2023-01-26 12:44:40 -0800

    Explain empty file – will's feedback

--- packages/pwa-kit-dev/src/utils/test-fixtures/minimal-built-app/ssr.js
+++ packages/pwa-kit-dev/src/utils/test-fixtures/minimal-built-app/ssr.js
@@ -4,3 +4,5 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+
+// This file needs to exist as a text fixture, but doesn't need content!
