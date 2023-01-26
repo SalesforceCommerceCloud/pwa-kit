bdb730925d603dcae33a78dad4ab9a957cc9df00
Ben Chypak <bchypak@mobify.com>
2023-01-26 14:11:13 -0800

    Allow support for multiple sites concurrently w/ `commerce-sdk-react` (#911)
    * Namespace storage keys using the current siteId.
    
    Co-authored-by: Will Harney <62956339+wjhsf@users.noreply.github.com>

--- packages/commerce-sdk-react/src/auth/index.ts
+++ packages/commerce-sdk-react/src/auth/index.ts
@@ -7,7 +7,8 @@
 import {helpers, ShopperLogin, ShopperLoginTypes} from 'commerce-sdk-isomorphic'
 import jwtDecode from 'jwt-decode'
 import {ApiClientConfigParams} from '../hooks/types'
-import {BaseStorage, LocalStorage, CookieStorage} from './storage'
+import {BaseStorage, LocalStorage, CookieStorage, MemoryStorage, StorageType} from './storage'
+import {onClient} from '../utils'
 
 type Helpers = typeof helpers
 interface AuthConfig extends ApiClientConfigParams {
@@ -41,70 +42,66 @@ type AuthDataKeys =
 type AuthDataMap = Record<
     AuthDataKeys,
     {
-        storage: BaseStorage
+        storageType: StorageType
         key: string
-        callback?: () => void
+        callback?: (storage: BaseStorage) => void
     }
 >
 
-const onClient = typeof window !== 'undefined'
-const localStorage = onClient ? new LocalStorage() : new Map()
-const cookieStorage = onClient ? new CookieStorage() : new Map()
-
 /**
  * A map of the data that this auth module stores. This maps the name of the property to
- * the storage and the key when stored in that storage. You can also pass in a "callback"
+ * the storage type and the key when stored in that storage. You can also pass in a "callback"
  * function to do extra operation after a property is set.
  */
 const DATA_MAP: AuthDataMap = {
     access_token: {
-        storage: localStorage,
+        storageType: 'local',
         key: 'access_token'
     },
     customer_id: {
-        storage: localStorage,
+        storageType: 'local',
         key: 'customer_id'
     },
     usid: {
-        storage: localStorage,
+        storageType: 'local',
         key: 'usid'
     },
     enc_user_id: {
-        storage: localStorage,
+        storageType: 'local',
         key: 'enc_user_id'
     },
     expires_in: {
-        storage: localStorage,
+        storageType: 'local',
         key: 'expires_in'
     },
     id_token: {
-        storage: localStorage,
+        storageType: 'local',
         key: 'id_token'
     },
     idp_access_token: {
-        storage: localStorage,
+        storageType: 'local',
         key: 'idp_access_token'
     },
     token_type: {
-        storage: localStorage,
+        storageType: 'local',
         key: 'token_type'
     },
     refresh_token_guest: {
-        storage: cookieStorage,
+        storageType: 'cookie',
         key: 'cc-nx-g',
-        callback: () => {
-            cookieStorage.delete('cc-nx')
+        callback: (store) => {
+            store.delete('cc-nx')
         }
     },
     refresh_token_registered: {
-        storage: cookieStorage,
+        storageType: 'cookie',
         key: 'cc-nx',
-        callback: () => {
-            cookieStorage.delete('cc-nx-g')
+        callback: (store) => {
+            store.delete('cc-nx-g')
         }
     },
     site_id: {
-        storage: cookieStorage,
+        storageType: 'cookie',
         key: 'cc-site-id'
     }
 }
@@ -122,6 +119,7 @@ class Auth {
     private redirectURI: string
     private pendingToken: Promise<ShopperLoginTypes.TokenResponse> | undefined
     private REFRESH_TOKEN_EXPIRATION_DAYS = 90
+    private stores: Record<StorageType, BaseStorage>
 
     constructor(config: AuthConfig) {
         this.client = new ShopperLogin({
@@ -136,39 +134,39 @@ class Auth {
             fetchOptions: config.fetchOptions
         })
 
-        if (this.get('site_id') && this.get('site_id') !== config.siteId) {
-            // if site is switched, remove all existing auth data in storage
-            // and the next auth.ready() call with restart the auth flow
-            this.clearStorage()
-            this.pendingToken = undefined
+        const storageOptions = {keyPrefix: config.siteId}
+        const serverStorageOptions = {
+            keyPrefix: config.siteId,
+            sharedContext: true // This allows use to reused guest authentication tokens accross lambda runs.
         }
 
-        if (!this.get('site_id')) {
-            this.set('site_id', config.siteId, {
-                expires: this.REFRESH_TOKEN_EXPIRATION_DAYS
-            })
-        }
+        this.stores = onClient()
+            ? {
+                  cookie: new CookieStorage(storageOptions),
+                  local: new LocalStorage(storageOptions),
+                  memory: new MemoryStorage(storageOptions)
+              }
+            : {
+                  // Always use MemoryStorage on the server.
+                  cookie: new MemoryStorage(serverStorageOptions),
+                  local: new MemoryStorage(serverStorageOptions),
+                  memory: new MemoryStorage(serverStorageOptions)
+              }
 
         this.redirectURI = config.redirectURI
     }
 
     get(name: AuthDataKeys) {
-        const storage = DATA_MAP[name].storage
-        const key = DATA_MAP[name].key
+        const {key, storageType} = DATA_MAP[name]
+        const storage = this.stores[storageType]
         return storage.get(key)
     }
 
     private set(name: AuthDataKeys, value: string, options?: unknown) {
-        const {key, storage} = DATA_MAP[name]
+        const {key, storageType} = DATA_MAP[name]
+        const storage = this.stores[storageType]
         storage.set(key, value, options)
-        DATA_MAP[name].callback?.()
-    }
-
-    private clearStorage() {
-        Object.keys(DATA_MAP).forEach((key) => {
-            type Key = keyof AuthDataMap
-            DATA_MAP[key as Key].storage.delete(DATA_MAP[key as Key].key)
-        })
+        DATA_MAP[name].callback?.(storage)
     }
 
     /**
