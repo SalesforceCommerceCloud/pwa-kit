1abba9df5dded36457a45d5217149fae50932483
Alex Vuong <alex.vuong@salesforce.com>
2023-01-25 16:16:02 -0800

    cart update quantity, and remove item

--- packages/template-retail-react-app/app/components/header/index.jsx
+++ packages/template-retail-react-app/app/components/header/index.jsx
@@ -257,7 +257,9 @@ const Header = ({
                         icon={
                             <>
                                 <BasketIcon />
-                                {basket && <Badge variant="notification">{totalItems}</Badge>}
+                                {basket && totalItems > 0 && (
+                                    <Badge variant="notification">{totalItems}</Badge>
+                                )}
                             </>
                         }
                         variant="unstyled"
