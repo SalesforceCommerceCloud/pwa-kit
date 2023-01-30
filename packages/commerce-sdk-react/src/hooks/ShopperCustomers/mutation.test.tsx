a5d1516b433c534aa9edd869e9f2f010bd5776ff
Alex Vuong <alex.vuong@salesforce.com>
2023-01-30 15:12:28 -0800

    uncomment tests

--- packages/commerce-sdk-react/src/hooks/ShopperCustomers/mutation.test.tsx
+++ packages/commerce-sdk-react/src/hooks/ShopperCustomers/mutation.test.tsx
@@ -200,48 +200,48 @@ const tests = (Object.keys(mutationPayloads) as ShopperCustomersMutationType[]).
                             expect(queryClient.getQueryState(queryKey)).toBeFalsy()
                         })
                     }
+                },
+                {
+                    name: 'error',
+                    assertions: async () => {
+                        nock(DEFAULT_TEST_HOST)
+                            .patch((uri) => {
+                                return uri.includes('/customer/shopper-customers/')
+                            })
+                            .reply(500, {})
+                            .put((uri) => {
+                                return uri.includes('/customer/shopper-customers/')
+                            })
+                            .reply(500, {})
+                            .post((uri) => {
+                                return uri.includes('/customer/shopper-customers/')
+                            })
+                            .reply(500, {})
+                            .delete((uri) => {
+                                return uri.includes('/customer/shopper-customers/')
+                            })
+                            .reply(500, {})
+
+                        renderWithProviders(
+                            <CustomerMutationComponent
+                                action={mutationName as ShopperCustomersMutationType}
+                            />
+                        )
+                        await waitFor(() =>
+                            screen.getByRole('button', {
+                                name: mutationName
+                            })
+                        )
+
+                        const button = screen.getByRole('button', {
+                            name: mutationName
+                        })
+
+                        fireEvent.click(button)
+                        await waitFor(() => screen.getByText(/error/i))
+                        expect(screen.getByText(/error/i)).toBeInTheDocument()
+                    }
                 }
-                // {
-                //     name: 'error',
-                //     assertions: async () => {
-                //         nock(DEFAULT_TEST_HOST)
-                //             .patch((uri) => {
-                //                 return uri.includes('/customer/shopper-customers/')
-                //             })
-                //             .reply(500, {})
-                //             .put((uri) => {
-                //                 return uri.includes('/customer/shopper-customers/')
-                //             })
-                //             .reply(500, {})
-                //             .post((uri) => {
-                //                 return uri.includes('/customer/shopper-customers/')
-                //             })
-                //             .reply(500, {})
-                //             .delete((uri) => {
-                //                 return uri.includes('/customer/shopper-customers/')
-                //             })
-                //             .reply(500, {})
-                //
-                //         renderWithProviders(
-                //             <CustomerMutationComponent
-                //                 action={mutationName as ShopperCustomersMutationType}
-                //             />
-                //         )
-                //         await waitFor(() =>
-                //             screen.getByRole('button', {
-                //                 name: mutationName
-                //             })
-                //         )
-                //
-                //         const button = screen.getByRole('button', {
-                //             name: mutationName
-                //         })
-                //
-                //         fireEvent.click(button)
-                //         await waitFor(() => screen.getByText(/error/i))
-                //         expect(screen.getByText(/error/i)).toBeInTheDocument()
-                //     }
-                // }
             ]
         }
     }
