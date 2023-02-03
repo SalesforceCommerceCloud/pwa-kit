15e7dfe77ddf05aca873b12e2cb90aba6870b29b
Will Harney <wharney@salesforce.com>
2023-02-03 15:59:16 -0500

    Add temp file for easier validation that things work.

---
+++ packages/commerce-sdk-react/src/hooks/Test/temp.ts
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2023, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+// Temp test file to validate that types are working as expected
+
+import {cacheUpdateMatrix} from './config'
+import {useShopperBasketsMutation} from './mutation'
+import {useBasket} from './query'
+
+const query = useBasket({
+    parameters: {
+        basketId: 'basketId'
+    }
+})
+const queryData = query.data
+
+const updates = cacheUpdateMatrix.removeItemFromBasket?.(
+    'customerId',
+    {
+        parameters: {
+            basketId: 'basketId',
+            itemId: 'itemId'
+        }
+    },
+    {basketId: 'basketId'}
+)
+if (updates) {
+    const {invalidate} = updates
+}
+
+const mutation = useShopperBasketsMutation('addCouponToBasket')
+const {data: mutationData, mutate} = mutation
+mutate({
+    body: {code: 'code'},
+    parameters: {basketId: 'basketId'}
+})
