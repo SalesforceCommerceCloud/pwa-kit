f32f4b82145e3aaa484426578ef3ac3e9c0c9875
yunakim714 <yunakim@salesforce.com>
2023-02-10 19:48:54 -0500

    lint

--- packages/template-retail-react-app/app/hooks/use-add-to-cart-modal.js
+++ packages/template-retail-react-app/app/hooks/use-add-to-cart-modal.js
@@ -68,11 +68,7 @@ export const AddToCartModal = () => {
     return (
         <Modal size={size} isOpen={isOpen} onClose={onClose} scrollBehavior="inside" isCentered>
             <ModalOverlay />
-            <ModalContent
-                margin="0"
-                borderRadius={{base: 'none', md: 'base'}}
-                bgColor="gray.50"
-            >
+            <ModalContent margin="0" borderRadius={{base: 'none', md: 'base'}} bgColor="gray.50">
                 <ModalHeader paddingY="8" bgColor="white" fontSize="2xl" fontWeight="700">
                     {intl.formatMessage(
                         {
