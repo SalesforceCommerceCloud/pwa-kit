0a9ef34810ccbd5a4d191a9febc6e20928af2971
Will Harney <wharney@salesforce.com>
2023-02-13 14:04:50 -0500

    Convert getGiftCertificate from mutation hook to query hook.

---
+++ packages/commerce-sdk-react/src/hooks/ShopperGiftCertificates/query.ts
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2023, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import {UseQueryOptions, UseQueryResult} from '@tanstack/react-query'
+import {ApiClients, Argument, DataType, MergedOptions} from '../types'
+import useCommerceApi from '../useCommerceApi'
+import {useQuery} from '../useQuery'
+
+type Client = ApiClients['shopperGiftCertificates']
+
+/**
+ * A hook for `ShopperGiftCertificates#getGiftCertificate`.
+ * Action to retrieve an existing gift certificate.
+ * @see {@link https://developer.salesforce.com/docs/commerce/commerce-api/references/shopper-gift-certificates?meta=getGiftCertificate} for more information about the API endpoint.
+ * @see {@link https://salesforcecommercecloud.github.io/commerce-sdk-isomorphic/classes/shoppergiftcertificates.shoppergiftcertificates-1.html#getgiftcertificate} for more information on the parameters and returned data type.
+ * @returns An object describing the state of the request.
+ */
+export const useGiftCertificate = (
+    apiOptions: Argument<Client['getGiftCertificate']>,
+    queryOptions: Omit<UseQueryOptions<DataType<Client['getGiftCertificate']>>, 'queryFn'> = {}
+): UseQueryResult<DataType<Client['getGiftCertificate']>> => {
+    const {shopperGiftCertificates: client} = useCommerceApi()
+    const method = (arg: Argument<Client['getGiftCertificate']>) => client.getGiftCertificate(arg)
+    const requiredParameters = ['organizationId', 'siteId'] as const
+    // Parameters can be set in `apiOptions` or `client.clientConfig`; they are merged in the helper
+    // hook, so we use a callback here that receives that merged object.
+    const getQueryKey = ({
+        parameters
+    }: MergedOptions<Client, Argument<Client['getGiftCertificate']>>) =>
+        [
+            '/organizations/',
+            parameters.organizationId,
+            '/gift-certificate',
+            // Full parameters last for easy lookup
+            parameters
+        ] as const
+
+    return useQuery(apiOptions, queryOptions, {
+        client,
+        method,
+        requiredParameters,
+        getQueryKey
+    })
+}
