2630f0d1bde3577b972cf8128ff8329a5a9cab9b
Adam Raya Navarro <arayanavarro@salesforce.com>
2023-02-16 12:00:55 -0800

    Add more ImageWithText props

--- packages/template-retail-react-app/app/components/experience/image-with-text/index.jsx
+++ packages/template-retail-react-app/app/components/experience/image-with-text/index.jsx
@@ -6,33 +6,55 @@
  */
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Image} from '@chakra-ui/react'
+import {Image, Box, Text, Link} from '@chakra-ui/react'
 
 /**
  *
  */
-const ImageWithText = ({image}) => {
+const ImageWithText = ({ITCLink, ITCText, image, heading, alt}) => {
     return (
         <div className={'image-with-text'}>
             <figure className={'image-with-text-figure'}>
                 <picture>
                     <source srcSet={image?.src?.tablet} media="(min-width: 48em)" />
                     <source srcSet={image?.src?.desktop} media="(min-width: 64em)" />
-                    <Image
-                        className={'image-with-text-image'}
-                        data-testid={'image-with-text-image'}
-                        src={image?.src?.mobile ? image?.src?.mobile : image?.url}
-                        ignoreFallback={true}
-                        alt={image?.alt}
-                        title={image?.alt}
-                    />
+                    <Link to={ITCLink}>
+                        <Image
+                            className={'image-with-text-image'}
+                            data-testid={'image-with-text-image'}
+                            src={image?.src?.mobile ? image?.src?.mobile : image?.url}
+                            ignoreFallback={true}
+                            alt={alt}
+                            title={alt}
+                        />
+                    </Link>
                 </picture>
+                {(ITCText || heading) && (
+                    <Text as="figcaption">
+                        {heading && (
+                            <Box className={'image-with-text-heading-container'}>
+                                <Text as="span" className={'image-with-text-heading-text'}>
+                                    {heading}
+                                </Text>
+                            </Box>
+                        )}
+                        {ITCText && (
+                            <Box>
+                                <Text as="span" className={'image-with-text-text-underneath'}>
+                                    {ITCText}
+                                </Text>
+                            </Box>
+                        )}
+                    </Text>
+                )}
             </figure>
         </div>
     )
 }
 
 ImageWithText.propTypes = {
+    ITCLink: PropTypes.string,
+    ITCText: PropTypes.string,
     image: PropTypes.shape({
         _type: PropTypes.string,
         focalPoint: PropTypes.shape({
@@ -40,10 +62,16 @@ ImageWithText.propTypes = {
             x: PropTypes.number,
             y: PropTypes.number
         }),
+        metaData: PropTypes.shape({
+            _type: PropTypes.string,
+            height: PropTypes.number,
+            width: PropTypes.number
+        }),
         url: PropTypes.string,
-        alt: PropTypes.string,
         src: PropTypes.string || PropTypes.object
-    })
+    }),
+    heading: PropTypes.string,
+    alt: PropTypes.string
 }
 
 export default ImageWithText
