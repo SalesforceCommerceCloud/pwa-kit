2dd71ecbabcf317b9fa6d3ef5565f31892be7aa2
Will Harney <wharney@salesforce.com>
2023-02-20 15:48:56 -0500

    Add test to validate all mutations have cache update logic.

--- packages/commerce-sdk-react/src/hooks/ShopperBaskets/index.test.ts
+++ packages/commerce-sdk-react/src/hooks/ShopperBaskets/index.test.ts
@@ -6,11 +6,19 @@
  */
 import {ShopperBaskets} from 'commerce-sdk-isomorphic'
 import {expectAllEndpointsHaveHooks} from '../../test-utils'
+import {cacheUpdateMatrix} from './cache'
 import {ShopperBasketsMutations} from './mutation'
 import * as queries from './query'
 
 describe('Shopper Baskets hooks', () => {
     test('all endpoints have hooks', () => {
         expectAllEndpointsHaveHooks(ShopperBaskets, queries, ShopperBasketsMutations)
+    })
+
+    test('all mutation hooks have cache update logic', () => {
+        // If this test fails, add the missing mutation as a no-op or a TODO that throws NotYetImplemented
+        const cacheUpdates = Object.keys(cacheUpdateMatrix).sort()
+        const mutations = Object.values(ShopperBasketsMutations).sort()
+        expect(cacheUpdates).toEqual(mutations)
     })
 })
