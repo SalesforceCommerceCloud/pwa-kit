4d16fabd2494ddfb2f081625d2074258cc3271ec
Adam Raya Navarro <arayanavarro@salesforce.com>
2023-02-27 23:36:51 -0800

    Backport fixes on generated script

--- packages/pwa-kit-create-app/scripts/create-mobify-app-dev.js
+++ packages/pwa-kit-create-app/scripts/create-mobify-app-dev.js
@@ -43,9 +43,7 @@
 
 const p = require('path')
 const sh = require('shelljs')
-const fs = require('fs')
 const cp = require('child_process')
-const semver = require('semver')
 
 sh.set('-e')
 
@@ -56,19 +54,19 @@ const logFileName = p.join(__dirname, '..', 'local-npm-repo', 'verdaccio.log')
  */
 const withLocalNPMRepo = (func) => {
     const monorepoRoot = p.resolve(__dirname, '..', '..', '..')
-    const verdaccio = p.join(__dirname, '..', 'node_modules', '.bin', 'verdaccio')
+    const verdaccioBinary = p.join(__dirname, '..', 'node_modules', '.bin', 'verdaccio')
     const verdaccioConfigDir = p.join(__dirname, '..', 'local-npm-repo')
 
     // Clear any cached packages from a previous run.
     sh.rm('-rf', p.join(verdaccioConfigDir, 'storage'))
     sh.mkdir(p.join(verdaccioConfigDir, 'storage'))
 
-    let child
+    let verdaccioServerProcess
 
     const cleanup = () => {
         console.log('Shutting down local NPM repository')
         delete process.env['npm_config_registry']
-        child.kill()
+        verdaccioServerProcess.kill()
     }
 
     return Promise.resolve()
@@ -77,7 +75,7 @@ const withLocalNPMRepo = (func) => {
                 new Promise((resolve) => {
                     console.log('Starting up local NPM repository')
 
-                    child = cp.exec(`${verdaccio} --config config.yaml`, {
+                    verdaccioServerProcess = cp.exec(`${verdaccioBinary} --config config.yaml`, {
                         cwd: verdaccioConfigDir,
                         stdio: 'inherit',
                         env: {
@@ -88,40 +86,15 @@ const withLocalNPMRepo = (func) => {
                         }
                     })
 
-                    const CHECK_TIME = 1000
-
-                    const waitForLogFileExists = () => {
-                        setTimeout(() => {
-                            fs.readFile(logFileName, 'utf8', (err, data) => {
-                                if (err || !data) {
-                                    waitForLogFileExists()
-                                } else {
-                                    const readStream = fs.createReadStream(logFileName, 'utf8')
-
-                                    readStream.on('error', (error) => {
-                                        console.error(
-                                            `Failed to read ${logFileName}: ${error.message}`
-                                        )
-                                        waitForLogFileExists()
-                                    })
-
-                                    readStream.on('data', (data) => {
+                    verdaccioServerProcess.stdout.on('data', (data) => {
+                        // we know verdaccio server is up when
+                        // 'http address' is in log output
                                         if (data.includes('http address')) {
-                                            // Verdaccio is running once it logs the HTTP address. Configure
-                                            // NPM to use the local repo, through env vars.
-                                            process.env['npm_config_registry'] =
-                                                'http://localhost:4873/'
+                            console.log('local NPM repository is up')
+                            process.env['npm_config_registry'] = 'http://localhost:4873/'
                                             resolve()
-                                        } else {
-                                            waitForLogFileExists()
-                                        }
-                                    })
                                 }
                             })
-                        }, CHECK_TIME)
-                    }
-
-                    waitForLogFileExists()
                 })
         )
         .then(() => {
@@ -148,12 +121,7 @@ const runGenerator = () => {
     // Shelljs can't run interactive programs, so we have to switch to child_process.
     // See https://github.com/shelljs/shelljs/wiki/FAQ#running-interactive-programs-with-exec
 
-    const extension = process.platform === 'win32' ? '.cmd' : ''
-    const npm = `npm${extension}`
-    const foundNpm = cp.spawnSync(npm, ['-v']).stdout.toString().trim()
-    const flags = semver.satisfies(foundNpm, '>=7') ? '-y' : ''
-
-    cp.execSync(`npx ${flags} pwa-kit-create-app@latest ${process.argv.slice(2).join(' ')}`, {
+    cp.execSync(`npx pwa-kit-create-app ${process.argv.slice(2).join(' ')}`, {
         stdio: 'inherit'
     })
 }
