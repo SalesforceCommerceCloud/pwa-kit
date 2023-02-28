14127028ef32d48a61bf341b51afa7075e05793d
Evan Chessman <echessman@echessm-ltm7wpc.internal.salesforce.com>
2023-02-28 14:54:39 -0500

    test

--- packages/template-retail-react-app/app/components/forms/useProfileFields.jsx
+++ packages/template-retail-react-app/app/components/forms/useProfileFields.jsx
@@ -76,6 +76,7 @@ export default function useProfileFields({form: {control, errors}, prefix = ''})
             },
             error: errors[`${prefix}phone`],
             inputProps: ({onChange}) => ({
+                inputmode: 'numeric',
                 onChange(evt) {
                     onChange(formatPhoneNumber(evt.target.value))
                 }
