de60f7a59b16a4c108057c22ec368a6711f06dbd
Kevin He <kevin.he@salesforce.com>
2023-03-01 23:12:35 -0800

    add/update address

--- packages/template-retail-react-app/app/pages/checkout/partials/shipping-address.jsx
+++ packages/template-retail-react-app/app/pages/checkout/partials/shipping-address.jsx
@@ -5,11 +5,14 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import React, {useState} from 'react'
+import { nanoid } from 'nanoid'
 import {defineMessage, useIntl} from 'react-intl'
 import {useCheckout} from '../util/checkout-context'
 import {ToggleCard, ToggleCardEdit, ToggleCardSummary} from '../../../components/toggle-card'
 import ShippingAddressSelection from './shipping-address-selection'
 import AddressDisplay from '../../../components/address-display'
+import {useShopperCustomersMutation} from 'commerce-sdk-react-preview'
+import { useCurrentCustomer } from '../../../hooks/use-current-customer'
 
 const submitButtonMessage = defineMessage({
     defaultMessage: 'Continue to Shipping Method',
@@ -22,16 +25,61 @@ export default function ShippingAddress() {
     const {
         step,
         checkoutSteps,
+        isGuestCheckout,
         selectedShippingAddress,
         setShippingAddress,
         setCheckoutStep,
         goToNextStep
     } = useCheckout()
     const [isLoading, setIsLoading] = useState()
+    const {data: customer} = useCurrentCustomer()
+
+    const createCustomerAddress = useShopperCustomersMutation({action: 'createCustomerAddress'})
+    const updateCustomerAddress = useShopperCustomersMutation({action: 'updateCustomerAddress'})
 
     const submitAndContinue = async (address) => {
         setIsLoading(true)
-        await setShippingAddress(address)
+        // await setShippingAddress(address)
+
+        // @TODO:
+        // await basket.setShippingAddress(address)
+
+        //         // Add/Update the address to the customer's account if they are registered.
+        //         if (!state.isGuestCheckout) {
+        //             !addressId
+        //                 ? customer.addSavedAddress(address)
+        //                 : customer.updateSavedAddress({...address, addressId: addressId})
+        //         }
+
+        // step 1
+        // basket setShippingAddress
+        // step 2
+        //  if registered
+        //      if new -> add saved address
+        //      if old -> update saved address
+
+        const {addressId} = address
+        if (!isGuestCheckout && !addressId) {
+            const body = {
+                ...address,
+                addressId: nanoid()
+            }
+            await createCustomerAddress.mutateAsync({
+                body,
+                parameters: {customerId: customer.customerId}
+            })
+        }
+
+        if (!isGuestCheckout && addressId) {
+            await updateCustomerAddress.mutateAsync({
+                body: address,
+                parameters: {
+                    customerId: customer.customerId,
+                    addressName: addressId
+                }
+            })
+        }
+
         goToNextStep()
         setIsLoading(false)
     }
