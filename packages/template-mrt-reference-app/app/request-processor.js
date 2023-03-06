a402f66da2f130a817f76acaa127c3ae2da75041
Oliver Brook <o.brook@salesforce.com>
2023-03-06 11:39:33 -0800

    Move the MRT reference app to the SDKs, so that we can verify eg. Node support (#966)

---
+++ packages/template-mrt-reference-app/app/request-processor.js
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2023, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import {QueryParameters} from 'pwa-kit-runtime/utils/ssr-request-processing'
+
+const exclusions = ['removeme']
+
+export const processRequest = ({path, querystring, parameters}) => {
+    console.assert(parameters, 'Missing parameters')
+
+    // Query string filtering
+
+    // Build a first QueryParameters object from the given querystring
+    const queryParameters = new QueryParameters(querystring)
+
+    // Build a second QueryParameters from the first, with all
+    // excluded parameters removed
+    const filtered = QueryParameters.from(
+        queryParameters.parameters.filter(
+            // parameter.key is always lower-case
+            (parameter) => !exclusions.includes(parameter.key)
+        )
+    )
+
+    // Re-generate the querystring
+    querystring = filtered.toString()
+
+    return {
+        path,
+        querystring
+    }
+}
