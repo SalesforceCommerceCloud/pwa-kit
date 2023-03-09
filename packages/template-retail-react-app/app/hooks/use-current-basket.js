b0807b8447ff5fe3a5222cc5b6532c78e2672d7c
Alex Vuong <alex.vuong@salesforce.com>
2023-03-08 18:17:35 -0800

    create basket if there is non after getting

--- packages/template-retail-react-app/app/hooks/use-current-basket.js
+++ packages/template-retail-react-app/app/hooks/use-current-basket.js
@@ -4,7 +4,13 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import {useCustomerBaskets, useCustomerId, useProducts} from 'commerce-sdk-react-preview'
+import {
+    useCustomerBaskets,
+    useCustomerId,
+    useProducts,
+    useShopperBasketsMutation
+} from 'commerce-sdk-react-preview'
+import {useCurrentCustomer} from './use-current-customer'
 
 /**
  * This hook combine some commerce-react-sdk hooks to provide more derived data for Retail App baskets
@@ -12,7 +18,9 @@ import {useCustomerBaskets, useCustomerId, useProducts} from 'commerce-sdk-react
  * @param shouldFetchProductDetail - boolean to indicate if the baskets should fetch product details based on basket items
  */
 export const useCurrentBasket = ({id = ''} = {}) => {
-    const customerId = useCustomerId() || ''
+    const createBasket = useShopperBasketsMutation('createBasket')
+    const {data: customer} = useCurrentCustomer()
+    const {customerId} = customer
     const {
         data: basketsData,
         isLoading: isBasketsLoading,
@@ -20,9 +28,19 @@ export const useCurrentBasket = ({id = ''} = {}) => {
     } = useCustomerBaskets(
         {parameters: {customerId}},
         {
-            enabled: !!customerId
+            onSuccess: (data) => {
+                console.log('dATA', data)
+                if (!data.total) {
+                    console.log('creating basket')
+                    createBasket.mutate({
+                        body: {}
+                    })
+                }
+            }
         }
     )
+
+    console.log('basketsData', basketsData)
 
     // if id is not defined, by default use the first basket in the list
     const basket =
