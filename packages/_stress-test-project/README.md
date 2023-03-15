a4567919966a6837f74b92f55ff7a1c0e4e4ebb3
Brian Feister <bfeister@salesforce.com>
2023-03-15 11:22:54 -0600

    remove stress test files post-merge

--- packages/_stress-test-project/README.md
+++
@@ -1,3 +0,0 @@
-# Steps
-
-1. `npm i full-icu@^1.3.1 pwa-kit-dev cross-env`
--- packages/_stress-test-project/pwa-kit/overrides/app/theme/foundations/shadows.js
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2021, salesforce.com, inc.
- * All rights reserved.
- * SPDX-License-Identifier: BSD-3-Clause
- * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
- */
-export default {
-    top: '0px -1px 3px rgba(0, 0, 0, 0.1), 0px -1px 2px rgba(0, 0, 0, 0.06)'
-}
