92997a549295fce494245d8c6c7f321d0333b1ec
yunakim714 <yunakim@salesforce.com>
2023-03-23 14:24:08 -0400

    customize the component slot in pdp in overrides

--- packages/spike-extendend-retail-app/pwa-kit/overrides/app/components/product-detail/above-fold.jsx
+++ packages/spike-extendend-retail-app/pwa-kit/overrides/app/components/product-detail/above-fold.jsx
@@ -5,8 +5,58 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
+import React from 'react';
+import { Box, SimpleGrid, Icon, Text, Stack, Flex } from '@chakra-ui/react';
+import { HeartIcon, SocialTwitterIcon, DukeIcon } from '../icons'
+
+const Feature = ({title, text, icon}) => {
+  return (
+    <Stack>
+      <Flex
+        w={16}
+        h={16}
+        align={'center'}
+        justify={'center'}
+        color={'white'}
+        rounded={'full'}
+        bg={'gray.100'}
+        mb={1}>
+        {icon}
+      </Flex>
+      <Text fontWeight={600}>{title}</Text>
+      <Text color={'gray.600'}>{text}</Text>
+    </Stack>
+  );
+};
+
 const AbovePDP = () => {
-    return (null)
+  return (
+    <Box p={4}>
+      <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
+        <Feature
+          icon={<Icon as={HeartIcon} w={10} h={10} />}
+          title={'Hello there'}
+          text={
+            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
+          }
+        />
+        <Feature
+          icon={<Icon as={SocialTwitterIcon} w={10} h={10} />}
+          title={'This is'}
+          text={
+            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
+          }
+        />
+        <Feature
+          icon={<Icon as={DukeIcon} w={10} h={10} />}
+          title={'A component slot'}
+          text={
+            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
+          }
+        />
+      </SimpleGrid>
+    </Box>
+  );
 }
 
 export default AbovePDP
