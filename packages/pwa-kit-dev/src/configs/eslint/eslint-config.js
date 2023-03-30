d36ceb995e405924e7c01a4554c75e9fce82dfa3
Will Harney <wharney@salesforce.com>
2023-03-30 16:27:07 -0400

    Disable rules about the `any` type.

--- packages/pwa-kit-dev/src/configs/eslint/eslint-config.js
+++ packages/pwa-kit-dev/src/configs/eslint/eslint-config.js
@@ -45,6 +45,16 @@ module.exports = {
         {
             files: ['**/*.ts', '**/*.tsx'],
             extends: 'plugin:@typescript-eslint/recommended-requiring-type-checking',
+            rules: {
+                // These rules all deal with the `any` type in some capacity;
+                // we want to be more permissive than @typescript-eslint is by default
+                '@typescript-eslint/no-explicit-any': 'off',
+                '@typescript-eslint/no-unsafe-argument': 'off',
+                '@typescript-eslint/no-unsafe-assignment': 'off',
+                '@typescript-eslint/no-unsafe-call': 'off',
+                '@typescript-eslint/no-unsafe-member-access': 'off',
+                '@typescript-eslint/no-unsafe-return': 'off'
+            },
             parserOptions: {
                 project: true
             }
