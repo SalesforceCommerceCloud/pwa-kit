4ff954d31a83fa7f9f4c15eb907dfb4277ec1545
Will Harney <wharney@salesforce.com>
2023-04-04 14:59:45 -0400

    Fix lint errors in template-typescript-minimal.

--- packages/template-typescript-minimal/app/components/hello-typescript.tsx
+++ packages/template-typescript-minimal/app/components/hello-typescript.tsx
@@ -4,9 +4,6 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-// The eslint config used from pwa-kit-dev does not properly parse TypeScript. :\
-// TODO: Remove this directive when the eslint config is updated.
-/* eslint-disable no-undef */
 import React from 'react'
 
 interface Props {
