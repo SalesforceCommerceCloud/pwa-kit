cd43682f1d749aed9dfde50a31e188d34c929816
Will Harney <wharney@salesforce.com>
2023-04-04 14:19:45 -0400

    Fix lint errors in template-express-minimal.

--- packages/template-express-minimal/app/ssr.test.js
+++ packages/template-express-minimal/app/ssr.test.js
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-const request = require('supertest')
-const app = require('./ssr')
+import request from 'supertest'
+import app from './ssr'
 
 describe('server', () => {
     afterAll(() => app.server.close())
