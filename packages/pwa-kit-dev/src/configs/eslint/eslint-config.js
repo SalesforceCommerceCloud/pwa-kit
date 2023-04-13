b68ebedb5332e5d31de4ab051b736024178fe6e5
Will Harney <wharney@salesforce.com>
2023-04-13 16:52:39 -0400

    Pluck changes that aren't lint fixes from wjh/update-eslint.

--- packages/pwa-kit-dev/src/configs/eslint/eslint-config.js
+++ packages/pwa-kit-dev/src/configs/eslint/eslint-config.js
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2021, salesforce.com, inc.
+ * Copyright (c) 2023, Salesforce, Inc.
  * All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 module.exports = {
-    parser: '@babel/eslint-parser',
+    parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 2017,
         sourceType: 'module',
@@ -16,19 +16,61 @@ module.exports = {
     env: {
         es6: true,
         node: true,
-        browser: true,
-        jest: true
+        browser: true
     },
-    extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier', 'prettier/react'],
-    plugins: ['header', 'react', 'prettier'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:prettier/recommended'
+    ],
+    plugins: ['jsx-a11y', 'prettier', 'react', 'react-hooks', 'use-effect-no-deps'],
     settings: {
         react: {
             version: 'detect'
         }
     },
+    reportUnusedDisableDirectives: true,
     rules: {
-        'prettier/prettier': ['error'],
-        'no-console': 'off',
-        'no-unused-vars': ['error', {ignoreRestSiblings: true}]
-    }
+        'react-hooks/rules-of-hooks': 'error',
+        // react-hooks/exhaustive-deps has too many false positives; use-effect-no-deps is nicer
+        'use-effect-no-deps/use-effect-no-deps': 'warn'
+    },
+    overrides: [
+        {
+            // Jest (v29) default test match pattern
+            files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+            plugins: ['jest'],
+            extends: ['plugin:jest/recommended', 'plugin:jest/style'],
+            rules: {
+                'jest/expect-expect': [
+                    'warn',
+                    {
+                        // Anything starting with `expect` counts as an assertion
+                        assertFunctionNames: ['expect*', '**.expect*']
+                    }
+                ],
+                // This doesn't reliably report all errors :/
+                'jest/no-standalone-expect': ['off']
+            }
+        },
+        {
+            files: ['**/*.ts', '**/*.tsx'],
+            extends: ['plugin:@typescript-eslint/recommended-requiring-type-checking'],
+            rules: {
+                // These rules all deal with the `any` type in some capacity;
+                // we want to be more permissive than @typescript-eslint is by default
+                '@typescript-eslint/no-explicit-any': 'off',
+                '@typescript-eslint/no-unsafe-argument': 'off',
+                '@typescript-eslint/no-unsafe-assignment': 'off',
+                '@typescript-eslint/no-unsafe-call': 'off',
+                '@typescript-eslint/no-unsafe-member-access': 'off',
+                '@typescript-eslint/no-unsafe-return': 'off'
+            },
+            parserOptions: {
+                project: true
+            }
+        }
+    ]
 }
