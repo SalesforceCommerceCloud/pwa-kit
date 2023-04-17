20d83883a13062a50986607942d4455db165cc31
Will Harney <wharney@salesforce.com>
2023-04-17 15:49:57 -0400

    Update eslint configs to point to new files.

--- packages/pwa-kit-dev/.eslintrc.js
+++ packages/pwa-kit-dev/.eslintrc.js
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+
 module.exports = {
-    extends: './dist/configs/eslint/eslint-config.js'
+    extends: ['./dist/configs/eslint/no-react.js']
 }
