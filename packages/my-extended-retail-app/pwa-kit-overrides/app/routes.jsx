83f07fe4be94badca130e9dbba99b8a6c8c8f70f
vcua-mobify <vcua@salesforce.com>
2023-05-03 11:42:24 -0700

    WIP

--- packages/my-extended-retail-app/pwa-kit-overrides/app/routes.jsx
+++ packages/my-extended-retail-app/pwa-kit-overrides/app/routes.jsx
@@ -14,19 +14,20 @@
 
 import React from 'react'
 import loadable from '@loadable/component'
-import {getConfig} from 'pwa-kit-runtime/utils/ssr-config'
+import { getConfig } from 'pwa-kit-runtime/utils/ssr-config'
 
 // Components
-import {Skeleton} from '@chakra-ui/react'
-import {configureRoutes} from 'retail-react-app/app/utils/routes-utils'
+import { Skeleton } from '@chakra-ui/react'
+import { configureRoutes } from 'retail-react-app/app/utils/routes-utils'
 
-import {routes as _routes} from 'retail-react-app/app/routes'
+import { routes as _routes } from 'retail-react-app/app/routes'
 
 const fallback = <Skeleton height="75vh" width="100%" />
 
 // Pages
-const Home = loadable(() => import('./pages/home'), {fallback})
+const Home = loadable(() => import('./pages/home'), { fallback })
 const MyNewRoute = loadable(() => import('./pages/my-new-route'))
+const Login = loadable(() => import('./pages/login'))
 
 const routes = [
     {
@@ -37,6 +38,10 @@ const routes = [
     {
         path: '/my-new-route',
         component: MyNewRoute
+    },
+    {
+        path: '/login',
+        component: Login
     },
     // NOTE: the final item in the array must be the { path: '*', component: PageNotFound } so
     // routes added after re-inserting the base templates routes here might fail to
