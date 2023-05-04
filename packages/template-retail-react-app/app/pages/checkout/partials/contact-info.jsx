a71349937599024412c3a9919ad136430e1c564e
vcua-mobify <vcua@salesforce.com>
2023-05-04 15:00:45 -0700

    Add a redirect to login page after user signs out from checkout page

--- packages/template-retail-react-app/app/pages/checkout/partials/contact-info.jsx
+++ packages/template-retail-react-app/app/pages/checkout/partials/contact-info.jsx
@@ -29,11 +29,13 @@ import useLoginFields from '../../../components/forms/useLoginFields'
 import {ToggleCard, ToggleCardEdit, ToggleCardSummary} from '../../../components/toggle-card'
 import Field from '../../../components/field'
 import {AuthModal, useAuthModal} from '../../../hooks/use-auth-modal'
+import useNavigation from '../../../hooks/use-navigation'
 
 const ContactInfo = () => {
     const {formatMessage} = useIntl()
     const history = useHistory()
     const authModal = useAuthModal('password')
+    const navigate = useNavigation()
 
     const {
         customer,
@@ -183,8 +185,8 @@ const ContactInfo = () => {
                     onClose={() => setSignOutConfirmDialogIsOpen(false)}
                     onConfirm={async () => {
                         await customer.logout()
-                        await basket.getOrCreateBasket()
                         history.replace('/')
+                        navigate('/login')
                         setSignOutConfirmDialogIsOpen(false)
                     }}
                 />
