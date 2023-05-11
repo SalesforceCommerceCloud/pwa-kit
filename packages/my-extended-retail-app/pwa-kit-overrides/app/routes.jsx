787ccbf8c7ba8c60523cc16774cbb1005cb1fa28
Ben Chypak <bchypak@salesforce.com>
2023-05-11 11:29:59 -0700

    Remove extrainious overrides in sample override project

--- packages/my-extended-retail-app/pwa-kit-overrides/app/routes.jsx
+++ packages/my-extended-retail-app/pwa-kit-overrides/app/routes.jsx
@@ -5,53 +5,14 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-/* istanbul ignore file */
-// NOTE!
-// This file is being ignored in the test coverage report for now. It reports `0%` functions
-// tested, which brings down the overall coverage and blocks CI. There are tests still, but
-// we don't want it to count toward coverage until we figure out how to cover the `functions`
-// metric for this file in its test.
+import {routes} from 'retail-react-app/app/routes'
 
-import React from 'react'
-import loadable from '@loadable/component'
-import {getConfig} from 'pwa-kit-runtime/utils/ssr-config'
+// NOTE: This is where you can add custom routes.
 
-// Components
-import {Skeleton} from '@chakra-ui/react'
-import {configureRoutes} from 'retail-react-app/app/utils/routes-utils'
+// TODO: Make `configureRoutes` happen behind the scenes so partner dev's don't have to
+// worry about doing it themselves.
 
-import {routes as _routes} from 'retail-react-app/app/routes'
+// TODO: Lets an a new route for a "whats new" or "Extensibility overview" page, hopefully 
+// we can link directly to it when running the dev server.
 
-const fallback = <Skeleton height="75vh" width="100%" />
-
-// Pages
-const Home = loadable(() => import('./pages/home'), {fallback})
-const MyNewRoute = loadable(() => import('./pages/my-new-route'))
-const Login = loadable(() => import('./pages/login'))
-
-const routes = [
-    {
-        path: '/',
-        component: Home,
-        exact: true
-    },
-    {
-        path: '/my-new-route',
-        component: MyNewRoute
-    },
-    {
-        path: '/login',
-        component: Login
-    },
-    // NOTE: the final item in the array must be the { path: '*', component: PageNotFound } so
-    // routes added after re-inserting the base templates routes here might fail to
-    // show up unless the '*' route is filtered out and re-inserted
-    ..._routes
-]
-
-export default () => {
-    const config = getConfig()
-    return configureRoutes(routes, config, {
-        ignoredRoutes: ['/callback', '*']
-    })
-}
+export default routes
