add42e03b88ac8cb9e2643a01f8f326c979f133b
Ben Chypak <bchypak@salesforce.com>
2023-05-12 11:45:34 -0700

    Update create-mobify-app.js

--- packages/pwa-kit-create-app/scripts/create-mobify-app.js
+++ packages/pwa-kit-create-app/scripts/create-mobify-app.js
@@ -487,8 +487,8 @@ const downloadAndExtractTemplate = (templateName, outputDir) => {
         cwd: p.join(tmp),
         sync: true
     })
-    console.log(`copying ${p.join(tmp, 'package', '*.*')} to ${outputDir}`)
-    sh.cp('-R', p.join(tmp, 'package', '*.*'), outputDir)
+    console.log(`copying ${p.join(tmp, 'package', '*')} to ${outputDir}`)
+    sh.cp('-R', p.join(tmp, 'package', '*'), outputDir)
     sh.rm('-rf', tmp)
 }
 
