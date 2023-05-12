c1a29be8a1f8bb8fb8604a9a5ad4cdc6fd9b9daf
vcua-mobify <vcua@salesforce.com>
2023-05-12 12:25:34 -0700

    Remove Einstein proxy and send Einstein events directly from app

--- packages/my-extended-retail-app/config/default.js
+++ packages/my-extended-retail-app/config/default.js
@@ -30,10 +30,11 @@ module.exports = {
             }
         },
         einsteinAPI: {
-            proxyPath: `/mobify/proxy/einstein`,
+            host: 'https://api.cquotient.com',
             einsteinId: '1ea06c6e-c936-4324-bcf0-fada93f83bb1',
             // This differs from the siteId in commerceAPIConfig for testing purposes
-            siteId: 'aaij-MobileFirst'
+            siteId: 'aaij-MobileFirst',
+            isProduction: false
         }
     },
     externals: [],
@@ -57,10 +58,6 @@ module.exports = {
             {
                 host: 'zzrf-001.sandbox.us03.dx.commercecloud.salesforce.com',
                 path: 'ocapi'
-            },
-            {
-                host: 'api.cquotient.com',
-                path: 'einstein'
             }
         ]
     }
