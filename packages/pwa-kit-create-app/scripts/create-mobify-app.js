8f44b7ab67de284a4b5999a164230676cde29c15
Ben Chypak <bchypak@salesforce.com>
2023-05-12 10:24:30 -0700

    Update create-mobify-app.js

--- packages/pwa-kit-create-app/scripts/create-mobify-app.js
+++ packages/pwa-kit-create-app/scripts/create-mobify-app.js
@@ -176,6 +176,7 @@ const runGenerator = (answers, {outputDir, verbose, extensible}) => {
         // Copy folder to destination and process template if required.
         getAllFiles(inputDir).forEach((inputFile) => {
             const outputFile = outputDir + inputFile.replace(inputDir, '')
+            console.log('outputFile: ', outputFile)
             const destDir = outputFile.split(p.sep).slice(0, 1).join(p.sep)
             console.log('destDir: ', destDir)
             // Create folder if we are doing a deep copy
