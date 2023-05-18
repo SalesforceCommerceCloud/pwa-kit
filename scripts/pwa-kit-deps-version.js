20c9c9d22ceb3205236d5a6a9de36715ad97cff8
Vincent Marta <vmarta@salesforce.com>
2023-05-18 15:12:10 -0700

    Some refactoring

--- scripts/pwa-kit-deps-version.js
+++ scripts/pwa-kit-deps-version.js
@@ -9,29 +9,27 @@ const sh = require('shelljs')
 const path = require('path')
 const {saveJSONToFile} = require('./utils')
 
-const {stdout} = sh.exec('lerna list --all --json', {silent: true})
-const monorepoPackages = JSON.parse(stdout.toString())
+const publicPackages = JSON.parse(sh.exec('lerna list --json', {silent: true}))
 
 // Assuming that this is run within a specific package,
-// the script would update the version of all its pwa-kit/sdk dependencies.
+// the script would update its pwa-kit/sdk dependencies.
 const main = () => {
-    // option = latest OR sync
-    const option = process.argv[2]
+    const updateBehaviour = process.argv[2]
 
     const pathToPackageJson = path.join(process.cwd(), 'package.json')
     const pkgJson = JSON.parse(sh.cat(pathToPackageJson))
 
-    if (option === 'latest') {
-        monorepoPackages.forEach(({name}) => {
+    if (updateBehaviour === 'latest') {
+        publicPackages.forEach(({name}) => {
             if (pkgJson.dependencies?.[name]) {
                 pkgJson.dependencies[name] = getLatestVersion(name)
             } else if (pkgJson.devDependencies?.[name]) {
                 pkgJson.devDependencies[name] = getLatestVersion(name)
             }
         })
-    } else if (option === 'sync') {
+    } else if (updateBehaviour === 'sync') {
         // Sync version with what's in the monorepo
-        monorepoPackages.forEach(({version, name}) => {
+        publicPackages.forEach(({version, name}) => {
             if (pkgJson.dependencies?.[name]) {
                 pkgJson.dependencies[name] = version
             } else if (pkgJson.devDependencies?.[name]) {
