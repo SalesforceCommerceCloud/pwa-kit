23699b8f17d7ba40bcf90612e543107e7685c0a9
Alex Vuong <alex.vuong@salesforce.com>
2023-05-18 13:52:07 -0700

    replace relative with absolute path fetchTranslations

---
+++ packages/template-retail-react-app/scripts/build-translations.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+/*
+ * Copyright (c) 2023, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+/* eslint @typescript-eslint/no-var-requires: "off" */
+
+const {exec} = require('child_process')
+const fs = require('fs')
+const path = require('path')
+const packagePath = path.join(process.cwd(), '.', 'package.json')
+const pkgJSON = JSON.parse(fs.readFileSync(packagePath))
+
+const getAllFilesByExtensions = (dirPath, arrayOfFiles = [], extensions = []) => {
+    const files = fs.readdirSync(dirPath)
+
+    files.forEach(function (file) {
+        if (fs.statSync(path.join(dirPath, file)).isDirectory()) {
+            arrayOfFiles = getAllFilesByExtensions(
+                path.join(dirPath, file),
+                arrayOfFiles,
+                extensions
+            )
+        } else {
+            arrayOfFiles.push(path.join(dirPath, file))
+        }
+    })
+    if (extensions.length) {
+        return arrayOfFiles.filter((filePath) => {
+            const getExtension = path.extname(filePath).replace('.', '')
+            return extensions.includes(getExtension)
+        })
+    }
+
+    return arrayOfFiles
+}
+// look for overridden files
+try {
+    const overridesDir = path.join(process.cwd(), '.', pkgJSON.ccExtensibility?.overridesDir)
+    const overridesAppDir = path.join(overridesDir, 'app')
+    const fileNames = getAllFilesByExtensions(
+        path.join(overridesDir, 'app'),
+        [],
+        ['js', 'jsx', 'ts', 'tsx']
+    )
+    // files that will be ignored in translation extraction
+    const filesToIgnore = fileNames
+        .map((path) => {
+            return path.replace(
+                overridesDir,
+                `--ignore ./node_modules/${pkgJSON.ccExtensibility?.extends}`
+            )
+        })
+        .join(' ')
+    // const ignoreString = filesToIgnore.map((path) => `--ignore ${path}`).join(' ')
+    const extractCommand = `formatjs extract ${overridesAppDir}/**/*.{js,jsx} ./node_modules/${pkgJSON.ccExtensibility?.extends}/app/pages/**/*.{js,jsx} ${filesToIgnore} --out-file translations/en-US.json --id-interpolation-pattern [sha512:contenthash:base64:6]`
+    exec(extractCommand, (err, stdout) => {
+        if (err) {
+            console.error(err)
+        }
+        console.log(stdout)
+    })
+} catch (error) {
+    console.error(error)
+}
