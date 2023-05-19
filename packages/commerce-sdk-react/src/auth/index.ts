b56aaefb902d69e044b89275c86f6d99df8a0aab
Jainam Sheth <99490559+shethj@users.noreply.github.com>
2023-05-19 16:19:21 -0400

    Update packages/commerce-sdk-react/src/auth/index.ts
    Co-authored-by: Kevin He <kevin.he@salesforce.com>

--- packages/commerce-sdk-react/src/auth/index.ts
+++ packages/commerce-sdk-react/src/auth/index.ts
@@ -276,7 +276,9 @@ class Auth {
             .then(async () => {
                 const token = await fn()
                 this.handleTokenResponse(token, isGuest)
-                onClient() && (await this.createOCAPISession())
+                if (onClient() && this.OCAPISessionsURL) {
+                    await this.createOCAPISession())
+                }
                 // Q: Why don't we just return token? Why re-construct the same object again?
                 // A: because a user could open multiple tabs and the data in memory could be out-dated
                 // We must always grab the data from the storage (cookie/localstorage) directly
