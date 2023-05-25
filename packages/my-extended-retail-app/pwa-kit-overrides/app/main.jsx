438aff370cb4bd0065b4d5c1c08c07267a7c3657
Brian Feister <bfeister@salesforce.com>
2023-05-25 14:08:49 -0600

    bringing back example project, not ready to delete yet

---
+++ packages/my-extended-retail-app/pwa-kit-overrides/app/main.jsx
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import {start, registerServiceWorker} from 'pwa-kit-react-sdk/ssr/browser/main'
+
+const main = () => {
+    // The path to your service worker should match what is set up in ssr.js
+    return Promise.all([start(), registerServiceWorker('/worker.js')])
+}
+
+main()
