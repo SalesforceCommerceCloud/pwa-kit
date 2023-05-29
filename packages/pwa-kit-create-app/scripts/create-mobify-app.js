1b287be7524c7de6b7b71a7b6aa99c37cccb1604
Ben Chypak <bchypak@salesforce.com>
2023-05-29 10:08:40 -0700

    Fix typo

--- packages/pwa-kit-create-app/scripts/create-mobify-app.js
+++ packages/pwa-kit-create-app/scripts/create-mobify-app.js
@@ -531,7 +531,7 @@ const processTemplate = (relFile, inputDir, outputDir, context) => {
  * @param {*} answers
  * @param {*} param2
  */
-const runGenerator = (context, {outputDir, templateVesion, verbose}) => {
+const runGenerator = (context, {outputDir, templateVersion, verbose}) => {
     const {answers, preset} = context
     const {templateSource} = preset
     const {extend = false} = answers.project
@@ -549,7 +549,7 @@ const runGenerator = (context, {outputDir, templateVesion, verbose}) => {
     switch (type) {
         case TEMPLATE_SOURCE_NPM: {
             const tarFile = sh
-                .exec(`npm pack ${id}@${templateVesion} --pack-destination="${tmp}"`, {
+                .exec(`npm pack ${id}@${templateVersion} --pack-destination="${tmp}"`, {
                     silent: true
                 })
                 .stdout.trim()
@@ -638,7 +638,7 @@ const main = async (opts) => {
     // to "general" and "project" questions. It'll also be populated with details of the selected project,
     // like its `package.json` value.
     let context = INITIAL_CONTEXT
-    let {outputDir, verbose, preset, templateVesion = 'latest'} = opts
+    let {outputDir, verbose, preset, templateVersion = 'latest'} = opts
     const {prompt} = inquirer
     const OUTPUT_DIR_FLAG_ACTIVE = !!outputDir
     const presetId = preset || process.env.GENERATOR_PRESET
@@ -685,7 +685,7 @@ const main = async (opts) => {
     // Inject the packageJSON into the context for extensibile projects.
     if (context.answers.project.extend) {
         const pkgJSON = JSON.parse(
-            sh.exec(`npm view ${selectedPreset.templateSource.id}@${templateVesion} --json`, {
+            sh.exec(`npm view ${selectedPreset.templateSource.id}@${templateVersion} --json`, {
                 silent: true
             }).stdout
         )
@@ -710,7 +710,7 @@ const main = async (opts) => {
     }
 
     // Generate the project.
-    runGenerator(context, {outputDir, templateVesion, verbose})
+    runGenerator(context, {outputDir, templateVersion, verbose})
 
     // Return the folder in which the project was generated in.
     return outputDir
@@ -738,7 +738,7 @@ Examples:
             ).join(', ')})`
         )
         .option(
-            '--template-version',
+            '--templateVersion',
             `The version of the template to be generated. This defaults to 'latest'. NOTE: This option only applies to templates being downloaded from NPM.`,
             false
         )
