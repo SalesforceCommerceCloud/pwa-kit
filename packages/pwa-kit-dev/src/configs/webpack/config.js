98a4939f5032f28ab48bffe395cf15ca448b365d
Brian Feister <bfeister@salesforce.com>
2023-05-31 17:50:53 -0600

    different path

--- packages/pwa-kit-dev/src/configs/webpack/config.js
+++ packages/pwa-kit-dev/src/configs/webpack/config.js
@@ -118,7 +118,7 @@ const entryPointExists = (segments) => {
 }
 
 const getAppEntryPoint = () => {
-    return EXT_OVERRIDES_DIR_NO_SLASH + `/app/main`
+    return resolve('./', EXT_OVERRIDES_DIR_NO_SLASH, 'app', 'main')
 }
 
 const findDepInStack = (pkg) => {
