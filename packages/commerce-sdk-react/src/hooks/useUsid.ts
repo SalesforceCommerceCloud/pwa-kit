47ea4ff92e8907d9f61fee68c8c70f9b564a5a9e
vcua-mobify <vcua@salesforce.com>
2023-06-06 15:34:29 -0700

    Remove unused vars

--- packages/commerce-sdk-react/src/hooks/useUsid.ts
+++ packages/commerce-sdk-react/src/hooks/useUsid.ts
@@ -5,9 +5,6 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import useAuthContext from './useAuthContext'
-import useConfig from './useConfig'
-
-const onClient = typeof window !== 'undefined'
 
 /**
  * Hook that returns the usid associated with the current access token.
@@ -16,9 +13,7 @@ const onClient = typeof window !== 'undefined'
  * @category Shopper Authentication
  */
 const useUsid = (): string | null => {
-    const config = useConfig()
     const auth = useAuthContext()
-
     return auth.get('usid')
 }
 
