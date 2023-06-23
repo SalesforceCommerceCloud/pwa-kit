3c39b02e51d798a5b5980196f98cc036054af594
Brian Feister <bfeister@salesforce.com>
2023-06-23 09:26:18 -0600

    migrate exports from `export {Thing as Thing} from './Thing'` to `export {Thing} from './Thing' and fix default exports in skipnav

--- packages/template-retail-react-app/app/components/shared/ui/Checkbox/index.jsx
+++ packages/template-retail-react-app/app/components/shared/ui/Checkbox/index.jsx
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2023, Salesforce, Inc.
+ * Copyright (c) 2023, salesforce.com, inc.
  * All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-export {Checkbox as Checkbox} from '@chakra-ui/react'
+export {Checkbox} from '@chakra-ui/react';
--- packages/template-retail-react-app/app/components/shared/util/SkipNavContent/index.jsx
+++ packages/template-retail-react-app/app/components/shared/util/SkipNavContent/index.jsx
@@ -5,4 +5,4 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-export {SkipNavContent as SkipNavContent} from '@chakra-ui/react'
+export {SkipNavContent} from '@chakra-ui/react'
