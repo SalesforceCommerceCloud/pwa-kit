7f2910d022c4688186734bb2ae57ec528a7a9e4d
Brian Redmond <bredmond@salesforce.com>
2023-06-27 14:19:21 -0400

    Add testing

--- packages/pwa-kit-runtime/src/ssr/server/express.test.js
+++ packages/pwa-kit-runtime/src/ssr/server/express.test.js
@@ -293,7 +293,7 @@ describe('SSRServer operation', () => {
             })
     })
 
-    test('SSRServer rendering gets and sends no cookies', () => {
+    test('SSRServer rendering blocks cookie setting by default', () => {
         const route = (req, res) => {
             res.setHeader('set-cookie', 'blah123')
             res.sendStatus(200)
@@ -309,6 +309,41 @@ describe('SSRServer operation', () => {
                 expect(console.warn.mock.calls[0][0]).toContain(`Discarding "Set-Cookie: blah123"`)
                 expect(res.headers['Set-Cookie']).toBeUndefined()
                 expect(res.headers['set-cookie']).toBeUndefined()
+            })
+    })
+
+    test('SSRServer rendering allows setting cookies with ssrAllowCookies option', () => {
+        const route = (req, res) => {
+            res.setHeader('set-cookie', 'blah123')
+            res.sendStatus(200)
+        }
+        const app = RemoteServerFactory._createApp(opts({ssrAllowCookies: true}))
+        app.get('/*', route)
+
+        return request(app)
+            .get('/')
+            .expect(200)
+            .then((res) => {
+                expect(res.headers['set-cookie']).toEqual(['blah123'])
+            })
+    })
+
+    test('SSRServer rendering allows setting cookies with SSR_ALLOW_COOKIES env', () => {
+        process.env = {
+            SSR_ALLOW_COOKIES: 'true',
+        }
+        const route = (req, res) => {
+            res.setHeader('set-cookie', 'blah123')
+            res.sendStatus(200)
+        }
+        const app = RemoteServerFactory._createApp(opts())
+        app.get('/*', route)
+
+        return request(app)
+            .get('/')
+            .expect(200)
+            .then((res) => {
+                expect(res.headers['set-cookie']).toEqual(['blah123'])
             })
     })
 
@@ -494,10 +529,43 @@ describe('SSRServer operation', () => {
             })
     })
 
-    test('should strip cookies before passing the request to the handler', () => {
+    test('should strip cookies before passing the request to the handler by default', () => {
         const app = RemoteServerFactory._createApp(opts())
         const route = (req, res) => {
             expect(req.headers.cookie).toBeUndefined()
+            res.sendStatus(200)
+        }
+        app.get('/*', route)
+        return request(app)
+            .get('/')
+            .set('cookie', 'xyz=456')
+            .then((response) => {
+                expect(response.status).toBe(200)
+                expect(response.headers['set-cookie']).toBeUndefined()
+            })
+    })
+
+    test('should allow cookies in the request with ssrAllowCookies option', () => {
+        const app = RemoteServerFactory._createApp(opts({ssrAllowCookies: true}))
+        const route = (req, res) => {
+            expect(req.headers.cookie).toEqual('xyz=456')
+            res.sendStatus(200)
+        }
+        app.get('/*', route)
+        return request(app)
+            .get('/')
+            .set('cookie', 'xyz=456')
+            .then((response) => {
+                expect(response.status).toBe(200)
+                expect(response.headers['set-cookie']).toBeUndefined()
+            })
+    })
+
+    test('should allow cookies in the request with SSR_ALLOW_COOKIES env', () => {
+        process.env = { 'SSR_ALLOW_COOKIES': 'true' }
+        const app = RemoteServerFactory._createApp(opts())
+        const route = (req, res) => {
+            expect(req.headers.cookie).toEqual('xyz=456')
             res.sendStatus(200)
         }
         app.get('/*', route)
