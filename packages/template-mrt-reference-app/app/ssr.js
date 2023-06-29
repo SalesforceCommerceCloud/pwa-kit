97a4a1245efb5f23dec7fb1be934de31ed10c20d
Brian Redmond <bredmond@salesforce.com>
2023-06-29 12:18:25 -0400

    Add max-age

--- packages/template-mrt-reference-app/app/ssr.js
+++ packages/template-mrt-reference-app/app/ssr.js
@@ -185,7 +185,7 @@ const cookieTest = async (req, res) => {
     if (Object.hasOwn(req.query, 'name')) {
         res.cookie(req.query.name, req.query?.value)
     }
-    res.set('Cache-Control', 'private')
+    res.set('Cache-Control', 'private, max-age=60')
     res.json(jsonFromRequest(req))
 }
 
