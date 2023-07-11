9dc7c15d895445e09a874f729062549d94ec077f
Jainam Tushar Sheth <j.sheth@salesforce.com>
2023-07-11 13:55:43 -0400

    Fix spacing for eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
-  root: true,
-  plugins: ['header'],
-  rules: {
-    'header/header': [
-      2,
-      'block',
-      [
-        '',
-        {
-          pattern:
-            '^ \\* Copyright \\(c\\) \\d{4}, (salesforce.com, inc|Salesforce, Inc)\\.$',
-          template: ` * Copyright (c) ${new Date().getFullYear()}, Salesforce, Inc.`,
-        },
-        ' * All rights reserved.',
-        ' * SPDX-License-Identifier: BSD-3-Clause',
-        ' * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause',
-        ' ',
-      ],
-    ],
-  },
+    root: true,
+    plugins: ['header'],
+    rules: {
+        'header/header': [
+            2,
+            'block',
+            [
+                '',
+                {
+                    pattern:
+                        '^ \\* Copyright \\(c\\) \\d{4}, (salesforce.com, inc|Salesforce, Inc)\\.$',
+                    template: ` * Copyright (c) ${new Date().getFullYear()}, Salesforce, Inc.`,
+                },
+                ' * All rights reserved.',
+                ' * SPDX-License-Identifier: BSD-3-Clause',
+                ' * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause',
+                ' ',
+            ],
+        ],
+    },
 };
