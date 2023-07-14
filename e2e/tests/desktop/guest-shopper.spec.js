deac65253333b1582bfbeeb33b662907d173d25a
Jainam Tushar Sheth <j.sheth@salesforce.com>
2023-07-13 23:28:38 -0400

    Remove test groups

--- e2e/tests/desktop/guest-shopper.spec.js
+++ e2e/tests/desktop/guest-shopper.spec.js
@@ -8,43 +8,39 @@
 const { test, expect } = require("@playwright/test");
 const config = require("../../config");
 
-test.describe("Guest shopper can checkout items as guest", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto(config.RETAIL_APP_HOME);
-  });
+test("Guest shopper can checkout items as guest", async ({ page }) => {
+  await page.goto(config.RETAIL_APP_HOME);
 
-  test("Guest shopper can add items to cart", async ({ page }) => {
-    await page.getByRole("link", { name: "Womens" }).hover();
+  await page.getByRole("link", { name: "Womens" }).hover();
 
-    await page.getByRole("link", { name: "Dresses" }).click();
+  await page.getByRole("link", { name: "Dresses" }).click();
 
-    await expect(page.getByRole("heading", { name: "Dresses" })).toBeVisible();
+  await expect(page.getByRole("heading", { name: "Dresses" })).toBeVisible();
 
-    await page.getByRole("link", { name: /Drape Neck Dress/i }).click();
+  await page.getByRole("link", { name: /Drape Neck Dress/i }).click();
 
-    await expect(
-      page.getByRole("heading", { name: /Drape Neck Dress/i })
-    ).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: /Drape Neck Dress/i })
+  ).toBeVisible();
 
-    await page.getByRole("radio", { name: "M", exact: true }).click();
+  await page.getByRole("radio", { name: "M", exact: true }).click();
 
-    await page.getByRole("button", { name: "+" }).click();
+  await page.getByRole("button", { name: "+" }).click();
 
-    // Selected Size and Color texts are broken into multiple elements on the page.
-    // So we need to look at the page URL to verify selected variants
-    const updatedPageURL = await page.url();
-    const params = updatedPageURL.split("?")[1];
-    expect(params).toMatch(/size=9MD/i);
-    expect(params).toMatch(/color=JJ3WDXX/i);
+  // Selected Size and Color texts are broken into multiple elements on the page.
+  // So we need to look at the page URL to verify selected variants
+  const updatedPageURL = await page.url();
+  const params = updatedPageURL.split("?")[1];
+  expect(params).toMatch(/size=9MD/i);
+  expect(params).toMatch(/color=JJ3WDXX/i);
 
-    await page.getByRole("button", { name: /Add to Cart/i }).click();
+  await page.getByRole("button", { name: /Add to Cart/i }).click();
 
-    const addedToCartModal = page.locator(
-      "[role='dialog'] heading:has-text('2 items added to cart')"
-    );
+  const addedToCartModal = page.locator(
+    "[role='dialog'] heading:has-text('2 items added to cart')"
+  );
 
-    page.on("dialog", async (_) => {
-      await expect(addedToCartModal).toBeVisible();
-    });
+  page.on("dialog", async (_) => {
+    await expect(addedToCartModal).toBeVisible();
   });
 });
--- e2e/tests/mobile/guest-shopper.spec.js
+++ e2e/tests/mobile/guest-shopper.spec.js
@@ -8,54 +8,52 @@
 const { test, expect } = require("@playwright/test");
 const config = require("../../config");
 
-test.describe("Guest shopper can checkout items as guest", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto(config.RETAIL_APP_HOME);
-  });
+test("Guest shopper can checkout items as guest", async ({ page }) => {
+  await page.goto(config.RETAIL_APP_HOME);
 
-  test("Guest shopper can add items to cart", async ({ page }) => {
-    await page.getByLabel("Menu").click();
+  await page.getByLabel("Menu").click();
 
-    // SSR nav loads top level categories as direct links so we wait till all sub-categories load in the accordion
-    const categoryAccordion = page.locator("#category-nav .chakra-accordion__button svg+:text('Womens')")
-    await categoryAccordion.waitFor();
+  // SSR nav loads top level categories as direct links so we wait till all sub-categories load in the accordion
+  const categoryAccordion = page.locator(
+    "#category-nav .chakra-accordion__button svg+:text('Womens')"
+  );
+  await categoryAccordion.waitFor();
 
-    await page.getByRole("button", { name: "Womens" }).click();
+  await page.getByRole("button", { name: "Womens" }).click();
 
-    // Wait for accoridion open animation to end
-    await page.waitForLoadState('domcontentloaded')
+  // Wait for accoridion open animation to end
+  await page.waitForLoadState("domcontentloaded");
 
-    await page.getByRole("button", { name: "Clothing" }).click();
+  await page.getByRole("button", { name: "Clothing" }).click();
 
-    await page.getByRole("link", { name: "Dresses" }).click();
+  await page.getByRole("link", { name: "Dresses" }).click();
 
-    await expect(page.getByRole("heading", { name: "Dresses" })).toBeVisible();
+  await expect(page.getByRole("heading", { name: "Dresses" })).toBeVisible();
 
-    await page.getByRole("link", { name: /Drape Neck Dress/i }).click();
+  await page.getByRole("link", { name: /Drape Neck Dress/i }).click();
 
-    await expect(
-      page.getByRole("heading", { name: /Drape Neck Dress/i })
-    ).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: /Drape Neck Dress/i })
+  ).toBeVisible();
 
-    await page.getByRole("radio", { name: "M", exact: true }).click();
+  await page.getByRole("radio", { name: "M", exact: true }).click();
 
-    await page.getByRole("button", { name: "+" }).click();
+  await page.getByRole("button", { name: "+" }).click();
 
-    // Selected Size and Color texts are broken into multiple elements on the page.
-    // So we need to look at the page URL to verify selected variants
-    const updatedPageURL = await page.url();
-    const params = updatedPageURL.split("?")[1];
-    expect(params).toMatch(/size=9MD/i);
-    expect(params).toMatch(/color=JJ3WDXX/i);
+  // Selected Size and Color texts are broken into multiple elements on the page.
+  // So we need to look at the page URL to verify selected variants
+  const updatedPageURL = await page.url();
+  const params = updatedPageURL.split("?")[1];
+  expect(params).toMatch(/size=9MD/i);
+  expect(params).toMatch(/color=JJ3WDXX/i);
 
-    await page.getByRole("button", { name: /Add to Cart/i }).click();
+  await page.getByRole("button", { name: /Add to Cart/i }).click();
 
-    const addedToCartModal = page.locator(
-      "[role='dialog'] heading:has-text('2 items added to cart')"
-    );
+  const addedToCartModal = page.locator(
+    "[role='dialog'] heading:has-text('2 items added to cart')"
+  );
 
-    page.on("dialog", async (_) => {
-      await expect(addedToCartModal).toBeVisible();
-    });
+  page.on("dialog", async (_) => {
+    await expect(addedToCartModal).toBeVisible();
   });
 });
