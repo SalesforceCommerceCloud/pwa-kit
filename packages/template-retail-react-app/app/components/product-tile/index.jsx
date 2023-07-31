df9ff43054c3bac0b6df56c2e356ba459306f73e
Vincent Marta <vmarta@salesforce.com>
2023-07-31 15:41:52 -0700

    Extract the product title

--- packages/template-retail-react-app/app/components/product-tile/index.jsx
+++ packages/template-retail-react-app/app/components/product-tile/index.jsx
@@ -21,6 +21,7 @@ import Link from '@salesforce/retail-react-app/app/components/link'
 import {useCurrency} from '@salesforce/retail-react-app/app/hooks'
 // TODO: do we need to sort by sections for these imports?
 import FavouriteButton from '@salesforce/retail-react-app/app/components/product-tile/favourite-button'
+import Title from '@salesforce/retail-react-app/app/components/product-tile/title'
 
 /**
  * The ProductTile is a simple visual representation of a
@@ -40,12 +41,6 @@ const ProductTile = (props) => {
     } = props
 
     const {currency, image, price, productId, hitType} = product
-
-    // ProductTile is used by two components, RecommendedProducts and ProductList.
-    // RecommendedProducts provides a localized product name as `name` and non-localized product
-    // name as `productName`. ProductList provides a localized name as `productName` and does not
-    // use the `name` property.
-    const localizedProductName = product.name ?? product.productName
 
     const {currency: activeCurrency} = useCurrency()
     const styles = useMultiStyleConfig('ProductTile')
@@ -68,9 +63,7 @@ const ProductTile = (props) => {
                 )}
             </Box>
 
-            {/* TODO: extract title */}
-            {/* Title */}
-            <Text {...styles.title}>{localizedProductName}</Text>
+            <Title product={product} />
 
             {/* TODO: extract price */}
             {/* Price */}
@@ -104,17 +97,7 @@ ProductTile.propTypes = {
             link: PropTypes.string
         }),
         price: PropTypes.number,
-        // `name` is present and localized when `product` is provided by a RecommendedProducts component
-        // (from Shopper Products `getProducts` endpoint), but is not present when `product` is
-        // provided by a ProductList component.
-        // See: https://developer.salesforce.com/docs/commerce/commerce-api/references/shopper-products?meta=getProducts
         name: PropTypes.string,
-        // `productName` is localized when provided by a ProductList component (from Shopper Search
-        // `productSearch` endpoint), but is NOT localized when provided by a RecommendedProducts
-        // component (from Einstein Recommendations `getRecommendations` endpoint).
-        // See: https://developer.salesforce.com/docs/commerce/commerce-api/references/shopper-search?meta=productSearch
-        // See: https://developer.salesforce.com/docs/commerce/einstein-api/references/einstein-api-quick-start-guide?meta=getRecommendations
-        // Note: useEinstein() transforms snake_case property names from the API response to camelCase
         productName: PropTypes.string,
         productId: PropTypes.string,
         hitType: PropTypes.string
---
+++ packages/template-retail-react-app/app/components/product-tile/title.jsx
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2023, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import {Text, useMultiStyleConfig} from '@salesforce/retail-react-app/app/components/shared/ui'
+
+const Title = ({product}) => {
+    // ProductTile is used by two components, RecommendedProducts and ProductList.
+    // RecommendedProducts provides a localized product name as `name` and non-localized product
+    // name as `productName`. ProductList provides a localized name as `productName` and does not
+    // use the `name` property.
+    const localizedProductName = product.name ?? product.productName
+
+    const styles = useMultiStyleConfig('ProductTile')
+
+    return <Text {...styles.title}>{localizedProductName}</Text>
+}
+
+Title.propTypes = {
+    product: PropTypes.shape({
+        // `name` is present and localized when `product` is provided by a RecommendedProducts component
+        // (from Shopper Products `getProducts` endpoint), but is not present when `product` is
+        // provided by a ProductList component.
+        // See: https://developer.salesforce.com/docs/commerce/commerce-api/references/shopper-products?meta=getProducts
+        name: PropTypes.string,
+        // `productName` is localized when provided by a ProductList component (from Shopper Search
+        // `productSearch` endpoint), but is NOT localized when provided by a RecommendedProducts
+        // component (from Einstein Recommendations `getRecommendations` endpoint).
+        // See: https://developer.salesforce.com/docs/commerce/commerce-api/references/shopper-search?meta=productSearch
+        // See: https://developer.salesforce.com/docs/commerce/einstein-api/references/einstein-api-quick-start-guide?meta=getRecommendations
+        // Note: useEinstein() transforms snake_case property names from the API response to camelCase
+        productName: PropTypes.string
+    })
+}
+
+export default Title
