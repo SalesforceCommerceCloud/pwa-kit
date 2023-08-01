3dd7b5e5caeee1cc3b741d74c9408a4b18c42186
Vincent Marta <vmarta@salesforce.com>
2023-08-01 13:01:05 -0700

    Generate a project as temporary playground

---
+++ packages/generated-project/overrides/app/pages/my-new-route/index.jsx
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import React from 'react'
+
+const MyNewRoute = () => {
+    return <h1 style={{textAlign: 'center', fontSize: '4rem'}}>hello new route!</h1>
+}
+
+export default MyNewRoute
---
+++ packages/generated-project/overrides/app/ssr.js
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2023, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+/* eslint-disable @typescript-eslint/no-var-requires */
+'use strict'
+
+const path = require('path')
+const {getRuntime} = require('@salesforce/pwa-kit-runtime/ssr/server/express')
+const {isRemote} = require('@salesforce/pwa-kit-runtime/utils/ssr-server')
+const {getConfig} = require('@salesforce/pwa-kit-runtime/utils/ssr-config')
+const helmet = require('helmet')
+
+const options = {
+    // The build directory (an absolute path)
+    buildDir: path.resolve(process.cwd(), 'build'),
+
+    // The cache time for SSR'd pages (defaults to 600 seconds)
+    defaultCacheTimeSeconds: 600,
+
+    // This is the value of the 'mobify' object from package.json
+    mobify: getConfig(),
+
+    // The port that the local dev server listens on
+    port: 3000,
+
+    // The protocol on which the development Express app listens.
+    // Note that http://localhost is treated as a secure context for development,
+    // except by Safari.
+    protocol: 'http'
+}
+
+const runtime = getRuntime()
+
+const {handler} = runtime.createHandler(options, (app) => {
+    // Set HTTP security headers
+    app.use(
+        helmet({
+            contentSecurityPolicy: {
+                useDefaults: true,
+                directives: {
+                    'img-src': ["'self'", '*.commercecloud.salesforce.com', 'data:'],
+                    'script-src': ["'self'", "'unsafe-eval'", 'storage.googleapis.com'],
+                    'connect-src': ["'self'", 'api.cquotient.com'],
+
+                    // Do not upgrade insecure requests for local development
+                    'upgrade-insecure-requests': isRemote() ? [] : null
+                }
+            },
+            hsts: isRemote()
+        })
+    )
+
+    // Handle the redirect from SLAS as to avoid error
+    app.get('/callback?*', (req, res) => {
+        res.send()
+    })
+    app.get('/robots.txt', runtime.serveStaticFile('static/robots.txt'))
+    app.get('/favicon.ico', runtime.serveStaticFile('static/ico/favicon.ico'))
+
+    app.get('/worker.js(.map)?', runtime.serveServiceWorker)
+    app.get('*', runtime.render)
+})
+// SSR requires that we export a single handler function called 'get', that
+// supports AWS use of the server that we created above.
+exports.get = handler
