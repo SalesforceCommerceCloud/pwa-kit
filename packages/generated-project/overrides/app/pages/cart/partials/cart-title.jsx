5609439435e88d285c4d3ea5beac0451054f9c87
Vincent Marta <vmarta@salesforce.com>
2023-08-07 22:00:17 -0700

    Explore alternative way without template hooks

---
+++ packages/generated-project/overrides/app/pages/cart/partials/cart-title.jsx
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2023, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import React from 'react'
+import OriginalCartTitle from '@salesforce/retail-react-app/app/pages/cart/partials/cart-title'
+
+const CartTitle = () => {
+    return (
+        <>
+            <div>Above Cart Title</div>
+            <OriginalCartTitle />
+        </>
+    )
+}
+
+export default CartTitle
