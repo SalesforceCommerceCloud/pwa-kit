7fc2caf79935f543d887cef3a813e54a2bc837e4
Will Harney <wharney@salesforce.com>
2023-08-31 13:28:36 -0400

    Use parent origin when loading client script.

--- packages/pwa-kit-react-sdk/src/ssr/universal/hooks/storefront-preview/index.jsx
+++ packages/pwa-kit-react-sdk/src/ssr/universal/hooks/storefront-preview/index.jsx
@@ -15,14 +15,36 @@ const TRUSTED_ORIGINS = [
     'https://runtime-admin-preview.mobify-storefront.com'
 ]
 
+/** Detects whether the storefront is running in an iframe. */
+const detectInIframe = () => typeof window !== 'undefined' && window.parent !== window.self
+
+/** Gets the parent origin when running in an iframe. */
+const getParentOrigin = () => {
+    if (detectInIframe()) {
+        if (window.location.ancestorOrigins) return window.location.ancestorOrigins[0]
+        // ancestorOrigins does not exist in Firefox, so we use referrer as a fallback
+        if (document.referrer) return new URL(document.referrer).origin
+    }
+}
+
+const isParentOriginTrusted = (parentOrigin) => {
+    return window.location.hostname === 'localhost'
+        ? parentOrigin === 'http://localhost:4000' // Development
+        : TRUSTED_ORIGINS.includes(parentOrigin) // Production
+}
+
 /** Detects whether the storefront is running in an iframe as part of Storefront Preview. */
 const detectStorefrontPreview = () => {
-    if (typeof window === 'undefined' || window.parent === window.self) return false
-    const parentOrigin = window.location.ancestorOrigins?.[0] ?? new URL(document.referrer).origin
-    if (window.location.hostname === 'localhost') {
-        return parentOrigin === 'http://localhost:4000'
-    }
-    return TRUSTED_ORIGINS.includes(parentOrigin)
+    const parentOrigin = getParentOrigin()
+    return Boolean(parentOrigin) && isParentOriginTrusted(parentOrigin)
+}
+
+/** Returns the URL to load the Storefront Preview client script from the parent origin. */
+const getClientScript = () => {
+    const parentOrigin = getParentOrigin() ?? 'https://runtime.commercecloud.com'
+    return parentOrigin === 'http://localhost:4000'
+        ? 'http://localhost:4000/mobify/bundle/development/static/storefront-preview.js'
+        : `${parentOrigin}/cc/b2c/preview/preview.client.js`
 }
 
 /**
@@ -34,14 +56,11 @@ const detectStorefrontPreview = () => {
  */
 export const useStorefrontPreview = (customizations = {}, enabled = detectStorefrontPreview()) => {
     const [, forceUpdate] = useReducer((x) => x + 1, 0)
-    const defaults = {
-        rerender: () => forceUpdate()
-    }
 
     useEffect(() => {
         if (enabled) {
             window.STOREFRONT_PREVIEW = {
-                ...defaults,
+                rerender: () => forceUpdate(),
                 ...window.STOREFRONT_PREVIEW,
                 ...customizations
             }
@@ -52,15 +71,10 @@ export const useStorefrontPreview = (customizations = {}, enabled = detectStoref
         }
     }, [enabled, forceUpdate])
 
-    const clientScript =
-        process.env.NODE_ENV === 'development'
-            ? 'http://localhost:4000/mobify/bundle/development/static/storefront-preview.js'
-            : 'https://runtime.commercecloud.com/cc/b2c/preview/preview.client.js'
-
     const StorefrontPreview = () =>
         enabled && (
             <Helmet>
-                <script src={clientScript} type="text/javascript"></script>
+                <script src={getClientScript()} type="text/javascript"></script>
             </Helmet>
         )
 
