fe482d8401e271feb6ccea3eab4ab49e71ef6ea3
Adam Raya Navarro <arayanavarro@salesforce.com>
2023-09-06 16:44:23 -0700

    Use runtime-admin-preview.commercecloud.com

--- packages/template-retail-react-app/app/components/_app/index.jsx
+++ packages/template-retail-react-app/app/components/_app/index.jsx
@@ -153,7 +153,7 @@ const App = (props) => {
     const storefrontPreviewClientScript =
         process.env.NODE_ENV === 'development'
             ? 'http://localhost:4000/mobify/bundle/development/static/storefront-preview.js'
-            : 'https://runtime.commercecloud.com/cc/b2c/preview/preview.client.js'
+            : 'https://runtime-admin-preview.commercecloud.com/cc/b2c/preview/preview.client.js'
 
     return (
         <Box className="sf-app" {...styles.container}>
