1e337d5052ffd6a2878b1b87dd1daa80307b6a75
Will Harney <wharney@salesforce.com>
2023-09-21 14:23:37 -0400

    Add utils to detect local storage and cookies.

--- packages/commerce-sdk-react/src/utils.ts
+++ packages/commerce-sdk-react/src/utils.ts
@@ -5,6 +5,7 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
+import Cookies, {CookieAttributes} from 'js-cookie'
 import {IFRAME_HOST_ALLOW_LIST} from './constant'
 
 /**
@@ -13,6 +14,7 @@ import {IFRAME_HOST_ALLOW_LIST} from './constant'
 export const onClient = (): boolean => typeof window !== 'undefined'
 
 export const getCookieSameSiteAttribute = () => {
+    if (!onClient()) return
     // document.location?.ancestorOrigins?.[0] will provide the parent host url, but it only works for Chrome and Safari.
     // Firefox does not have this field. document.referrer is common use for parent url, but it could be less reliable.
     // It is best to use it a fallback option for Firefox
@@ -22,4 +24,40 @@ export const getCookieSameSiteAttribute = () => {
     const isLocalHost = window.location.hostname === 'localhost'
 
     return !isLocalHost && isParentSiteTrusted ? 'none' : 'Lax'
+}
+
+/**
+ * Determines whether local storage is available by trying to set a test value.
+ */
+export function detectLocalStorageAvailable(): boolean {
+    if (typeof window === 'undefined') return false
+    try {
+        // If `localStorage` is not available, simply accessing the property will throw an error
+        window.localStorage
+        return true
+    } catch {
+        return false
+    }
+}
+
+/**
+ * Determines whether cookies are available by trying to set a test value.
+ */
+export function detectCookiesAvailable() {
+    if (typeof document === 'undefined') return false
+    const testKey = 'commerce-sdk-react-temp'
+    const testValue = '1'
+    const options: CookieAttributes = {
+        // These options MUST match what is used by CookieStorage for this test to be reliable
+        secure: !onClient() || window.location.protocol === 'https:',
+        sameSite: getCookieSameSiteAttribute()
+    }
+    try {
+        Cookies.set(testKey, testValue, options)
+        const success = Cookies.get(testKey) === testValue
+        Cookies.remove(testKey, options)
+        return success
+    } catch {
+        return false
+    }
 }
