faf3e560752bf15038b18215b92c56b0154a7af4
Ashutosh Raval <ashutosh.raval@salesforce.com>
2023-10-11 20:55:35 -0700

    W-13969523: address review feedback

--- packages/template-retail-react-app/app/hooks/use-active-data.js
+++ packages/template-retail-react-app/app/hooks/use-active-data.js
@@ -3,95 +3,100 @@ import {ACTIVE_DATA_ENABLE} from "@salesforce/retail-react-app/app/constants";
 const useActiveData = () => {
     return {
         async sendViewProduct(category, product, type) {
-            if (ACTIVE_DATA_ENABLE == 1) {
-                try {
-                    if (dw.ac) {
-                        if (category && category.id) {
-                            console.log("Applying category context")
-                            dw.ac.applyContext({category: category.id});
-                        }
-                        if (product && product.id) {
-                            console.log("Capturing product")
-                            dw.ac._capture({id: product.id, type: type});
-                        }
+            if (ACTIVE_DATA_ENABLE == 0) {
+                return;
+            }
+            try {
+                if (dw.ac) {
+                    if (category && category.id) {
+                        console.log("Applying category context");
+                        dw.ac.applyContext({category: category.id});
+                    }
+                    if (product && product.id) {
+                        console.log("Capturing product");
+                        dw.ac._capture({id: product.id, type: type});
+                    }
+                }
+                if (typeof dw.ac._scheduleDataSubmission === "function") {
+                    dw.ac._scheduleDataSubmission();
+                }
+            } catch (err) {
+                console.error(err);
+            }
+
+        },
+        async sendViewSearch(searchParams, productSearchResult) {
+            if (ACTIVE_DATA_ENABLE == 0) {
+                return
+            }
+            try {
+                console.log("Setting DW Search Context");
+                if (dw.ac) {
+                    console.log("Applying search context");
+                    dw.ac.applyContext({searchData: searchParams});
+                    if (typeof dw.ac._scheduleDataSubmission === "function") {
+                        dw.ac._scheduleDataSubmission();
+                    }
+                    productSearchResult.hits.map((productSearchItem) => {
+                        dw.ac._capture({id: productSearchItem.productId, type: "searchhit"});
+                    })
+                }
+            } catch (err) {
+                console.error(err);
+            }
+        },
+        async sendViewCategory(searchParams, category, productSearchResult) {
+            if (ACTIVE_DATA_ENABLE == 0) {
+                return;
+            }
+            try {
+                console.log("Set DW Category Context");
+                if (dw.ac) {
+                    if (category && category.id) {
+                        console.log("Applying category context");
+                        dw.ac.applyContext({category: category.id, searchData: searchParams});
                     }
                     if (typeof dw.ac._scheduleDataSubmission === "function") {
                         dw.ac._scheduleDataSubmission();
                     }
-                } catch (err) {
-                    console.log("Error capturing product - " + err)
+                    productSearchResult.hits.map((productSearchItem) => {
+                        dw.ac._capture({id: productSearchItem.productId, type: "searchhit"});
+                    })
                 }
-            }
-        },
-        async sendViewSearch(searchParams, productSearchResult) {
-            if (ACTIVE_DATA_ENABLE == 1) {
-                try {
-                    console.log("Setting DW Search Context")
-                    if (dw.ac) {
-                        console.log("Applying search context")
-                        dw.ac.applyContext({searchData: searchParams});
-                        if (typeof dw.ac._scheduleDataSubmission === "function") {
-                            dw.ac._scheduleDataSubmission();
-                        }
-                        productSearchResult.hits.map((productSearchItem) => {
-                            dw.ac._capture({id: productSearchItem.productId, type: "searchhit"});
-                        })
-                    }
-                } catch (err) {
-                    console.log("Error submitting search data - " + err)
-                }
-            }
-        },
-        async sendViewCategory(searchParams, category, productSearchResult) {
-            if (ACTIVE_DATA_ENABLE == 1) {
-                try {
-                    console.log("Set DW Category Context")
-                    if (dw.ac) {
-                        if (category && category.id) {
-                            console.log("Applying category context")
-                            dw.ac.applyContext({category: category.id, searchData: searchParams});
-                        }
-                        if (typeof dw.ac._scheduleDataSubmission === "function") {
-                            dw.ac._scheduleDataSubmission();
-                        }
-                        productSearchResult.hits.map((productSearchItem) => {
-                            dw.ac._capture({id: productSearchItem.productId, type: "searchhit"});
-                        })
-                    }
-                } catch (err) {
-                    console.log("Error submitting category data - " + err)
+            } catch (err) {
+                console.error(err);
 
-                }
             }
         },
         async trackPage(siteId, localeId, currency) {
-            if (ACTIVE_DATA_ENABLE == 1) {
-                try {
-                    var activeDataUrl =
-                        '/mobify/proxy/ocapi/on/demandware.store/Sites-' +
-                        siteId +
-                        '-Site/' +
-                        localeId +
-                        '/__Analytics-Start'
-                    console.log('Tracking enabled for ' + activeDataUrl)
-                    var dwAnalytics = dw.__dwAnalytics.getTracker(activeDataUrl);
-                    if (typeof dw.ac == "undefined") {
-                        console.log('Tracking page view')
-                        dwAnalytics.trackPageView();
-                    } else {
-                        try {
-                            if (typeof dw.ac._setSiteCurrency === "function") {
-                                dw.ac._setSiteCurrency(currency);
-                            }
-                        } catch (err) {
-                            console.log(err)
+            if (ACTIVE_DATA_ENABLE == 0) {
+                return;
+            }
+            try {
+                var activeDataUrl =
+                    '/mobify/proxy/ocapi/on/demandware.store/Sites-' +
+                    siteId +
+                    '-Site/' +
+                    localeId +
+                    '/__Analytics-Start';
+                console.log('Tracking enabled for ' + activeDataUrl);
+                var dwAnalytics = dw.__dwAnalytics.getTracker(activeDataUrl);
+                if (typeof dw.ac == "undefined") {
+                    console.log('Tracking page view');
+                    dwAnalytics.trackPageView();
+                } else {
+                    try {
+                        if (typeof dw.ac._setSiteCurrency === "function") {
+                            dw.ac._setSiteCurrency(currency);
                         }
-                        console.log('Setting DW Analytics')
-                        dw.ac.setDWAnalytics(dwAnalytics);
+                    } catch (err) {
+                        console.error(err);
                     }
-                } catch (err) {
-                    console.log("Error setting DW Analytics context - " + err)
+                    console.log('Setting DW Analytics');
+                    dw.ac.setDWAnalytics(dwAnalytics);
                 }
+            } catch (err) {
+                console.error(err);
             }
         }
     }
