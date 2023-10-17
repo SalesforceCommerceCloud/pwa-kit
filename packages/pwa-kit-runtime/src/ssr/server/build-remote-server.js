e861f31258669b605fbbaa83c169d0080cf47642
Will Harney <wharney@salesforce.com>
2023-10-17 14:08:17 -0400

    Move `upgrade-insecure-requests` into required directives.

--- packages/pwa-kit-runtime/src/ssr/server/build-remote-server.js
+++ packages/pwa-kit-runtime/src/ssr/server/build-remote-server.js
@@ -950,14 +950,17 @@ export const enforceSecurityHeaders = (req, res, next) => {
     // localhost doesn't include a protocol because different browsers behave differently :\
     const runtimeAdmin = isRemote() ? 'https://runtime.commercecloud.com' : 'localhost:*'
     /**
-     * Map of directive names/values that are required for PWA Kit to work.
-     * @type Object.<string, string[]>
+     * Map of directive names/values that are required for PWA Kit to work. Array values will be
+     * merged with user-provided values; boolean values will replace user-provided values.
+     * @type Object.<string, string[] | boolean>
      */
     const directives = {
         'connect-src': ["'self'", 'api.cquotient.com', runtimeAdmin],
         'frame-ancestors': [runtimeAdmin],
         'img-src': ["'self'", '*.commercecloud.salesforce.com', 'data:'],
-        'script-src': ["'self'", "'unsafe-eval'", runtimeAdmin]
+        'script-src': ["'self'", "'unsafe-eval'", runtimeAdmin],
+        // Always upgrade insecure requests when deployed, never upgrade on local dev server
+        'upgrade-insecure-requests': isRemote()
     }
 
     const setHeader = res.setHeader
@@ -970,8 +973,8 @@ export const enforceSecurityHeaders = (req, res, next) => {
                 // ensure that our required directives will work as expected.
                 // Ref: https://w3c.github.io/webappsec-csp/#multiple-policies
                 modifiedValue = Array.isArray(value)
-                    ? value.map((item) => addRequiredDirectives(item, directives))
-                    : addRequiredDirectives(value, directives)
+                    ? value.map((item) => modifyDirectives(item, directives))
+                    : modifyDirectives(value, directives)
                 break
             }
             case STRICT_TRANSPORT_SECURITY: {
@@ -1000,7 +1003,7 @@ export const enforceSecurityHeaders = (req, res, next) => {
  * @returns {string} Modified Content-Security-Policy header
  * @private
  */
-const addRequiredDirectives = (original, required) => {
+const modifyDirectives = (original, required) => {
     const directives = original
         .trim()
         .split(';')
@@ -1012,17 +1015,22 @@ const addRequiredDirectives = (original, required) => {
             }
             return acc
         }, {})
+
     // Add missing required CSP directives
-    for (const [name, values] of Object.entries(required)) {
-        // Wrapping with `[...new Set(array)]` removes duplicate entries
-        directives[name] = [...new Set([...(directives[name] ?? []), ...values])]
+    for (const [name, value] of Object.entries(required)) {
+        if (value === true) {
+            // Boolean directive (required) - overwrite original value
+            directives[name] = []
+        } else if (value === false) {
+            // Boolean directive (disabled) - delete original value
+            delete directives[name]
+        } else {
+            // Regular string[] directive - merge values
+            // Wrapping with `[...new Set(array)]` removes duplicate entries
+            directives[name] = [...new Set([...(directives[name] ?? []), ...value])]
+        }
     }
-    // Always upgrade insecure requests when deployed, never upgrade on local dev server
-    if (isRemote()) {
-        directives['upgrade-insecure-requests'] = []
-    } else {
-        delete directives['upgrade-insecure-requests']
-    }
+
     // Re-construct header string
     return Object.entries(directives)
         .map(([name, values]) => [name, ...values].join(' '))
