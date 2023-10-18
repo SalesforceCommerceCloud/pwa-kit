885478428cfe0dd1bea0113eab7d322ab89059f5
Will Harney <wharney@salesforce.com>
2023-10-18 11:04:01 -0400

    Update generated ssr.js

--- packages/pwa-kit-create-app/assets/bootstrap/js/overrides/app/ssr.js
+++ packages/pwa-kit-create-app/assets/bootstrap/js/overrides/app/ssr.js
@@ -4,13 +4,13 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-/* eslint-disable @typescript-eslint/no-var-requires */
+
 'use strict'
 
-const path = require('path')
-const {getRuntime} = require('@salesforce/pwa-kit-runtime/ssr/server/express')
-const {getConfig} = require('@salesforce/pwa-kit-runtime/utils/ssr-config')
-const helmet = require('helmet')
+import path from 'path'
+import {getRuntime} from '@salesforce/pwa-kit-runtime/ssr/server/express'
+import {getConfig} from '@salesforce/pwa-kit-runtime/utils/ssr-config'
+import helmet from 'helmet'
 
 const options = {
     // The build directory (an absolute path)
@@ -19,7 +19,7 @@ const options = {
     // The cache time for SSR'd pages (defaults to 600 seconds)
     defaultCacheTimeSeconds: 600,
 
-    // This is the value of the 'mobify' object from package.json
+    // The contents of the config file for the current environment
     mobify: getConfig(),
 
     // The port that the local dev server listens on
@@ -42,9 +42,17 @@ const {handler} = runtime.createHandler(options, (app) => {
             contentSecurityPolicy: {
                 useDefaults: true,
                 directives: {
-                    scriptSrc: [
-                        // used by the service worker in /worker/main.js
+                    'img-src': [
+                        // Default source for product images - replace with your CDN
+                        '*.commercecloud.salesforce.com'
+                    ],
+                    'script-src': [
+                        // Used by the service worker in /worker/main.js
                         'storage.googleapis.com'
+                    ],
+                    'connect-src': [
+                        // Connect to Einstein APIs
+                        'api.cquotient.com'
                     ]
                 }
             }
@@ -66,4 +74,4 @@ const {handler} = runtime.createHandler(options, (app) => {
 })
 // SSR requires that we export a single handler function called 'get', that
 // supports AWS use of the server that we created above.
-exports.get = handler
+export const get = handler
