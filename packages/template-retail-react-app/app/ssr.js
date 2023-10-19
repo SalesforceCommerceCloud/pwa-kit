8323225300f15c90a1acca2f5297feecfee5178c
Will Harney <wharney@salesforce.com>
2023-10-19 14:42:40 -0400

    Move Content-Security-Policy header enforcement to pwa-kit-runtime.

--- packages/template-retail-react-app/app/ssr.js
+++ packages/template-retail-react-app/app/ssr.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, salesforce.com, inc.
+ * Copyright (c) 2023, Salesforce, Inc.
  * All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
@@ -8,9 +8,7 @@
 
 import path from 'path'
 import {getRuntime} from 'pwa-kit-runtime/ssr/server/express'
-import {isRemote} from 'pwa-kit-runtime/utils/ssr-server'
 import {getConfig} from 'pwa-kit-runtime/utils/ssr-config'
-import {getAppOrigin} from 'pwa-kit-react-sdk/utils/url'
 import helmet from 'helmet'
 
 const options = {
@@ -20,80 +18,43 @@ const options = {
     // The cache time for SSR'd pages (defaults to 600 seconds)
     defaultCacheTimeSeconds: 600,
 
-    // This is the value of the 'mobify' object from package.json
+    // The contents of the config file for the current environment
     mobify: getConfig(),
 
     // The port that the local dev server listens on
     port: 3000,
 
     // The protocol on which the development Express app listens.
-    // Note that http://localhost is treated as a secure context for development.
+    // Note that http://localhost is treated as a secure context for development,
+    // except by Safari.
     protocol: 'http'
 }
 
 const runtime = getRuntime()
 
 const {handler} = runtime.createHandler(options, (app) => {
-    const getRuntimeEnv = () => {
-        if (process.env.NODE_ENV !== 'production') return process.env.NODE_ENV ?? 'development'
-        const origin = getAppOrigin()
-        // mobify-storefront-staging sites have NODE_ENV set to production, but for the purposes
-        // of CSP we consider the sites to be staging.
-        return origin.endsWith('.mobify-storefront-staging.com') ? 'staging' : 'production'
-    }
-
-    // This is a temporary solution while we work on Storefront Preview - a full solution will
-    // prevent required CSP headers from being modified.
-    const getCSP = () => {
-        const trustedMap = {
-            development: [
-                'localhost:*',
-                '*.commercecloud.salesforce.com',
-                '*.demandware.net',
-                '*.mobify-staging.com',
-                '*.mobify-storefront-staging.com',
-                '*.mobify-storefront.com',
-                'runtime.commercecloud.com'
-            ],
-            staging: [
-                '*.demandware.net',
-                '*.mobify-staging.com',
-                '*.mobify-storefront-staging.com',
-                '*.mobify-storefront.com',
-                '*.commercecloud.salesforce.com',
-                'runtime.commercecloud.com'
-            ],
-            production: [
-                '*.demandware.com',
-                '*.mobify.com',
-                '*.mobify-storefront.com',
-                '*.commercecloud.salesforce.com',
-                'runtime.commercecloud.com'
-            ]
-        }
-
-        const env = getRuntimeEnv()
-        const trusted = ["'self'", ...(trustedMap[env] ? trustedMap[env] : [])]
-
-        return {
-            'connect-src': ['api.cquotient.com', ...trusted],
-            'frame-ancestors': [...trusted],
-            'img-src': ['data:', ...trusted],
-            'script-src': ["'unsafe-eval'", 'storage.googleapis.com', ...trusted],
-
-            // Do not upgrade insecure requests for local development
-            'upgrade-insecure-requests': isRemote() ? [] : null
-        }
-    }
-
     // Set HTTP security headers
     app.use(
         helmet({
+            // pwa-kit-runtime ensures that the Content-Security-Policy header always contains the
+            // directives required for PWA Kit to function. Add custom directives here.
             contentSecurityPolicy: {
                 useDefaults: true,
-                directives: getCSP()
-            },
-            hsts: isRemote()
+                directives: {
+                    'img-src': [
+                        // Default source for product images - replace with your CDN
+                        '*.commercecloud.salesforce.com'
+                    ],
+                    'script-src': [
+                        // Used by the service worker in /worker/main.js
+                        'storage.googleapis.com'
+                    ],
+                    'connect-src': [
+                        // Connect to Einstein APIs
+                        'api.cquotient.com'
+                    ]
+                }
+            }
         })
     )
 
