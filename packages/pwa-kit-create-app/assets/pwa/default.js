104f61097e92d14cafda3ffcf6121b20b9d82985
Adam Raya <adamraya@users.noreply.github.com>
2023-11-03 11:24:17 -0700

    Update `ssrFunctionNodeVersion` to Node 18 (#1521)

--- packages/pwa-kit-create-app/assets/pwa/default.js
+++ packages/pwa-kit-create-app/assets/pwa/default.js
@@ -65,7 +65,7 @@ module.exports = {
     ],
     // Additional parameters that configure Express app behavior.
     ssrParameters: {
-        ssrFunctionNodeVersion: '16.x',
+        ssrFunctionNodeVersion: '18.x',
         proxyConfigs: [
             {
                 host: '${commerceApi.shortCode}.api.commercecloud.salesforce.com',
