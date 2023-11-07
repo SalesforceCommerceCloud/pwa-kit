1e35ba1fccad562890c1b6e93dad960c6d2907d2
Kevin He <kevin.he@salesforce.com>
2023-11-07 14:04:39 -0800

    add list of egpt components

--- packages/commerce-sdk-react/src/components/ShopperExperience/Component/index.tsx
+++ packages/commerce-sdk-react/src/components/ShopperExperience/Component/index.tsx
@@ -17,6 +17,100 @@ type ComponentProps = {
 const ComponentNotFound = ({typeId}: ComponentType) => (
     <div>{`Component type '${typeId}' not found!`}</div>
 )
+
+// The main use case for the JSX parser is to leverage eGPT Generative Components
+// from page designer. At the moment, only Chakra UI components are supported. There isn't
+// a good place to put this list of required components, so we'll just put it here for now.
+// In the future, we need to have a place to share the list.
+export const REQUIRED_EGPT_JSX_COMPONENTS = [
+    './Accordion',
+    './AccordionButton',
+    './AccordionIcon',
+    './AccordionItem',
+    './AccordionPanel',
+    './Alert',
+    './AlertDialog',
+    './AlertDialogBody',
+    './AlertDialogContent',
+    './AlertDialogFooter',
+    './AlertDialogHeader',
+    './AlertDialogOverlay',
+    './AlertIcon',
+    './AlertTitle',
+    './AspectRatio',
+    './Badge',
+    './Box',
+    './Breadcrumb',
+    './BreadcrumbItem',
+    './BreadcrumbLink',
+    './Button',
+    './ButtonGroup',
+    './Center',
+    './ChakraProvider',
+    './Checkbox',
+    './CloseButton',
+    './Container',
+    './Divider',
+    './Drawer',
+    './DrawerBody',
+    './DrawerCloseButton',
+    './DrawerContent',
+    './DrawerFooter',
+    './DrawerHeader',
+    './DrawerOverlay',
+    './Fade',
+    './Flex',
+    './FormControl',
+    './FormErrorMessage',
+    './FormLabel',
+    './Grid',
+    './GridItem',
+    './HStack',
+    './Heading',
+    './Icon',
+    './IconButton',
+    './Image',
+    './Img',
+    './Input',
+    './InputGroup',
+    './InputLeftElement',
+    './InputRightElement',
+    './Link',
+    './List',
+    './ListItem',
+    './Modal',
+    './ModalBody',
+    './ModalCloseButton',
+    './ModalContent',
+    './ModalFooter',
+    './ModalHeader',
+    './ModalOverlay',
+    './Popover',
+    './PopoverArrow',
+    './PopoverBody',
+    './PopoverCloseButton',
+    './PopoverContent',
+    './PopoverFooter',
+    './PopoverHeader',
+    './PopoverTrigger',
+    './Portal',
+    './Radio',
+    './RadioGroup',
+    './Select',
+    './SimpleGrid',
+    './Skeleton',
+    './Spacer',
+    './Spinner',
+    './Stack',
+    './StackDivider',
+    './StylesProvider',
+    './Text',
+    './Tooltip',
+    './VStack',
+    './Wrap',
+    './WrapItem'
+]
+
 /**
  * This component will render a page designer page given its serialized data object.
  *
@@ -30,7 +124,14 @@ export const Component = ({component, code}: ComponentProps) => {
     const {data, ...rest} = component
     let instance = <ComponentNotFound {...rest} {...data} />
     if (code) {
-        instance = <JsxParser components={pageContext?.jsxParserComponents} jsx={code} />
+        instance = (
+            <JsxParser
+                components={pageContext?.jsxParserComponents}
+                jsx={code}
+                {...rest}
+                {...data}
+            />
+        )
     } else if (pageContext?.components[component.typeId]) {
         const ComponentClass = pageContext?.components[component.typeId]
         instance = <ComponentClass {...rest} {...data} />
