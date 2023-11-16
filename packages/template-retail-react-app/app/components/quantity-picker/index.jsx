e7ec6e952771b827add1c7836f29f22fa59f5c75
Will Harney <wharney@salesforce.com>
2023-11-16 13:04:52 -0500

    QuantityPicker accessibility improvements.
    1. Add aria-labels for the buttons
    2. Allow keyboard focus on the buttons
    3. Allow space/enter to trigger button click

--- packages/template-retail-react-app/app/components/quantity-picker/index.jsx
+++ packages/template-retail-react-app/app/components/quantity-picker/index.jsx
@@ -12,7 +12,7 @@ import {
     Input,
     useNumberInput
 } from '@salesforce/retail-react-app/app/components/shared/ui'
-import {useIntl} from 'react-intl'
+import {FormattedMessage, useIntl} from 'react-intl'
 
 /**
  * This is the mobile implementation of the Chakra NumberInput. This simple component essentially
@@ -45,8 +45,20 @@ const QuantityPicker = (props) => {
         }
     })
 
-    const inc = getIncrementButtonProps({variant: 'outline'})
-    const dec = getDecrementButtonProps({variant: 'outline'})
+    const inc = getIncrementButtonProps({
+        variant: 'outline',
+        'aria-label': intl.formatMessage({
+            defaultMessage: 'Increment Quantity',
+            id: 'product_view.label.aria.quantity_increment'
+        })
+    })
+    const dec = getDecrementButtonProps({
+        variant: 'outline',
+        'aria-label': intl.formatMessage({
+            defaultMessage: 'Decrement Quantity',
+            id: 'product_view.label.aria.quantity_decrement'
+        })
+    })
     const input = getInputProps({
         maxWidth: '44px',
         textAlign: 'center',
@@ -56,14 +68,39 @@ const QuantityPicker = (props) => {
         })
     })
 
+    // Accessibility improvements:
+    // 1. Allow keyboard focus on the buttons - Chakra overrides values passed to get*ButtonProps()
+    inc.tabIndex = ''
+    dec.tabIndex = ''
+    // 2. Allow Space or Enter key to trigger buttons
+    // Hitting space/enter triggers a "click" event, but the component listens for "mousedown".
+    // We can't reuse the buttons' onMouseDown handler, so instead we hijack the input's onKeyDown.
+    // @ref https://github.com/chakra-ui/chakra-ui/blob/%40chakra-ui/react%402.7.0/packages/components/number-input/src/use-number-input.ts#L333-L334
+    inc.onKeyDown = (evt) => {
+        if (evt.key === ' ' || evt.key === 'Enter') {
+            evt.key = 'ArrowUp'
+            input.onKeyDown(evt)
+        }
+    }
+    dec.onKeyDown = (evt) => {
+        if (evt.key === ' ' || evt.key === 'Enter') {
+            evt.key = 'ArrowDown'
+            input.onKeyDown(evt)
+        }
+    }
+
     return (
         <HStack>
             <Button data-testid="quantity-decrement" {...dec}>
-                -
+                {/* "-" is "hyphen", but we want "minus" */}
+                <FormattedMessage
+                    id="product_view.label.quantity_decrement"
+                    defaultMessage="&minus;"
+                />
             </Button>
             <Input {...input} />
             <Button data-testid="quantity-increment" {...inc}>
-                +
+                <FormattedMessage id="product_view.label.quantity_increment" defaultMessage="+" />
             </Button>
         </HStack>
     )
