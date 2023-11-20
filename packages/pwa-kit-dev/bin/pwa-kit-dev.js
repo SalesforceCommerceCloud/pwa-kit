e10a893aa1d0143c5528ee28c8af30a92e57c2e6
Ben Chypak <bchypak@salesforce.com>
2023-11-20 10:55:40 -0800

    Lint

--- packages/pwa-kit-dev/bin/pwa-kit-dev.js
+++ packages/pwa-kit-dev/bin/pwa-kit-dev.js
@@ -218,7 +218,7 @@ const main = async () => {
                 env: {
                     ...process.env,
                     ...(noHMR ? {HMR: 'false'} : {}),
-                    ...({WEBPACK_INSPECT: inspect})
+                    ...{WEBPACK_INSPECT: inspect}
                 }
             })
         })
