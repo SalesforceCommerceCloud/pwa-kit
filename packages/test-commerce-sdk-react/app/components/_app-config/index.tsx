c5a8101e08e5836e114c21b16d9d1cc83dd7b523
Ben Chypak <bchypak@salesforce.com>
2023-11-20 11:33:48 -0800

    Possible update to ts caused this lint issue

--- packages/test-commerce-sdk-react/app/components/_app-config/index.tsx
+++ packages/test-commerce-sdk-react/app/components/_app-config/index.tsx
@@ -25,8 +25,6 @@ const AppConfig = (props: AppConfigProps): ReactElement => {
     const anotherSite = siteId === defaultSiteId ? 'RefArch' : defaultSiteId
     const anotherLocale = locale === defaultLocale ? 'en-CA' : defaultLocale
     return (
-        // @ts-expect-error: Types of property 'key' are incompatible.
-        // Possible conflicting types due to duplicate @types/react installs
         <CommerceApiProvider
             siteId={siteId}
             shortCode="8o7m175y"
