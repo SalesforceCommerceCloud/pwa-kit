eb9171719a9d4b25e97818b0e740d5be2b791a7a
Will Harney <wharney@salesforce.com>
2023-12-05 13:03:25 -0500

    Export all hooks from index.

--- packages/template-retail-react-app/app/hooks/index.js
+++ packages/template-retail-react-app/app/hooks/index.js
@@ -1,17 +1,35 @@
 /*
- * Copyright (c) 2021, salesforce.com, inc.
+ * Copyright (c) 2023, Salesforce, Inc.
  * All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-
 /* istanbul ignore file */
-export {useSearchParams} from '@salesforce/retail-react-app/app/hooks/use-search-params'
-export {useLimitUrls} from '@salesforce/retail-react-app/app/hooks/use-limit-urls'
-export {usePageUrls} from '@salesforce/retail-react-app/app/hooks/use-page-urls'
-export {useSortUrls} from '@salesforce/retail-react-app/app/hooks/use-sort-urls'
-export {useVariant} from '@salesforce/retail-react-app/app/hooks/use-variant'
-export {useVariationAttributes} from '@salesforce/retail-react-app/app/hooks/use-variation-attributes'
-export {useVariationParams} from '@salesforce/retail-react-app/app/hooks/use-variation-params'
-export {useDerivedProduct} from '@salesforce/retail-react-app/app/hooks/use-derived-product'
-export {useCurrency} from '@salesforce/retail-react-app/app/hooks/use-currency'
+
+// This file should only re-export the hooks themselves. If users want to access providers/helpers,
+// they can do so by importing the specific file.
+export {default as useActiveData} from './use-active-data'
+export {useAddToCartModal, useAddToCartModalContext} from './use-add-to-cart-modal'
+export {useAuthModal} from './use-auth-modal'
+export {useCurrency} from './use-currency'
+export {useCurrentBasket} from './use-current-basket'
+export {useCurrentCustomer} from './use-current-customer'
+export {useDerivedProduct} from './use-derived-product'
+export {default as useEinstein} from './use-einstein'
+export {default as useIntersectionObserver} from './use-intersection-observer'
+export {useLimitUrls} from './use-limit-urls'
+export {default as useMultiSite} from './use-multi-site'
+export {default as useNavigation} from './use-navigation'
+export {usePageUrls} from './use-page-urls'
+export {usePDPSearchParams} from './use-pdp-search-params'
+export {usePrevious} from './use-previous'
+export {useProductViewModal} from './use-product-view-modal'
+export {useSearchParams} from './use-search-params'
+export {useSortUrls} from './use-sort-urls'
+export {useToast} from './use-toast'
+export {useVariant} from './use-variant'
+export {useVariationAttributes} from './use-variation-attributes'
+export {useVariationParams} from './use-variation-params'
+export {useWishList} from './use-wish-list'
+
+import * as constants from '@salesforce/retail-react-app/app/constants'
