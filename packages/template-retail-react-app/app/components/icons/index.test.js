75f75ae270f57397ddce02751964f85f2acd6d15
Joel Uong <joel.uong@salesforce.com>
2023-12-08 10:32:48 -0600

    add unit tests

--- packages/template-retail-react-app/app/components/icons/index.test.js
+++ packages/template-retail-react-app/app/components/icons/index.test.js
@@ -5,9 +5,20 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import React from 'react'
+import {useIntl} from 'react-intl'
 import {within} from '@testing-library/dom'
+import {render} from '@testing-library/react'
 import {renderWithProviders} from '@salesforce/retail-react-app/app/utils/test-utils'
 import * as Icons from '@salesforce/retail-react-app/app/components/icons/index'
+
+jest.mock('react-intl', () => ({
+    ...jest.requireActual('react-intl'),
+    useIntl: jest.fn()
+}))
+
+beforeEach(() => {
+    jest.clearAllMocks()
+})
 
 test('renders svg icons with Chakra Icon component', () => {
     renderWithProviders(<Icons.CheckIcon />)
@@ -17,4 +28,19 @@ test('renders svg icons with Chakra Icon component', () => {
     expect(use).toBeInTheDocument()
     expect(svg).toHaveAttribute('viewBox', '0 0 24 24')
     expect(use).toHaveAttribute('xlink:href', '#check')
+})
+
+// the icon component can exist outside the provider tree via the error component
+// therefore we cannot use the useIntl hook because the <IntlProvider> component
+// will not exist in the component tree, so we pass the intl object as a prop
+test('uses intl object from props and not from useIntl hook', () => {
+    const mockIntl = {
+        formatMessage: jest.fn()
+    }
+
+    // render without providers
+    render(<Icons.LockIcon intl={mockIntl} />)
+
+    expect(mockIntl.formatMessage).toHaveBeenCalled()
+    expect(useIntl).not.toHaveBeenCalled()
 })
