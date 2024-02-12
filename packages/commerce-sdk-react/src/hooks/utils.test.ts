a8bb7fa7ebcd896b7e6a84dbdc16c00e8b9cfab9
Alex Vuong <alex.vuong@salesforce.com>
2024-02-12 11:13:29 -0800

    add test for getCustom key

--- packages/commerce-sdk-react/src/hooks/utils.test.ts
+++ packages/commerce-sdk-react/src/hooks/utils.test.ts
@@ -6,7 +6,7 @@
  */
 
 import {ShopperBaskets} from 'commerce-sdk-isomorphic'
-import {mergeOptions} from './utils'
+import {mergeOptions, getCustomKeys} from './utils'
 
 describe('Hook utils', () => {
     test('mergeOptions merges body, header, and options', () => {
@@ -44,5 +44,28 @@ describe('Hook utils', () => {
                 optionsHeader: 'optionsHeader'
             }
         })
+    })
+})
+
+describe('getCustomerKey', function () {
+    test('returns empty array when input is not an object', () => {
+        const res = getCustomKeys([])
+        expect(res).toEqual([])
+    })
+
+    test('returns custom key c_ as output', () => {
+        const res = getCustomKeys({
+            some_key: 'hello',
+            c_key: 'custom key value',
+            c_custom: 'another value'
+        })
+        expect(res).toEqual(['c_key', 'c_custom'])
+    })
+
+    test('returns empty when there is no custom key in the input', () => {
+        const res = getCustomKeys({
+            some_key: 'hello'
+        })
+        expect(res).toEqual([])
     })
 })
