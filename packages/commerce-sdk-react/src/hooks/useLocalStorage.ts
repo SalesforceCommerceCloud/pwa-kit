28d23271ddf1b70fcc2164a3e609f4d4233d8695
vcua-mobify <vcua@salesforce.com>
2024-03-20 15:00:55 -0700

    Clean up the code

--- packages/commerce-sdk-react/src/hooks/useLocalStorage.ts
+++ packages/commerce-sdk-react/src/hooks/useLocalStorage.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import {useEffect, useState, useCallback, useSyncExternalStore} from 'react'
+import {useSyncExternalStore} from 'react'
 
 type Value = string | null
 
@@ -13,7 +13,6 @@ type Value = string | null
  *
  */
 function useLocalStorage(key: string): Value {
-
     const readValue = (): Value => {
         // TODO: Use detectLocalStorageAvailable when app can better handle clients without storage
         if (typeof window === 'undefined') {
@@ -26,46 +25,20 @@ function useLocalStorage(key: string): Value {
     const useLocalStorageSubscribe = (callback: any) => {
         window.addEventListener('storage', callback)
         return () => window.removeEventListener('storage', callback)
-    };
+    }
 
     const getLocalStorageServerSnapshot = () => {
         // local store is not available on the server
         return null
-    };
+    }
 
-    // function dispatchStorageEvent(newValue: Value) {
-    //     window.dispatchEvent(new StorageEvent("storage", { key, newValue }));
-    // }
-
-    const getSnapshot = () => readValue();
+    const getLocalStorageSnapshot = () => readValue()
 
     const store: Value = useSyncExternalStore(
         useLocalStorageSubscribe,
-        getSnapshot,
+        getLocalStorageSnapshot,
         getLocalStorageServerSnapshot
-    );
-
-    // const setStoredValue = useCallback(() => {
-    //     try {
-    //         const nextState = readValue()
-    //         if (nextState === null) {
-    //             window.localStorage.removeItem(key);
-    //         } else {
-    //             window.localStorage.setItem(key, nextState);
-    //         }
-    //     } catch (e) {
-    //         console.warn(e);
-    //     }
-    // }, [key, store])
-
-    // const handleStorageChange = (event: StorageEvent) => {
-    //     if (event.key !== key) {
-    //         return
-    //     }
-    //     setStoredValue()
-    // }
-
-    // const [storedValue, setStoredValue] = useState<Value>(readValue)
+    )
 
     return store
 }
