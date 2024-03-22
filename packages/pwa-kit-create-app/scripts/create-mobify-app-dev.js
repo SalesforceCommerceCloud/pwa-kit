bb492996644150d9ba8139ced63e26c51a38498c
Jainam Tushar Sheth <j.sheth@salesforce.com>
2024-03-22 18:17:00 -0400

    lerna force publish all packages

--- packages/pwa-kit-create-app/scripts/create-mobify-app-dev.js
+++ packages/pwa-kit-create-app/scripts/create-mobify-app-dev.js
@@ -104,7 +104,7 @@ const withLocalNPMRepo = (func) => {
             // packages to it. This is safe to do – Verdaccio does not forward these
             // the public NPM repo.
             console.log('Publishing packages to the local NPM repository')
-            sh.exec('npm run lerna -- publish from-package force-publish=* --yes --concurrency 1 --loglevel warn', {
+            sh.exec('npm run lerna -- publish from-package --yes --concurrency 1 --loglevel warn --force-publish=*', {
                 cwd: monorepoRoot,
                 fatal: true,
                 silent: false
