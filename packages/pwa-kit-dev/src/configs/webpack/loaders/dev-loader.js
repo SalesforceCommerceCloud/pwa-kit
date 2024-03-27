b3d1960528d04f8c0d9b54bf794473afaaafabcf
Ben Chypak <bchypak@salesforce.com>
2024-03-27 08:48:37 -0700

    Add some docs to the dev loader

--- packages/pwa-kit-dev/src/configs/webpack/loaders/dev-loader.js
+++ packages/pwa-kit-dev/src/configs/webpack/loaders/dev-loader.js
@@ -1,9 +1,20 @@
+// NOTE: This loader should only be applied when in develop mode, and there should be a cli option to opt out.
 module.exports = function (source) {
     
-    
-
-    const getNewCode = (moduleName, filePath) => `
-        
+    // NOTE: This probably isn't how we'll end up doing this in production. What we will want it a template
+    // that we can import and apply with provided data (module name, component implementation string, etc)
+    //
+    // It should provide the following:
+    //
+    // 1. When a hovered we need to show the components information, including the path to it's implementation,
+    //    a link to it's docs in github, description (via jsdoc or something), and a link to eject the code into
+    //    the current project.
+    // 2. It should visually communicate the boundaries of the component by either adding a border or using a zoom
+    //    effect to raise the component.
+    // 3. It would be nice to have a separate wrapper/enhancer template to wrap the entire application to allow us to
+    //    turn on/off this developer mode so it doesn't get annoying if someone want to see what the project will look
+    //    like in production. 
+    const componentWrapper = ({moduleName, filePath}) => `
         export default (props) => {
             const [hidden, setHidden] = React.useState(true)
             const [style, setStyle] = React.useState({})
@@ -49,11 +60,20 @@ module.exports = function (source) {
             )
         }
     `
+    const enhanceSourceWith = (template, data) => template(data)
 
+    // NOTE: For the actual implementation we'll want to have a generic set of rules that we use to wrap our components.
+    //
+    // There will be some criteria that nees to pass first.
+    //
+    // 1. The file must be either tsx or jsx
+    // 2. It must have a single export.
+    // 3. Ideally we know if it's overridable (e.g. it is imported via the wildcard import syntax) (hard)
     const map = {
         ['home.tsx']: 'Home',
         ['account.tsx']: 'Account',
         ['product-detail.tsx']: 'ProductDetail',
+        ['product-list.tsx']: 'ProductList',
         ['hello-typescript.tsx']: 'HelloTS',
         ['store-finder.tsx']: 'StoreFinder'
     }
@@ -62,10 +82,17 @@ module.exports = function (source) {
     const fileName = this.resourcePath.split('/').pop()
     const moduleName = map[fileName]
     if (moduleName) {
-        // console.log('RESOURCE PATH:', this.resourcePath)
-        // console.log('SOURCE: ', source)
-        source = source.replace(/export default ([A-Z]\w+)/, getNewCode(moduleName, this.resourcePath))
-        // console.log('NEW SOURCE: ', source)
+        console.log('Enhancing Module: ', moduleName)
+        source = source.replace(
+            /export default ([A-Z]\w+)/, 
+            enhanceSourceWith(
+                componentWrapper, 
+                {
+                    moduleName, 
+                    filePath: this.resourcePath
+                }
+            )
+        )
     }
     return source
 }
