40ad6b8d1cb7b0375e83756d96c4ea49c080c70b
Ben Chypak <bchypak@salesforce.com>
2024-04-01 13:15:59 -0700

    Force discover pages extension to work
    We don't resolve into the sdk yet.. adding the app and app-config file defaults in this extension ensures it works.

---
+++ packages/extension-retail-react-app-discovery-pages/app/components/_app/index.jsx
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import React from 'react'
+import PropTypes from 'prop-types'
+
+/**
+ * @module progressive-web-sdk/ssr/universal/components/_app
+ */
+
+/**
+ * The PWAApp is a special component that will be overridden in your project. Its
+ * main purpose is to provide the layout for your application as well as set up any
+ * global actions and data fetching via the `getProps` method. Typically this is
+ * where you will set up components like your application header, footer and
+ * navigation. The contents of this component are project specific, but the rule
+ * of thumb is that anything outside of your pages will be added here.
+ */
+const App = (props) => <div id="app">{props.children}</div>
+
+App.propTypes = {
+    children: PropTypes.element.isRequired
+}
+
+export default App
