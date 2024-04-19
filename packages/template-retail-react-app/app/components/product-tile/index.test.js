72675f2ee47004b7ee02a1627762a143b5322bd1
Alex Vuong <alex.vuong@salesforce.com>
2024-04-19 11:17:10 -0700

    fix tests

--- packages/template-retail-react-app/app/components/product-tile/index.test.js
+++ packages/template-retail-react-app/app/components/product-tile/index.test.js
@@ -8,6 +8,7 @@ import React from 'react'
 import ProductTile, {Skeleton} from '@salesforce/retail-react-app/app/components/product-tile/index'
 import {renderWithProviders} from '@salesforce/retail-react-app/app/utils/test-utils'
 import {fireEvent} from '@testing-library/react'
+import mockProductSet from '@salesforce/retail-react-app/app/mocks/product-set-winter-lookM'
 
 const mockProductSearchItem = {
     currency: 'USD',
@@ -20,56 +21,56 @@ const mockProductSearchItem = {
     productName: 'Charcoal Single Pleat Wool Suit'
 }
 
-const mockProductSet = {
-    currency: 'GBP',
-    hitType: 'set',
-    image: {
-        alt: 'Winter Look, , large',
-        disBaseLink:
-            'https://edge.disstg.commercecloud.salesforce.com/dw/image/v2/ZZRF_001/on/demandware.static/-/Sites-apparel-m-catalog/default/dwe1c4cd52/images/large/PG.10205921.JJ5FUXX.PZ.jpg',
-        link: 'https://zzrf-001.dx.commercecloud.salesforce.com/on/demandware.static/-/Sites-apparel-m-catalog/default/dwe1c4cd52/images/large/PG.10205921.JJ5FUXX.PZ.jpg',
-        title: 'Winter Look, '
-    },
-    orderable: true,
-    price: 44.16,
-    priceMax: 71.03,
-    pricePerUnit: 44.16,
-    pricePerUnitMax: 71.03,
-    productId: 'winter-lookM',
-    productName: 'Winter Look',
-    productType: {
-        set: true
-    },
-    representedProduct: {
-        id: '701642853695M'
-    },
-    representedProducts: [
-        {id: '701642853695M'},
-        {id: '701642853718M'},
-        {id: '701642853725M'},
-        {id: '701642853701M'},
-        {id: '740357357531M'},
-        {id: '740357358095M'},
-        {id: '740357357623M'},
-        {id: '740357357609M'},
-        {id: '740357358156M'},
-        {id: '740357358132M'},
-        {id: '740357358101M'},
-        {id: '740357357562M'},
-        {id: '740357357548M'},
-        {id: '740357358187M'},
-        {id: '740357357593M'},
-        {id: '740357357555M'},
-        {id: '740357357524M'},
-        {id: '740357358149M'},
-        {id: '740357358088M'},
-        {id: '701642867098M'},
-        {id: '701642867111M'},
-        {id: '701642867104M'},
-        {id: '701642867128M'},
-        {id: '701642867135M'}
-    ]
-}
+// const mockProductSet = {
+//     currency: 'GBP',
+//     hitType: 'set',
+//     image: {
+//         alt: 'Winter Look, , large',
+//         disBaseLink:
+//             'https://edge.disstg.commercecloud.salesforce.com/dw/image/v2/ZZRF_001/on/demandware.static/-/Sites-apparel-m-catalog/default/dwe1c4cd52/images/large/PG.10205921.JJ5FUXX.PZ.jpg',
+//         link: 'https://zzrf-001.dx.commercecloud.salesforce.com/on/demandware.static/-/Sites-apparel-m-catalog/default/dwe1c4cd52/images/large/PG.10205921.JJ5FUXX.PZ.jpg',
+//         title: 'Winter Look, '
+//     },
+//     orderable: true,
+//     price: 44.16,
+//     priceMax: 71.03,
+//     pricePerUnit: 44.16,
+//     pricePerUnitMax: 71.03,
+//     productId: 'winter-lookM',
+//     productName: 'Winter Look',
+//     productType: {
+//         set: true
+//     },
+//     representedProduct: {
+//         id: '701642853695M'
+//     },
+//     representedProducts: [
+//         {id: '701642853695M'},
+//         {id: '701642853718M'},
+//         {id: '701642853725M'},
+//         {id: '701642853701M'},
+//         {id: '740357357531M'},
+//         {id: '740357358095M'},
+//         {id: '740357357623M'},
+//         {id: '740357357609M'},
+//         {id: '740357358156M'},
+//         {id: '740357358132M'},
+//         {id: '740357358101M'},
+//         {id: '740357357562M'},
+//         {id: '740357357548M'},
+//         {id: '740357358187M'},
+//         {id: '740357357593M'},
+//         {id: '740357357555M'},
+//         {id: '740357357524M'},
+//         {id: '740357358149M'},
+//         {id: '740357358088M'},
+//         {id: '701642867098M'},
+//         {id: '701642867111M'},
+//         {id: '701642867104M'},
+//         {id: '701642867128M'},
+//         {id: '701642867135M'}
+//     ]
+// }
 
 test('Renders links and images', () => {
     const {getAllByRole} = renderWithProviders(<ProductTile product={mockProductSearchItem} />)
@@ -108,14 +109,66 @@ test('Remove from wishlist cannot be muti-clicked', () => {
     expect(onClick).toHaveBeenCalledTimes(1)
 })
 
-test('renders strike through price', () => {
-    const {getByText} = renderWithProviders(<ProductTile product={data} />)
+test('renders strike through price with variants product', () => {
+    const {getByText} = renderWithProviders(<ProductTile product={mockProductWithVariants} />)
     expect(getByText(/black flat front wool suit/i)).toBeInTheDocument()
     expect(getByText(/£191\.99/i)).toBeInTheDocument()
     expect(getByText(/£320\.00/i)).toBeInTheDocument()
 })
 
-const data = {
+test('renders strike through price with set', () => {
+    const {getByText} = renderWithProviders(<ProductTile product={mockProductSet} />)
+    expect(getByText(/Winter Look/i)).toBeInTheDocument()
+    expect(getByText(/Starting at/i)).toBeInTheDocument()
+    expect(getByText(/£44\.16/i)).toBeInTheDocument()
+})
+
+test('renders strike through price with standard product', () => {
+    const {getByText} = renderWithProviders(<ProductTile product={mockStandardProduct} />)
+    expect(getByText(/Laptop Briefcase with wheels \(37L\)/i)).toBeInTheDocument()
+    expect(getByText(/£63\.99/i)).toBeInTheDocument()
+})
+
+const mockStandardProduct = {
+    currency: 'GBP',
+    hitType: 'product',
+    image: {
+        alt: 'Laptop Briefcase with wheels (37L), , large',
+        disBaseLink:
+            'https://edge.disstg.commercecloud.salesforce.com/dw/image/v2/ZZRF_001/on/demandware.static/-/Sites-apparel-m-catalog/default/dw7cb2d401/images/large/P0048_001.jpg',
+        link: 'https://zzrf-001.dx.commercecloud.salesforce.com/on/demandware.static/-/Sites-apparel-m-catalog/default/dw7cb2d401/images/large/P0048_001.jpg',
+        title: 'Laptop Briefcase with wheels (37L), '
+    },
+    orderable: true,
+    price: 63.99,
+    pricePerUnit: 63.99,
+    productId: 'P0048M',
+    productName: 'Laptop Briefcase with wheels (37L)',
+    productType: {
+        item: true
+    },
+    representedProduct: {
+        id: 'P0048M',
+        c_styleNumber: 'P0048',
+        c_tabDescription:
+            'Perfect for business travel, this briefcase is ultra practical with plenty of space for your laptop and all its extras, as well as storage for documents, paperwork and all your essential items. The wheeled system allows you to travel comfortably with your work and when you reach your destination, you can remove the laptop compartment and carry over your shoulder to meetings. It’s the business.',
+        c_tabDetails:
+            '1682 ballistic nylon and genuine leather inserts| Spacious main storage compartment for documents and binders|Removable, padded laptop sleeve with D-rings for carrying with shoulder strap|Change handle system and cantilever wheels|Zip pull in gunmetal with black rubber insert Leather “comfort” insert detailed handle|Internal storage pockets for CD-Rom and peripherals|Real leather inserts'
+    },
+    representedProducts: [
+        {
+            id: 'P0048M'
+        }
+    ],
+    tieredPrices: [
+        {
+            price: 63.99,
+            pricebook: 'gbp-m-list-prices',
+            quantity: 1
+        }
+    ]
+}
+const mockProductWithVariants = {
     currency: 'GBP',
     hitType: 'master',
     image: {
