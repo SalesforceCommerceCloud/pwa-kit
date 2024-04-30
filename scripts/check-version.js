54c53eb98757e213803cf5211bc3a0f53e470010
Adam Raya Navarro <arayanavarro@salesforce.com>
2024-04-30 11:37:04 -0600

    Debug GA Windows builds

--- scripts/check-version.js
+++ scripts/check-version.js
@@ -15,7 +15,8 @@ const npm = `npm${extension}`
 const spawnSync = (...args) => {
     const proc = childProc.spawnSync(...args)
     if (proc.status !== 0) {
-        throw proc.stderr.toString()
+        console.error(proc)
+        throw proc.stderr ? proc.stderr.toString() : 'Unknown error'
     }
     return proc
 }
