e495bc5aabfb259d325fb2796e45a32f9c52978d
Adam Raya Navarro <arayanavarro@salesforce.com>
2024-05-02 14:21:48 -0600

    Add docs

--- packages/template-retail-react-app/app/commerce-api/pkce.js
+++ packages/template-retail-react-app/app/commerce-api/pkce.js
@@ -14,6 +14,10 @@ const randomstring = require('randomstring')
 // Globals
 const isServer = typeof window === 'undefined'
 
+/**
+ * Adds entropy to nanoid() using seedrandom to ensure that the code_challenge sent to SCAPI by Google's crawler browser is unique.
+ * Solves the issue with Google's crawler getting the same result from nanoid() in two different runs, which results in the same PKCE code_challenge being used twice.
+ */
 const nanoid = () => {
     const rng = seedrandom(+new Date(), {entropy: true})
     return customRandom(urlAlphabet, 128, (size) => new Uint8Array(size).map(() => 256 * rng()))()
