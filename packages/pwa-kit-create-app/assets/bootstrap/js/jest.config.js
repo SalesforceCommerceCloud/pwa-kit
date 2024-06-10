2a7c22cf050b900bced813bf8667279cf6d79894
Joel Uong <joel.uong@salesforce.com>
2024-06-10 14:22:55 -0400

    lint

--- packages/pwa-kit-create-app/assets/bootstrap/js/jest.config.js
+++ packages/pwa-kit-create-app/assets/bootstrap/js/jest.config.js
@@ -1,7 +1,12 @@
-const base = require('@salesforce/pwa-kit-dev/configs/jest/jest.config.js')
+/*
+ * Copyright (c) 2024, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
 
 module.exports = {
-    ...base,
+    ...require('@salesforce/pwa-kit-dev/configs/jest/jest.config.js'),
     // Empty array to ensure no files are ignored
-    transformIgnorePatterns: [],
-};
+    transformIgnorePatterns: []
+}
