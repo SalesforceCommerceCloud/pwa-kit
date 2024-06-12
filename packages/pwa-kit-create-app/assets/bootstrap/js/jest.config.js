b7c6c76227b466261a7f8200af36ee350ba1e269
Joel Uong <joel.uong@salesforce.com>
2024-06-12 11:56:33 -0400

    narrow down transformIgnorePatterns to only transform retail-react-app

--- packages/pwa-kit-create-app/assets/bootstrap/js/jest.config.js
+++ packages/pwa-kit-create-app/assets/bootstrap/js/jest.config.js
@@ -5,8 +5,18 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const base = require('@salesforce/pwa-kit-dev/configs/jest/jest.config.js')
+
 module.exports = {
-    ...require('@salesforce/pwa-kit-dev/configs/jest/jest.config.js'),
-    // Empty array to ensure no files are ignored
-    transformIgnorePatterns: []
+    ...base,
+    // Don't ignore @salesforce/retail-react-app
+    transformIgnorePatterns: ['/node_modules/(?!@salesforce/retail-react-app/.*)'],
+    moduleNameMapper: {
+        ...base.moduleNameMapper,
+        // pulled from @salesforce/retail-react-app jest.config.js
+        // allows jest to resolve imports for these packages
+        '^is-what$': '<rootDir>/node_modules/is-what/dist/cjs/index.cjs',
+        '^copy-anything$': '<rootDir>/node_modules/copy-anything/dist/cjs/index.cjs'
+    }
 }
