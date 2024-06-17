e5cf431b1450f9f57f2e9f6d58ffaab6530a742a
Adam Raya Navarro <arayanavarro@salesforce.com>
2024-06-17 15:10:50 -0700

    Move logger back to pwa-kit-runtime

--- packages/pwa-kit-react-sdk/src/utils/logger.js
+++ packages/pwa-kit-react-sdk/src/utils/logger.js
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import createLogger from '@salesforce/pwa-kit-dev/utils/logger'
+import createLogger from '@salesforce/pwa-kit-runtime/utils/loggerFactory'
 
 const logger = createLogger('pwa-kit-react-sdk')
 export default logger
--- packages/pwa-kit-runtime/src/utils/logger.js
+++ packages/pwa-kit-runtime/src/utils/logger.js
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import createLogger from '@salesforce/pwa-kit-dev/utils/logger'
+import createLogger from './loggerFactory'
 
 const logger = createLogger('pwa-kit-runtime')
 export default logger
