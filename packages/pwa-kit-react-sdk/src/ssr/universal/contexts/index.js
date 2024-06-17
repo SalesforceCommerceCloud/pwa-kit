b02b22e4b3d0dc1445517e103ebafd7273fc94da
Adam Raya Navarro <arayanavarro@salesforce.com>
2024-06-17 14:29:09 -0700

    PR Feedback

--- packages/pwa-kit-react-sdk/src/ssr/universal/contexts/index.js
+++ packages/pwa-kit-react-sdk/src/ssr/universal/contexts/index.js
@@ -8,7 +8,7 @@
 import React, {useEffect, useRef} from 'react'
 import PropTypes from 'prop-types'
 import {useLocation} from 'react-router-dom'
-import logger from '@salesforce/pwa-kit-dev/utils/logger'
+import logger from '@salesforce/pwa-kit-react-sdk/utils/logger'
 
 const CorrelationIdContext = React.createContext()
 const ServerContext = React.createContext()
@@ -26,7 +26,8 @@ const CorrelationIdProvider = ({children, correlationId, resetOnPageChange = tru
     const _correlationId = typeof correlationId !== 'function' && correlationId
     if (resetOnPageChange && !_correlationIdFn) {
         logger.warn(
-            'correlationId needs to be a function returning a uuid string when resetOnPageChange is true'
+            'correlationId needs to be a function returning a uuid string when resetOnPageChange is true',
+            {namespace: 'contexts.CorrelationIdProvider'}
         )
     }
     const [id, setId] = React.useState(_correlationId || _correlationIdFn())
