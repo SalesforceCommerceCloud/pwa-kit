a55eb13bee2f5ecfadf8c7f03249890e4782ecb4
Adam Raya Navarro <arayanavarro@salesforce.com>
2024-06-20 15:33:46 -0700

    Update types.ts

--- packages/commerce-sdk-react/src/types.ts
+++ packages/commerce-sdk-react/src/types.ts
@@ -14,33 +14,33 @@ export interface Logger {
      * @param message - The debug message.
      * @param args - Optional additional properties to include in the log.
      */
-    debug(message: string, args?: Record<string, unknown>): void;
+    debug(message: string, args?: Record<string, unknown>): void
 
     /**
      * Logs an informational message.
      * @param message - The info message.
      * @param args - Optional additional properties to include in the log.
      */
-    info(message: string, args?: Record<string, unknown>): void;
+    info(message: string, args?: Record<string, unknown>): void
 
     /**
      * Logs a generic log message.
      * @param message - The log message.
      * @param args - Optional additional properties to include in the log.
      */
-    log(message: string, args?: Record<string, unknown>): void;
+    log(message: string, args?: Record<string, unknown>): void
 
     /**
      * Logs a warning message.
      * @param message - The warning message.
      * @param args - Optional additional properties to include in the log.
      */
-    warn(message: string, args?: Record<string, unknown>): void;
+    warn(message: string, args?: Record<string, unknown>): void
 
     /**
      * Logs an error message.
      * @param message - The error message.
      * @param args - Optional additional properties to include in the log.
      */
-    error(message: string, args?: Record<string, unknown>): void;
+    error(message: string, args?: Record<string, unknown>): void
 }
