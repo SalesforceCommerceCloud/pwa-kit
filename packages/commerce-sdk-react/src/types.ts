3d12b7a63be256fd4703218564046e0a0a309c82
Adam Raya Navarro <arayanavarro@salesforce.com>
2024-06-20 15:07:10 -0700

    Update types.ts

--- packages/commerce-sdk-react/src/types.ts
+++ packages/commerce-sdk-react/src/types.ts
@@ -6,7 +6,41 @@
  */
 
 /**
- * Represents a logger with methods for logging messages at various levels.
- * This type mirrors the methods available on the `console` object.
+ * A logger interface for logging messages at various levels.
  */
-export type Logger = typeof console
+export interface Logger {
+    /**
+     * Logs a debug message.
+     * @param message - The debug message.
+     * @param args - Optional additional properties to include in the log.
+     */
+    debug(message: string, args?: Record<string, unknown>): void;
+
+    /**
+     * Logs an informational message.
+     * @param message - The info message.
+     * @param args - Optional additional properties to include in the log.
+     */
+    info(message: string, args?: Record<string, unknown>): void;
+
+    /**
+     * Logs a generic log message.
+     * @param message - The log message.
+     * @param args - Optional additional properties to include in the log.
+     */
+    log(message: string, args?: Record<string, unknown>): void;
+
+    /**
+     * Logs a warning message.
+     * @param message - The warning message.
+     * @param args - Optional additional properties to include in the log.
+     */
+    warn(message: string, args?: Record<string, unknown>): void;
+
+    /**
+     * Logs an error message.
+     * @param message - The error message.
+     * @param args - Optional additional properties to include in the log.
+     */
+    error(message: string, args?: Record<string, unknown>): void;
+}
