b79d8d1bda6e5175ce640d6f012e951d19460121
vcua-mobify <47404250+vcua-mobify@users.noreply.github.com>
2024-06-21 10:57:04 -0700

    @W-15907094@ Refactor /mobify paths (#1824)
    * Use dynamic public path for HMR
    
    * Consolidate /mobify definition
    
    * Remove hard coded mobify path from jest config
    
    * Replace /mobify paths in template-retail-react-app
    
    * Move commerce-sdk-react /mobify definitions
    
    * Updates to create-app and commerce-sdk-react
    
    * Fix active-data hook
    
    * Separate public-path entry point from main
    
    * Webpack lint
    
    * Restore jest config
    
    * Tweak commerce-sdk-react and add comments
    
    * Fix app-config templates
    
    * Add placeholder for /mobify namespace
    
    * Renaming
    
    * Lint
    
    * Remove the branching conditional
    
    * Rename ssr-paths
    
    * Namespace for webpack HMR publicPath
    
    * Reduce calls to getSLASPrivateProxyPath
    
    * Restore constants
    
    * Move webpack public path setter to pwa-kit-dev
    
    * Rename ssr-namespace-paths exports methods
    
    * Update copyright
    
    * Small clean up
    
    ---------
    
    Signed-off-by: vcua-mobify <47404250+vcua-mobify@users.noreply.github.com>

--- packages/commerce-sdk-react/src/test-utils.tsx
+++ packages/commerce-sdk-react/src/test-utils.tsx
@@ -15,6 +15,7 @@ import {
 } from '@tanstack/react-query'
 import nock from 'nock'
 import CommerceApiProvider, {CommerceApiProviderProps} from './provider'
+import {PROXY_PATH} from './constant'
 
 // Note: this host does NOT exist
 // it is intentional b/c we can catch those unintercepted requests
@@ -22,7 +23,7 @@ import CommerceApiProvider, {CommerceApiProviderProps} from './provider'
 export const DEFAULT_TEST_HOST = 'http://localhost:8888'
 
 export const DEFAULT_TEST_CONFIG = {
-    proxy: `${DEFAULT_TEST_HOST}/mobify/proxy/api`,
+    proxy: `${DEFAULT_TEST_HOST}${PROXY_PATH}/api`,
     redirectURI: `${DEFAULT_TEST_HOST}/callback`,
     clientId: '12345678-1234-1234-1234-123412341234',
     organizationId: 'f_ecom_zzrmy_orgf_001',
@@ -31,7 +32,7 @@ export const DEFAULT_TEST_CONFIG = {
     locale: 'en-US',
     currency: 'USD',
     fetchedToken: 'test-token',
-    OCAPISessionsURL: `${DEFAULT_TEST_HOST}/mobify/proxy/ocapi/s/RefArch/dw/shop/v22_8/sessions`
+    OCAPISessionsURL: `${DEFAULT_TEST_HOST}${PROXY_PATH}/ocapi/s/RefArch/dw/shop/v22_8/sessions`
 }
 
 export const createQueryClient = () => {
