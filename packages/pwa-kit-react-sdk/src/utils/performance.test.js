166c0019867f99894de6b38590fdbdc23bff1521
Kevin He <kevin.he@salesforce.com>
2024-07-12 09:35:57 -0700

    add copyrights

--- packages/pwa-kit-react-sdk/src/utils/performance.test.js
+++ packages/pwa-kit-react-sdk/src/utils/performance.test.js
@@ -1,6 +1,12 @@
 /**
  * @jest-environment node
  */
+/*
+ * Copyright (c) 2024, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
 import {getPerformanceMetrics, clearPerformanceMarks, buildServerTimingHeader} from './performance'
 
 describe('getPerformanceMetrics', () => {
