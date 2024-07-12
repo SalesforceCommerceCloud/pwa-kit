f4d5bac106696123f1077892278cd370ef268f26
Kevin He <kevin.he@salesforce.com>
2024-07-12 09:10:55 -0700

    fix tests

--- packages/pwa-kit-react-sdk/src/utils/performance.test.js
+++ packages/pwa-kit-react-sdk/src/utils/performance.test.js
@@ -1,62 +1,55 @@
-import {getPerformanceMetrics} from './performance'
+/**
+ * @jest-environment node
+ */
+import {getPerformanceMetrics, clearPerformanceMarks, buildServerTimingHeader} from './performance'
 
 describe('getPerformanceMetrics', () => {
-    let getEntriesByType
-
-    beforeAll(() => {
-        // Store the original performance.mark
-        getEntriesByType = performance.getEntriesByType
-    })
-
-    beforeEach(() => {
-        // Mock performance.mark using Object.defineProperty
-        Object.defineProperty(performance, 'getEntriesByType', {
-            configurable: true,
-            writable: true,
-            value: jest.fn().mockReturnValue([
-                {
-                    name: 'pwa-kit-react-sdk:ssr:total:start',
-                    startTime: 100
-                },
-                {
-                    name: 'pwa-kit-react-sdk:ssr:total:end',
-                    startTime: 2000
-                },
-                {
-                    name: 'pwa-kit-react-sdk:ssr:fetch-stragegies:react-query:use-query:start:0',
-                    startTime: 500
-                },
-                {
-                    name: 'pwa-kit-react-sdk:ssr:fetch-stragegies:react-query:use-query:end:0',
-                    startTime: 1500,
-                    detail: 'useProduct'
-                },
-                {
-                    name: 'other:random:marks',
-                    startTime: 100
-                }
-            ])
-        })
-    })
-
     afterEach(() => {
-        // Restore the original performance.mark after each test
-        Object.defineProperty(performance, 'getEntriesByType', {
-            configurable: true,
-            writable: true,
-            value: getEntriesByType
-        })
+        performance.clearMarks()
     })
 
     test('should return performance metrics', () => {
+        performance.mark('pwa-kit-react-sdk:ssr:total:start')
+        performance.mark('pwa-kit-react-sdk:ssr:total:end')
         const result = getPerformanceMetrics()
-        expect(result).toEqual([
-            {detail: null, duration: 1900, name: 'pwa-kit-react-sdk:ssr:total'},
+        expect(result.length).toEqual(1)
+        expect(result[0].name).toEqual('pwa-kit-react-sdk:ssr:total')
+    })
+})
+
+describe('buildServerTimingHeader', () => {
+    test('should return an empty string if no performance metrics are provided', () => {
+        const result = buildServerTimingHeader([])
+        expect(result).toBe('')
+    })
+
+    test('should build the Server-Timing header', () => {
+        const metrics = [
             {
-                detail: 'useProduct',
+                name: 'pwa-kit-react-sdk:ssr:fetch-stragegies:react-query:use-query:0',
                 duration: 1000,
-                name: 'pwa-kit-react-sdk:ssr:fetch-stragegies:react-query:use-query:0'
-            }
-        ])
+                detail: 'useProduct'
+            },
+            {name: 'pwa-kit-react-sdk:ssr:total', duration: 1900, detail: null}
+        ]
+        const result = buildServerTimingHeader(metrics)
+        expect(result).toBe(
+            'fetch-stragegies:react-query:use-query:0;dur=1000;desc="useProduct", total;dur=1900'
+        )
+    })
+})
+
+describe('clearPerformanceMarks', () => {
+    afterEach(() => {
+        performance.clearMarks()
+    })
+    test('should clear all performance marks created by the sdk', () => {
+        performance.mark('pwa-kit-react-sdk:ssr:total:start')
+        performance.mark('non-sdk:random:mark')
+        const marks = performance.getEntriesByType('mark')
+        expect(marks).toHaveLength(2)
+        clearPerformanceMarks()
+        const clearedMarks = performance.getEntriesByType('mark')
+        expect(clearedMarks).toHaveLength(1)
     })
 })
