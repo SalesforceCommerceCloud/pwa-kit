fc3e8b0d9c7b37c6b4ddc959175d64943f46b2c6
vcua-mobify <vcua@salesforce.com>
2024-08-12 19:47:56 -0700

    Improve test coverage

--- packages/pwa-kit-runtime/src/utils/ssr-namespace-paths.test.js
+++ packages/pwa-kit-runtime/src/utils/ssr-namespace-paths.test.js
@@ -5,31 +5,50 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import {getEnvBasePath, getProxyPath, getBundlePath} from './ssr-namespace-paths'
+import {getConfig} from './ssr-config'
+
+jest.mock('./ssr-config', () => {
+    return {
+        getConfig: jest.fn()
+    }
+})
 
 const mockConfig = {
     envBasePath: '/test'
 }
 
-jest.mock('./ssr-config', () => {
-    return {
-        getConfig: () => mockConfig
-    }
-})
-
 describe('environment base path tests', () => {
     beforeEach(() => {
         jest.resetModules()
+        jest.resetAllMocks()
     })
 
     test('basePath is returned from config', () => {
+        getConfig.mockImplementation(() => mockConfig)
         expect(getEnvBasePath()).toBe('/test')
     })
 
     test('basePath is included in proxy path', () => {
+        getConfig.mockImplementation(() => mockConfig)
         expect(getProxyPath()).toBe('/test/mobify/proxy')
     })
 
     test('basePath is included in bundle path', () => {
+        getConfig.mockImplementation(() => mockConfig)
         expect(getBundlePath()).toBe('/test/mobify/bundle')
+    })
+
+    test('basePath is set to empty string if there is no config', () => {
+        getConfig.mockImplementation(() => {})
+        expect(getEnvBasePath()).toBe('')
+    })
+
+    test('basePath is set to empty string if envBasePath is not a string', () => {
+        getConfig.mockImplementation(() => {
+            return {
+                envBasePath: () => {}
+            }
+        })
+        expect(getEnvBasePath()).toBe('')
     })
 })
