de53420f2955d364ac0c07f52d58595dc30d27e9
Ben Chypak <bchypak@salesforce.com>
2024-09-03 10:46:12 -0700

    Fixed left in static folder paths

--- packages/pwa-kit-dev/src/utils/extensibility-utils.test.js
+++ packages/pwa-kit-dev/src/utils/extensibility-utils.test.js
@@ -92,16 +92,14 @@ describe('extensibilityUtils', () => {
                 name: 'returns an defined object with correct mapping when application extensions are provided',
                 extensions: ['@extension-good'],
                 expected: {
-                    '@extension-good/setup-app':
-                        '/Users/bchypak/Projects/pwa-kit/packages/pwa-kit-dev/node_modules/@extension-good/src/setup-app.ts'
+                    '@extension-good/setup-app': `${process.cwd()}/node_modules/@extension-good/src/setup-app.ts`
                 }
             },
             {
                 name: 'returns an defined object with correct mapping when application extensions are provided including a bad extension',
                 extensions: ['@extension-bad', '@extension-good'],
                 expected: {
-                    '@extension-good/setup-app':
-                        '/Users/bchypak/Projects/pwa-kit/packages/pwa-kit-dev/node_modules/@extension-good/src/setup-app.ts'
+                    '@extension-good/setup-app': `${process.cwd()}/node_modules/@extension-good/src/setup-app.ts`
                 }
             }
         ].forEach((testCase) => {
