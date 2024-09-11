c4b3b152133ef39779e0a87f9b5e2afb21c4cb45
Adam Raya Navarro <arayanavarro@salesforce.com>
2024-09-11 10:42:47 -0700

    Fix tests

--- packages/pwa-kit-runtime/src/ssr/server/test_fixtures/extensions/another-extension/setup-server.js
+++ packages/pwa-kit-runtime/src/ssr/server/test_fixtures/extensions/another-extension/setup-server.js
@@ -4,13 +4,25 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-/* istanbul ignore next */
-const extension = ({app}) => {
-    app.get('/another-extension', (req, res) => {
-        res.send('test')
-    })
+
+class AnotherExtension {
+    options
+
+    constructor(options) {
+        this.options = options
+    }
+
+    getName() {
+        return 'AnotherExtension'
+    }
+
+    extendApp(app) {
+        app.get('/another-extension', (req, res) => {
+            res.send('test')
+        })
+
+        return app
+    }
 }
 
-module.exports = {
-    default: extension
-}
+export default AnotherExtension
