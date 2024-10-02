2e4cbc96eb8a4b259f3cfc1829dd78bcd4c8831a
Adam Raya Navarro <arayanavarro@salesforce.com>
2024-10-02 15:58:08 -0700

    lint

--- packages/pwa-kit-create-app/scripts/create-mobify-app.js
+++ packages/pwa-kit-create-app/scripts/create-mobify-app.js
@@ -127,7 +127,10 @@ const APPLICATION_EXTENSIBILITY_QUESTIONS = [
         message: 'Which Application Extensions do you want to install?',
         type: 'checkbox',
         // TODO: Get the list of available extensions dynamically
-        choices: [{name: 'extension-sample', value: 'extension-sample'}, {name: 'extension-sample-dos', value: 'extension-sample-dos'}],
+        choices: [
+            {name: 'extension-sample', value: 'extension-sample'},
+            {name: 'extension-sample-dos', value: 'extension-sample-dos'}
+        ],
         when: (answers) => answers.project.generationType === 'appExtensions'
     },
     {
