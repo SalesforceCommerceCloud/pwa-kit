29372e97e4f0fd2c8f6e11628d7b2b53f039fd9d
yunakim714 <yunakim@salesforce.com>
2024-10-09 10:37:04 -0400

    create redirect page

---
+++ packages/template-retail-react-app/app/pages/social-login-redirect/index.jsx
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import React, {Fragment} from 'react'
+import {FormattedMessage} from 'react-intl'
+import {Box, Container, Stack, Text} from '@salesforce/retail-react-app/app/components/shared/ui'
+import {BrandLogo} from '@salesforce/retail-react-app/app/components/icons'
+
+const SocialLoginRedirect = () => {
+    return (
+        <Box data-testid="login-redirect-page" bg="gray.50" py={[8, 16]}>
+            <Container
+                paddingTop={16}
+                width={['100%', '407px']}
+                bg="white"
+                paddingBottom={14}
+                marginTop={8}
+                marginBottom={8}
+                borderRadius="base"
+            >
+                <Fragment>
+                    <Stack justify="center" align="center" spacing={8} marginBottom={8}>
+                        <BrandLogo width="60px" height="auto" />
+                        <Text align="center" fontSize="xl" fontWeight="semibold">
+                            <FormattedMessage defaultMessage="Authenticating..." />
+                        </Text>
+                        <Text align="center" fontSize="m">
+                            <FormattedMessage
+                                defaultMessage="If you are not automatically redirected, click <link>this link</link> to proceed."
+                                values={{
+                                    link: (chunks) => (
+                                        <a
+                                            href="/account"
+                                            style={{color: '#0176D3', textDecoration: 'underline'}}
+                                        >
+                                            {chunks}
+                                        </a>
+                                    )
+                                }}
+                            />
+                        </Text>
+                    </Stack>
+                </Fragment>
+            </Container>
+        </Box>
+    )
+}
+
+SocialLoginRedirect.getTemplateName = () => 'social-login-redirect'
+
+export default SocialLoginRedirect
