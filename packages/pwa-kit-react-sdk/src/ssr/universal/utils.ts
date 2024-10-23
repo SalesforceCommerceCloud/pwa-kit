e68db4451ece75a04438580c3e6d63109dc64162
Ben Chypak <bchypak@salesforce.com>
2024-10-23 11:34:06 -0700

    Remove old code

--- packages/pwa-kit-react-sdk/src/ssr/universal/utils.ts
+++ packages/pwa-kit-react-sdk/src/ssr/universal/utils.ts
@@ -101,22 +101,3 @@ export const getProxyConfigs = () => {
     // Clone to avoid accidental mutation of important configuration variables.
     return configs.map((config: any) => ({...config}))
 }
-
-/**
- * Applies a series of Higher-Order Components (HOCs) to a given React component.
- *
- * @template T - The type of the React component.
- * @param {T} Component - The React component to which the HOCs will be applied.
- * @param {Array<(component: T) => T>} hocs - An array of Higher-Order Components (HOCs) to apply to the component.
- * @returns {T} - The React component wrapped with the provided HOCs.
- */
-export const applyHOCs = <T extends React.ComponentType<any>>(
-    Component: T,
-    hocs: Array<(component: T) => T>
-): T => {
-    return hocs.reduce((AccumulatedComponent, hoc) => {
-        const WrappedComponent = hoc(AccumulatedComponent)
-        return hoistNonReactStatics(WrappedComponent, AccumulatedComponent) as T
-    }, Component)
-}
-
