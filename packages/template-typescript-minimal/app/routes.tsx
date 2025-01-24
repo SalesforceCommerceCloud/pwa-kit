5ac8edc3259198c0d553ba58ec8586b1345f8d6d
Vincent Marta <vmarta@salesforce.com>
2025-01-24 15:54:10 -0800

    Fix linting error
    It was complaining about the unnecessary type assertion.

--- packages/template-typescript-minimal/app/routes.tsx
+++ packages/template-typescript-minimal/app/routes.tsx
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import loadable, {LoadableComponent} from '@loadable/component'
+import loadable from '@loadable/component'
 
 const Home = loadable(() => import('./pages/home'))
 
@@ -12,9 +12,7 @@ const routes = [
     {
         path: '/__pwa-kit/start',
         exact: true,
-        // Type assertion because otherwise we encounter this error:
-        // Exported variable 'routes' has or is using name 'Props' from external module "./app/pages/home" but cannot be named.
-        component: Home as LoadableComponent<unknown>
+        component: Home
     }
 ]
 
