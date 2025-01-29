394f9b3e34f26bbd0683f7ae8a3c3ab70ee05169
Adam Raya Navarro <arayanavarro@salesforce.com>
2025-01-29 09:23:26 -0800

    Remove Babel ignore flag

--- packages/pwa-kit-extension-sdk/src/configs/babel/utils.ts
+++ packages/pwa-kit-extension-sdk/src/configs/babel/utils.ts
@@ -11,10 +11,6 @@ import {getConfiguredExtensions} from '../../shared/utils'
 /**
  * Constants used for building Babel extensibility arguments:
  *
- * IGNORE_PATH: Provides a placeholder path that does not exist, used in Babel's `--ignore`
- * option to prevent processing of any unintended files by specifying a path that cannot match
- * any actual file. This effectively disables Babel's ignore behavior when no other paths are specified.
- *
  * SERVER_PATH: Absolute path to the server entry point (`build-remote-server.js`) within the
  * `@salesforce/pwa-kit-runtime` package. Required for Babel processing to ensure that server-side
  * code, especially for SSR, is correctly transpiled as part of the extensibility setup.
@@ -25,7 +21,6 @@ import {getConfiguredExtensions} from '../../shared/utils'
  * if no actual extensions are added.
  */
 const NODE_MODULES_PATH = 'node_modules'
-const IGNORE_PATH = `${NODE_MODULES_PATH}/does_not_exist`
 const SERVER_PATH = `${NODE_MODULES_PATH}/@salesforce/pwa-kit-runtime/ssr/server/build-remote-server.js`
 const PLACEHOLDER_PATH = `${NODE_MODULES_PATH}/@salesforce/pwa-kit-extension-sdk/express/placeholders/application-extensions.js`
 
@@ -50,7 +45,7 @@ export const buildBabelExtensibilityArgs = (config: any) => {
 
     const extensionsPathsStr = extensionSrcPaths.length > 0 ? `,${extensionSrcPaths.join(',')}` : ''
 
-    const babelArgs = `--ignore "${IGNORE_PATH}" --only "app/**,${serverPath},${placeHolderPath}${extensionsPathsStr}"`
+    const babelArgs = `--only "app/**,${serverPath},${placeHolderPath}${extensionsPathsStr}"`
 
     return babelArgs
 }
