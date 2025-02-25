d253785d4340a05f68cf5b41b5c5d30049ed04db
Adam Raya Navarro <arayanavarro@salesforce.com>
2025-02-25 12:04:17 -0800

    Replace event-emitter with EventTarget

--- packages/pwa-kit-react-sdk/src/ssr/universal/events.js
+++ packages/pwa-kit-react-sdk/src/ssr/universal/events.js
@@ -5,30 +5,27 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import ee from 'event-emitter'
-
 export const PAGEEVENTS = {
     PAGELOAD: 'PAGELOAD',
     ERROR: 'ERROR'
 }
-class Pages {
+
+class Pages extends EventTarget {
     pageLoad(templateName, start, end) {
         const payload = {
             templateName,
             start,
             end
         }
-        this.emit(PAGEEVENTS.PAGELOAD, payload)
+        this.dispatchEvent(new CustomEvent(PAGEEVENTS.PAGELOAD, {detail: payload}))
     }
     error(name, content) {
         const payload = {
             name,
             content
         }
-        this.emit(PAGEEVENTS.ERROR, payload)
+        this.dispatchEvent(new CustomEvent(PAGEEVENTS.ERROR, {detail: payload}))
     }
 }
-
-ee(Pages.prototype)
 
 export const pages = new Pages()
