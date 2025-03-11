b1bab0310ffb8bd33d4f77df6290022bd1f532ad
Kevin He <kevin.he@salesforce.com>
2025-03-11 14:46:22 -0700

    fix missing import

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -8,6 +8,7 @@
 import React, {useEffect} from 'react'
 import PropTypes from 'prop-types'
 import {useIntl, defineMessage} from 'react-intl'
+import {keepPreviousData} from '@tanstack/react-query'
 import {Box, Container} from '@chakra-ui/react'
 import {
     AuthHelpers,
