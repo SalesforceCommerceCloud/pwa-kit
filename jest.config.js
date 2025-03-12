942a816bb2eb1b826d78db39ba70175476892121
Kevin He <kevin.he@salesforce.com>
2025-03-12 12:55:46 -0700

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,7 @@ module.exports = {
         '^react-router-dom(.*)$': '<rootDir>/node_modules/react-router-dom/index.js',
         '^@salesforce/retail-react-app(.*)$': '<rootDir>$1',
         '^@tanstack/react-query$':
-            '<rootDir>/node_modules/@tanstack/react-query/build/lib/index.js',
+            '<rootDir>/node_modules/@tanstack/react-query/build/modern/index.cjs',
         '^is-what$': '<rootDir>/node_modules/is-what/dist/cjs/index.cjs',
         '^copy-anything$': '<rootDir>/node_modules/copy-anything/dist/cjs/index.cjs',
         // handle pwa-kit extensibility special import
