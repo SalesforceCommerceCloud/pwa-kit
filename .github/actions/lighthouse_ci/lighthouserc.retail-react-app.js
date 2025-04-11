6e64b348f0fe00f777024a1b6b6a9133c17d35ec
Vincent Marta <vmarta@salesforce.com>
2025-04-11 11:13:50 -0700

    Lighthouse test now accepts config filename to be more reusable

--- .github/actions/lighthouse_ci/lighthouserc.retail-react-app.js
+++ .github/actions/lighthouse_ci/lighthouserc.retail-react-app.js
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+
 module.exports = {
     // The different Lighthouse CI configuration options:
     // https://github.com/GoogleChrome/lighthouse-ci/blob/master/docs/configuration.md
