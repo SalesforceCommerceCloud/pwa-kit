a7d1eab896f7aff77be7e2e4d64655ff22711cd2
Vincent Marta <vmarta@salesforce.com>
2025-04-21 10:58:44 -0700

    Add comments

--- scripts/report-bundle-size.js
+++ scripts/report-bundle-size.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /*
- * Copyright (c) 2025, Salesforce, Inc.
+ * Copyright (c) 2023, Salesforce, Inc.
  * All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
