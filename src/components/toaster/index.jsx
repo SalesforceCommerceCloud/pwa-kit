f54e6ee60ab5907e04fd5e910d80e928e79b8014
Kevin He <kevin.he@salesforce.com>
2025-05-26 16:25:19 -0700

    add toaster component

---
+++ src/components/toaster/index.jsx
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2025, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import React from 'react'
+import {
+  Toaster as ChakraToaster,
+  Portal,
+  Stack,
+  Toast
+} from "@chakra-ui/react"
+
+// export const toaster = createToaster({
+//   placement: "bottom-end",
+//   pauseOnPageIdle: true,
+// })
+
+export const Toaster = ({ toaster }) => {
+  return (
+    <Portal>
+      <ChakraToaster toaster={toaster} insetInline={{ mdDown: "4" }}>
+        {(toast) => (
+          <Toast.Root width={{ md: "sm" }}>
+            <Toast.Indicator />
+            <Stack gap="1" flex="1" maxWidth="100%">
+              {toast.title && <Toast.Title>{toast.title}</Toast.Title>}
+              {toast.description && (
+                <Toast.Description>{toast.description}</Toast.Description>
+              )}
+            </Stack>
+            {toast.action && (
+              <Toast.ActionTrigger>{toast.action.label}</Toast.ActionTrigger>
+            )}
+            {toast.closable && <Toast.CloseTrigger />}
+          </Toast.Root>
+        )}
+      </ChakraToaster>
+    </Portal>
+  )
+}
