598c05b76c8a6550a966fca97998d7e870fa7890
kzheng <kzheng@salesforce.com>
2025-06-09 11:20:40 -0700

    removed unused import

--- packages/pwa-kit-create-app/scripts/tree-shake.test.js
+++ packages/pwa-kit-create-app/scripts/tree-shake.test.js
@@ -5,7 +5,6 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 const fs = require('fs');
-const path = require('path');
 
 jest.mock('../assets/plugin-config', () => ({
     plugins: {
