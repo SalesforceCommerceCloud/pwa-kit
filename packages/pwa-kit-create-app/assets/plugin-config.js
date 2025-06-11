9b691d39a9b62ddba2caee3abbd8cb8b3ec92bc3
Brian Feister <bfeister@salesforce.com>
2025-06-11 11:33:58 -0600

    add to plugin-config.js conf file

--- packages/pwa-kit-create-app/assets/plugin-config.js
+++ packages/pwa-kit-create-app/assets/plugin-config.js
@@ -1,6 +1,6 @@
 /**
  * Plugin configuration for the PWA Kit
- * This file defines the available plugins during the creation of a new PWA Kit project. 
+ * This file defines the available plugins during the creation of a new PWA Kit project.
  * The elements in the plugins object should be consistent with the plugins defined in the webpack config in the @salesforce/pwa-kit-dev package.
  * All plugins should start with SFDC_EXT_, so that it's clear that they are extensions and not part of the core PWA Kit.
  */
@@ -10,5 +10,8 @@ module.exports = {
         // SFDC_EXT_HELLO_WORLD_ENABLED: {
         //     description: 'The Hello World Extension'
         // },
+        SFDC_EXT_STORE_LOCATOR_ENABLED: {
+            description: 'The Store Locator Extension'
+        }
     }
-}; 
+}
