e8144639c67e93d0b3c325e67095a83228adbec3
Brian Feister <bfeister@salesforce.com>
2025-06-12 12:57:50 -0600

    Add back missing translation compile scripts + remove V3 extensibility

---
+++ packages/template-chakra-storefront/scripts/translations/compile-folder.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+/*
+ * Copyright (c) 2023, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+/* eslint @typescript-eslint/no-var-requires: "off" */
+const {exec} = require('child_process')
+const {OUTPUT_FOLDER_PATH} = require('./utils')
+
+const main = () => {
+    const inputFolder = process.argv[2]
+    const command = `formatjs compile-folder --ast ${inputFolder} ${OUTPUT_FOLDER_PATH}`
+
+    console.log('Compiling translations into the folder:', OUTPUT_FOLDER_PATH)
+    exec(command, (err) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+    })
+}
+
+main()
