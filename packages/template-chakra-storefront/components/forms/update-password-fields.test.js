6d2b8484bd9fff835c21ffd8f27a67f42b38c0eb
Brian Feister <bfeister@salesforce.com>
2025-06-19 11:16:01 -0600

    fix outdated references to `@salesforce/retail-react-app`

--- packages/template-chakra-storefront/components/forms/update-password-fields.test.js
+++ packages/template-chakra-storefront/components/forms/update-password-fields.test.js
@@ -5,9 +5,8 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import React from 'react'
-import {renderWithProviders} from '@salesforce/retail-react-app/app/utils/test-utils'
 import {useForm} from 'react-hook-form'
-import UpdatePasswordFields from '@salesforce/retail-react-app/app/components/forms/update-password-fields'
+import UpdatePasswordFields from './update-password-fields'
 import {screen} from '@testing-library/react'
 import PropTypes from 'prop-types'
 
