09452316ba63bd99e39b1daf7f708dd922ed8f7a
jbisa <jbisa@salesforce.com>
2025-06-19 18:59:06 -0400

    Fix webpack config: revert to CommonJS format for webpack compatibility

--- packages/template-retail-react-app/webpack.config.js
+++ packages/template-retail-react-app/webpack.config.js
@@ -4,11 +4,11 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import config from '@salesforce/pwa-kit-dev/configs/webpack/config'
-import configNames from '@salesforce/pwa-kit-dev/configs/webpack/config-names'
-import {isRemote} from '@salesforce/pwa-kit-runtime/utils/ssr-server'
+var config = require('@salesforce/pwa-kit-dev/configs/webpack/config')
+var configNames = require('@salesforce/pwa-kit-dev/configs/webpack/config-names')
+const {isRemote} = require('@salesforce/pwa-kit-runtime/utils/ssr-server')
 
-export default config.map((configItem) => {
+module.exports = config.map((configItem) => {
     if (configItem.name === configNames.CLIENT || configItem.name === configNames.SERVER) {
         return {
             ...configItem,
