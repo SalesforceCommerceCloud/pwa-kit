d2949234decf74e3161f2f3c9ed635f3c53c7a84
jbisa <jbisa@salesforce.com>
2025-06-20 10:57:53 -0400

    Copy over ssr.js changes into pwa-kit-create-app

--- packages/template-retail-react-app/webpack.config.js
+++ packages/template-retail-react-app/webpack.config.js
@@ -5,9 +5,9 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-const config = require('@salesforce/pwa-kit-dev/configs/webpack/config.js')
-const configNames = require('@salesforce/pwa-kit-dev/configs/webpack/config-names.js')
-const {isRemote} = require('@salesforce/pwa-kit-runtime/utils/ssr-server.js')
+const config = require('@salesforce/pwa-kit-dev/configs/webpack/config')
+const configNames = require('@salesforce/pwa-kit-dev/configs/webpack/config-names')
+const {isRemote} = require('@salesforce/pwa-kit-runtime/utils/ssr-server')
 
 module.exports = config.map((configItem) => {
     if (configItem.name === configNames.CLIENT || configItem.name === configNames.SERVER) {
