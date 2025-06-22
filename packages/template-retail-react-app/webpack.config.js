2db22db93d673741b76414caa4fd7eab66e04f78
jbisa <jbisa@salesforce.com>
2025-06-22 12:26:04 -0400

    Fix webpack alias to resolve absolute imports for @salesforce/retail-react-app/app

--- packages/template-retail-react-app/webpack.config.js
+++ packages/template-retail-react-app/webpack.config.js
@@ -8,12 +8,20 @@
 const config = require('@salesforce/pwa-kit-dev/configs/webpack/config.js')
 const configNames = require('@salesforce/pwa-kit-dev/configs/webpack/config-names.js')
 const {isRemote} = require('@salesforce/pwa-kit-runtime/utils/ssr-server.js')
+const path = require('path')
 
 module.exports = config.map((configItem) => {
     if (configItem.name === configNames.CLIENT || configItem.name === configNames.SERVER) {
         return {
             ...configItem,
             devtool: isRemote() ? false : 'source-map',
+            resolve: {
+                ...configItem.resolve,
+                alias: {
+                    ...configItem.resolve?.alias,
+                    '@salesforce/retail-react-app/app': path.resolve(__dirname, 'app')
+                }
+            },
             module: {
                 ...configItem.module,
                 rules: [
