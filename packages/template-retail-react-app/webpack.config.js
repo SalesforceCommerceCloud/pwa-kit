367678aefb432fbdb9be81d3a2a85f4b254359c2
jbisa <jbisa@salesforce.com>
2025-06-22 11:27:40 -0400

    Fix build issues in webpack config

--- packages/template-retail-react-app/webpack.config.js
+++ packages/template-retail-react-app/webpack.config.js
@@ -4,11 +4,11 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import config from '@salesforce/pwa-kit-dev/configs/webpack/config'
-import configNames from '@salesforce/pwa-kit-dev/configs/webpack/config-names'
-import {isRemote} from '@salesforce/pwa-kit-runtime/utils/ssr-server'
+import config from '@salesforce/pwa-kit-dev/configs/webpack/config.js'
+import configNames from '@salesforce/pwa-kit-dev/configs/webpack/config-names.js'
+import {isRemote} from '@salesforce/pwa-kit-runtime/utils/ssr-server.js'
 
-module.exports = config.map((configItem) => {
+export default config.map((configItem) => {
     if (configItem.name === configNames.CLIENT || configItem.name === configNames.SERVER) {
         return {
             ...configItem,
