d14d1904c86dc5a4ee838a14df056e8d28b7cea0
jbisa <jbisa@salesforce.com>
2025-06-22 11:20:02 -0400

    Add webpack config and necessary dependencies

---
+++ packages/template-retail-react-app/webpack.config.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2025, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import config from '@salesforce/pwa-kit-dev/configs/webpack/config'
+import configNames from '@salesforce/pwa-kit-dev/configs/webpack/config-names'
+import {isRemote} from '@salesforce/pwa-kit-runtime/utils/ssr-server'
+
+module.exports = config.map((configItem) => {
+    if (configItem.name === configNames.CLIENT || configItem.name === configNames.SERVER) {
+        return {
+            ...configItem,
+            devtool: isRemote() ? false : 'source-map',
+            module: {
+                ...configItem.module,
+                rules: [
+                    ...configItem.module.rules,
+                    {
+                        test: /\.css$/i,
+                        use: ['style-loader', 'css-loader']
+                    }
+                ]
+            }
+        }
+    } else {
+        return configItem
+    }
+})
