c29731ce19ee94c53f43a33ccbb524c0a6723a7a
Yuna Kim <84923642+yunakim714@users.noreply.github.com>
2025-06-24 15:02:06 -0400

    @W-18670790 - [BOPIS] Enhance the Cart (#2632)
    * add firstName lastName and phone to pickup
    
    * add order type to cart page
    
    * add translations
    
    * update text
    
    * lint
    
    * fix tests
    
    * add store name to header in cart
    
    * add store name to header

--- packages/template-retail-react-app/app/pages/product-detail/index.jsx
+++ packages/template-retail-react-app/app/pages/product-detail/index.jsx
@@ -123,7 +123,6 @@ const ProductDetail = () => {
     const {
         configurePickupShipment,
         configureRegularShippingMethod,
-        hasPickupItems: checkHasPickupItems,
         addInventoryIdsToPickupItems,
         getPickupShippingMethodId,
         getDefaultShippingMethodId,
@@ -131,7 +130,7 @@ const ProductDetail = () => {
     } = usePickupShipment()
 
     // Hook for shipping methods - we'll use refetch when needed
-    const {data: shippingMethods, refetch: refetchShippingMethods} = useShippingMethodsForShipment(
+    const {refetch: refetchShippingMethods} = useShippingMethodsForShipment(
         {
             parameters: {
                 basketId: basket?.basketId,
