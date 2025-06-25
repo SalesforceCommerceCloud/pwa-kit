bbaacda24833c1f3fe00eb29b675593d7c780c27
Shikhar Prasoon <214730309+sf-shikhar-prasoon@users.noreply.github.com>
2025-06-25 13:00:08 -0400

    fix Edit button appearing briefly

--- packages/template-retail-react-app/app/pages/cart/partials/cart-secondary-button-group.jsx
+++ packages/template-retail-react-app/app/pages/cart/partials/cart-secondary-button-group.jsx
@@ -101,14 +101,16 @@ const CartSecondaryButtonGroup = ({
                             />
                         </Button>
                     )}
-                    {!variant.type?.item && (
-                        <Button variant="link" size="sm" onClick={() => onEditClick(variant)}>
-                            <FormattedMessage
-                                defaultMessage="Edit"
-                                id="cart_secondary_button_group.action.edit"
-                            />
-                        </Button>
-                    )}
+                    {/* Only show edit button if it's a standard product */}
+                    {variant.id &&
+                        !variant.type?.item && ( // the variant.id ensures complete product data. without it, Edit button appears briefly
+                            <Button variant="link" size="sm" onClick={() => onEditClick(variant)}>
+                                <FormattedMessage
+                                    defaultMessage="Edit"
+                                    id="cart_secondary_button_group.action.edit"
+                                />
+                            </Button>
+                        )}
                 </ButtonGroup>
                 <Flex alignItems="center">
                     <Checkbox
