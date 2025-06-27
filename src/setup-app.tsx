9cdc8a81cb983730e58c41873efefcea14dea7f5
Henry Semaganda <henry.semaganda@salesforce.com>
2025-06-27 12:36:23 -0700

    linting

--- src/setup-app.tsx
+++ src/setup-app.tsx
@@ -117,7 +117,7 @@ class ChakraStorefront extends ApplicationExtension<Config> {
             {
                 path: config.pages.ProductList && config.pages.ProductList.path,
                 component: Pages.ProductList
-            },
+            }
         ].filter((route) => route.path !== false)
 
         return extensionRoutes as RouteProps[]
