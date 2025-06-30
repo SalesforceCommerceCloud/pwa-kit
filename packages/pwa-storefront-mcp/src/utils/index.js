9dd6df486c28ccf708af72566b22b8b2ae33fe2c
Ben Chypak <bchypak@salesforce.com>
2025-06-30 11:04:01 -0700

    Get program information from tty

--- packages/pwa-storefront-mcp/src/utils/index.js
+++ packages/pwa-storefront-mcp/src/utils/index.js
@@ -4,7 +4,8 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import CreateProjectTool from './pwa-create-app-guideline-tool.js'
+import CreateAppGuidelinesTool from './pwa-create-app-guideline-tool.js'
+import CreateNewComponentTool from './create-new-component-tool.js'
 import DeveloperGuidelinesTool from './pwa-developer-guideline-tool.js'
 
-export {CreateProjectTool, DeveloperGuidelinesTool}
+export {CreateAppGuidelinesTool, CreateNewComponentTool, DeveloperGuidelinesTool}
