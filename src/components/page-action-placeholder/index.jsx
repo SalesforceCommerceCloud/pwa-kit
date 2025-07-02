8db7df1823efbf7eecb197bdab05da86eebcf994
Ben Chypak <bchypak@salesforce.com>
2025-07-02 09:47:46 -0700

    Migrate spinner and page-action-placeholder components

--- src/components/page-action-placeholder/index.jsx
+++ src/components/page-action-placeholder/index.jsx
@@ -13,7 +13,7 @@ import {PlusIcon} from '../../components/icons'
 const PageActionPlaceHolder = ({heading, text, icon, buttonText, buttonProps, onButtonClick}) => {
     return (
         <Stack
-            spacing={2}
+            gap={2}
             py={12}
             px={4}
             alignItems="center"
@@ -21,7 +21,7 @@ const PageActionPlaceHolder = ({heading, text, icon, buttonText, buttonProps, on
             background="gray.50"
         >
             <Box>{icon}</Box>
-            <Stack spacing={6} alignItems="center">
+            <Stack gap={6} alignItems="center">
                 <Box>
                     <Text align="center" fontSize="lg" fontWeight="bold">
                         {heading}
