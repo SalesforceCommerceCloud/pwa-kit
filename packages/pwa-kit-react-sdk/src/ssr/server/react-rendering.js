fe536885d92281b0f55c884bd6a8531b295bc50e
Avinash Kasipathy <akasipathy@salesforce.com>
2025-07-01 21:45:46 -0700

    rebase develop branch (#2707)

--- packages/pwa-kit-react-sdk/src/ssr/server/react-rendering.js
+++ packages/pwa-kit-react-sdk/src/ssr/server/react-rendering.js
@@ -81,7 +81,7 @@ const logAndFormatError = (err) => {
 // as best as we can.
 export const getLocationSearch = (req, opts = {}) => {
     const {interpretPlusSignAsSpace = false} = opts
-    const [, search] = req.originalUrl.split('?')
+    const [_, search] = req.originalUrl.split('?')
     const params = new URLSearchParams(search)
 
     const newParams = new URLSearchParams()
