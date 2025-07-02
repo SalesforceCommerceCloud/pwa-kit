a4f9d6f649ebb0c280ed865a46bdd4552a9e0518
daniel-diaz <daniel.diaz@salesforce.com>
2025-07-02 14:57:19 -0400

    Refactored onblur and onChange to be functions

--- packages/template-retail-react-app/app/components/product-item/product-quantity-picker.jsx
+++ packages/template-retail-react-app/app/components/product-item/product-quantity-picker.jsx
@@ -19,6 +19,29 @@ const ProductQuantityPicker = ({
 }) => {
     const intl = useIntl()
 
+    const handleQuantityChange = (stringValue, numberValue) => {
+        // Set the Quantity of product to value of input if value number
+        if (numberValue >= 0) {
+            // Call handler
+            onItemQuantityChange(numberValue).then(
+                (isValidChange) => isValidChange && setQuantity(numberValue)
+            )
+        } else if (stringValue === '') {
+            // We want to allow the use to clear the input to start a new input so here we set the quantity to '' so NAN is not displayed
+            // User will not be able to add '' quantity to the cart due to the add to cart button enablement rules
+            setQuantity(stringValue)
+        }
+    }
+
+    const handleQuantityBlur = (e) => {
+        // Default to last known quantity if a user leaves the box with an invalid value
+        const {value} = e.target
+
+        if (!value) {
+            setQuantity(product.quantity)
+        }
+    }
+
     return (
         <>
             <Text
@@ -43,27 +66,8 @@ const ProductQuantityPicker = ({
                 value={quantity}
                 min={0}
                 clampValueOnBlur={false}
-                onBlur={(e) => {
-                    // Default to last known quantity if a user leaves the box with an invalid value
-                    const {value} = e.target
-
-                    if (!value) {
-                        setQuantity(product.quantity)
-                    }
-                }}
-                onChange={(stringValue, numberValue) => {
-                    // Set the Quantity of product to value of input if value number
-                    if (numberValue >= 0) {
-                        // Call handler
-                        onItemQuantityChange(numberValue).then(
-                            (isValidChange) => isValidChange && setQuantity(numberValue)
-                        )
-                    } else if (stringValue === '') {
-                        // We want to allow the use to clear the input to start a new input so here we set the quantity to '' so NAN is not displayed
-                        // User will not be able to add '' quantity to the cart due to the add to cart button enablement rules
-                        setQuantity(stringValue)
-                    }
-                }}
+                onBlur={handleQuantityBlur}
+                onChange={handleQuantityChange}
                 productName={product?.name}
             />
             <VisuallyHidden role="status">
