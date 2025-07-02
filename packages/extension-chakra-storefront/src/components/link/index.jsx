9df31eb43dfa89b9769b02e3dc2fe0046cb0b7f2
Alex Vuong <alex.vuong@salesforce.com>
2025-07-02 16:50:27 -0700

    W-18672799 Chakra UI v3 header migration (#2691)
    * Migrate Header to Chakra v3
    ---------
    
    Co-authored-by: Vincent Marta <vmarta@salesforce.com>

--- packages/extension-chakra-storefront/src/components/link/index.jsx
+++ packages/extension-chakra-storefront/src/components/link/index.jsx
@@ -10,23 +10,38 @@ import {Link as ChakraLink} from '@chakra-ui/react'
 import {Link as SPALink, NavLink as NavSPALink} from 'react-router-dom'
 import useMultiSite from '../../hooks/use-multi-site'
 
-const Link = React.forwardRef(({href, to, useNavLink = false, ...props}, ref) => {
+const Link = React.forwardRef(({href, to, useNavLink = false, css, children, ...props}, ref) => {
     const _href = to || href
     const {buildUrl} = useMultiSite()
     const updatedHref = buildUrl(_href)
+
+    const isActive = useNavLink
+        ? (_, location) => {
+              return location.pathname.endsWith(_href)
+          }
+        : undefined
+
     return (
-        <ChakraLink
-            as={useNavLink ? NavSPALink : SPALink}
-            {...(useNavLink && {exact: true})}
-            {...props}
-            to={updatedHref}
-            ref={ref}
-        />
+        <ChakraLink asChild {...props} css={css} ref={ref}>
+            {useNavLink ? (
+                <NavSPALink to={updatedHref} isActive={isActive}>
+                    {children}
+                </NavSPALink>
+            ) : (
+                <SPALink to={updatedHref}>{children}</SPALink>
+            )}
+        </ChakraLink>
     )
 })
 
 Link.displayName = 'Link'
 
-Link.propTypes = {href: PropTypes.string, to: PropTypes.string, useNavLink: PropTypes.bool}
+Link.propTypes = {
+    href: PropTypes.string,
+    to: PropTypes.string,
+    useNavLink: PropTypes.bool,
+    children: PropTypes.node,
+    css: PropTypes.oneOfType([PropTypes.object, PropTypes.func, PropTypes.array, PropTypes.string])
+}
 
 export default React.memo(Link)
