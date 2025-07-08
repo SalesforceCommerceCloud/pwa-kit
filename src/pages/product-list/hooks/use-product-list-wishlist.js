9ba3dd9773ac63d8b051ba61c05ed36640b2f68b
Kevin He <kevin.he@salesforce.com>
2025-07-08 15:24:09 -0700

    add copyright

--- src/pages/product-list/hooks/use-product-list-wishlist.js
+++ src/pages/product-list/hooks/use-product-list-wishlist.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2025, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
 import {useIntl} from 'react-intl'
 import {useShopperCustomersMutation, useCustomerId} from '@salesforce/commerce-sdk-react'
 import {Button} from '@chakra-ui/react'
