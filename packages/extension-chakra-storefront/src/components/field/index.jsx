4bf4b7e6884b00125185992614bcf298fd0a59d0
Ujwala <59447810+unandyala@users.noreply.github.com>
2025-07-09 17:42:26 -0400

    Fix billing address (#2718)
    * Fix billing address

--- packages/extension-chakra-storefront/src/components/field/index.jsx
+++ packages/extension-chakra-storefront/src/components/field/index.jsx
@@ -111,8 +111,9 @@ const Field = ({
 
                         {type === 'select' && (
                             <>
-                                <NativeSelect.Root ref={ref} value={value} {..._inputProps}>
+                                <NativeSelect.Root value={value} {..._inputProps}>
                                     <NativeSelect.Field
+                                        ref={ref}
                                         value={value || ''}
                                         onChange={onChange}
                                         placeholder={placeholder}
