9fcf416260ed49b83495309fcc63357ce2e5f2c1
Kevin He <kevin.he@salesforce.com>
2025-07-10 16:31:05 -0700

    extract product grid and pagination

---
+++ src/pages/product-list/partials/pagination-footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {Flex} from '@chakra-ui/react'
+import Pagination from '../../../components/pagination'
+
+const PaginationFooter = ({basePath, pageUrls}) => {
+    if (!pageUrls || pageUrls.length < 2) {
+        return null
+    }
+    return (
+        <Flex justifyContent={['center', 'center', 'flex-start']} paddingTop={8}>
+            <Pagination currentURL={basePath} urls={pageUrls} />
+        </Flex>
+    )
+}
+
+PaginationFooter.propTypes = {
+    basePath: PropTypes.string,
+    pageUrls: PropTypes.array
+}
+
+export default PaginationFooter
