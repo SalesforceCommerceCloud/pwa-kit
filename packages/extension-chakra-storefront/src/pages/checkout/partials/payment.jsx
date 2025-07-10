e10ee7bfd02cc5a0c98bf9eca30c89f321b0cc7f
kzheng <kzheng@salesforce.com>
2025-07-10 13:46:17 -0700

    @W-18910688 refactored cart-utils to error-utils and consolidate usage

--- packages/extension-chakra-storefront/src/pages/checkout/partials/payment.jsx
+++ packages/extension-chakra-storefront/src/pages/checkout/partials/payment.jsx
@@ -9,7 +9,6 @@ import PropTypes from 'prop-types'
 import {defineMessage, FormattedMessage, useIntl} from 'react-intl'
 import {Box, Button, Checkbox, Container, Heading, Stack, Text, Separator} from '@chakra-ui/react'
 import {useForm} from 'react-hook-form'
-import useToast from '../../../hooks/use-toast'
 import {useShopperBasketsMutation} from '@salesforce/commerce-sdk-react'
 import {useCurrentBasket} from '../../../hooks/use-current-basket'
 import {useCheckout} from '../../../pages/checkout/util/checkout-context'
@@ -23,7 +22,7 @@ import PaymentForm from '../../../pages/checkout/partials/payment-form'
 import ShippingAddressSelection from '../../../pages/checkout/partials/shipping-address-selection'
 import AddressDisplay from '../../../components/address-display'
 import {PromoCode, usePromoCode} from '../../../components/promo-code'
-import {API_ERROR_MESSAGE} from '../../../constants'
+import {useErrorHandler} from '../../../utils/error-utils'
 
 const Payment = () => {
     const {formatMessage} = useIntl()
@@ -42,13 +41,7 @@ const Payment = () => {
     const {mutateAsync: removePaymentInstrumentFromBasket} = useShopperBasketsMutation(
         'removePaymentInstrumentFromBasket'
     )
-    const toast = useToast()
-    const showError = () => {
-        toast({
-            title: formatMessage(API_ERROR_MESSAGE),
-            type: 'error'
-        })
-    }
+    const showError = useErrorHandler()
 
     const {step, STEPS, goToStep, goToNextStep} = useCheckout()
 
--- packages/extension-chakra-storefront/src/utils/error-utils.js
+++ packages/extension-chakra-storefront/src/utils/error-utils.js
@@ -5,14 +5,14 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import {useIntl} from 'react-intl'
-import useToast from '../../../hooks/use-toast'
-import {API_ERROR_MESSAGE} from '../../../constants'
+import useToast from '../hooks/use-toast'
+import {API_ERROR_MESSAGE} from '../constants'
 
 /**
- * Custom hook for cart error handling
+ * Custom hook for error handling
  * @returns {Function} showError function
  */
-export const useCartErrorHandler = () => {
+export const useErrorHandler = () => {
     const {formatMessage} = useIntl()
     const toast = useToast()
 
