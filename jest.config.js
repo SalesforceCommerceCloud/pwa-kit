aa66fedfe7f13bcfa476083c7f1c1a7c46f8308b
Ben Chypak <bchypak@salesforce.com>
2025-07-14 13:38:49 -0700

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -56,7 +56,6 @@ module.exports = {
         createTestGlob('pages/login'),
         createTestGlob('pages/login-redirect'),
         createTestGlob('pages/social-login-redirect'),
-        createTestGlob('hooks/cart'),
         createTestGlob('hooks'),
         createTestGlob('pages/registration'),
         // createTestGlob('pages/registration'), // TODO: enable after Account page has been migrated
