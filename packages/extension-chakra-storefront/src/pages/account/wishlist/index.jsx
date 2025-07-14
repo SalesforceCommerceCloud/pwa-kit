a6950d774261108a4211d81f390ab85ca6ca1b36
jie-dai <jie.dai@salesforce.com>
2025-07-14 17:14:17 -0400

    fix perttier issue

--- packages/extension-chakra-storefront/src/pages/account/wishlist/index.jsx
+++ packages/extension-chakra-storefront/src/pages/account/wishlist/index.jsx
@@ -158,7 +158,7 @@ const AccountWishlist = () => {
                         >
                             <Flex w="full" align="flex-start">
                                 <Skeleton boxSize={['88px', '36']} mr="4" />
-                                    <Stack gap="2">
+                                <Stack gap="2">
                                     <Skeleton h="20px" w="112px" />
                                     <Skeleton h="20px" w="84px" />
                                     <Skeleton h="20px" w="140px" />
