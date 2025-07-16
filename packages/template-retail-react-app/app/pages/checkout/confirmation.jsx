963b7f1ab0b0ea6fcb6da2374286a89c59234790
Gurpreet Saini <gurpreet.saini@salesforce.com>
2025-07-16 12:21:32 -0700

    revert

--- packages/template-retail-react-app/app/pages/checkout/confirmation.jsx
+++ packages/template-retail-react-app/app/pages/checkout/confirmation.jsx
@@ -108,9 +108,10 @@ const CheckoutConfirmation = () => {
         const saveShippingAddress = async (customerId) => {
             try {
                 const shippingAddress = order.shipments[0].shippingAddress
+                let {id, ...shippingAddressWithoutId} = shippingAddress
                 const bodyShippingAddress = {
                     addressId: nanoid(),
-                    ...shippingAddress
+                    ...shippingAddressWithoutId
                 }
                 await createCustomerAddress.mutateAsync({
                     body: bodyShippingAddress,
