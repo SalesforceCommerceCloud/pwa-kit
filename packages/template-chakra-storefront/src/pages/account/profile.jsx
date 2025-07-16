a4d55c2c106870135337da45c836c5c319b278e5
mitesh.patel <mitesh.patel@salesforce.com>
2025-07-16 15:39:34 -0700

    minor change to MarketingConsentCard declaration.

--- packages/template-chakra-storefront/src/pages/account/profile.jsx
+++ packages/template-chakra-storefront/src/pages/account/profile.jsx
@@ -33,9 +33,9 @@ import {
 } from '@salesforce/commerce-sdk-react'
 import {useCurrentCustomer} from '../../hooks'
 import loadable from '@loadable/component'
-const MarketingConsentCard = SFDC_EXT_MARKETING_CONSENT_ENABLED
-    ? loadable(() => import('../../components/marketing-consent-card'))
-    : null
+const MarketingConsentCard =
+    SFDC_EXT_MARKETING_CONSENT_ENABLED &&
+    loadable(() => import('../../components/marketing-consent-card'))
 
 /**
  * This is a specialized Skeleton component that which uses the customers authtype as the
