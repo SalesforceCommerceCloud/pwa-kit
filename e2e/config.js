d0b6c81c198140c00c4792311db2531aa02ec7e3
Ben Chypak <bchypak@salesforce.com>
2025-07-17 16:19:10 -0700

    Update base command

--- e2e/config.js
+++ e2e/config.js
@@ -13,7 +13,7 @@ module.exports = {
   GENERATED_PROJECTS_DIR: "../generated-projects",
   GENERATE_PROJECTS: ["retail-app-demo", "retail-app-ext", "retail-app-no-ext"],
   GENERATOR_CMD:
-    "node packages/pwa-kit-create-app/scripts/create-mobify-app-dev.js --outputDir",
+    "node packages/pwa-kit-create-app/scripts/create-mobify-app-dev.js --initGit --outputDir",
   CLI_RESPONSES: {
     "retail-app-demo": [
       {
--- e2e/scripts/generate-project.js
+++ e2e/scripts/generate-project.js
@@ -39,14 +39,14 @@ const main = async (opts) => {
         // Explicitly create outputDir because generator runs into permissions issue when generating no-ext projects.
         await mkdirIfNotExists(config.GENERATED_PROJECTS_DIR)
         const outputDir = `${config.GENERATED_PROJECTS_DIR}/${projectDir}`
-        let generateAppCommand = `${config.GENERATOR_CMD} ${outputDir} --initGit`
+        let generateAppCommand = `${config.GENERATOR_CMD} ${outputDir}`
         // TODO: Update script to setup local verdaccio npm repo to allow running 'npx @salesforce/pwa-kit-create-app' to generate apps
         if (preset) {
-            generateAppCommand = `${config.GENERATOR_CMD} ${outputDir} --preset ${preset} --initGit`
+            generateAppCommand = `${config.GENERATOR_CMD} ${outputDir} --preset ${preset}`
         }
 
         if (templateVersion) {
-            generateAppCommand = `${generateAppCommand} --templateVersion ${templateVersion} --initGit`
+            generateAppCommand = `${generateAppCommand} --templateVersion ${templateVersion}`
         }
         console.log('Running command:', generateAppCommand)
         return await runGeneratorWithResponses(generateAppCommand, cliResponses)
