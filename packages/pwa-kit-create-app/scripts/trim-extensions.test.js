6aef279487bf79d581977fd13ad6cbdb77fa8ad0
kzheng <kzheng@salesforce.com>
2025-07-18 17:23:57 -0700

    beef up tests

--- packages/pwa-kit-create-app/scripts/trim-extensions.test.js
+++ packages/pwa-kit-create-app/scripts/trim-extensions.test.js
@@ -9,16 +9,18 @@ const fs = require('fs')
 const {execSync} = require('child_process')
 const path = require('path')
 
-jest.mock('../assets/plugin-config', () => ({
-    plugins: {
-        SFDC_EXT_featureA: {
-            description: 'Feature A'
-        },
-        SFDC_EXT_featureB: {
-            description: 'Feature B'
-        }
+let mockedPluginConfig = {
+    SFDC_EXT_featureA: {
+        description: 'Feature A'
+    },
+    SFDC_EXT_featureB: {
+        description: 'Feature B'
     }
+}
+jest.mock('../assets/plugin-config', () => ({
+    plugins: mockedPluginConfig
 }))
+const trimExtensions = require('./trim-extensions')
 
 jest.mock('fs')
 jest.mock('child_process')
@@ -57,7 +59,166 @@ expect.extend({
     }
 })
 
-const trimExtensions = require('./trim-extensions')
+const mockFs = () => {
+    fs.statSync.mockImplementation((filePath) => {
+        return {
+            isDirectory: () => {
+                return !filePath.endsWith('.jsx')
+            }
+        }
+    })
+    fs.rmSync.mockImplementation(() => jest.fn())
+    fs.readFileSync.mockImplementation(() => jest.fn())
+    fs.existsSync.mockImplementation((filePath) => {
+        if (
+            filePath.includes('featureAComponent') ||
+            filePath.includes('featureBComponent') ||
+            filePath.includes('featureBPage') ||
+            filePath.includes('store-locator') ||
+            filePath.includes('partial')
+        ) {
+            if (
+                filePath.endsWith('.jsx') ||
+                filePath.endsWith('.tsx') ||
+                filePath.endsWith('.js') ||
+                filePath.endsWith('.ts')
+            ) {
+                return false
+            } else {
+                return true
+            }
+        } else {
+            return true
+        }
+    })
+    fs.unlinkSync.mockReturnValue(true)
+}
+
+const testDeleteError = (errorCode = 'EPERM', expectedLogMessage) => {
+    fs.rmSync.mockImplementation((filePath) => {
+        if (filePath.includes('featureBComponent')) {
+            const error = new Error('Permission denied')
+            error.code = errorCode
+            throw error
+        }
+    })
+    jest.spyOn(console, 'log').mockImplementation(() => jest.fn())
+
+    const code = `
+        import loadable from '@loadable/component'
+        const ComponentA = SFDC_EXT_featureA && loadable(() => import('./featureAComponent'))
+        const ComponentB = SFDC_EXT_featureB && loadable(() => import('./featureBComponent'))
+    `
+
+    const trimmedCode = `
+        import loadable from '@loadable/component'
+        const ComponentA = loadable(() => import('./featureAComponent'))
+    `
+    const componentACode = `
+        export default ComponentA
+    `
+    const componentBCode = `
+        export default ComponentB
+    `
+    const featureBPageCode = `
+        export const FeatureBPage = 'FeatureBPage'
+    `
+    let trimExtensionsCalled = false
+    fs.readFileSync.mockImplementation((filePath) => {
+        if (filePath.includes('featureComponent.jsx')) {
+            if (!trimExtensionsCalled) {
+                trimExtensionsCalled = true
+                return code
+            } else {
+                return trimmedCode
+            }
+        } else if (filePath.includes('featureAComponent')) {
+            return componentACode
+        } else if (filePath.includes('featureBComponent')) {
+            return componentBCode
+        } else if (filePath.includes('featureBPage')) {
+            return featureBPageCode
+        } else {
+            console.error('Unhandled file', filePath)
+        }
+    })
+
+    trimExtensions('/mock/dir', {SFDC_EXT_featureA: true, SFDC_EXT_featureB: false})
+
+    expect(fs.rmSync).toHaveBeenCalledWith(expect.stringContaining('featureBComponent'), {
+        force: true,
+        recursive: true
+    })
+}
+
+describe('trim-extensions without config', () => {
+    beforeEach(() => {
+        jest.resetModules()
+    })
+    it('returns early if no plugins is defined', () => {
+        jest.mock('../assets/plugin-config', () => ({
+            plugins: {}
+        }))
+        const trimExtensions = require('./trim-extensions')
+        jest.spyOn(console, 'log').mockImplementation(() => jest.fn())
+        trimExtensions('/mock/dir', {})
+        expect(console.log).toHaveBeenCalledWith('No plugins found, skipping trim')
+        console.log.mockRestore()
+    })
+})
+
+describe('trim-extensions with nested directories', () => {
+    beforeEach(() => {
+        jest.resetAllMocks()
+    })
+    beforeEach(() => {
+        fs.readdirSync.mockImplementation((filePath) => {
+            if (filePath.endsWith('/mock/dir')) {
+                return ['src']
+            } else if (filePath.endsWith('src')) {
+                return ['pages', 'route.jsx']
+            } else if (filePath.endsWith('pages')) {
+                return ['store-locator']
+            } else if (filePath.endsWith('partial')) {
+                return ['modal.jsx']
+            } else if (filePath.endsWith('store-locator')) {
+                return ['index.jsx']
+            } else {
+                return []
+            }
+        })
+        mockFs()
+    })
+
+    it('recursively removes unused directories', () => {
+        const routeCode = `
+            const storeLocatorPage = SFDC_EXT_featureA && loadable(() => import('./pages/store-locator'))
+        `
+        const storeLocatorCode = `
+            import { Modal } from './partial/modal' 
+            export default StoreLocator = 'StoreLocatorModal'
+        `
+        const modalCode = `
+            export const StoreLocator = 'StoreLocatorModal'
+        `
+        fs.readFileSync.mockImplementation((filePath) => {
+            if (filePath.includes('route.jsx')) {
+                return routeCode
+            } else if (filePath.includes(`store-locator${path.sep}index.jsx`)) {
+                return storeLocatorCode
+            } else if (filePath.includes(`store-locator${path.sep}partial${path.sep}modal.jsx`)) {
+                return modalCode
+            } else {
+                console.error('Unhandled file', filePath)
+            }
+        })
+        trimExtensions('/mock/dir', {SFDC_EXT_featureA: false})
+        expect(fs.rmSync).toHaveBeenCalledWith(
+            expect.stringContaining(`pages${path.sep}store-locator`),
+            {force: true, recursive: true}
+        )
+    })
+})
 
 describe('trim-extensions', () => {
     beforeEach(() => {
@@ -68,28 +229,7 @@ describe('trim-extensions', () => {
             '/src/components/featureBComponent/index.jsx',
             '/src/pages/featureBPage/index.jsx'
         ])
-        fs.statSync.mockReturnValue({isDirectory: () => false})
-        fs.existsSync.mockImplementation((filePath) => {
-            if (
-                filePath.includes('featureAComponent') ||
-                filePath.includes('featureBComponent') ||
-                filePath.includes('featureBPage')
-            ) {
-                if (
-                    filePath.endsWith('.jsx') ||
-                    filePath.endsWith('.tsx') ||
-                    filePath.endsWith('.js') ||
-                    filePath.endsWith('.ts')
-                ) {
-                    return false
-                } else {
-                    return true
-                }
-            } else {
-                return true
-            }
-        })
-        fs.unlinkSync.mockReturnValue(true)
+        mockFs()
         execSync.mockReturnValue(true)
     })
 
@@ -178,7 +318,7 @@ describe('trim-extensions', () => {
         )
     })
 
-    it('handles variable with ternary expressions correctly', () => {
+    it('handles variable with ternary expressions correctly when true', () => {
         const code = `const showFeature = SFDC_EXT_featureA ? Feature_A : Feature_B;`
         fs.readFileSync.mockImplementation((filePath) => {
             if (filePath.includes('featureComponent.jsx')) {
@@ -197,6 +337,37 @@ describe('trim-extensions', () => {
         expect(fs.writeFileSync).toHaveBeenCalledWith(
             expect.any(String),
             expect.not.stringContaining('const showFeature = Feature_B')
+        )
+        expect(execSync).toHaveBeenCalledWith(
+            `npx prettier --write ${path.join(
+                '/mock',
+                'dir',
+                'src',
+                'components',
+                'featureComponent.jsx'
+            )}`
+        )
+    })
+
+    it('handles variable with ternary expressions correctly when false', () => {
+        const code = `const showFeature = SFDC_EXT_featureA ? Feature_A : Feature_B;`
+        fs.readFileSync.mockImplementation((filePath) => {
+            if (filePath.includes('featureComponent.jsx')) {
+                return code
+            } else {
+                return ''
+            }
+        })
+
+        trimExtensions('/mock/dir', {SFDC_EXT_featureA: false})
+
+        expect(fs.writeFileSync).toHaveBeenCalledWith(
+            expect.any(String),
+            expect.toEqualTrimmedLines('const showFeature = Feature_B;')
+        )
+        expect(fs.writeFileSync).toHaveBeenCalledWith(
+            expect.any(String),
+            expect.not.stringContaining('const showFeature = Feature_A')
         )
         expect(execSync).toHaveBeenCalledWith(
             `npx prettier --write ${path.join(
@@ -275,6 +446,40 @@ describe('trim-extensions', () => {
         expect(fs.writeFileSync).not.toHaveBeenCalledWith(
             expect.any(String),
             expect.stringContaining('featureBProp: PropTypes.string')
+        )
+        expect(execSync).toHaveBeenCalledWith(
+            `npx prettier --write ${path.join(
+                '/mock',
+                'dir',
+                'src',
+                'components',
+                'featureComponent.jsx'
+            )}`
+        )
+    })
+
+    it('handles ternary expressions in return statements correctly', () => {
+        const code = `
+            function test() {
+                return SFDC_EXT_featureA ? "componentA" : "componentB";
+            }
+        `
+        fs.readFileSync.mockReturnValue(code)
+
+        trimExtensions('/mock/dir', {SFDC_EXT_featureA: false})
+
+        const expected = `
+            function test() {
+                return "componentB";
+            }
+        `
+        expect(fs.writeFileSync).toHaveBeenCalledWith(
+            expect.any(String),
+            expect.toEqualTrimmedLines(expected)
+        )
+        expect(fs.writeFileSync).not.toHaveBeenCalledWith(
+            expect.any(String),
+            expect.stringContaining('componentA')
         )
         expect(execSync).toHaveBeenCalledWith(
             `npx prettier --write ${path.join(
@@ -405,67 +610,51 @@ describe('trim-extensions', () => {
 
         trimExtensions('/mock/dir', {SFDC_EXT_featureA: true, SFDC_EXT_featureB: false})
 
-        expect(fs.unlinkSync).not.toHaveBeenCalledWith(expect.stringContaining('featureAComponent'))
-        expect(fs.unlinkSync).toHaveBeenCalledWith(expect.stringContaining('featureBComponent'))
+        expect(fs.rmSync).not.toHaveBeenCalledWith(expect.stringContaining('featureAComponent'))
+        expect(fs.rmSync).toHaveBeenCalledWith(expect.stringContaining('featureBComponent'), {
+            force: true,
+            recursive: true
+        })
+    })
+
+    it('reports error when updating file fails', () => {
+        fs.writeFileSync.mockImplementation(() => {
+            throw new Error('Failed to write file')
+        })
+        jest.spyOn(console, 'error').mockImplementation(() => jest.fn())
+        const code = `
+        function test() {
+            return SFDC_EXT_featureA ? Feature_A : Feature_B;
+        }
+        `
+        fs.readFileSync.mockReturnValue(code)
+        try {
+            trimExtensions('/mock/dir', {SFDC_EXT_featureA: true, SFDC_EXT_featureB: false})
+        } catch (error) {
+            // do nothing
+        }
+        expect(console.error).toHaveBeenCalledWith(
+            expect.stringContaining(
+                'Error updating file /mock/dir/src/components/featureComponent.jsx: Failed to write file'
+            )
+        )
+        console.error.mockRestore()
     })
 
     it('reports error when delete permission is denied', () => {
-        fs.unlinkSync.mockImplementation((filePath) => {
-            if (filePath.includes('featureBComponent')) {
-                const error = new Error('Permission denied')
-                error.code = 'EPERM'
-                throw error
-            }
-        })
-        jest.spyOn(console, 'log').mockImplementation(() => jest.fn())
-
-        const code = `
-            import loadable from '@loadable/component'
-            const ComponentA = SFDC_EXT_featureA && loadable(() => import('./featureAComponent'))
-            const ComponentB = SFDC_EXT_featureB && loadable(() => import('./featureBComponent'))
-        `
-
-        const trimmedCode = `
-            import loadable from '@loadable/component'
-            const ComponentA = loadable(() => import('./featureAComponent'))
-        `
-        const componentACode = `
-            export default ComponentA
-        `
-        const componentBCode = `
-            export default ComponentB
-        `
-        const featureBPageCode = `
-            export const FeatureBPage = 'FeatureBPage'
-        `
-        let trimExtensionsCalled = false
-        fs.readFileSync.mockImplementation((filePath) => {
-            if (filePath.includes('featureComponent.jsx')) {
-                if (!trimExtensionsCalled) {
-                    trimExtensionsCalled = true
-                    return code
-                } else {
-                    return trimmedCode
-                }
-            } else if (filePath.includes('featureAComponent')) {
-                return componentACode
-            } else if (filePath.includes('featureBComponent')) {
-                return componentBCode
-            } else if (filePath.includes('featureBPage')) {
-                return featureBPageCode
-            } else {
-                console.error('Unhandled file', filePath)
-            }
-        })
-
-        trimExtensions('/mock/dir', {SFDC_EXT_featureA: true, SFDC_EXT_featureB: false})
-
-        expect(fs.unlinkSync).toHaveBeenCalledWith(expect.stringContaining('featureBComponent'))
-
+        testDeleteError('EPERM')
         expect(console.log).toHaveBeenCalledWith(
             expect.stringContaining(
                 '✗ Permission denied - cannot delete. You may need to run with sudo or check permissions.'
             )
+        )
+        console.log.mockRestore()
+    })
+
+    it('reports error when delete fails', () => {
+        testDeleteError(null)
+        expect(console.log).toHaveBeenCalledWith(
+            expect.stringContaining('✗ Error deleting: Permission denied')
         )
         console.log.mockRestore()
     })
@@ -537,7 +726,13 @@ describe('trim-extensions', () => {
         trimExtensions('/mock/dir', {SFDC_EXT_featureA: true, SFDC_EXT_featureB: false})
 
         expect(fs.unlinkSync).not.toHaveBeenCalledWith(expect.stringContaining('featureAComponent'))
-        expect(fs.unlinkSync).toHaveBeenCalledWith(expect.stringContaining('featureBComponent'))
-        expect(fs.unlinkSync).toHaveBeenCalledWith(expect.stringContaining('featureBPage'))
+        expect(fs.rmSync).toHaveBeenCalledWith(expect.stringContaining('featureBComponent'), {
+            force: true,
+            recursive: true
+        })
+        expect(fs.rmSync).toHaveBeenCalledWith(expect.stringContaining('featureBPage'), {
+            force: true,
+            recursive: true
+        })
     })
 })
