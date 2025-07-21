b8d5fba80ed03845b0ae28a7f9ecea41c058ab92
Alex Vuong <alex.vuong@salesforce.com>
2025-07-21 15:11:59 -0700

    add favicon.ico

--- packages/template-chakra-storefront/src/ssr.js
+++ packages/template-chakra-storefront/src/ssr.js
@@ -357,8 +357,8 @@ const {handler} = runtime.createHandler(options, (app) => {
         })
     })
 
-    app.get('/robots.txt', runtime.serveStaticFile('../../static/robots.txt'))
-    app.get('/favicon.ico', runtime.serveStaticFile('../../static/ico/favicon.ico'))
+    app.get('/robots.txt', runtime.serveStaticFile('static/robots.txt'))
+    app.get('/favicon.ico', runtime.serveStaticFile('static/ico/favicon.ico'))
 
     app.get('/worker.js(.map)?', runtime.serveServiceWorker)
     app.get('*', runtime.render)
