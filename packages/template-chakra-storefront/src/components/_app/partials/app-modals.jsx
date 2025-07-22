df34af118a2ee3230d7278786b83b2ecad02fdb1
Kevin He <kevin.he@salesforce.com>
2025-07-22 07:20:38 -0700

    fix missing toast provider

--- packages/template-chakra-storefront/src/components/_app/partials/app-modals.jsx
+++ packages/template-chakra-storefront/src/components/_app/partials/app-modals.jsx
@@ -9,6 +9,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {AuthModal} from '../../../hooks/use-auth-modal'
 import {DntNotification} from '../../../hooks/use-dnt-notification'
+import Toaster, {toaster} from '../../../components/toaster'
 // import StoreLocatorModal from '../../../components/store-locator-modal'
 
 /**
@@ -29,6 +30,8 @@ const AppModals = ({
 
             {/* Do Not Track Notification */}
             <DntNotification {...dntNotification} />
+
+            <Toaster toaster={toaster} />
 
             {/* Store Locator Modal - Disabled until extension is moved */}
             {/*
