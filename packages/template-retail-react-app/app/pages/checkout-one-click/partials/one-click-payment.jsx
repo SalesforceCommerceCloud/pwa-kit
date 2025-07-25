54fc4de07afc7b3b42c0add3366b09d897c4ff4c
Danny Phan <125327707+dannyphan2000@users.noreply.github.com>
2025-07-25 18:28:33 -0400

    @W-18927217: New component for user registration (#2876)
    Add a new user registration ("Save for Future Use") box in the 1CC layout. After placing order with this option checked, account registration will be initiated.

--- packages/template-retail-react-app/app/pages/checkout-one-click/partials/one-click-payment.jsx
+++ packages/template-retail-react-app/app/pages/checkout-one-click/partials/one-click-payment.jsx
@@ -16,7 +16,7 @@ import {
     Divider
 } from '@salesforce/retail-react-app/app/components/shared/ui'
 import {useToast} from '@salesforce/retail-react-app/app/hooks/use-toast'
-import {useShopperBasketsMutation} from '@salesforce/commerce-sdk-react'
+import {useShopperBasketsMutation, useCustomerType} from '@salesforce/commerce-sdk-react'
 import {useCurrentBasket} from '@salesforce/retail-react-app/app/hooks/use-current-basket'
 import {useCurrentCustomer} from '@salesforce/retail-react-app/app/hooks/use-current-customer'
 import {useCheckout} from '@salesforce/retail-react-app/app/pages/checkout-container/util/checkout-context'
@@ -621,6 +621,13 @@ Payment.propTypes = {
     onPaymentMethodSaved: PropTypes.func,
     /** Callback when save preference changes */
     onSavePreferenceChange: PropTypes.func
+}
+
+Payment.propTypes = {
+    /** Whether user registration is enabled */
+    enableUserRegistration: PropTypes.bool,
+    /** Callback to set user registration state */
+    setEnableUserRegistration: PropTypes.func
 }
 
 const PaymentCardSummary = ({payment}) => {
