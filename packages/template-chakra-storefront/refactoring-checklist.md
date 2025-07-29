2b01b658a900981a3130ff1efde723cd1dab47a0
Kevin He <kevin.he@salesforce.com>
2025-07-28 21:37:21 -0700

    refactor translations in src/components/promo-popover/index.jsx

--- packages/template-chakra-storefront/refactoring-checklist.md
+++ packages/template-chakra-storefront/refactoring-checklist.md
@@ -107,7 +107,7 @@ Total files: 222
 - [x] src/components/product-view-modal/index.jsx
 - [x] src/components/product-view/index.jsx
 - [x] src/components/promo-code/index.jsx
-- [ ] src/components/promo-popover/index.jsx
+- [x] src/components/promo-popover/index.jsx
 - [x] src/components/quantity-picker/index.jsx
 - [ ] src/components/radio-card/index.jsx
 - [ ] src/components/recommended-products/index.jsx
--- packages/template-chakra-storefront/src/components/promo-popover/index.jsx
+++ packages/template-chakra-storefront/src/components/promo-popover/index.jsx
@@ -8,7 +8,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {Box, CloseButton, Flex, IconButton, Popover, Text} from '@chakra-ui/react'
 import {InfoIcon} from '../icons'
-import {FormattedMessage, useIntl} from 'react-intl'
+import {useIntl} from 'react-intl'
 
 /**
  * This component renders a small info icon and displays a popover when hovered. It could be adapted
@@ -16,7 +16,19 @@ import {FormattedMessage, useIntl} from 'react-intl'
  * promotions applied to products and/or orders on cart, checkout, order confirmation and order history.
  */
 const PromoPopover = ({header, children, ...props}) => {
-    const intl = useIntl()
+    const {formatMessage} = useIntl()
+    
+    const messages = {
+        info: formatMessage({
+            id: 'promo_popover.assistive_msg.info',
+            defaultMessage: 'Info'
+        }),
+        promoApplied: formatMessage({
+            id: 'promo_popover.heading.promo_applied',
+            defaultMessage: 'Promotions Applied'
+        })
+    }
+    
     return (
         <Box position="relative" {...props}>
             <Popover.Root size="xs" lazyMount unmountOnExit positioning={{placement: 'top'}}>
@@ -29,10 +41,7 @@ const PromoPopover = ({header, children, ...props}) => {
                         minWidth="auto"
                         position="relative"
                         variant="unstyled"
-                        aria-label={intl.formatMessage({
-                            id: 'promo_popover.assistive_msg.info',
-                            defaultMessage: 'Info'
-                        })}
+                        aria-label={messages.info}
                     >
                         <InfoIcon
                             display="block"
@@ -51,10 +60,7 @@ const PromoPopover = ({header, children, ...props}) => {
                             <Flex justifyContent="space-between" alignItems="baseline">
                                 {header || (
                                     <Text fontWeight="bold" fontSize="md">
-                                        <FormattedMessage
-                                            defaultMessage="Promotions Applied"
-                                            id="promo_popover.heading.promo_applied"
-                                        />
+                                        {messages.promoApplied}
                                     </Text>
                                 )}
                                 <Popover.CloseTrigger asChild>
