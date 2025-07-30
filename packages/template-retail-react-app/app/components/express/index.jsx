8c4e8d4a6d9f7251618505cca990a8da38374d41
mmccullom <mmccullom@salesforce.com>
2025-07-30 15:50:24 -0400

    merge with Google Pay

--- packages/template-retail-react-app/app/components/express/index.jsx
+++ packages/template-retail-react-app/app/components/express/index.jsx
@@ -10,6 +10,7 @@ import {useLocation} from 'react-router-dom'
 import {useAccessToken, useCustomerId} from '@salesforce/commerce-sdk-react'
 import {AdyenExpressCheckoutProvider} from '@adyen/adyen-salesforce-pwa'
 
+import {ApplePayExpress} from '@salesforce/retail-react-app/app/components/apple-pay-express/index'
 import {GooglePayExpress} from '@salesforce/retail-react-app/app/components/google-pay-express/index'
 import useMultiSite from '@salesforce/retail-react-app/app/hooks/use-multi-site'
 import useNavigation from '@salesforce/retail-react-app/app/hooks/use-navigation'
