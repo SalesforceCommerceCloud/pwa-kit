728e83366efa8c016758c84e39c991679ae3b434
Avinash Kasipathy <akasipathy@salesforce.com>
2025-07-31 18:30:04 -0700

    @W-19179026: disable streaming responses (#2964)

--- packages/template-retail-react-app/app/hooks/use-miaw.js
+++ packages/template-retail-react-app/app/hooks/use-miaw.js
@@ -32,7 +32,7 @@ const initEmbeddedMessaging = (
             window.embeddedservice_bootstrap.settings
         ) {
             window.embeddedservice_bootstrap.settings.language = locale
-            window.embeddedservice_bootstrap.settings.disableStreamingResponses = false
+            window.embeddedservice_bootstrap.settings.disableStreamingResponses = true
             window.embeddedservice_bootstrap.init(
                 salesforceOrgId,
                 embeddedServiceDeploymentName,
