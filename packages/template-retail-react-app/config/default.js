50ffabb247fff6240e1ee2ea55dae8e473dd42ac
smahbubani <smahbubani@salesforce.com>
2025-08-15 10:54:12 -0400

    1cc config

--- packages/template-retail-react-app/config/default.js
+++ packages/template-retail-react-app/config/default.js
@@ -44,9 +44,9 @@ module.exports = {
         commerceAPI: {
             proxyPath: `/mobify/proxy/api`,
             parameters: {
-                clientId: 'c9c45bfd-0ed3-4aa2-9971-40f88962b836',
-                organizationId: 'f_ecom_zzrf_001',
-                shortCode: '8o7m175y',
+                clientId: 'd020b443-1be5-4140-a7db-bd381507c6ae',
+                organizationId: 'f_ecom_bhbg_stg',
+                shortCode: 'sandbox-001',
                 siteId: 'RefArchGlobal'
             }
         },
@@ -81,11 +81,11 @@ module.exports = {
         ssrFunctionNodeVersion: '22.x',
         proxyConfigs: [
             {
-                host: 'kv7kzm78.api.commercecloud.salesforce.com',
+                host: 'sandbox-001.api.commercecloud.salesforce.com',
                 path: 'api'
             },
             {
-                host: 'zzrf-001.dx.commercecloud.salesforce.com',
+                host: 'bhbg_stg.dx.commercecloud.salesforce.com',
                 path: 'ocapi'
             }
         ]
