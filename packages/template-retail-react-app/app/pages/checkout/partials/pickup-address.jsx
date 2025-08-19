553cb9b2d9a37efe4234ee6d3abf4a9470756120
Henry Semaganda <henry.semaganda@salesforce.com>
2025-08-19 16:07:45 -0700

    add regenerated build files

--- packages/template-retail-react-app/app/pages/checkout/partials/pickup-address.jsx
+++ packages/template-retail-react-app/app/pages/checkout/partials/pickup-address.jsx
@@ -5,7 +5,7 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import React, {useMemo} from 'react'
-import {FormattedMessage, useIntl, IntlProvider} from 'react-intl'
+import {FormattedMessage, useIntl} from 'react-intl'
 
 // Components
 import {
