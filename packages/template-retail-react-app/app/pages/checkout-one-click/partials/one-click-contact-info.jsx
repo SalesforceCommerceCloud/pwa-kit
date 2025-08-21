6f54b8204e53901d1aa4286a4a49e8479deefda8
kumaravinashcommercecloud <kumaravinash@salesforce.com>
2025-08-21 18:57:43 -0400

    Merge pull request #3149 from SalesforceCommerceCloud/avinash.W-19389577.emailFormatInternationalization
    @W-19389577 email format internationalization

--- packages/template-retail-react-app/app/pages/checkout-one-click/partials/one-click-contact-info.jsx
+++ packages/template-retail-react-app/app/pages/checkout-one-click/partials/one-click-contact-info.jsx
@@ -130,7 +130,8 @@ const ContactInfo = ({isSocialEnabled = false, idps = [], onRegisteredUserChoseG
     // Helper function to validate email format
     const isValidEmail = (email) => {
         const emailRegex =
-            /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+            /^[\p{L}\p{N}._!#$%&'*+/=?^`{|}~-]+@(?:[\p{L}\p{N}](?:[\p{L}\p{N}-]{0,61}[\p{L}\p{N}])?\.)+[\p{L}\p{N}](?:[\p{L}\p{N}-]{0,61}[\p{L}\p{N}])?$/u
+
         return emailRegex.test(email)
     }
 
