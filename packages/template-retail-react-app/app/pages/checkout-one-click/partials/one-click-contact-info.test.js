4ed54a70faca268657b5f19fc88a7d5a07a64cc6
kumaravinash <kumaravinash@salesforce.com>
2025-08-21 10:00:31 -0400

    Internationalization of email address check

--- packages/template-retail-react-app/app/pages/checkout-one-click/partials/one-click-contact-info.test.js
+++ packages/template-retail-react-app/app/pages/checkout-one-click/partials/one-click-contact-info.test.js
@@ -220,7 +220,7 @@ describe('ContactInfo Component', () => {
             'josé@example.españa',
             'email@bücher.de',
             '用户@例子.中国',
-            '!#$%&'*+/=?^_{|}~-@example.com`'
+            '!#$%&*+/=?^_{|}~-@example.com'
         ]
 
         for (const email of validEmails) {
