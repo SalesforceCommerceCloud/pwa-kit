4124d144575b34831ef06aa349399c4c67f1e545
jhagama <jhageman@salesforce.com>
2025-08-27 16:48:12 -0400

    @ W-19440669 -- Adding email and phone number on ecom order from GPay (#3195)
    * Making sure email is on ecom order
    
    * Require + save phone number as well

--- packages/template-retail-react-app/app/components/google-pay-express/index.jsx
+++ packages/template-retail-react-app/app/components/google-pay-express/index.jsx
@@ -41,7 +41,7 @@ export const getGooglePaymentMethodConfig = (paymentMethodsResponse) => {
     return getPaymentMethodConfig(paymentMethodsResponse, PAYMENT_METHOD)
 }
 
-export const getCustomerShippingDetails = (shippingAddress) => {
+export const getCustomerShippingDetails = (shippingAddress, email) => {
     return {
         deliveryAddress: {
             city: shippingAddress.locality,
@@ -53,7 +53,9 @@ export const getCustomerShippingDetails = (shippingAddress) => {
         },
         profile: {
             firstName: shippingAddress.name?.split(' ')[0] || '',
-            lastName: shippingAddress.name?.split(' ').slice(1).join(' ') || ''
+            lastName: shippingAddress.name?.split(' ').slice(1).join(' ') || '',
+            email: email || '',
+            phone: shippingAddress.phoneNumber || ''
         }
     }
 }
@@ -77,7 +79,7 @@ export const updateShippingAddress = async (authToken, site, basket, shippingAdd
         const adyenShippingAddressService = new AdyenShippingAddressService(authToken, site)
         const response = await adyenShippingAddressService.updateShippingAddress(
             basket.basketId,
-            getCustomerShippingDetails(shippingAddress)
+            getCustomerShippingDetails(shippingAddress, basket?.customerInfo?.email)
         )
 
         if (response.error) {
@@ -279,7 +281,7 @@ export const getGoogleButtonConfig = (
         buttonType: 'plain',
         isExpress: true,
         shippingAddressRequired: true,
-        // shippingAddressParameters: {"allowedCountryCodes": ["US"]}, // If you want to restrict country codes, you can do that here
+        shippingAddressParameters: {phoneNumberRequired: true},
         shippingOptionRequired: true,
         billingAddressRequired: true,
         billingAddressParameters: {format: 'FULL'},
@@ -301,7 +303,7 @@ export const getGoogleButtonConfig = (
                             type: 'googlepay',
                             googlePayToken: data.paymentMethodData.tokenizationData.token
                         },
-                        ...getCustomerShippingDetails(data?.shippingAddress),
+                        ...getCustomerShippingDetails(data?.shippingAddress, data?.email),
                         ...getCustomerBillingDetails(
                             data?.paymentMethodData?.info?.billingAddress || data?.shippingAddress
                         )
