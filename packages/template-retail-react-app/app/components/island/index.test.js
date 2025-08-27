d8d29f90e50550774339541c24ebf5fc087f9633
vcua-mobify <vcua@salesforce.com>
2025-08-27 12:48:58 -0700

    Update changelog

--- packages/template-retail-react-app/app/components/island/index.test.js
+++ packages/template-retail-react-app/app/components/island/index.test.js
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-/* eslint-disable no-import-assign */
+
 import React from 'react'
 import {act, render, screen, cleanup} from '@testing-library/react'
 import {renderToString} from 'react-dom/server'
