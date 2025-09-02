946e287f5f3730932dced8c3c34e16cfe116026d
Vincent Marta <vmarta@salesforce.com>
2025-09-02 12:38:08 -0700

    Remove commented out code

--- packages/template-retail-react-app/app/utils/config-utils.js
+++ packages/template-retail-react-app/app/utils/config-utils.js
@@ -7,48 +7,6 @@
 
 import {getConfig} from '@salesforce/pwa-kit-runtime/utils/ssr-config'
 
-/**
- * Safely parses commerce agent settings from either a JSON string or object
- * @param {string|object} settings - The commerce agent settings
- * @returns {object} Parsed commerce agent settings object
- */
-// function parseCommerceAgentSettings(settings) {
-//     // Default configuration when no settings are provided
-//     const defaultConfig = {
-//         enabled: 'false',
-//         askAgentOnSearch: 'false',
-//         embeddedServiceName: '',
-//         embeddedServiceEndpoint: '',
-//         scriptSourceUrl: '',
-//         scrt2Url: '',
-//         salesforceOrgId: '',
-//         commerceOrgId: '',
-//         siteId: ''
-//     }
-
-//     // If settings is already an object, return it
-//     if (typeof settings === 'object' && settings !== null) {
-//         return settings
-//     }
-
-//     // If settings is a string, try to parse it
-//     if (typeof settings === 'string') {
-//         try {
-//             return JSON.parse(settings)
-//         } catch (error) {
-//             console.warn('Invalid COMMERCE_AGENT_SETTINGS format, using defaults:', error.message)
-//             return defaultConfig
-//         }
-//     }
-
-//     // If settings is undefined/null, return defaults
-//     return defaultConfig
-// }
-
-// module.exports = {
-//     parseCommerceAgentSettings
-// }
-
 export const getCommerceAgentConfig = () => {
     const defaults = {
         enabled: 'false',
