79c7bfba55f2d8847fd522ab5e1e3456bf4a3c73
Vincent Marta <vmarta@salesforce.com>
2025-09-02 12:27:42 -0700

    Sync the eslint comments

--- packages/template-retail-react-app/config/default.js
+++ packages/template-retail-react-app/config/default.js
@@ -4,13 +4,9 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const sites = require('./sites.js')
-const {
-    parseSettings
-    // If generating an extensible project, this require path needs to be: @salesforce/retail-react-app/app/utils/config-utils.js
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-} = require('./utils.js')
+const {parseSettings} = require('./utils.js')
 
 module.exports = {
     app: {
