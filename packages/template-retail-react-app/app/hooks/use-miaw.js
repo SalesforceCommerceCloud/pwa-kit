8de7f22480654cc6b573def14c5fa77b31011be3
Avinash Kasipathy <akasipathy@salesforce.com>
2025-09-03 16:47:17 -0700

    fix: disable input when agent is processing the request (#3242)

--- packages/template-retail-react-app/app/hooks/use-miaw.js
+++ packages/template-retail-react-app/app/hooks/use-miaw.js
@@ -33,6 +33,7 @@ const initEmbeddedMessaging = (
         ) {
             window.embeddedservice_bootstrap.settings.language = locale
             window.embeddedservice_bootstrap.settings.disableStreamingResponses = true
+            window.embeddedservice_bootstrap.settings.enableUserInputForConversationWithBot = false
             window.embeddedservice_bootstrap.init(
                 salesforceOrgId,
                 embeddedServiceDeploymentName,
