7be6632f8103232f64cfd4ecdf23c0e135119fec
Shaurye Mahajan <s.mahajan@salesforce.com>
2025-10-15 17:16:02 -0400

    Fix pick up stock level bug

--- packages/template-retail-react-app/app/pages/cart/index.jsx
+++ packages/template-retail-react-app/app/pages/cart/index.jsx
@@ -1083,6 +1083,9 @@ const Cart = () => {
                                                             renderSecondaryActions={
                                                                 renderSecondaryActions
                                                             }
+                                                            getShipmentInfoForProduct={
+                                                                getShipmentInfoForProduct
+                                                            }
                                                             renderDeliveryActions={(
                                                                 productItem
                                                             ) => {
