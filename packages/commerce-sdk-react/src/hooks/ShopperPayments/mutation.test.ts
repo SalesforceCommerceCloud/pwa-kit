97d2ea19de5c4867f0b716dc68813d6796c291ee
Alicia LaFemina <alafemina@salesforce.com>
2025-10-15 11:31:59 -0400

    add ShopperPayments tests

---
+++ packages/commerce-sdk-react/src/hooks/ShopperPayments/mutation.test.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2025, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+describe('Shopper Payments mutations', () => {
+    test('no mutations implemented yet', () => {
+        // ShopperPayments currently has no mutations
+        // This test is a placeholder to maintain test coverage
+        expect(true).toBe(true)
+    })
+})
+
