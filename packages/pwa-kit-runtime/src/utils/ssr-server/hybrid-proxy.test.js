04bf75a5a71931c0663306ef7c3fc1f844ffbf72
Yuming Hsieh <yhsieh@salesforce.com>
2025-10-21 16:37:26 -0400

    rename to hybrid-proxy

--- packages/pwa-kit-runtime/src/utils/ssr-server/hybrid-proxy.test.js
+++ packages/pwa-kit-runtime/src/utils/ssr-server/hybrid-proxy.test.js
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import {iterate, hybridProxy} from './hybridProxy'
+import {iterate, hybridProxy} from './hybrid-proxy'
 import logger from '../logger-instance'
 import * as utils from './utils'
 
