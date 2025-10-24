3f135e5a78ff7219e3a3243a08d574d02aad0d5e
Shikhar Prasoon <214730309+sf-shikhar-prasoon@users.noreply.github.com>
2025-10-24 17:34:21 -0400

    fix tests

--- packages/template-retail-react-app/app/components/bonus-product-view-modal/index.test.js
+++ packages/template-retail-react-app/app/components/bonus-product-view-modal/index.test.js
@@ -16,7 +16,10 @@ import {
     getRemainingAvailableBonusProductsForProduct,
     findAvailableBonusDiscountLineItemIds
 } from '@salesforce/retail-react-app/app/utils/bonus-product'
-import {useBonusProductCounts} from '@salesforce/retail-react-app/app/utils/bonus-product/hooks'
+import {
+    useBonusProductCounts,
+    useRuleBasedPromotionIds
+} from '@salesforce/retail-react-app/app/utils/bonus-product/hooks'
 import {processProductsForBonusCart} from '@salesforce/retail-react-app/app/utils/bonus-product/cart'
 import {useCurrentBasket} from '@salesforce/retail-react-app/app/hooks/use-current-basket'
 import {useShopperBasketsMutationHelper} from '@salesforce/commerce-sdk-react'
@@ -130,7 +133,8 @@ jest.mock('@salesforce/retail-react-app/app/utils/bonus-product', () => ({
 
 // Mock bonus product hooks
 jest.mock('@salesforce/retail-react-app/app/utils/bonus-product/hooks', () => ({
-    useBonusProductCounts: jest.fn()
+    useBonusProductCounts: jest.fn(),
+    useRuleBasedPromotionIds: jest.fn()
 }))
 
 // Mock bonus product cart helpers
@@ -183,6 +187,9 @@ beforeEach(() => {
         finalSelectedBonusItems: 2,
         finalMaxBonusItems: 5
     })
+
+    // Mock useRuleBasedPromotionIds to return empty array by default
+    useRuleBasedPromotionIds.mockReturnValue([])
 
     // Mock findAvailableBonusDiscountLineItemIds to return array of pairs
     findAvailableBonusDiscountLineItemIds.mockReturnValue([['bonus-1', 1]])
