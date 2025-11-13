d8da4317fe9cc939b06f6bf9e2ff6ec49dcbcf12
Jinsu Ha <j.ha@salesforce.com>
2025-11-13 13:54:37 -0500

    feat: enable SLAS private client authentication

--- packages/template-retail-react-app/config/default.js
+++ packages/template-retail-react-app/config/default.js
@@ -59,7 +59,7 @@ module.exports = {
         commerceAPI: {
             proxyPath: `/mobify/proxy/api`,
             parameters: {
-                clientId: 'c9c45bfd-0ed3-4aa2-9971-40f88962b836',
+                clientId: '475ad705-e2c1-4808-af78-81661f754511',
                 organizationId: 'f_ecom_zzrf_001',
                 shortCode: '8o7m175y',
                 siteId: 'RefArchGlobal'
