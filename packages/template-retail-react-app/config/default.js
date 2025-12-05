3dbb3a7a784f5959e92fdc1055b3c969d847fecc
Jinsu Ha <j.ha@salesforce.com>
2025-12-05 09:34:35 -0500

    Enable SLAS private client and passwordless login, update Commerce API parameters to test with staging-001

--- packages/template-retail-react-app/config/default.js
+++ packages/template-retail-react-app/config/default.js
@@ -31,7 +31,7 @@ module.exports = {
         },
         login: {
             passwordless: {
-                enabled: false,
+                enabled: true,
                 callbackURI:
                     process.env.PASSWORDLESS_LOGIN_CALLBACK_URI || '/passwordless-login-callback',
                 landingPath: '/passwordless-login-landing'
@@ -55,9 +55,10 @@ module.exports = {
         commerceAPI: {
             proxyPath: `/mobify/proxy/api`,
             parameters: {
-                clientId: 'c9c45bfd-0ed3-4aa2-9971-40f88962b836',
+                clientId: 'bb1e8c16-7767-4ed8-bc5d-83efc59a208a',
+                // secret: FctBOLiYAMFFJVu4NiuxYYON6pTIcXjsf_4Wtj7zd80
                 organizationId: 'f_ecom_zzrf_001',
-                shortCode: '8o7m175y',
+                shortCode: 'staging-001',
                 siteId: 'RefArchGlobal'
             }
         },
