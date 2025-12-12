34bc9fdfd37849de6964e3032999fb750c7fef38
shivaniverma <shivaniverma@salesforce.com>
2025-12-12 09:48:53 +0530

    Add endpoint to test presence of file in ssr_shared

--- packages/template-mrt-reference-app/app/ssr.test.js
+++ packages/template-mrt-reference-app/app/ssr.test.js
@@ -148,4 +148,11 @@ describe('server', () => {
             expect(header).toBe(header.toLowerCase())
         }
     })
+
+    test('Path "/ssr-shared" serves the example.json file', async () => {
+        const response = await request(app).get('/ssr-shared')
+        expect(response.body.message).toBe(
+            'This file is used in the E2E tests to verify that correct header values are set.'
+        )
+    })
 })
