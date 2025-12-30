735ae0ca82a73d65e879bf2cc94ab0cef85e7bc5
Sushma Yadupathi <syadupathi@salesforce.com>
2025-12-30 13:01:53 -0500

    One click checkout

--- packages/commerce-sdk-react/src/hooks/ShopperConfigurations/index.test.ts
+++ packages/commerce-sdk-react/src/hooks/ShopperConfigurations/index.test.ts
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+import {renderHook} from '@testing-library/react'
 import {useConfigurations} from './query'
 
 describe('ShopperConfigurations', () => {
