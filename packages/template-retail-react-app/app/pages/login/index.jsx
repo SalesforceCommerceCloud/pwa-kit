d9a6b1247439d8412ac6d6a03eac56d1324bccea
Jinsu Ha <j.ha@salesforce.com>
2025-12-31 12:06:24 -0500

    move handleMergeBaskets to a custom hook

--- packages/template-retail-react-app/app/pages/login/index.jsx
+++ packages/template-retail-react-app/app/pages/login/index.jsx
@@ -14,8 +14,7 @@ import {
     useAuthHelper,
     useCustomerBaskets,
     useCustomerId,
-    useCustomerType,
-    useShopperBasketsMutation
+    useCustomerType
 } from '@salesforce/commerce-sdk-react'
 import useNavigation from '@salesforce/retail-react-app/app/hooks/use-navigation'
 import Seo from '@salesforce/retail-react-app/app/components/seo'
@@ -34,8 +33,8 @@ import {
     PASSWORDLESS_LOGIN_LANDING_PATH,
     PASSWORDLESS_ERROR_MESSAGES
 } from '@salesforce/retail-react-app/app/constants'
-import {usePrevious} from '@salesforce/retail-react-app/app/hooks/use-previous'
 import {isServer, noop} from '@salesforce/retail-react-app/app/utils/utils'
+import {useMergeBasket} from '@salesforce/retail-react-app/app/hooks/use-merge-basket'
 import {getConfig} from '@salesforce/pwa-kit-runtime/utils/ssr-config'
 
 const LOGIN_ERROR_MESSAGE = defineMessage({
@@ -55,7 +54,7 @@ const Login = ({initialView = LOGIN_VIEW}) => {
     const {path} = useRouteMatch()
     const einstein = useEinstein()
     const dataCloud = useDataCloud()
-    const {isRegistered, customerType} = useCustomerType()
+    const {isRegistered} = useCustomerType()
     const login = useAuthHelper(AuthHelpers.LoginRegisteredUserB2C)
     const loginPasswordless = useAuthHelper(AuthHelpers.LoginPasswordlessUser)
     const authorizePasswordlessLogin = useAuthHelper(AuthHelpers.AuthorizePasswordless)
@@ -66,42 +65,13 @@ const Login = ({initialView = LOGIN_VIEW}) => {
     const idps = social?.idps
 
     const customerId = useCustomerId()
-    const prevAuthType = usePrevious(customerType)
-    const {data: baskets, isSuccess: isSuccessCustomerBaskets} = useCustomerBaskets(
+    const {isSuccess: isSuccessCustomerBaskets} = useCustomerBaskets(
         {parameters: {customerId}},
         {enabled: !!customerId && !isServer, keepPreviousData: true}
     )
-    const mergeBasket = useShopperBasketsMutation('mergeBasket')
+    const handleMergeBasket = useMergeBasket()
     const [redirectPath, setRedirectPath] = useState('')
     const [isOtpAuthOpen, setIsOtpAuthOpen] = useState(false)
-
-    const handleMergeBasket = () => {
-        const hasBasketItem = baskets?.baskets?.[0]?.productItems?.length > 0
-        // we only want to merge basket when the user is logged in as a recurring user
-        // only recurring users trigger the login mutation, new user triggers register mutation
-        // this logic needs to stay in this block because this is the only place that tells if a user is a recurring user
-        // if you change logic here, also change it in login page
-        const shouldMergeBasket = hasBasketItem && prevAuthType === 'guest'
-        if (shouldMergeBasket) {
-            try {
-                mergeBasket.mutate({
-                    headers: {
-                        // This is not required since the request has no body
-                        // but CommerceAPI throws a '419 - Unsupported Media Type' error if this header is removed.
-                        'Content-Type': 'application/json'
-                    },
-                    parameters: {
-                        createDestinationBasket: true
-                    }
-                })
-            } catch (e) {
-                form.setError('global', {
-                    type: 'manual',
-                    message: formatMessage(API_ERROR_MESSAGE)
-                })
-            }
-        }
-    }
 
     const handlePasswordlessLogin = async (email) => {
         try {
@@ -170,7 +140,12 @@ const Login = ({initialView = LOGIN_VIEW}) => {
     useEffect(() => {
         if (isRegistered) {
             setIsOtpAuthOpen(false)
-            handleMergeBasket()
+            handleMergeBasket().catch(() => {
+                form.setError('global', {
+                    type: 'manual',
+                    message: formatMessage(API_ERROR_MESSAGE)
+                })
+            })
             const redirectTo = redirectPath ? redirectPath : '/account'
             navigate(redirectTo)
         }
