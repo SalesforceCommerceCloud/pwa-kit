60166b140f81e2e0c82d3bdd68cf897c074c55da
Jinsu Ha <j.ha@salesforce.com>
2026-01-02 14:12:11 -0500

    Update mobile e2e tests to verify 'email' mode for passwordless login and password reset requests

--- e2e/tests/mobile/extra-features.spec.js
+++ e2e/tests/mobile/extra-features.spec.js
@@ -49,9 +49,8 @@ test('Verify passwordless login request on mobile', async ({page}) => {
     const params = new URLSearchParams(postData)
 
     expect(params.get('user_id')).toBe(config.PWA_E2E_USER_EMAIL)
-    expect(params.get('mode')).toBe('callback')
+    expect(params.get('mode')).toBe('email')
     expect(params.get('channel_id')).toBe(config.EXTRA_FEATURES_E2E_RETAIL_APP_HOME_SITE)
-    expect(params.get('callback_uri')).toMatch(/.*\/passwordless-login-callback$/)
 })
 
 test('Verify password reset callback request on mobile (extra features enabled)', async ({
@@ -92,9 +91,8 @@ test('Verify password reset callback request on mobile (extra features enabled)'
     const params = new URLSearchParams(postData)
 
     expect(params.get('user_id')).toBe(config.PWA_E2E_USER_EMAIL)
-    expect(params.get('mode')).toBe('callback')
+    expect(params.get('mode')).toBe('email')
     expect(params.get('channel_id')).toBe(config.EXTRA_FEATURES_E2E_RETAIL_APP_HOME_SITE)
-    expect(params.get('callback_uri')).toMatch(/.*\/reset-password-callback$/)
     expect(params.get('hint')).toBe('cross_device')
 })
 
@@ -136,9 +134,8 @@ test('Verify password reset callback request on mobile when extra login features
     const params = new URLSearchParams(postData)
 
     expect(params.get('user_id')).toBe(config.PWA_E2E_USER_EMAIL)
-    expect(params.get('mode')).toBe('callback')
+    expect(params.get('mode')).toBe('email')
     expect(params.get('channel_id')).toBe(config.RETAIL_APP_HOME_SITE)
-    expect(params.get('callback_uri')).toMatch(/.*\/reset-password-callback$/)
     expect(params.get('hint')).toBe('cross_device')
 })
 
