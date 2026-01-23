d9c53242d117a07735d6a3388091c3c73dd016cd
Jinsu Ha <j.ha@salesforce.com>
2026-01-23 14:12:17 -0500

    revert unnecessary changes to bundle size and eslint

--- packages/pwa-kit-dev/src/configs/eslint/partials/base.js
+++ packages/pwa-kit-dev/src/configs/eslint/partials/base.js
@@ -9,7 +9,7 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
     parserOptions: {
-        ecmaVersion: '2017',
+        ecmaVersion: 2017,
         sourceType: 'module',
         ecmaFeatures: {
             jsx: true
