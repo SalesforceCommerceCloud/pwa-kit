68da03063a91bd3d779a5964923315218af3fa2c
Jinsu Ha <j.ha@salesforce.com>
2026-01-27 11:39:57 -0500

    lint

--- packages/template-retail-react-app/app/pages/login/index.jsx
+++ packages/template-retail-react-app/app/pages/login/index.jsx
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+
 import React, {useEffect, useState} from 'react'
 import PropTypes from 'prop-types'
 import {useIntl, defineMessage, FormattedMessage} from 'react-intl'
