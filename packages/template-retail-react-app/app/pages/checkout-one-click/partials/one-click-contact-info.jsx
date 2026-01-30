bbf8ec8b5e82666cd338f534d4489156c15522bd
d.phan <d.phan@salesforce.com>
2026-01-31 03:52:21 +0700

    fix import
    Signed-off-by: d.phan <d.phan@salesforce.com>

--- packages/template-retail-react-app/app/pages/checkout-one-click/partials/one-click-contact-info.jsx
+++ packages/template-retail-react-app/app/pages/checkout-one-click/partials/one-click-contact-info.jsx
@@ -47,6 +47,7 @@ import {
     useShopperCustomersMutation
 } from '@salesforce/commerce-sdk-react'
 import {API_ERROR_MESSAGE} from '@salesforce/retail-react-app/app/constants'
+import {getPasswordlessErrorMessage} from '@salesforce/retail-react-app/app/utils/auth-utils'
 import {isValidEmail} from '@salesforce/retail-react-app/app/utils/email-utils'
 import {formatPhoneNumber} from '@salesforce/retail-react-app/app/utils/phone-utils'
 import useMultiSite from '@salesforce/retail-react-app/app/hooks/use-multi-site'
