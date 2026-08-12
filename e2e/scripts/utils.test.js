710a8ca79c18044ab3f40c8351a40d21b7c88de3
Jainam Sheth <j.sheth@salesforce.com>
2026-08-12 11:19:54 -0500

    Fix E2E unit tests on Node 18

--- e2e/scripts/utils.test.js
+++ e2e/scripts/utils.test.js
@@ -5,6 +5,9 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
+jest.mock('@axe-core/playwright', () => jest.fn())
+jest.mock('@playwright/test', () => ({expect: jest.fn()}))
+
 const {sanitizeFailureSummary} = require('./utils')
 
 describe('sanitizeFailureSummary', () => {
