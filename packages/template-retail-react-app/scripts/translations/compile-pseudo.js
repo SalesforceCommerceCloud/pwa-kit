62d436083db272d5f6dcf7fe98439a80feb54af6
Jie Dai <jie.dai@salesforce.com>
2026-09-15 08:14:48 -0400

    fix: support FormatJS on Windows

--- packages/template-retail-react-app/scripts/translations/compile-pseudo.js
+++ packages/template-retail-react-app/scripts/translations/compile-pseudo.js
@@ -8,9 +8,10 @@
 /* eslint @typescript-eslint/no-var-requires: "off" */
 const {execFileSync} = require('child_process')
 const {getOutputFolder} = require('./utils')
+const formatjs = process.platform === 'win32' ? 'formatjs.cmd' : 'formatjs'
 
 const runFormatjs = (args) => {
-    execFileSync('formatjs', args, {stdio: 'inherit'})
+    execFileSync(formatjs, args, {stdio: 'inherit'})
 }
 
 const main = () => {
--- packages/template-retail-react-app/scripts/translations/extract-default-messages.js
+++ packages/template-retail-react-app/scripts/translations/extract-default-messages.js
@@ -16,9 +16,10 @@ const fs = require('fs')
 const path = require('path')
 const packagePath = path.join(process.cwd(), 'package.json')
 const pkgJSON = JSON.parse(fs.readFileSync(packagePath))
+const formatjs = process.platform === 'win32' ? 'formatjs.cmd' : 'formatjs'
 
 const runFormatjs = (args) => {
-    execFileSync('formatjs', args, {stdio: 'inherit'})
+    execFileSync(formatjs, args, {stdio: 'inherit'})
 }
 
 const getAllFilesByExtensions = (dirPath, arrayOfFiles = [], extensions = []) => {
